acefa2577039b5c2a73bb0acc99b1f48f60c40d0
caneleex <git@mlnr.dev>
2021-06-18 14:53:50 +0200

    remove useless debug import

--- integrations/java/pl/jakubweg/SponsorBlockUtils.java
+++ integrations/java/pl/jakubweg/SponsorBlockUtils.java
@@ -9,7 +9,6 @@ import android.os.Looper;
 import android.text.Html;
 import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
