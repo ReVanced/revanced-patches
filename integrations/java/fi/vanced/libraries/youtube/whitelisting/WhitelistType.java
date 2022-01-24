7bbd97ee4087e410fde5dae245077f02de0c884c
caneleex <git@mlnr.dev>
2022-01-24 15:36:55 +0100

    add strings for whitelist types

--- integrations/java/fi/vanced/libraries/youtube/whitelisting/WhitelistType.java
+++ integrations/java/fi/vanced/libraries/youtube/whitelisting/WhitelistType.java
@@ -1,18 +1,20 @@
 package fi.vanced.libraries.youtube.whitelisting;
 
+import static pl.jakubweg.StringRef.str;
+
 import pl.jakubweg.SponsorBlockSettings;
 
 public enum WhitelistType {
-    ADS("Ads", "youtube", "vanced_whitelist_ads_enabled"),
-    SPONSORBLOCK("SponsorBlock", SponsorBlockSettings.PREFERENCES_NAME, "vanced_whitelist_sb_enabled");
+    ADS("youtube", "vanced_whitelist_ads_enabled"),
+    SPONSORBLOCK(SponsorBlockSettings.PREFERENCES_NAME, "vanced_whitelist_sb_enabled");
 
     private final String friendlyName;
     private final String preferencesName;
     private final String sharedPreferencesName;
     private final String preferenceEnabledName;
 
-    WhitelistType(String friendlyName, String sharedPreferencesName, String preferenceEnabledName) {
-        this.friendlyName = friendlyName;
+    WhitelistType(String sharedPreferencesName, String preferenceEnabledName) {
+        this.friendlyName = str("vanced_whitelisting_" + name().toLowerCase());
         this.sharedPreferencesName = sharedPreferencesName;
         this.preferencesName = "whitelist_" + name();
         this.preferenceEnabledName = preferenceEnabledName;
