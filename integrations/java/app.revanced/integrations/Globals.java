94c15e9acbeef438a4d9dc1e20540f0551c6707b
oSumAtrIX <johan.melkonyan1@web.de>
2022-04-12 20:55:12 +0200

    feat: `Globals` class for proxying `getAppContext`

---
+++ integrations/java/app.revanced/integrations/Globals.java
@@ -0,0 +1,18 @@
+package app.revanced.integrations;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.util.Log;
+
+public class Globals {
+    public static Context context;
+
+    public static Context getAppContext() {
+
+        if (context != null) {
+            return context;
+        }
+        Log.e("Globals", "Context is null!");
+        return null;
+    }
+}
--- integrations/java/com/google/android/apps/youtube/app/YouTubeTikTokRoot_Application.java
+++ integrations/java/com/google/android/apps/youtube/app/YouTubeTikTokRoot_Application.java
@@ -2,13 +2,8 @@ package com.google.android.apps.youtube.app;
 
 import android.app.Application;
 import android.content.Context;
-import android.os.Bundle;
 
 public class YouTubeTikTokRoot_Application extends Application {
-    protected void onCreate(final Bundle bundle) {
-        super.onCreate();
-    }
-
     public static Context getAppContext() {
         return null;
     }
