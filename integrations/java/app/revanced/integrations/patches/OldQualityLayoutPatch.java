7028f42c947235c37519ae1eb18e8527a81aeb59
OxrxL <108184954+OxrxL@users.noreply.github.com>
2022-07-19 01:58:49 +0200

    fix: quick quality list flyout view visibility in `old-quality-layout` patch (#86)

--- integrations/java/app/revanced/integrations/patches/OldQualityLayoutPatch.java
+++ integrations/java/app/revanced/integrations/patches/OldQualityLayoutPatch.java
@@ -1,10 +1,11 @@
 package app.revanced.integrations.patches;
 
-import android.os.Handler;
-import android.os.Looper;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 
@@ -14,15 +15,26 @@ import app.revanced.integrations.settings.SettingsEnum;
 import app.revanced.integrations.utils.LogHelper;
 
 public class OldQualityLayoutPatch {
-    // TODO: hide listView, OnHierarchyChange will not be triggered though
+    public static Window window;
+    private static boolean hideWindow = true;
+    
     public static void showOldQualityMenu(ListView listView)
     {
         if (!SettingsEnum.OLD_STYLE_QUALITY_SETTINGS.getBoolean()) return;
+
+        hideWindow = true;
 
         listView.setOnHierarchyChangeListener(new ViewGroup.OnHierarchyChangeListener() {
             @Override
             public void onChildViewAdded(View parent, View child) {
                 LogHelper.debug(OldQualityLayoutPatch.class, "Added: " + child);
+
+                parent.setVisibility(View.GONE);
+                if (window != null && hideWindow) {
+                    window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+                    hideWindow = false;
+                }
 
                 final var indexOfAdvancedQualityMenuItem = 4;
                 if (listView.indexOfChild(child) != indexOfAdvancedQualityMenuItem) return;
