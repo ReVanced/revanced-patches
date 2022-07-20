576aacd76d8b258d2d21f6306be7808f1427b5aa
TheJeterLP <joey.peter1998@gmail.com>
2022-07-20 22:56:29 +0200

    refactor: cleanup code

--- integrations/java/app/revanced/integrations/patches/HideAutoplayButtonPatch.java
+++ integrations/java/app/revanced/integrations/patches/HideAutoplayButtonPatch.java
@@ -7,9 +7,4 @@ public class HideAutoplayButtonPatch {
     public static boolean isButtonShown() {
         return SettingsEnum.AUTOPLAY_BUTTON_SHOWN.getBoolean();
     }
-
-    public static boolean isButtonHidden() {
-        return !isButtonShown();
-    }
-
 }
