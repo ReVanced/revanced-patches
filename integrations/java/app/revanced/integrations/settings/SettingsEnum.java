cb91664626efee89fc05f3385d360c36a29c14a2
afn <47723802+afnzmn@users.noreply.github.com>
2022-08-25 21:23:38 -0400

    fix: change default value for autorepeat setting (#126)

--- integrations/java/app/revanced/integrations/settings/SettingsEnum.java
+++ integrations/java/app/revanced/integrations/settings/SettingsEnum.java
@@ -62,7 +62,7 @@ public enum SettingsEnum {
     //Misc. Settings
     //ToDo: Not used atm, Patch missing
     CAPTIONS_ENABLED("revanced_pref_captions", false, ReturnType.BOOLEAN),
-    PREFERRED_AUTO_REPEAT("revanced_pref_auto_repeat", true, ReturnType.BOOLEAN),
+    PREFERRED_AUTO_REPEAT("revanced_pref_auto_repeat", false, ReturnType.BOOLEAN),
     USE_HDR_AUTO_BRIGHTNESS("revanced_pref_hdr_autobrightness", true, ReturnType.BOOLEAN),
     TAP_SEEKING_ENABLED("revanced_enable_tap_seeking", true, ReturnType.BOOLEAN),
     ENABLE_MINIMIZED_PLAYBACK("revanced_enable_minimized_playback", true, ReturnType.BOOLEAN),
