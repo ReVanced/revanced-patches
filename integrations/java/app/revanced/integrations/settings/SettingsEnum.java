4f660596d7e17179add6007123d893ce970c670a
oSumAtrIX <johan.melkonyan1@web.de>
2022-10-13 06:02:44 +0200

    feat(youtube/remember-video-quality): remember the quality until changed by default

--- integrations/java/app/revanced/integrations/settings/SettingsEnum.java
+++ integrations/java/app/revanced/integrations/settings/SettingsEnum.java
@@ -20,7 +20,7 @@ public enum SettingsEnum {
     //Video Settings
     OLD_STYLE_QUALITY_SETTINGS("revanced_use_old_style_quality_settings", true, ReturnType.BOOLEAN),
     PREFERRED_VIDEO_SPEED("revanced_pref_video_speed", -2.0f, ReturnType.FLOAT),
-    REMEMBER_VIDEO_QUALITY("revanced_remember_video_quality_selection", true, ReturnType.BOOLEAN),
+    REMEMBER_VIDEO_QUALITY("revanced_remember_video_quality_selection", false, ReturnType.BOOLEAN),
 
     //Whitelist Settings
     //ToDo: Not used atm, Patch missing
