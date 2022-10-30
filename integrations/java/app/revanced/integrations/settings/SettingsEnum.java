f8e0d9ef675c22c01ae6502571290ebbbd24cde6
inotia00 <108592928+inotia00@users.noreply.github.com>
2022-10-30 21:02:22 +0900

    feat: `hide-watch-in-vr` patch (#191)

--- integrations/java/app/revanced/integrations/settings/SettingsEnum.java
+++ integrations/java/app/revanced/integrations/settings/SettingsEnum.java
@@ -77,6 +77,7 @@ public enum SettingsEnum {
     HIDE_PREVIEW_COMMENT("revanced_hide_preview_comment", false, ReturnType.BOOLEAN, true),
     HIDE_ALBUM_CARDS("revanced_hide_album_cards", false, ReturnType.BOOLEAN, true),
     HIDE_ARTIST_CARD("revanced_hide_artist_card", false, ReturnType.BOOLEAN),
+    HIDE_WATCH_IN_VR("revanced_hide_watch_in_vr", false, ReturnType.BOOLEAN, true),
 
     // Misc. Settings
     CAPTIONS_ENABLED("revanced_autocaptions_enabled", false, ReturnType.BOOLEAN, false),
