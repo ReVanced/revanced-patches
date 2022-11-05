a3de12c07505aa841c6025b25a9c58af75ca17ef
oSumAtrIX <johan.melkonyan1@web.de>
2022-11-05 06:24:32 +0100

    fix(fix-playback): seek to maximum end

--- integrations/java/app/revanced/integrations/patches/FixPlaybackPatch.java
+++ integrations/java/app/revanced/integrations/patches/FixPlaybackPatch.java
@@ -1,33 +1,37 @@
 package app.revanced.integrations.patches;
 
-import java.util.Timer;
-
 import app.revanced.integrations.settings.SettingsEnum;
 import app.revanced.integrations.utils.LogHelper;
 
 public final class FixPlaybackPatch {
     private static Thread currentThread = null;
-    public static void newVideoLoaded(final String _videoId) {
+    private static String videoId;
+
+    public static void newVideoLoaded(final String videoId) {
         if (!SettingsEnum.FIX_PLAYBACK.getBoolean()) return;
+
+        if (videoId.equals(FixPlaybackPatch.videoId)) return;
+        else FixPlaybackPatch.videoId = videoId;
 
         if (currentThread != null) {
             currentThread.interrupt();
         }
 
         currentThread = new Thread(() -> {
-            while (true) {
-                var currentVideoLength = PlayerControllerPatch.getCurrentVideoLength();
-                if (currentVideoLength > 1) {
-                    PlayerControllerPatch.seekTo(currentVideoLength);
-                    PlayerControllerPatch.seekTo(1);
-                    return;
-                }
+            try {
+                while (true) {
+                    var currentVideoTime = VideoInformation.getVideoTime();
 
-                try {
+                    if (currentVideoTime > -1) {
+                        VideoInformation.seekTo(Integer.MAX_VALUE);
+                        VideoInformation.seekTo(currentVideoTime);
+                        return;
+                    }
+
                     Thread.sleep(10);
-                } catch (InterruptedException e) {
-                    LogHelper.debug(FixPlaybackPatch.class, "Thread was interrupted");
                 }
+            } catch (InterruptedException e) {
+                LogHelper.debug(FixPlaybackPatch.class, "Thread was interrupted");
             }
         });
 
