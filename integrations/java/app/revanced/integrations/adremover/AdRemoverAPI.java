00cb361d5722397ba91653ee10ce5203628a211d
hyunsu15 <48500411+hyunsu15@users.noreply.github.com>
2022-12-19 23:05:33 +0900

    refactor : simplify `AdRemoverAPI.HideViewWithLayout1dp` (#240)

--- integrations/java/app/revanced/integrations/adremover/AdRemoverAPI.java
+++ integrations/java/app/revanced/integrations/adremover/AdRemoverAPI.java
@@ -3,11 +3,6 @@ package app.revanced.integrations.adremover;
 
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.Toolbar;
-
 import app.revanced.integrations.utils.LogHelper;
 
 public class AdRemoverAPI {
@@ -19,24 +14,12 @@ public class AdRemoverAPI {
      */
     //ToDo: refactor this
     public static void HideViewWithLayout1dp(View view) {
-        if (view instanceof LinearLayout) {
-            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(1, 1);
+        if (view instanceof ViewGroup) {
+            ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(1, 1);
             view.setLayoutParams(layoutParams);
-        } else if (view instanceof FrameLayout) {
-            FrameLayout.LayoutParams layoutParams2 = new FrameLayout.LayoutParams(1, 1);
-            view.setLayoutParams(layoutParams2);
-        } else if (view instanceof RelativeLayout) {
-            RelativeLayout.LayoutParams layoutParams3 = new RelativeLayout.LayoutParams(1, 1);
-            view.setLayoutParams(layoutParams3);
-        } else if (view instanceof Toolbar) {
-            Toolbar.LayoutParams layoutParams4 = new Toolbar.LayoutParams(1, 1);
-            view.setLayoutParams(layoutParams4);
-        } else if (view instanceof ViewGroup) {
-            ViewGroup.LayoutParams layoutParams5 = new ViewGroup.LayoutParams(1, 1);
-            view.setLayoutParams(layoutParams5);
-        } else {
-            LogHelper.printDebug(() -> "HideViewWithLayout1dp - Id: " + view.getId() + " Type: " + view.getClass().getName());
+            return;
         }
+        LogHelper.printDebug(
+                () -> "HideViewWithLayout1dp - Id: " + view.getId() + " Type: " + view.getClass().getName());
     }
-
 }
