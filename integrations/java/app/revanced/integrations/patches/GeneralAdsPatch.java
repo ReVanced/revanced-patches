516a8118084a468cb1f6d1385c31df310ae99e5e
0xrxL <120989892+0xrxL@users.noreply.github.com>
2022-12-19 16:01:21 +0100

    refactor(youtube/general-ads-patch): ignore comment components #238)

--- integrations/java/app/revanced/integrations/patches/GeneralAdsPatch.java
+++ integrations/java/app/revanced/integrations/patches/GeneralAdsPatch.java
@@ -12,6 +12,7 @@ public final class GeneralAdsPatch extends Filter {
             "home_video_with_context",
             "related_video_with_context",
             "comment_thread", // skip blocking anything in the comments
+            "|comment.", // skip blocking anything in the comments replies
             "download_",
             "library_recent_shelf",
             "playlist_add_to_option_wrapper" // do not block on "add to playlist" flyout menu
