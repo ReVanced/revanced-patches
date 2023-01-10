42af7523dd8c065101d3153e8c6ea34ba63a44c4
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2023-01-10 22:27:43 +0400

    feat(youtube): `spoof-app-version` patch (#282)
    Co-authored-by: oSumAtrIX <johan.melkonyan1@web.de>

--- integrations/java/app/revanced/integrations/settings/SettingsEnum.java
+++ integrations/java/app/revanced/integrations/settings/SettingsEnum.java
@@ -1,7 +1,6 @@
 package app.revanced.integrations.settings;
 
 import android.content.Context;
-import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -68,6 +67,7 @@ public enum SettingsEnum {
     DISABLE_STARTUP_SHORTS_PLAYER("revanced_startup_shorts_player_enabled", false, ReturnType.BOOLEAN),
     PLAYER_POPUP_PANELS("revanced_player_popup_panels_enabled", false, ReturnType.BOOLEAN),
     USE_TABLET_MINIPLAYER("revanced_tablet_miniplayer", false, ReturnType.BOOLEAN, true),
+    SPOOF_APP_VERSION("revanced_spoof_app_version", false, ReturnType.BOOLEAN, true),
     WIDE_SEARCHBAR("revanced_wide_searchbar", false, ReturnType.BOOLEAN, true),
     HIDE_ALBUM_CARDS("revanced_hide_album_cards", false, ReturnType.BOOLEAN, true),
     HIDE_ARTIST_CARD("revanced_hide_artist_card", false, ReturnType.BOOLEAN),
