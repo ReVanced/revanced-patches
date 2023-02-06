6babd77230766f3bd9175c23c06b681500e292ad
oSumAtrIX <johan.melkonyan1@web.de>
2023-02-06 23:07:19 +0100

    refactor(youtube/microg-support): make use of `MICROG_VENDOR` field
    Signed-off-by: oSumAtrIX <johan.melkonyan1@web.de>

--- integrations/java/app/revanced/integrations/patches/MicroGSupport.java
+++ integrations/java/app/revanced/integrations/patches/MicroGSupport.java
@@ -1,24 +1,23 @@
 package app.revanced.integrations.patches;
 
-import static app.revanced.integrations.sponsorblock.StringRef.str;
-
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.widget.Toast;
-
-import java.util.Objects;
-
 import app.revanced.integrations.utils.LogHelper;
 import app.revanced.integrations.utils.ReVancedUtils;
 
+import java.util.Objects;
+
+import static app.revanced.integrations.sponsorblock.StringRef.str;
+
 public class MicroGSupport {
     private static final String MICROG_VENDOR = "com.mgoogle";
-    private static final String MICROG_PACKAGE_NAME = "com.mgoogle.android.gms";
+    private static final String MICROG_PACKAGE_NAME = MICROG_VENDOR + ".android.gms";
     private static final String VANCED_MICROG_DOWNLOAD_LINK = "https://github.com/TeamVanced/VancedMicroG/releases/latest";
     private static final String DONT_KILL_MY_APP_LINK = "https://dontkillmyapp.com";
-    private static final Uri VANCED_MICROG_PROVIDER = Uri.parse("content://com.mgoogle.android.gsf.gservices/prefix");
+    private static final Uri VANCED_MICROG_PROVIDER = Uri.parse("content://" + MICROG_VENDOR + ".android.gsf.gservices/prefix");
 
     private static void startIntent(Context context, String uriString, String message) {
         Toast.makeText(context, message, Toast.LENGTH_LONG).show();
