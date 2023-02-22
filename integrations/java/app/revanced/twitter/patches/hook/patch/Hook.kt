4dadcfdf1f9dc35c11c6d8b44efa18a92e3a1863
oSumAtrIX <johan.melkonyan1@web.de>
2023-02-22 05:42:28 +0100

    fix: remove nullable annotation in Kotlin code
    Signed-off-by: oSumAtrIX <johan.melkonyan1@web.de>

--- integrations/java/app/revanced/twitter/patches/hook/patch/Hook.kt
+++ integrations/java/app/revanced/twitter/patches/hook/patch/Hook.kt
@@ -1,11 +1,9 @@
 package app.revanced.twitter.patches.hook.patch
 
-import androidx.annotation.NonNull
-
 interface Hook<T> {
     /**
      * Hook the given type.
      * @param type The type to hook
      */
-    fun hook(@NonNull type: T): T
+    fun hook(type: T): T
 }
