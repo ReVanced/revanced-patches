a5ce48fb4d499c34f0dc42317019de249c1458c7
oSumAtrIX <johan.melkonyan1@web.de>
2023-02-26 23:19:31 +0100

    fix(youtube/hide-floating-microphone-button): reboot when changing settings
    Signed-off-by: oSumAtrIX <johan.melkonyan1@web.de>

--- integrations/java/app/revanced/integrations/patches/HideFloatingMicrophoneButtonPatch.java
+++ integrations/java/app/revanced/integrations/patches/HideFloatingMicrophoneButtonPatch.java
@@ -3,7 +3,7 @@ package app.revanced.integrations.patches;
 import app.revanced.integrations.settings.SettingsEnum;
 
 public final class HideFloatingMicrophoneButtonPatch {
-    public static boolean hideFloatingMicrophoneButton(boolean original) {
+    public static boolean hideFloatingMicrophoneButton(final boolean original) {
         return SettingsEnum.HIDE_FLOATING_MICROPHONE_BUTTON.getBoolean() || original;
     }
 }
