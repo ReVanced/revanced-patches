b8f7d9c2ec0a16e53a769b858604f924e035f109
oSumAtrIX <johan.melkonyan1@web.de>
2023-02-26 22:38:08 +0100

    feat(youtube/hide-autoplay-button): do not disable autoplay button when hidden
    Signed-off-by: oSumAtrIX <johan.melkonyan1@web.de>

--- integrations/java/app/revanced/integrations/patches/HideAutoplayButtonPatch.java
+++ integrations/java/app/revanced/integrations/patches/HideAutoplayButtonPatch.java
@@ -3,8 +3,7 @@ package app.revanced.integrations.patches;
 import app.revanced.integrations.settings.SettingsEnum;
 
 public class HideAutoplayButtonPatch {
-
     public static boolean isButtonShown() {
-        return SettingsEnum.HIDE_AUTOPLAY_BUTTON.getBoolean() == false;
+        return !SettingsEnum.HIDE_AUTOPLAY_BUTTON.getBoolean();
     }
 }
