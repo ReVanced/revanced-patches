6943a7aa0d2f239f454d21709a23362fad37c65d
oSumAtrIX <johan.melkonyan1@web.de>
2023-03-29 19:36:16 +0200

    feat(youtube/general-ads): hide new type of ad

--- integrations/java/app/revanced/integrations/patches/GeneralAdsPatch.java
+++ integrations/java/app/revanced/integrations/patches/GeneralAdsPatch.java
@@ -49,6 +49,7 @@ public final class GeneralAdsPatch extends Filter {
                 "_buttoned_layout",
                 "full_width_square_image_layout",
                 "_ad_with",
+                "video_display_button_group_layout",
                 "landscape_image_wide_button_layout"
         );
         var generalAds = new BlockRule(
