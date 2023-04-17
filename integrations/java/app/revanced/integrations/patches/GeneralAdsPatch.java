73e771d1c9176f94642466593d1114da66987081
oSumAtrIX <johan.melkonyan1@web.de>
2023-04-18 01:55:33 +0200

    feat(youtube/general-ads): hide new type of movie offer ad

--- integrations/java/app/revanced/integrations/patches/GeneralAdsPatch.java
+++ integrations/java/app/revanced/integrations/patches/GeneralAdsPatch.java
@@ -74,7 +74,8 @@ public final class GeneralAdsPatch extends Filter {
                 "compact_movie",
                 "horizontal_movie_shelf",
                 "movie_and_show_upsell_card",
-                "compact_tvfilm_item"
+                "compact_tvfilm_item",
+                "offer_module_root
         );
 
         this.pathRegister.registerAll(
