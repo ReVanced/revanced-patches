e624795d4774737622800afe9cb15b7ffb3e46da
oSumAtrIX <johan.melkonyan1@web.de>
2023-04-27 21:08:32 +0200

    refactor: use better method name

--- integrations/java/app/revanced/integrations/patches/MinimizedPlaybackPatch.java
+++ integrations/java/app/revanced/integrations/patches/MinimizedPlaybackPatch.java
@@ -4,7 +4,7 @@ import app.revanced.integrations.shared.PlayerType;
 
 public class MinimizedPlaybackPatch {
 
-    public static boolean playbackIsNotShort() {
+    public static boolean isPlaybackNotShort() {
         return !PlayerType.getCurrent().isNoneOrHidden();
     }
 
