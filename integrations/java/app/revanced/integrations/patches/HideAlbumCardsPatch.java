3d2c10c184f94b717868349a903ea0a4262e101b
oSumAtrIX <johan.melkonyan1@web.de>
2023-05-06 23:09:58 +0200

    refactor: simplify casting instructions

--- integrations/java/app/revanced/integrations/patches/HideAlbumCardsPatch.java
+++ integrations/java/app/revanced/integrations/patches/HideAlbumCardsPatch.java
@@ -6,8 +6,7 @@ import app.revanced.integrations.adremover.AdRemoverAPI;
 import app.revanced.integrations.settings.SettingsEnum;
 
 public class HideAlbumCardsPatch {
-    //Used by app.revanced.patches.youtube.layout.hidealbumcards.patch.HideAlbumCardsPatch
-    public static void hideAlbumCards(View view) {
+    public static void hideAlbumCard(View view) {
         if (!SettingsEnum.HIDE_ALBUM_CARDS.getBoolean()) return;
         AdRemoverAPI.HideViewWithLayout1dp(view);
     }
