507ce1e495f89e9668fac84ac1c9c5d59007145f
oSumAtrIX <johan.melkonyan1@web.de>
2023-05-18 17:34:59 +0200

    refactor(youtube/settings): increase fingerprint robustness

--- integrations/java/app/revanced/integrations/utils/ThemeHelper.java
+++ integrations/java/app/revanced/integrations/utils/ThemeHelper.java
@@ -3,13 +3,6 @@ package app.revanced.integrations.utils;
 public class ThemeHelper {
     private static int themeValue;
 
-    public static void setTheme(int value) {
-        if (themeValue != value) {
-            themeValue = value;
-            LogHelper.printDebug(() -> "Theme value: " + themeValue);
-        }
-    }
-
     public static void setTheme(Object value) {
         final int newOrdinalValue = ((Enum) value).ordinal();
         if (themeValue != newOrdinalValue) {
