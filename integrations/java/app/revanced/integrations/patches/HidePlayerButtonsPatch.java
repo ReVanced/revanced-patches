b1f73d754faaf082e8c259cc39ab0276a662d8cd
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2023-05-25 21:33:30 +0400

    fix(youtube/hide-player-buttons): fix previous/next button showing if previous video exists (#412)

--- integrations/java/app/revanced/integrations/patches/HidePlayerButtonsPatch.java
+++ integrations/java/app/revanced/integrations/patches/HidePlayerButtonsPatch.java
@@ -1,11 +1,16 @@
 package app.revanced.integrations.patches;
 
-
 import app.revanced.integrations.settings.SettingsEnum;
 
 public final class HidePlayerButtonsPatch {
 
-    public static boolean hideButtons() {
-        return SettingsEnum.HIDE_PLAYER_BUTTONS.getBoolean();
+    /**
+     * Injection point.
+     */
+    public static boolean previousOrNextButtonIsVisible(boolean previousOrNextButtonVisible) {
+        if (SettingsEnum.HIDE_PLAYER_BUTTONS.getBoolean()) {
+            return false;
+        }
+        return previousOrNextButtonVisible;
     }
 }
