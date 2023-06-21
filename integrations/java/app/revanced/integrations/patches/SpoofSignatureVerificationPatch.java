999ac23403c6aaa4318e84d91de8a1644f98af04
oSumAtrIX <johan.melkonyan1@web.de>
2023-06-22 00:58:56 +0200

    fix(youtube/client-spoof): use new visitor data to work around playback issues
    See https://github.com/yt-dlp/yt-dlp/commit/81ca451480051d7ce1a31c017e005358345a9149

--- integrations/java/app/revanced/integrations/patches/SpoofSignatureVerificationPatch.java
+++ integrations/java/app/revanced/integrations/patches/SpoofSignatureVerificationPatch.java
@@ -1,16 +1,20 @@
 package app.revanced.integrations.patches;
 
-import static app.revanced.integrations.utils.ReVancedUtils.containsAny;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-
 import app.revanced.integrations.settings.SettingsEnum;
 import app.revanced.integrations.shared.PlayerType;
 import app.revanced.integrations.utils.LogHelper;
 import app.revanced.integrations.utils.ReVancedUtils;
 
+import static app.revanced.integrations.utils.ReVancedUtils.containsAny;
+
 public class SpoofSignatureVerificationPatch {
+    /**
+     * Enable/disable all workarounds that are required due to signature spoofing.
+     */
+    private static final boolean WORKAROUND = true;
+
     /**
      * Protobuf parameters used for autoplay in scrim.
      * Prepend this parameter to mute video playback (for autoplay in feed)
@@ -18,10 +22,12 @@ public class SpoofSignatureVerificationPatch {
     private static final String PROTOBUF_PARAMETER_SCRIM = "SAFgAXgB";
 
     /**
-     * Protobuf parameter of shorts and YouTube stories.
+     * Protobuf parameter also used by
+     * <a href="https://github.com/yt-dlp/yt-dlp/blob/81ca451480051d7ce1a31c017e005358345a9149/yt_dlp/extractor/youtube.py#L3602">yt-dlp</a>
+     * <br>
      * Known issue: captions are positioned on upper area in the player.
      */
-    private static final String PROTOBUF_PARAMETER_SHORTS = "8AEB"; // "8AEByAMTuAQP"
+    private static final String PROTOBUF_PLAYER_PARAMS = "CgIQBg==";
 
     /**
      * Target Protobuf parameters.
@@ -66,17 +72,18 @@ public class SpoofSignatureVerificationPatch {
 
             LogHelper.printDebug(() -> "Original protobuf parameter value: " + originalValue);
 
-            // Video is Short or Story.
-            var isPlayingShorts = originalValue.contains(PROTOBUF_PARAMETER_SHORTS);
-            if (isPlayingShorts) return originalValue;
+            if (!WORKAROUND) return PROTOBUF_PLAYER_PARAMS;
+
+            var isPlayingVideo = originalValue.contains(PROTOBUF_PLAYER_PARAMS);
+            if (isPlayingVideo) return originalValue;
 
             boolean isPlayingFeed = containsAny(originalValue, PROTOBUF_PARAMETER_TARGETS) && PlayerType.getCurrent() == PlayerType.INLINE_MINIMAL;
             if (isPlayingFeed) {
                 // Videos in feed won't autoplay with sound.
-                return PROTOBUF_PARAMETER_SCRIM + PROTOBUF_PARAMETER_SHORTS;
+                return PROTOBUF_PARAMETER_SCRIM + PROTOBUF_PLAYER_PARAMS;
             } else {
                 // Spoof the parameter to prevent playback issues.
-                return PROTOBUF_PARAMETER_SHORTS;
+                return PROTOBUF_PLAYER_PARAMS;
             }
         } catch (Exception ex) {
             LogHelper.printException(() -> "overrideProtobufParameter failure", ex);
@@ -141,6 +148,10 @@ public class SpoofSignatureVerificationPatch {
                 lastVs = vs;
                 lastSd = sd;
             }
+        }
+
+        if (!WORKAROUND) {
+            return new int[]{ap, ah, av};
         }
 
         // Videos with custom captions that specify screen positions appear to always have correct screen positions (even with spoofing).
