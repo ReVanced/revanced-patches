885fa36079f9be2f91628ba1876d8cdc5c71685b
oSumAtrIX <johan.melkonyan1@web.de>
2023-09-25 19:57:53 +0200

    perf: Remove unnecessary api key parameter

--- integrations/java/app/revanced/integrations/patches/spoof/requests/StoryBoardRendererRoutes.java
+++ integrations/java/app/revanced/integrations/patches/spoof/requests/StoryBoardRendererRoutes.java
@@ -9,8 +9,6 @@ import java.net.HttpURLConnection;
 /** @noinspection unused*/
 public final class StoryBoardRendererRoutes {
     private static final String YT_API_URL = "https://www.youtube.com/youtubei/v1/";
-    private static final String YT_API_KEY = "AIzaSyA8eiZmM1FaDVjRy-df2KTyQ_vz_yYM39w";
-
     static final Route GET_STORYBOARD_SPEC = new Route(
             Route.Method.POST,
             "player" +
@@ -23,7 +21,7 @@ public final class StoryBoardRendererRoutes {
     }
 
     public static HttpURLConnection getPlayerResponseConnectionFromRoute() throws IOException {
-        var connection = Requester.getConnectionFromRoute(YT_API_URL, GET_STORYBOARD_SPEC, YT_API_KEY);
+        var connection = Requester.getConnectionFromRoute(YT_API_URL, GET_STORYBOARD_SPEC);
         connection.setRequestProperty("User-Agent", "com.google.android.youtube/18.37.36 (Linux; U; Android 12; GB) gzip");
         connection.setRequestProperty("X-Goog-Api-Format-Version", "2");
         connection.setRequestProperty("Content-Type", "application/json");
