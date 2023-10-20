9350f4b4516c784b79b40ac9b7aa06238397a456
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2023-10-20 13:18:02 +0300

    fix(YouTube - Minimized playback): Fix pip incorrectly showing for Short playback (#504)

--- integrations/java/app/revanced/integrations/patches/MinimizedPlaybackPatch.java
+++ integrations/java/app/revanced/integrations/patches/MinimizedPlaybackPatch.java
@@ -4,10 +4,16 @@ import app.revanced.integrations.shared.PlayerType;
 
 public class MinimizedPlaybackPatch {
 
-    public static boolean isPlaybackNotShort() {
+    /**
+     * Injection point.
+     */
+    public static boolean videoSupportsMinimizedPlayback() {
         return !PlayerType.getCurrent().isNoneHiddenOrSlidingMinimized();
     }
 
+    /**
+     * Injection point.
+     */
     public static boolean overrideMinimizedPlaybackAvailable() {
         // This could be done entirely in the patch,
         // but having a unique method to search for makes manually inspecting the patched apk much easier.
