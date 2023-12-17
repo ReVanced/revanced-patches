5044a86f703997dd9bf803d44689c7b2d8fcb4b5
d4rkk3y <vuhoanhuy99@gmail.com>
2023-12-18 06:27:09 +0700

    chore: add comments

--- src/main/kotlin/app/revanced/patches/tiktok/interaction/clearmode/RememberClearModePatch.kt
+++ src/main/kotlin/app/revanced/patches/tiktok/interaction/clearmode/RememberClearModePatch.kt
@@ -31,6 +31,8 @@ object RememberClearModePatch : BytecodePatch(
     override fun execute(context: BytecodeContext) {
         OnClearModeEventFingerprint.result?.mutableMethod?.let {
 
+            // Catches the clear mode configuration changed event and saved that configuration
+            // to apply to other videos.
             it.apply {
                 val injectIndex = indexOfFirstInstruction { opcode == Opcode.IGET_BOOLEAN } + 1
                 val register = getInstruction<Instruction22c>(injectIndex - 1).registerA
@@ -42,13 +44,21 @@ object RememberClearModePatch : BytecodePatch(
                 )
             }
 
+            // Changes the clear mode configuration on the first frame of video.
+            // Because the default behavior of TikTok is turn off clear mode when swiping to next video.
             val clearModeEventClass = it.parameters[0].type
             OnRenderFirstFrameFingerprint.result?.mutableMethod?.addInstructions(
                 0,
                 """
+                    # These instructions will create a clearModeEvent which will be posted to notify other 
+                    # app components. TikTok use https://github.com/greenrobot/EventBus 
+                    # To create a new clearModeEvent we need 3 arguments
+                    # First is a Integer which present the type of clear mode such as 0 = LONG_PRESS, 1 = SCREEN_RECORD,...
                     new-instance v0, $clearModeEventClass
                     const/4 v1, 0x0
+                    # Second is a String which is similar to the first but as String.
                     const-string v2, "long_press"
+                    # Third is a Boolean which is the state of clear mode.
                     invoke-static {}, Lapp/revanced/tiktok/clearmode/RememberClearModePatch;->getClearModeState()Z
                     move-result v3
                     invoke-direct {v0, v1, v2, v3}, $clearModeEventClass-><init>(ILjava/lang/String;Z)V
