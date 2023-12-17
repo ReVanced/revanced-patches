ca016d6050056c765a6d619809d74784d5827e11
d4rkk3y <vuhoanhuy99@gmail.com>
2023-12-18 06:06:35 +0700

    refactor: rename base on change request

--- src/main/kotlin/app/revanced/patches/tiktok/interaction/clearmode/RememberClearModePatch.kt
+++ src/main/kotlin/app/revanced/patches/tiktok/interaction/clearmode/RememberClearModePatch.kt
@@ -14,15 +14,15 @@ import com.android.tools.smali.dexlib2.Opcode
 import com.android.tools.smali.dexlib2.iface.instruction.formats.Instruction22c
 
 @Patch(
-    name = "Clear mode",
-    description = "Retains the clear mode configurations in between videos.",
+    name = "Clear display",
+    description = "Retains the clear display configurations in between videos.",
     compatiblePackages = [
         CompatiblePackage("com.ss.android.ugc.trill", ["32.5.3"]),
         CompatiblePackage("com.zhiliaoapp.musically", ["32.5.3"])
     ]
 )
 @Suppress("unused")
-object ClearModePatch : BytecodePatch(
+object RememberClearModePatch : BytecodePatch(
     setOf(
         OnClearModeEventFingerprint,
         OnRenderFirstFrameFingerprint
@@ -38,7 +38,7 @@ object ClearModePatch : BytecodePatch(
                 addInstructions(
                     injectIndex,
                     "invoke-static { v$register }, " +
-                            "Lapp/revanced/tiktok/clearmode/ClearModePatch;->saveClearModeState(Z)V"
+                            "Lapp/revanced/tiktok/clearmode/RememberClearModePatch;->rememberClearModeState(Z)V"
                 )
             }
 
@@ -49,7 +49,7 @@ object ClearModePatch : BytecodePatch(
                     new-instance v0, $clearModeEventClass
                     const/4 v1, 0x0
                     const-string v2, "long_press"
-                    invoke-static {}, Lapp/revanced/tiktok/clearmode/ClearModePatch;->getClearModeState()Z
+                    invoke-static {}, Lapp/revanced/tiktok/clearmode/RememberClearModePatch;->getClearModeState()Z
                     move-result v3
                     invoke-direct {v0, v1, v2, v3}, $clearModeEventClass-><init>(ILjava/lang/String;Z)V
                     invoke-virtual {v0}, $clearModeEventClass->post()Lcom/ss/android/ugc/governance/eventbus/IEvent;
