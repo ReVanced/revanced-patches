64534497c7c9d21ffd0ce1738fa912ca5e98a057
KobeW50 <84587632+KobeW50@users.noreply.github.com>
2023-12-19 22:55:37 -0500

    (Theme): Add default behavior of dark theme

--- src/main/kotlin/app/revanced/patches/youtube/layout/theme/ThemeBytecodePatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/theme/ThemeBytecodePatch.kt
@@ -19,7 +19,7 @@ import com.android.tools.smali.dexlib2.iface.instruction.OneRegisterInstruction
 
 @Patch(
     name = "Theme",
-    description = "Adds options for theming and applies a custom background theme.",
+    description = "Adds options for theming and applies a custom background theme (dark background defaults to amoled black).",
     dependencies = [
         LithoColorHookPatch::class,
         SeekbarColorBytecodePatch::class,
