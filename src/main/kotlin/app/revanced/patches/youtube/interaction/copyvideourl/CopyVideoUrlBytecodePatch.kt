6202d24d4c59ec78ce143b16ffcc05036f1e0217
KobeW50 <84587632+KobeW50@users.noreply.github.com>
2023-12-20 13:03:23 -0500

    (Copy video URL): Apply suggestions from review

--- src/main/kotlin/app/revanced/patches/youtube/interaction/copyvideourl/CopyVideoUrlBytecodePatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/interaction/copyvideourl/CopyVideoUrlBytecodePatch.kt
@@ -8,8 +8,8 @@ import app.revanced.patches.youtube.misc.playercontrols.PlayerControlsBytecodePa
 import app.revanced.patches.youtube.video.information.VideoInformationPatch
 
 @Patch(
-    name = "Copy video url",
-    description = "Adds options to add buttons in the video player to copy video links.",
+    name = "Copy video URL",
+    description = "Adds options to display buttons in the video player to copy video URLs.",
     dependencies = [
         CopyVideoUrlResourcePatch::class,
         PlayerControlsBytecodePatch::class,
