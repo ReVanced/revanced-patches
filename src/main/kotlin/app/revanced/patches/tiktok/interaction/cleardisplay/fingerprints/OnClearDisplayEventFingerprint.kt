d7e44cb887396bba27ee81c70c006f0cc247c56d
oSumAtrIX <johan.melkonyan1@web.de>
2023-12-21 15:32:18 +0100

    fix(Tiktok - Remember clear display): Use correct name

--- src/main/kotlin/app/revanced/patches/tiktok/interaction/cleardisplay/fingerprints/OnClearDisplayEventFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/tiktok/interaction/cleardisplay/fingerprints/OnClearDisplayEventFingerprint.kt
@@ -1,9 +1,10 @@
-package app.revanced.patches.tiktok.interaction.clearmode.fingerprints
+package app.revanced.patches.tiktok.interaction.cleardisplay.fingerprints
 
 import app.revanced.patcher.fingerprint.MethodFingerprint
 
-internal object OnClearModeEventFingerprint : MethodFingerprint(
+internal object OnClearDisplayEventFingerprint : MethodFingerprint(
     customFingerprint = { methodDef, _ ->
+        // Internally the feature is called "Clear mode".
         methodDef.definingClass.endsWith("/ClearModePanelComponent;") && methodDef.name == "onClearModeEvent"
     }
 )
