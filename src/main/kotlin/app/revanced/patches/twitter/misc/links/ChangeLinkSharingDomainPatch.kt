e7c83b6fab06b40fcdf0e4c7d8af14278473a357
oSumAtrIX <johan.melkonyan1@web.de>
2024-01-02 18:06:10 +0100

    simplify by deduplicating code

--- src/main/kotlin/app/revanced/patches/twitter/misc/links/ChangeLinkSharingDomainPatch.kt
+++ src/main/kotlin/app/revanced/patches/twitter/misc/links/ChangeLinkSharingDomainPatch.kt
@@ -18,6 +18,8 @@ import app.revanced.util.getReference
 import app.revanced.util.indexOfFirstInstruction
 import com.android.tools.smali.dexlib2.Opcode
 import com.android.tools.smali.dexlib2.builder.BuilderInstruction
+import com.android.tools.smali.dexlib2.iface.Method
+import com.android.tools.smali.dexlib2.iface.instruction.Instruction
 import com.android.tools.smali.dexlib2.iface.instruction.OneRegisterInstruction
 import com.android.tools.smali.dexlib2.iface.instruction.TwoRegisterInstruction
 import com.android.tools.smali.dexlib2.iface.reference.MethodReference
@@ -41,107 +43,104 @@ object ChangeLinkSharingDomainPatch : BytecodePatch(
     )
 
     override fun execute(context: BytecodeContext) {
-        val linkBuilderResult = LinkBuilderFingerprint.result ?: throw LinkBuilderFingerprint.exception
+        fun Method.indexOfFirstOrThrow(errorMessage: String, predicate: Instruction.() -> Boolean) =
+            indexOfFirstInstruction(predicate).also {
+                if (it == -1) throw PatchException(errorMessage)
+            }
 
-        // region Copy link button.
-        val buildShareLinkMethod = linkBuilderResult.mutableMethod.apply {
-            val stringIndex = linkBuilderResult.scanResult.stringsScanResult!!.matches
-                .first().index
+        // region Replace the domain name when copying a link with the button.
 
-            val targetRegister = getInstruction<OneRegisterInstruction>(stringIndex).registerA
-            replaceInstruction(
-                stringIndex,
-                "const-string v$targetRegister, \"https://$domainName/%1\$s/status/%2\$d\""
-            )
-        }
+        val buildShareLinkMethod = LinkBuilderFingerprint.result?.let {
+            it.mutableMethod.apply {
+                val linkStringIndex = it.scanResult.stringsScanResult!!.matches.first().index
+                val targetRegister = getInstruction<OneRegisterInstruction>(linkStringIndex).registerA
 
+                replaceInstruction(
+                    linkStringIndex,
+                    "const-string v$targetRegister, \"https://$domainName/%1\$s/status/%2\$d\""
+                )
+            }
+        } ?: throw LinkBuilderFingerprint.exception
 
         // endregion
 
         // Used in the Share via... dialog.
-        LinkResourceGetterFingerprint.result?.apply {
-            // Remove the original call to the method that uses the resources template to build the link.
-            val originalMethodIndex = mutableMethod.indexOfFirstInstruction {
+        LinkResourceGetterFingerprint.result?.mutableMethod?.apply {
+            // region Remove the original call to the method that uses the resources template to build the link.
+
+            val originalMethodIndex = indexOfFirstOrThrow("Could not find originalMethodIndex") {
                 getReference<MethodReference>()?.definingClass == "Landroid/content/res/Resources;"
             }
-            if (originalMethodIndex == -1) throw PatchException("Could not find originalMethodIndex")
 
-            val shareLinkRegister =
-                mutableMethod.getInstruction<OneRegisterInstruction>(
-                    // Offset to instruction that saves the result of the method call to the register.
-                    originalMethodIndex + 1
-                ).registerA
+            val shareLinkRegister = getInstruction<OneRegisterInstruction>(
+                // Offset to instruction that saves the result of the method call to the register.
+                originalMethodIndex + 1
+            ).registerA
 
-            mutableMethod.apply {
-                // Remove original method call and instruction to move the result to the shareLinkRegister.
-                removeInstructions(originalMethodIndex, 2)
+            // Remove original method call and instruction to move the result to the shareLinkRegister.
+            removeInstructions(originalMethodIndex, 2)
 
-                // Remove the instruction that uses the shareLinkRegister as an array reference to prevent an error.
-                // Original smali aput-object v3, v1, v5 (v1 is the shareLinkRegister)
-                removeInstructions(originalMethodIndex - 2, 1)
-            }
-
-
-            // region Get the nickname of the user that posted the tweet. This is used to build the link.
-            val getNicknameIndex = mutableMethod.indexOfFirstInstruction {
-                getReference<MethodReference>().toString().endsWith("Ljava/lang/String;")
-            }
-            if (getNicknameIndex == -1) throw PatchException("Could not find getNicknameIndex")
-
-            val sourceNicknameRegister =
-                this.mutableMethod.getInstruction<OneRegisterInstruction>(
-                    // Offset to instruction that saves the result of the method call to the register.
-                    getNicknameIndex + 1
-                ).registerA
-
-            // Instruction with a spare register that can be used to store the user nickname.
-            val tempInstructionIndex = mutableMethod.indexOfFirstInstruction {
-                opcode == Opcode.IGET_OBJECT
-            }
-            if (tempInstructionIndex == -1) throw PatchException("Could not find tempInstructionIndex")
-            if (tempInstructionIndex > getNicknameIndex) throw PatchException(
-                "tempInstructionIndex > getNicknameIndex, this indicates that the instruction was moved."
-            )
-
-            val tempInstruction = mutableMethod.getInstruction<TwoRegisterInstruction>(
-                tempInstructionIndex
-            )
-            val nicknameRegister = tempInstruction.registerA
-
-            // Save the user nickname to the register that was used to store "this".
-            mutableMethod.addInstruction(
-                // This offset is used to place the instruction after sourceNicknameRegister is filled with data.
-                getNicknameIndex + 2,
-                "move-object v${nicknameRegister}, v$sourceNicknameRegister"
-            )
+            // Remove the instruction that uses the shareLinkRegister as an array reference to prevent an error.
+            // Original SMALI aput-object v3, v1, v5 (v1 is the shareLinkRegister)
+            removeInstructions(originalMethodIndex - 2, 1)
             // endregion
 
+            // region Get the nickname of the user that posted the tweet. This is used to build the link.
+
+            val getNicknameIndex = indexOfFirstOrThrow("Could not find getNicknameIndex") {
+                getReference<MethodReference>().toString().endsWith("Ljava/lang/String;")
+            }
+
+            val sourceNicknameRegister = getInstruction<OneRegisterInstruction>(
+                // Offset to instruction that saves the result of the method call to the register.
+                getNicknameIndex + 1
+            ).registerA
+
+            // Instruction with a spare register that can be used to store the user nickname.
+            val tempInstructionIndex = indexOfFirstOrThrow("Could not find tempInstructionIndex") {
+                opcode == Opcode.IGET_OBJECT
+            }.also {
+                if (it > getNicknameIndex) throw PatchException(
+                    "tempInstructionIndex > getNicknameIndex, this indicates that the instruction was moved."
+                )
+            }
+
+            val tempInstruction = getInstruction<TwoRegisterInstruction>(tempInstructionIndex)
+
+            // Save the user nickname to the register that was used to store "this".
+            val tempNicknameRegister = tempInstruction.registerA
+            addInstruction(
+                // This offset is used to place the instruction after sourceNicknameRegister is filled with data.
+                getNicknameIndex + 2,
+                "move-object v${tempNicknameRegister}, v$sourceNicknameRegister"
+            )
+
+            // endregion
 
             // region Call the patched method and save the result to shareLinkRegister.
-            val convertTweetIdToLongIndex = mutableMethod.indexOfFirstInstruction {
-                opcode == Opcode.INVOKE_VIRTUAL &&
-                        getReference<MethodReference>()?.definingClass == "Ljava/lang/Long;"
-            }
-            if (convertTweetIdToLongIndex == -1) throw PatchException("Could not find convertTweetIdToLongIndex")
 
-            val tweetIdP1 =
-                mutableMethod.getInstruction<OneRegisterInstruction>(convertTweetIdToLongIndex + 1).registerA
+            val convertTweetIdToLongIndex = indexOfFirstOrThrow("Could not find convertTweetIdToLongIndex") {
+                opcode == Opcode.INVOKE_VIRTUAL && getReference<MethodReference>()?.definingClass == "Ljava/lang/Long;"
+            }
+
+            val tweetIdP1 = getInstruction<OneRegisterInstruction>(convertTweetIdToLongIndex + 1).registerA
             val tweetIdP2 = tweetIdP1 + 1
 
-            this.mutableMethod.addInstructions(
+            addInstructions(
                 // This offset is used to place the instruction after the save of the method result.
                 convertTweetIdToLongIndex + 2,
                 """
-                    invoke-static { v$tweetIdP1, v$tweetIdP2, v$nicknameRegister }, $buildShareLinkMethod
+                    invoke-static { v$tweetIdP1, v$tweetIdP2, v$tempNicknameRegister }, $buildShareLinkMethod
                     move-result-object v$shareLinkRegister
                 """
             )
 
             // Restore the borrowed nicknameRegister.
-            this.mutableMethod.addInstruction(
+            addInstruction(
                 convertTweetIdToLongIndex + 4,
                 tempInstruction as BuilderInstruction
             )
+
             // endregion
         } ?: throw LinkResourceGetterFingerprint.exception
     }
