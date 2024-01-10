b9e913d2bb0591040dbcaae8c506455a932fd699
d4rkk3y <43563783+d4rkk3y@users.noreply.github.com>
2024-01-10 15:17:58 +0700

    feat(Tiktok - Playback speed): Remember playback speed (#543)
    Co-authored-by: oSumAtrIX <johan.melkonyan1@web.de>

--- integrations/java/app/revanced/integrations/tiktok/settings/Settings.java
+++ integrations/java/app/revanced/integrations/tiktok/settings/Settings.java
@@ -5,6 +5,7 @@ import static java.lang.Boolean.TRUE;
 
 import app.revanced.integrations.shared.settings.BaseSettings;
 import app.revanced.integrations.shared.settings.BooleanSetting;
+import app.revanced.integrations.shared.settings.FloatSetting;
 import app.revanced.integrations.shared.settings.StringSetting;
 
 public class Settings extends BaseSettings {
@@ -14,9 +15,10 @@ public class Settings extends BaseSettings {
     public static final BooleanSetting HIDE_IMAGE = new BooleanSetting("hide_image", FALSE, true);
     public static final StringSetting MIN_MAX_VIEWS = new StringSetting("min_max_views", "0-" + Long.MAX_VALUE, true);
     public static final StringSetting MIN_MAX_LIKES = new StringSetting("min_max_likes", "0-" + Long.MAX_VALUE, true);
-    public static final StringSetting DOWNLOAD_PATH = new StringSetting("down_path","DCIM/TikTok");
+    public static final StringSetting DOWNLOAD_PATH = new StringSetting("down_path", "DCIM/TikTok");
     public static final BooleanSetting DOWNLOAD_WATERMARK = new BooleanSetting("down_watermark", TRUE);
     public static final BooleanSetting CLEAR_DISPLAY = new BooleanSetting("clear_display", FALSE);
+    public static final FloatSetting REMEMBERED_SPEED = new FloatSetting("REMEMBERED_SPEED", 1.0f);
     public static final BooleanSetting SIM_SPOOF = new BooleanSetting("simspoof", TRUE, true);
     public static final StringSetting SIM_SPOOF_ISO = new StringSetting("simspoof_iso", "us");
     public static final StringSetting SIMSPOOF_MCCMNC = new StringSetting("simspoof_mccmnc", "310160");
