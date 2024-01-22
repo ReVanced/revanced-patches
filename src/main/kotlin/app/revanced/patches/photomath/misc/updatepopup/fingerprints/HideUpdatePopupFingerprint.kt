a7a0b20cf2920fd2a698b8d25a71c593618a98e9
Marko <34315725+rospino74@users.noreply.github.com>
2024-01-22 19:30:53 +0100

    Reduced opcode list size
    Co-authored-by: oSumAtrIX <johan.melkonyan1@web.de>

--- src/main/kotlin/app/revanced/patches/photomath/misc/updatepopup/fingerprints/HideUpdatePopupFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/photomath/misc/updatepopup/fingerprints/HideUpdatePopupFingerprint.kt
@@ -8,19 +8,11 @@ import com.android.tools.smali.dexlib2.Opcode
 internal object HideUpdatePopupFingerprint : MethodFingerprint(
     customFingerprint = { _, classDef -> classDef.type == "Lcom/microblink/photomath/main/activity/MainActivity;" }, // The popup is shown only in the main activity
     opcodes = listOf(
-        Opcode.CONST_4,
-        Opcode.IGET_OBJECT,
-        Opcode.INVOKE_VIRTUAL, // View.setVisibility(0)
-        Opcode.IGET_OBJECT,
         Opcode.CONST_HIGH16,
         Opcode.INVOKE_VIRTUAL, // ViewPropertyAnimator.alpha(1.0f)
         Opcode.MOVE_RESULT_OBJECT,
         Opcode.CONST_WIDE_16,
         Opcode.INVOKE_VIRTUAL, // ViewPropertyAnimator.setDuration(1000L)
-        Opcode.MOVE_RESULT_OBJECT,
-        Opcode.NEW_INSTANCE,
-        Opcode.INVOKE_DIRECT,
-        Opcode.INVOKE_VIRTUAL, // ViewPropertyAnimator.setListener(new AnimatorListenerAdapter() { ... })
     ),
     accessFlags = AccessFlags.FINAL or AccessFlags.PUBLIC,
     returnType = "V",
