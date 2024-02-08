9f8369fedfc7ead54024132df4315c37f4bf5194
oSumAtrIX <johan.melkonyan1@web.de>
2024-02-08 21:10:27 +0100

    feat(YouTube - Change start page): Add more start pages

--- integrations/java/app/revanced/integrations/youtube/patches/ChangeStartPagePatch.java
+++ integrations/java/app/revanced/integrations/youtube/patches/ChangeStartPagePatch.java
@@ -1,9 +1,9 @@
 package app.revanced.integrations.youtube.patches;
 
 import android.content.Intent;
-
-import app.revanced.integrations.youtube.settings.Settings;
+import android.net.Uri;
 import app.revanced.integrations.shared.Logger;
+import app.revanced.integrations.youtube.settings.Settings;
 
 @SuppressWarnings("unused")
 public final class ChangeStartPagePatch {
@@ -12,6 +12,10 @@ public final class ChangeStartPagePatch {
         if (startPage.isEmpty()) return;
 
         Logger.printDebug(() -> "Changing start page to " + startPage);
-        intent.setAction("com.google.android.youtube.action." + startPage);
+
+        if (startPage.startsWith("www"))
+            intent.setData(Uri.parse(startPage));
+        else
+            intent.setAction("com.google.android.youtube.action." + startPage);
     }
 }
