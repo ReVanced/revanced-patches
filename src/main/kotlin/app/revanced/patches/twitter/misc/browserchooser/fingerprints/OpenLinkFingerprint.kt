66a6f2002b6c5f134a0b691f2af196e4c5b2aae4
oSumAtrIX <johan.melkonyan1@web.de>
2024-02-16 20:07:21 +0100

    refactor

--- src/main/kotlin/app/revanced/patches/twitter/misc/browserchooser/fingerprints/OpenLinkFingerprint.kt
+++
@@ -1,9 +0,0 @@
-package app.revanced.patches.twitter.misc.browserchooser.fingerprints
-
-import app.revanced.patcher.fingerprint.MethodFingerprint
-
-
-internal object OpenLinkFingerprint : MethodFingerprint(
-    returnType = "V",
-    parameters = listOf("Landroid/content/Context;", "Landroid/content/Intent;", "Landroid/os/Bundle;")
-)
