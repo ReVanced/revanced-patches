da07c67b43c0383bd01fb3cc4cac33c1d06c6275
oSumAtrIX <johan.melkonyan1@web.de>
2024-02-16 21:41:07 +0100

    only force english

--- src/main/kotlin/app/revanced/patches/mifitness/misc/locale/ForceEnglishLocalePatch.kt
+++ src/main/kotlin/app/revanced/patches/mifitness/misc/locale/ForceEnglishLocalePatch.kt
@@ -6,31 +6,21 @@ import app.revanced.patcher.extensions.InstructionExtensions.replaceInstruction
 import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.annotation.CompatiblePackage
 import app.revanced.patcher.patch.annotation.Patch
-import app.revanced.patcher.patch.options.PatchOption.PatchExtensions.stringPatchOption
 import app.revanced.patches.mifitness.misc.locale.fingerprints.SyncBluetoothLanguageFingerprint
 import app.revanced.patches.mifitness.misc.login.FixLoginPatch
 import app.revanced.util.exception
 import com.android.tools.smali.dexlib2.iface.instruction.OneRegisterInstruction
 
 @Patch(
-    name = "Force locale",
-    description = "Forces the locale to a specific language",
+    name = "Force English locale",
+    description = "Forces the app to use the English locale.",
     compatiblePackages = [CompatiblePackage("com.xiaomi.wearable")],
     dependencies = [FixLoginPatch::class],
 )
 @Suppress("unused")
-object ForceLocalePatch : BytecodePatch(
+object ForceEnglishLocalePatch : BytecodePatch(
     setOf(SyncBluetoothLanguageFingerprint),
 ) {
-    private val locale by stringPatchOption(
-        "locale",
-        "en_gb",
-        null,
-        "Locale",
-        "The locale to force the app to use.",
-        true,
-    )
-
     override fun execute(context: BytecodeContext) {
         SyncBluetoothLanguageFingerprint.result?.apply {
             val instructionIndex = scanResult.patternScanResult!!.startIndex
@@ -40,7 +30,7 @@ object ForceLocalePatch : BytecodePatch(
 
                 replaceInstruction(
                     instructionIndex,
-                    "const-string v$registerIndexToUpdate, \"$locale\"",
+                    "const-string v$registerIndexToUpdate, \"en_gb\"",
                 )
             }
         } ?: throw SyncBluetoothLanguageFingerprint.exception
