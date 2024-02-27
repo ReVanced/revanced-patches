aeec9e91d8ee03b1390c4835da956b84f72ec68d
AbakNacchan <153147835+AbakNacchan@users.noreply.github.com>
2024-02-27 18:49:29 +0700

    remove unneunnecessary patch

--- src/main/kotlin/app/revanced/patches/tumblr/annoyances/popups/fingerprints/ShowGiftMessagePopupFingerprint.kt
+++
@@ -1,9 +0,0 @@
-package app.revanced.patches.tumblr.annoyances.popups.fingerprints
-
-import app.revanced.patcher.fingerprint.MethodFingerprint
-
-// This method is responsible for loading and displaying the visual Layout of the Gift Message Popup.
-internal object ShowGiftMessagePopupFingerprint : MethodFingerprint(
-    strings = listOf("activity", "anchorView"),
-    customFingerprint = { methodDef, _ -> methodDef.definingClass.endsWith("GiftMessagePopup;") }
-)
