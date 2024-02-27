9923014ec092d328a32e308749dc212fe40da97a
Abak Nacchan <153147835+AbakNacchan@users.noreply.github.com>
2024-02-27 18:16:24 +0700

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,7 @@
 import org.gradle.kotlin.dsl.support.listFilesOrdered
 
 plugins {
-    kotlin("jvm") version "1.9.22"
-    alias(libs.plugins.binary.compatibility.validator)
-    `maven-publish`
+    kotlin("jvm") version "1.9.20"
 }
 
 group = "app.revanced"
@@ -18,39 +16,17 @@ repositories {
 dependencies {
     implementation(libs.revanced.patcher)
     implementation(libs.smali)
-    // TODO: Required because build fails without it. Find a way to remove this dependency.
-    implementation(libs.guava)
     // Used in JsonGenerator.
     implementation(libs.gson)
-
-    // A dependency to the Android library unfortunately fails the build, which is why this is required.
-    compileOnly(project("dummy"))
 }
 
 kotlin {
-    jvmToolchain(11)
-}
-
-tasks.withType(Jar::class) {
-    exclude("app/revanced/meta")
-
-    manifest {
-        attributes["Name"] = "ReVanced Patches"
-        attributes["Description"] = "Patches for ReVanced."
-        attributes["Version"] = version
-        attributes["Timestamp"] = System.currentTimeMillis().toString()
-        attributes["Source"] = "git@github.com:revanced/revanced-patches.git"
-        attributes["Author"] = "ReVanced"
-        attributes["Contact"] = "contact@revanced.app"
-        attributes["Origin"] = "https://revanced.app"
-        attributes["License"] = "GNU General Public License v3.0"
-    }
+    jvmToolchain(17)
 }
 
 tasks {
     register<DefaultTask>("generateBundle") {
-        description = "Generate DEX files and add them in the JAR file"
-
+        description = "Generate dex files from build and bundle them in the jar file"
         dependsOn(build)
 
         doLast {
@@ -74,50 +50,16 @@ tasks {
 
     register<JavaExec>("generateMeta") {
         description = "Generate metadata for this bundle"
-
         dependsOn(build)
 
         classpath = sourceSets["main"].runtimeClasspath
-        mainClass.set("app.revanced.meta.IPatchesFileGenerator")
+        mainClass.set("app.revanced.meta.PatchesFileGenerator")
     }
 
     // Required to run tasks because Gradle semantic-release plugin runs the publish task.
     // Tracking: https://github.com/KengoTODA/gradle-semantic-release-plugin/issues/435
-    named("publish") {
+    register<DefaultTask>("publish") {
         dependsOn("generateBundle")
         dependsOn("generateMeta")
-    }
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("revanced-patches-publication") {
-            from(components["java"])
-
-            pom {
-                name = "ReVanced Patches"
-                description = "Patches for ReVanced."
-                url = "https://revanced.app"
-
-                licenses {
-                    license {
-                        name = "GNU General Public License v3.0"
-                        url = "https://www.gnu.org/licenses/gpl-3.0.en.html"
-                    }
-                }
-                developers {
-                    developer {
-                        id = "ReVanced"
-                        name = "ReVanced"
-                        email = "contact@revanced.app"
-                    }
-                }
-                scm {
-                    connection = "scm:git:git://github.com/revanced/revanced-patches.git"
-                    developerConnection = "scm:git:git@github.com:revanced/revanced-patches.git"
-                    url = "https://github.com/revanced/revanced-patches"
-                }
-            }
-        }
     }
 }
