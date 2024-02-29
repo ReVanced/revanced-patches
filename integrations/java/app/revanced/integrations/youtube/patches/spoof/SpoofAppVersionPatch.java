ab472a359bf3e6009f8b7b06e491d31059819802
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2024-02-29 06:35:16 +0400

    fix(YouTube - Spoof app version): Remove broken versions (#573)

--- integrations/java/app/revanced/integrations/youtube/patches/spoof/SpoofAppVersionPatch.java
+++ integrations/java/app/revanced/integrations/youtube/patches/spoof/SpoofAppVersionPatch.java
@@ -1,12 +1,26 @@
 package app.revanced.integrations.youtube.patches.spoof;
 
+import app.revanced.integrations.shared.Logger;
 import app.revanced.integrations.youtube.settings.Settings;
 
 @SuppressWarnings("unused")
 public class SpoofAppVersionPatch {
 
-    private static final boolean SPOOF_APP_VERSION_ENABLED = Settings.SPOOF_APP_VERSION.get();
-    private static final String SPOOF_APP_VERSION_TARGET = Settings.SPOOF_APP_VERSION_TARGET.get();
+    private static final boolean SPOOF_APP_VERSION_ENABLED;
+    private static final String SPOOF_APP_VERSION_TARGET;
+
+    static {
+        // TODO: remove this migration code
+        // Spoof targets 16.x and 17.x that no longer reliably work.
+        if (Settings.SPOOF_APP_VERSION_TARGET.get().compareTo("18.01.01") < 0) {
+            Logger.printInfo(() -> "Resetting spoof app version target");
+            Settings.SPOOF_APP_VERSION_TARGET.resetToDefault();
+        }
+        // End migration
+
+        SPOOF_APP_VERSION_ENABLED = Settings.SPOOF_APP_VERSION.get();
+        SPOOF_APP_VERSION_TARGET = Settings.SPOOF_APP_VERSION_TARGET.get();
+    }
 
     /**
      * Injection point
