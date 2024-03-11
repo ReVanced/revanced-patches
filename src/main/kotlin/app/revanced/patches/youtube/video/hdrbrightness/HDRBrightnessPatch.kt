f044dde054861ff16846a6be6287f86fa3afb3d8
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2024-03-11 17:20:07 +0400

    feat(YouTube): Support version `19.05`, `19.06`, `19.07`, `19.08` and `19.09` (#2862)

--- src/main/kotlin/app/revanced/patches/youtube/video/hdrbrightness/HDRBrightnessPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/video/hdrbrightness/HDRBrightnessPatch.kt
@@ -10,6 +10,7 @@ import app.revanced.patches.shared.misc.settings.preference.SwitchPreference
 import app.revanced.patches.youtube.misc.integrations.IntegrationsPatch
 import app.revanced.patches.youtube.misc.settings.SettingsPatch
 import app.revanced.patches.youtube.video.hdrbrightness.fingerprints.HDRBrightnessFingerprint
+import app.revanced.util.exception
 import com.android.tools.smali.dexlib2.iface.instruction.ReferenceInstruction
 import com.android.tools.smali.dexlib2.iface.instruction.TwoRegisterInstruction
 import com.android.tools.smali.dexlib2.iface.reference.FieldReference
@@ -32,12 +33,16 @@ import com.android.tools.smali.dexlib2.iface.reference.FieldReference
                 "18.49.37",
                 "19.01.34",
                 "19.02.39",
-                "19.03.35",
                 "19.03.36",
-                "19.04.37",
-            ],
-        ),
-    ],
+                "19.04.38",
+                "19.05.36",
+                "19.06.39",
+                "19.07.40",
+                "19.08.36",
+                // 19.09+ is dramatically different and the patched code is not present.
+            ]
+        )
+    ]
 )
 @Suppress("unused")
 object HDRBrightnessPatch : BytecodePatch(
@@ -46,27 +51,31 @@ object HDRBrightnessPatch : BytecodePatch(
     override fun execute(context: BytecodeContext) {
         AddResourcesPatch(this::class)
 
+        if (HDRBrightnessFingerprint.result == null) throw HDRBrightnessFingerprint.exception
+
         SettingsPatch.PreferenceScreen.VIDEO.addPreferences(
             SwitchPreference("revanced_hdr_auto_brightness"),
         )
 
-        val method = HDRBrightnessFingerprint.result!!.mutableMethod
-
-        method.implementation!!.instructions.filter { instruction ->
-            val fieldReference = (instruction as? ReferenceInstruction)?.reference as? FieldReference
-            fieldReference?.let { it.name == "screenBrightness" } == true
-        }.forEach { instruction ->
-            val brightnessRegisterIndex = method.implementation!!.instructions.indexOf(instruction)
-            val register = (instruction as TwoRegisterInstruction).registerA
-
-            val insertIndex = brightnessRegisterIndex + 1
-            method.addInstructions(
-                insertIndex,
-                """
-                   invoke-static {v$register}, Lapp/revanced/integrations/youtube/patches/HDRAutoBrightnessPatch;->getHDRBrightness(F)F
-                   move-result v$register
-                """,
-            )
-        }
+        // FIXME
+        // One of the changes made here effectively does nothing:
+        // It calls getHDRBrightness() and ignores the results.
+        HDRBrightnessFingerprint.result?.mutableMethod?.apply {
+            implementation!!.instructions.filter { instruction ->
+                ((instruction as? ReferenceInstruction)?.reference as? FieldReference)
+                    ?.name == "screenBrightness"
+            }.forEach { instruction ->
+                val brightnessRegisterIndex = implementation!!.instructions.indexOf(instruction)
+                val register = (instruction as TwoRegisterInstruction).registerA
+                val insertIndex = brightnessRegisterIndex + 1
+                addInstructions(
+                    insertIndex,
+                    """
+                        invoke-static {v$register}, Lapp/revanced/integrations/youtube/patches/HDRAutoBrightnessPatch;->getHDRBrightness(F)F
+                        move-result v$register
+                    """,
+                )
+            }
+        } ?: throw HDRBrightnessFingerprint.exception
     }
 }
