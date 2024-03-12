a212f29bd33bb5e10f024e058d26e20ee926190b
oSumAtrIX <johan.melkonyan1@web.de>
2024-03-12 18:25:23 +0100

    feat(Instagram - Hide timeline ads): Make compatible with latest versions

---
+++ src/main/kotlin/app/revanced/patches/instagram/patches/ads/timeline/fingerprints/IsAdCheckTwoFingerprint.kt
@@ -0,0 +1,22 @@
+package app.revanced.patches.instagram.patches.ads.timeline.fingerprints
+
+import app.revanced.patcher.extensions.or
+import app.revanced.patcher.fingerprint.MethodFingerprint
+import com.android.tools.smali.dexlib2.AccessFlags
+import com.android.tools.smali.dexlib2.Opcode
+
+internal object IsAdCheckTwoFingerprint : MethodFingerprint(
+    returnType = "Z",
+    accessFlags = AccessFlags.PUBLIC or AccessFlags.STATIC,
+    parameters = listOf("L"),
+    opcodes = listOf(
+        Opcode.INVOKE_INTERFACE,
+        Opcode.MOVE_RESULT_OBJECT,
+        Opcode.INVOKE_INTERFACE,
+        Opcode.MOVE_RESULT_OBJECT,
+        Opcode.CONST_4,
+        Opcode.IF_EQZ,
+        Opcode.CONST_4,
+        Opcode.RETURN,
+    ),
+)
