6a2545ec17856b6e68a07674a872f9fd958e2d25
Pisek <pisekfm@o2.pl>
2024-03-20 11:06:14 +0100

    change of approach - resolving param id from instruction

--- src/main/kotlin/app/revanced/patches/mifitness/misc/login/FixLoginPatch.kt
+++ src/main/kotlin/app/revanced/patches/mifitness/misc/login/FixLoginPatch.kt
@@ -1,12 +1,14 @@
 package app.revanced.patches.mifitness.misc.login
 
 import app.revanced.patcher.data.BytecodeContext
+import app.revanced.patcher.extensions.InstructionExtensions.getInstruction
 import app.revanced.patcher.extensions.InstructionExtensions.addInstruction
 import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.annotation.CompatiblePackage
 import app.revanced.patcher.patch.annotation.Patch
 import app.revanced.patches.mifitness.misc.login.fingerprints.XiaomiAccountManagerConstructorFingerprint
 import app.revanced.util.exception
+import com.android.tools.smali.dexlib2.iface.instruction.OneRegisterInstruction
 
 @Patch(
     name = "Fix login",
@@ -18,9 +20,17 @@ object FixLoginPatch : BytecodePatch(
     setOf(XiaomiAccountManagerConstructorFingerprint),
 ) {
     override fun execute(context: BytecodeContext) {
-        XiaomiAccountManagerConstructorFingerprint.result?.mutableMethod?.addInstruction(
-            0,
-            "const/4 p2, 0x0",
-        ) ?: throw XiaomiAccountManagerConstructorFingerprint.exception
+        XiaomiAccountManagerConstructorFingerprint.result?.apply {
+            val resolveIfInstruction = scanResult.patternScanResult!!.startIndex
+
+            mutableMethod.apply {
+                val registerIndexToUpdate = getInstruction<OneRegisterInstruction>(resolveIfInstruction).registerA
+
+                addInstruction(
+                    resolveIfInstruction,
+                    "const/4 p$registerIndexToUpdate, 0x0",
+                )
+            }
+        } ?: throw XiaomiAccountManagerConstructorFingerprint.exception
     }
 }
