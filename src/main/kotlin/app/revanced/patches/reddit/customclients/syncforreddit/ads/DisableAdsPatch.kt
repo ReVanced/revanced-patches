8f5f55a2a655fa8e69368ebf0ba1873b89fe7188
oSumAtrIX <johan.melkonyan1@web.de>
2024-03-25 21:14:40 +0100

    refactor

--- src/main/kotlin/app/revanced/patches/reddit/customclients/syncforreddit/ads/DisableAdsPatch.kt
+++ src/main/kotlin/app/revanced/patches/reddit/customclients/syncforreddit/ads/DisableAdsPatch.kt
@@ -1,30 +1,8 @@
 package app.revanced.patches.reddit.customclients.syncforreddit.ads
 
-import app.revanced.util.exception
-import app.revanced.patcher.data.BytecodeContext
-import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
-import app.revanced.patcher.patch.BytecodePatch
-import app.revanced.patcher.patch.annotation.CompatiblePackage
-import app.revanced.patcher.patch.annotation.Patch
-import app.revanced.patches.reddit.customclients.syncforreddit.ads.fingerprints.IsAdsEnabledFingerprint
-import app.revanced.patches.reddit.customclients.syncforreddit.detection.piracy.DisablePiracyDetectionPatch
+import app.revanced.patches.reddit.customclients.ads.BaseDisableAdsPatch
 
-@Patch(
-    name = "Disable ads",
-    dependencies = [DisablePiracyDetectionPatch::class],
-    compatiblePackages = [CompatiblePackage("com.laurencedawson.reddit_sync")]
-)
 @Suppress("unused")
-object DisableAdsPatch : BytecodePatch(setOf(IsAdsEnabledFingerprint)) {
-    override fun execute(context: BytecodeContext) {
-        IsAdsEnabledFingerprint.result?.mutableMethod?.apply {
-            addInstructions(
-                0,
-                """
-                const/4 v0, 0x0
-                return v0
-            """
-            )
-        } ?: throw IsAdsEnabledFingerprint.exception
-    }
-}
+object DisableAdsPatch : BaseDisableAdsPatch(
+    compatiblePackages = setOf(CompatiblePackage("io.syncapps.lemmy_sync")),
+)
