b94ab2f10f8df1ca965d3e15ee922592da7ef6f4
oSumAtrIX <johan.melkonyan1@web.de>
2024-03-27 14:04:53 +0100

    fix some patches (1 missing)

--- src/main/kotlin/app/revanced/patches/music/ad/video/MusicVideoAdsPatch.kt
+++ src/main/kotlin/app/revanced/patches/music/ad/video/MusicVideoAdsPatch.kt
@@ -5,28 +5,26 @@ import app.revanced.patcher.extensions.InstructionExtensions.addInstruction
 import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.annotation.CompatiblePackage
 import app.revanced.patcher.patch.annotation.Patch
-import app.revanced.patches.music.ad.video.fingerprints.ShowMusicVideoAdsConstructorFingerprint
-import app.revanced.patches.music.ad.video.fingerprints.ShowMusicVideoAdsFingerprint
+import app.revanced.patcher.util.proxy.mutableTypes.MutableMethod
+import app.revanced.patches.music.ad.video.fingerprints.ShowMusicVideoAdsParentFingerprint
+import app.revanced.util.exception
 
 @Patch(
     name = "Music video ads",
     description = "Removes ads in the music player.",
-    compatiblePackages = [CompatiblePackage("com.google.android.apps.youtube.music")]
+    compatiblePackages = [CompatiblePackage("com.google.android.apps.youtube.music")],
 )
 @Suppress("unused")
 object MusicVideoAdsPatch : BytecodePatch(
-    setOf(ShowMusicVideoAdsConstructorFingerprint)
+    setOf(ShowMusicVideoAdsParentFingerprint),
 ) {
     override fun execute(context: BytecodeContext) {
-        ShowMusicVideoAdsFingerprint.resolve(context, ShowMusicVideoAdsConstructorFingerprint.result!!.classDef)
+        ShowMusicVideoAdsParentFingerprint.result?.let {
+            val showMusicVideoAdsMethod = context
+                .toMethodWalker(it.mutableMethod)
+                .nextMethod(it.scanResult.patternScanResult!!.startIndex + 1, true).getMethod() as MutableMethod
 
-        val result = ShowMusicVideoAdsFingerprint.result!!
-
-        result.mutableMethod.addInstruction(
-            result.scanResult.patternScanResult!!.startIndex,
-            """
-                const/4 p1, 0x0
-            """
-        )
+            showMusicVideoAdsMethod.addInstruction(0, "const/4 p1, 0x0")
+        } ?: throw ShowMusicVideoAdsParentFingerprint.exception
     }
 }
--- src/main/kotlin/app/revanced/patches/music/layout/premium/HideGetPremiumPatch.kt
+++ src/main/kotlin/app/revanced/patches/music/layout/premium/HideGetPremiumPatch.kt
@@ -2,41 +2,43 @@ package app.revanced.patches.music.layout.premium
 
 import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.extensions.InstructionExtensions.addInstruction
-import app.revanced.patcher.extensions.InstructionExtensions.replaceInstruction
+import app.revanced.patcher.extensions.InstructionExtensions.getInstruction
 import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.annotation.CompatiblePackage
 import app.revanced.patcher.patch.annotation.Patch
 import app.revanced.patches.music.layout.premium.fingerprints.HideGetPremiumFingerprint
 import app.revanced.patches.music.layout.premium.fingerprints.HideGetPremiumParentFingerprint
+import app.revanced.util.exception
+import com.android.tools.smali.dexlib2.iface.instruction.TwoRegisterInstruction
 
 @Patch(
     name = "Hide get premium",
     description = "Removes all \"Get Premium\" evidences from the avatar menu.",
-    compatiblePackages = [CompatiblePackage("com.google.android.apps.youtube.music")]
+    compatiblePackages = [CompatiblePackage("com.google.android.apps.youtube.music")],
 )
 @Suppress("unused")
-object HideGetPremiumPatch : BytecodePatch(setOf(HideGetPremiumParentFingerprint)) {
+object HideGetPremiumPatch : BytecodePatch(
+    setOf(HideGetPremiumParentFingerprint),
+) {
     override fun execute(context: BytecodeContext) {
+        HideGetPremiumParentFingerprint.result?.let {
+            it.mutableMethod.apply {
+                val insertIndex = it.scanResult.patternScanResult!!.startIndex + 1
+                val register = getInstruction<TwoRegisterInstruction>(insertIndex - 1).registerA
+
+                addInstruction(insertIndex, "const/4 v$register, 0x0")
+            }
+        } ?: throw HideGetPremiumParentFingerprint.exception
+
         val parentResult = HideGetPremiumParentFingerprint.result!!
         HideGetPremiumFingerprint.resolve(context, parentResult.classDef)
 
         val startIndex = parentResult.scanResult.patternScanResult!!.startIndex
-
-        val parentMethod = parentResult.mutableMethod
-        parentMethod.replaceInstruction(
-            startIndex,
-            """
-                const/4 v1, 0x0
-            """
-        )
-
-        val result = HideGetPremiumFingerprint.result!!
-        val method = result.mutableMethod
-        method.addInstruction(
+        HideGetPremiumFingerprint.result!!.mutableMethod.addInstruction(
             startIndex,
             """
                 const/16 v0, 0x8
-            """
+            """,
         )
     }
 }
