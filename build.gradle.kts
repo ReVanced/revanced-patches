dbb2f3e0df7529e40517c5bc0351b4b62b6a7809
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2024-03-31 23:24:22 +0400

    fix: Add ktlint gradle tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.kotlin)
+    alias(libs.plugins.ktlint)
     alias(libs.plugins.binary.compatibility.validator)
     `maven-publish`
     signing
