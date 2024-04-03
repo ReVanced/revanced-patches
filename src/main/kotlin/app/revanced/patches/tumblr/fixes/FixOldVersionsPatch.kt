d8396cfd6301ff5ca76c37d0c3ffb33c33acee25
Temm <samumert@gmail.com>
2024-04-03 23:47:29 +0200

    fix(tumblr): fix more functionality for old app versions

--- src/main/kotlin/app/revanced/patches/tumblr/fixes/FixOldVersionsPatch.kt
+++ src/main/kotlin/app/revanced/patches/tumblr/fixes/FixOldVersionsPatch.kt
@@ -6,6 +6,7 @@ import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.annotation.CompatiblePackage
 import app.revanced.patcher.patch.annotation.Patch
 import app.revanced.patches.tumblr.fixes.fingerprints.HttpPathParserFingerprint
+import app.revanced.patches.tumblr.fixes.fingerprints.AddQueryParamFingerprint
 import app.revanced.util.exception
 
 @Patch(
@@ -17,22 +18,46 @@ import app.revanced.util.exception
 )
 @Suppress("unused")
 object FixOldVersionsPatch : BytecodePatch(
-    setOf(HttpPathParserFingerprint),
+    setOf(HttpPathParserFingerprint, AddQueryParamFingerprint),
 ) {
-    override fun execute(context: BytecodeContext) =
+    override fun execute(context: BytecodeContext) {
+        val blockedStrings = listOf(
+            ",?live_now", ",?live_streaming_user_id"
+        )
+
         HttpPathParserFingerprint.result?.let {
             val endIndex = it.scanResult.patternScanResult!!.endIndex
-
-            it.mutableMethod.addInstructions(
-                endIndex + 1,
+            // Remove each of the blockedStrings elements from statically defined request URLs
+            for (blockedString in blockedStrings) {
+                it.mutableMethod.addInstructions(
+                    endIndex + 1,
+                    """
+                    # Remove blocked string from URL path (p2)
+                    # path = path.replace(blockedString, "")
+                    const-string p1, "$blockedString"
+                    const-string p3, ""
+                    invoke-virtual {p2, p1, p3}, Ljava/lang/String;->replace(Ljava/lang/CharSequence;Ljava/lang/CharSequence;)Ljava/lang/String;
+                    move-result-object p2
                 """
-                # Remove "?live_now" from the request path p2.
-                # p2 = p2.replace(p1, p3)
-                const-string p1, ",?live_now"
-                const-string p3, ""
-                invoke-virtual {p2, p1, p3}, Ljava/lang/String;->replace(Ljava/lang/CharSequence;Ljava/lang/CharSequence;)Ljava/lang/String;
-                move-result-object p2
-            """,
-            )
+                )
+            }
         } ?: throw HttpPathParserFingerprint.exception
+
+        AddQueryParamFingerprint.result?.let {
+            // Remove each of the blockedStrings elements when passed in as a @Query argument
+            for (blockedString in blockedStrings) {
+                it.mutableMethod.addInstructions(
+                    0,
+                    """
+                    # Remove blocked string from query parameter value (p2)
+                    # value = value.replace(blockedString, "")
+                    const-string v0, "$blockedString"
+                    const-string v1, ""
+                    invoke-virtual {p2, v0, v1}, Ljava/lang/String;->replace(Ljava/lang/CharSequence;Ljava/lang/CharSequence;)Ljava/lang/String;
+                    move-result-object p2
+                """
+                )
+            }
+        } ?: throw AddQueryParamFingerprint.exception
+    }
 }
