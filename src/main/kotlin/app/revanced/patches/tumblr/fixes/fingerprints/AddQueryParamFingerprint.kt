706e2a73aa0e752ff786ae803a993e04aa4c2857
oSumAtrIX <johan.melkonyan1@web.de>
2024-04-04 16:55:44 +0200

    fingerprints should be internal

--- src/main/kotlin/app/revanced/patches/tumblr/fixes/fingerprints/AddQueryParamFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/tumblr/fixes/fingerprints/AddQueryParamFingerprint.kt
@@ -5,7 +5,7 @@ import app.revanced.patcher.fingerprint.MethodFingerprint
 // Fingerprint for the addQueryParam method from retrofit2
 // https://github.com/square/retrofit/blob/trunk/retrofit/src/main/java/retrofit2/RequestBuilder.java#L186
 // Injecting here allows modifying dynamically set query parameters
-object AddQueryParamFingerprint : MethodFingerprint(
+internal object AddQueryParamFingerprint : MethodFingerprint(
     strings = listOf("Malformed URL. Base: ", ", Relative: "),
-    parameters = listOf("Ljava/lang/String;", "Ljava/lang/String;", "Z")
+    parameters = listOf("Ljava/lang/String;", "Ljava/lang/String;", "Z"),
 )
