0edeb26d6a81af84ecc10bbf05d78e11b8f7b3aa
oSumAtrIX <johan.melkonyan1@web.de>
2024-04-04 16:55:54 +0200

    refactor

--- src/main/kotlin/app/revanced/patches/tumblr/fixes/FixOldVersionsPatch.kt
+++ src/main/kotlin/app/revanced/patches/tumblr/fixes/FixOldVersionsPatch.kt
@@ -5,14 +5,14 @@ import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
 import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.annotation.CompatiblePackage
 import app.revanced.patcher.patch.annotation.Patch
-import app.revanced.patches.tumblr.fixes.fingerprints.HttpPathParserFingerprint
 import app.revanced.patches.tumblr.fixes.fingerprints.AddQueryParamFingerprint
+import app.revanced.patches.tumblr.fixes.fingerprints.HttpPathParserFingerprint
 import app.revanced.util.exception
 
 @Patch(
     name = "Fix old versions",
     description = "Fixes old versions of the app (v33.2 and earlier) breaking due to Tumblr removing remnants of Tumblr" +
-            " Live from the API, which causes many requests to fail. This patch has no effect on newer versions of the app.",
+        " Live from the API, which causes many requests to fail. This patch has no effect on newer versions of the app.",
     compatiblePackages = [CompatiblePackage("com.tumblr")],
     use = false,
 )
@@ -21,41 +21,40 @@ object FixOldVersionsPatch : BytecodePatch(
     setOf(HttpPathParserFingerprint, AddQueryParamFingerprint),
 ) {
     override fun execute(context: BytecodeContext) {
-        val blockedStrings = listOf(
-            ",?live_now", ",?live_streaming_user_id"
+        val liveQueryParameters = listOf(
+            ",?live_now",
+            ",?live_streaming_user_id",
         )
 
         HttpPathParserFingerprint.result?.let {
             val endIndex = it.scanResult.patternScanResult!!.endIndex
-            // Remove each of the blockedStrings elements from statically defined request URLs
-            for (blockedString in blockedStrings) {
+            // Remove the live query parameters from the path.
+            for (liveQueryParameter in liveQueryParameters) {
                 it.mutableMethod.addInstructions(
                     endIndex + 1,
                     """
-                    # Remove blocked string from URL path (p2)
-                    # path = path.replace(blockedString, "")
-                    const-string p1, "$blockedString"
+                    # urlPath = urlPath.replace(liveQueryParameter, "")
+                    const-string p1, "$liveQueryParameter"
                     const-string p3, ""
                     invoke-virtual {p2, p1, p3}, Ljava/lang/String;->replace(Ljava/lang/CharSequence;Ljava/lang/CharSequence;)Ljava/lang/String;
                     move-result-object p2
-                """
+                """,
                 )
             }
         } ?: throw HttpPathParserFingerprint.exception
 
         AddQueryParamFingerprint.result?.let {
-            // Remove each of the blockedStrings elements when passed in as a @Query argument
-            for (blockedString in blockedStrings) {
+            // Remove the live query parameters when passed via the @Query annotation.
+            for (liveQueryParameter in liveQueryParameters) {
                 it.mutableMethod.addInstructions(
                     0,
                     """
-                    # Remove blocked string from query parameter value (p2)
-                    # value = value.replace(blockedString, "")
-                    const-string v0, "$blockedString"
+                    # queryParameterValue = queryParameterValue.replace(liveQueryParameter, "")
+                    const-string v0, "$liveQueryParameter"
                     const-string v1, ""
                     invoke-virtual {p2, v0, v1}, Ljava/lang/String;->replace(Ljava/lang/CharSequence;Ljava/lang/CharSequence;)Ljava/lang/String;
                     move-result-object p2
-                """
+                """,
                 )
             }
         } ?: throw AddQueryParamFingerprint.exception
