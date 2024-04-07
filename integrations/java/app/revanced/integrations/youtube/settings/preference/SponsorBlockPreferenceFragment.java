fb18337ac83cd6f86c244115b7f8c6f8077fdbbc
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2024-04-07 23:55:33 +0400

    chore(YouTube): Fix typos, simplify some strings for translating

--- integrations/java/app/revanced/integrations/youtube/settings/preference/SponsorBlockPreferenceFragment.java
+++ integrations/java/app/revanced/integrations/youtube/settings/preference/SponsorBlockPreferenceFragment.java
@@ -100,10 +100,10 @@ public class SponsorBlockPreferenceFragment extends PreferenceFragment {
             privateUserId.setEnabled(enabled);
 
             // If the user has a private user id, then include a subtext that mentions not to share it.
-            String exportSummarySubText = SponsorBlockSettings.userHasSBPrivateId()
+            String importExportSummary = SponsorBlockSettings.userHasSBPrivateId()
                     ? str("revanced_sb_settings_ie_sum_warning")
-                    : "";
-            importExport.setSummary(str("revanced_sb_settings_ie_sum", exportSummarySubText));
+                    : str("revanced_sb_settings_ie_sum");
+            importExport.setSummary(importExportSummary);
 
             apiUrl.setEnabled(enabled);
             importExport.setEnabled(enabled);
