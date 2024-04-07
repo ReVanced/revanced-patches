9fca3d0e43bb5ecba1df7615e41ead270f860105
1fexd <PGFvy85nC@protonmail.com>
2024-04-07 23:35:12 +0200

    chore(amazon): Improve description

--- src/main/kotlin/app/revanced/patches/amazon/deeplinking/DeepLinkingPatch.kt
+++ src/main/kotlin/app/revanced/patches/amazon/deeplinking/DeepLinkingPatch.kt
@@ -8,7 +8,7 @@ import app.revanced.patcher.patch.annotation.Patch
 
 @Patch(
     name = "Always allow deep-linking",
-    description = "Always allow deep-linking",
+    description = "Allows the Amazon app to open Amazon links, even if it is not the default link handler for Amazon domains.",
     compatiblePackages = [CompatiblePackage("com.amazon.mShop.android.shopping")]
 )
 @Suppress("unused")
