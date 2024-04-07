edc86274a5f4d79d6537310e5db21e5b641eaeb2
1fexd <PGFvy85nC@protonmail.com>
2024-04-07 22:31:03 +0200

    feat(amazon): Always allow deep-linking

---
+++ src/main/kotlin/app/revanced/patches/amazon/deeplinking/DeepLinkingPatch.kt
@@ -0,0 +1,27 @@
+package app.revanced.patches.amazon.deeplinking
+
+import app.revanced.patcher.data.BytecodeContext
+import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
+import app.revanced.patcher.patch.BytecodePatch
+import app.revanced.patcher.patch.annotation.CompatiblePackage
+import app.revanced.patcher.patch.annotation.Patch
+
+@Patch(
+    name = "Always allow deep-linking",
+    description = "Always allow deep-linking",
+    compatiblePackages = [CompatiblePackage("com.amazon.mShop.android.shopping")]
+)
+@Suppress("unused")
+object DeepLinkingPatch : BytecodePatch(
+    setOf(DeepLinkingFingerprint)
+) {
+    override fun execute(context: BytecodeContext) {
+        DeepLinkingFingerprint.result!!.mutableMethod.addInstructions(
+            0,
+            """
+                const/4 v0, 0x1
+                return v0
+            """
+        )
+    }
+}
