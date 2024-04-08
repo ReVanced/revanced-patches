cf315cb113575ae84f47f036b8c5042c2a11fc6e
1fexd <58902674+1fexd@users.noreply.github.com>
2024-04-08 06:23:56 +0000

    Update src/main/kotlin/app/revanced/patches/amazon/deeplinking/DeepLinkingPatch.kt
    Co-authored-by: oSumAtrIX <johan.melkonyan1@web.de>

--- src/main/kotlin/app/revanced/patches/amazon/deeplinking/DeepLinkingPatch.kt
+++ src/main/kotlin/app/revanced/patches/amazon/deeplinking/DeepLinkingPatch.kt
@@ -8,7 +8,7 @@ import app.revanced.patcher.patch.annotation.Patch
 
 @Patch(
     name = "Always allow deep-linking",
-    description = "Allows the Amazon app to open Amazon links, even if it is not the default link handler for Amazon domains.",
+    description = "Open Amazon links, even if the app is not set to handle Amazon links.",
     compatiblePackages = [CompatiblePackage("com.amazon.mShop.android.shopping")]
 )
 @Suppress("unused")
