897b4dbce984270ae1fd7de5bd30bd05153e45f2
Temm <samumert@gmail.com>
2024-04-09 19:38:11 +0200

    fix(Tumblr - Fix old versions): Improve reliability by removing remnances of Tumblr Live  (#2988)
    Co-authored-by: oSumAtrIX <johan.melkonyan1@web.de>

--- src/main/kotlin/app/revanced/patches/tumblr/fixes/FixOldVersionsPatch.kt
+++ src/main/kotlin/app/revanced/patches/tumblr/fixes/FixOldVersionsPatch.kt
@@ -5,34 +5,63 @@ import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
 import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.annotation.CompatiblePackage
 import app.revanced.patcher.patch.annotation.Patch
+import app.revanced.patches.tumblr.fixes.fingerprints.AddQueryParamFingerprint
 import app.revanced.patches.tumblr.fixes.fingerprints.HttpPathParserFingerprint
 import app.revanced.util.exception
 
 @Patch(
     name = "Fix old versions",
     description = "Fixes old versions of the app (v33.2 and earlier) breaking due to Tumblr removing remnants of Tumblr" +
-            " Live from the API, which causes many requests to fail. This patch has no effect on newer versions of the app.",
+        " Live from the API, which causes many requests to fail. This patch has no effect on newer versions of the app.",
     compatiblePackages = [CompatiblePackage("com.tumblr")],
     use = false,
 )
 @Suppress("unused")
 object FixOldVersionsPatch : BytecodePatch(
-    setOf(HttpPathParserFingerprint),
+    setOf(HttpPathParserFingerprint, AddQueryParamFingerprint),
 ) {
-    override fun execute(context: BytecodeContext) =
+    override fun execute(context: BytecodeContext) {
+        val liveQueryParameters = listOf(
+            ",?live_now",
+            ",?live_streaming_user_id",
+        )
+
         HttpPathParserFingerprint.result?.let {
             val endIndex = it.scanResult.patternScanResult!!.endIndex
-
-            it.mutableMethod.addInstructions(
-                endIndex + 1,
-                """
-                # Remove "?live_now" from the request path p2.
-                # p2 = p2.replace(p1, p3)
-                const-string p1, ",?live_now"
-                const-string p3, ""
-                invoke-virtual {p2, p1, p3}, Ljava/lang/String;->replace(Ljava/lang/CharSequence;Ljava/lang/CharSequence;)Ljava/lang/String;
-                move-result-object p2
-            """,
-            )
+            // Remove the live query parameters from the path when it's specified via a @METHOD annotation.
+            for (liveQueryParameter in liveQueryParameters) {
+                it.mutableMethod.addInstructions(
+                    endIndex + 1,
+                    """
+                    # urlPath = urlPath.replace(liveQueryParameter, "")
+                    const-string p1, "$liveQueryParameter"
+                    const-string p3, ""
+                    invoke-virtual {p2, p1, p3}, Ljava/lang/String;->replace(Ljava/lang/CharSequence;Ljava/lang/CharSequence;)Ljava/lang/String;
+                    move-result-object p2
+                """,
+                )
+            }
         } ?: throw HttpPathParserFingerprint.exception
+
+        AddQueryParamFingerprint.result?.let {
+            // Remove the live query parameters when passed via a parameter which has the @Query annotation.
+            // e.g. an API call could be defined like this:
+            //  @GET("api/me/info")
+            //  ApiResponse getCurrentUserInfo(@Query("fields[blog]") String value)
+            // which would result in the path "api/me/inf0?fields[blog]=${value}"
+            // Here we make sure that this value doesn't contain the broken query parameters.
+            for (liveQueryParameter in liveQueryParameters) {
+                it.mutableMethod.addInstructions(
+                    0,
+                    """
+                    # queryParameterValue = queryParameterValue.replace(liveQueryParameter, "")
+                    const-string v0, "$liveQueryParameter"
+                    const-string v1, ""
+                    invoke-virtual {p2, v0, v1}, Ljava/lang/String;->replace(Ljava/lang/CharSequence;Ljava/lang/CharSequence;)Ljava/lang/String;
+                    move-result-object p2
+                """,
+                )
+            }
+        } ?: throw AddQueryParamFingerprint.exception
+    }
 }
