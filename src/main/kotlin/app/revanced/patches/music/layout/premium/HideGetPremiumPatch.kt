e3785ec5aca776f58dcf9b789cb0f57fcb8e58b8
Alberto Ponces <ponces26@gmail.com>
2024-04-19 23:53:40 +0100

    fix(YT Music): no need for the AccountMenuFooterFingerprint

--- src/main/kotlin/app/revanced/patches/music/layout/premium/HideGetPremiumPatch.kt
+++ src/main/kotlin/app/revanced/patches/music/layout/premium/HideGetPremiumPatch.kt
@@ -8,29 +8,19 @@ import app.revanced.patcher.extensions.InstructionExtensions.replaceInstruction
 import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.annotation.CompatiblePackage
 import app.revanced.patcher.patch.annotation.Patch
-import app.revanced.patcher.util.proxy.mutableTypes.MutableMethod
-import app.revanced.patches.music.layout.premium.fingerprints.AccountMenuFooterFingerprint
 import app.revanced.patches.music.layout.premium.fingerprints.HideGetPremiumFingerprint
 import app.revanced.patches.music.layout.premium.fingerprints.MembershipSettingsFingerprint
 import app.revanced.util.exception
-import app.revanced.util.indexOfFirstWideLiteralInstructionValue
-import com.android.tools.smali.dexlib2.Opcode
 import com.android.tools.smali.dexlib2.iface.instruction.FiveRegisterInstruction
-import com.android.tools.smali.dexlib2.iface.instruction.OneRegisterInstruction
-import com.android.tools.smali.dexlib2.iface.instruction.ReferenceInstruction
-import com.android.tools.smali.dexlib2.iface.instruction.TwoRegisterInstruction
-import com.android.tools.smali.dexlib2.iface.reference.Reference
 
 @Patch(
     name = "Hide 'Get Music Premium' label",
     description = "Hides the \"Get Music Premium\" label from the account menu and settings.",
-    dependencies = [HideGetPremiumResourcePatch::class],
     compatiblePackages = [CompatiblePackage("com.google.android.apps.youtube.music")],
 )
 @Suppress("unused")
 object HideGetPremiumPatch : BytecodePatch(
     setOf(
-        AccountMenuFooterFingerprint,
         HideGetPremiumFingerprint,
         MembershipSettingsFingerprint,
     ),
@@ -57,49 +47,16 @@ object HideGetPremiumPatch : BytecodePatch(
             }
         } ?: throw HideGetPremiumFingerprint.exception
 
-        AccountMenuFooterFingerprint.result?.let {
-            it.mutableMethod.apply {
-                val targetIndex = indexOfFirstWideLiteralInstructionValue(HideGetPremiumResourcePatch.privacyTosFooterId) + 4
-                targetReference = getInstruction<ReferenceInstruction>(targetIndex + 1).reference
-
-                with(
-                    context
-                        .toMethodWalker(this)
-                        .nextMethod(targetIndex, true)
-                        .getMethod() as MutableMethod
-                ) {
-                    this.implementation!!.instructions.apply {
-                        for ((index, instruction) in withIndex()) {
-                            if (instruction.opcode != Opcode.IGET_OBJECT) continue
-
-                            if (getInstruction<ReferenceInstruction>(index).reference == targetReference) {
-                                val targetRegister =
-                                    getInstruction<OneRegisterInstruction>(index + 2).registerA
-
-                                addInstruction(
-                                    index,
-                                    "const/16 v$targetRegister, 0x8"
-                                )
-
-                                break
-                            }
-                        }
-                    }
-                }
-            }
-        } ?: throw AccountMenuFooterFingerprint.exception
-
         MembershipSettingsFingerprint.result?.let {
             it.mutableMethod.apply {
                 addInstructions(
-                    0, """
+                    0,
+                    """
                         const/4 v0, 0x0
                         return-object v0
-                        """
+                    """
                 )
             }
         } ?: throw MembershipSettingsFingerprint.exception
     }
-    
-    private lateinit var targetReference: Reference
 }
