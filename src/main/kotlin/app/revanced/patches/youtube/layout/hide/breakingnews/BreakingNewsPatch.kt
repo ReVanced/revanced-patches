44db1f942f5ee553aa8d8c958bb5a9fef4414eaf
Ushie <ushiekane@gmail.com>
2024-05-03 21:10:32 +0300

    fix: Import correct dependency (patch hasn't been migrated yet)

--- src/main/kotlin/app/revanced/patches/youtube/layout/hide/breakingnews/BreakingNewsPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/hide/breakingnews/BreakingNewsPatch.kt
@@ -1,9 +1,10 @@
 package app.revanced.patches.youtube.layout.hide.breakingnews
 
 import app.revanced.patcher.patch.bytecodePatch
+import app.revanced.patches.youtube.layout.hide.general.HideLayoutComponentsPatch
 
 @Deprecated("This patch has been merged to HideLayoutComponentsPatch.")
 @Suppress("unused")
 val breakingNewsPatch = bytecodePatch {
-    dependsOn(hideLayoutComponentsPatch)
+    dependsOn(HideLayoutComponentsPatch)
 }
