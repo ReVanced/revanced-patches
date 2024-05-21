dddc9f96770837f17c8051ef063d516abdff1d19
oSumAtrIX <johan.melkonyan1@web.de>
2024-05-22 01:51:20 +0200

    hide player flyout menu

--- src/main/kotlin/app/revanced/patches/youtube/layout/hide/player/flyoutmenupanel/HidePlayerFlyoutMenuPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/hide/player/flyoutmenupanel/HidePlayerFlyoutMenuPatch.kt
@@ -1,62 +1,58 @@
 package app.revanced.patches.youtube.layout.hide.player.flyoutmenupanel
 
-import app.revanced.patcher.data.ResourceContext
-import app.revanced.patcher.patch.ResourcePatch
-import app.revanced.patcher.patch.annotation.CompatiblePackage
-import app.revanced.patcher.patch.annotation.Patch
-import app.revanced.patches.all.misc.resources.AddResourcesPatch
+import app.revanced.patcher.patch.bytecodePatch
+import app.revanced.patches.all.misc.resources.addResources
+import app.revanced.patches.all.misc.resources.addResourcesPatch
 import app.revanced.patches.shared.misc.settings.preference.PreferenceScreenPreference
 import app.revanced.patches.shared.misc.settings.preference.SwitchPreference
-import app.revanced.patches.youtube.misc.litho.filter.LithoFilterPatch
-import app.revanced.patches.youtube.misc.playertype.PlayerTypeHookPatch
-import app.revanced.patches.youtube.misc.settings.SettingsPatch
+import app.revanced.patches.youtube.misc.litho.filter.addFilter
+import app.revanced.patches.youtube.misc.litho.filter.lithoFilterPatch
+import app.revanced.patches.youtube.misc.playertype.playerTypeHookPatch
+import app.revanced.patches.youtube.misc.settings.PreferenceScreen
+import app.revanced.patches.youtube.misc.settings.settingsPatch
 
-@Patch(
-    name = "Player flyout menu",
-    description = "Adds options to hide menu items that appear when pressing the gear icon in the video player.",
-    dependencies = [
-        LithoFilterPatch::class,
-        PlayerTypeHookPatch::class,
-        SettingsPatch::class,
-        AddResourcesPatch::class,
-    ],
-    compatiblePackages = [
-        CompatiblePackage(
-            "com.google.android.youtube",
-            [
-                "18.32.39",
-                "18.37.36",
-                "18.38.44",
-                "18.43.45",
-                "18.44.41",
-                "18.45.43",
-                "18.48.39",
-                "18.49.37",
-                "19.01.34",
-                "19.02.39",
-                "19.03.36",
-                "19.04.38",
-                "19.05.36",
-                "19.06.39",
-                "19.07.40",
-                "19.08.36",
-                "19.09.38",
-                "19.10.39",
-                "19.11.43",
-            ],
-        ),
-    ],
-)
 @Suppress("unused")
-object HidePlayerFlyoutMenuPatch : ResourcePatch() {
+val hidePlayerFlyoutMenuPatch = bytecodePatch(
+    name = "Hide player flyout menu",
+    description = "Adds options to hide menu items that appear when pressing the gear icon in the video player.",
+) {
+    dependsOn(
+        lithoFilterPatch,
+        playerTypeHookPatch,
+        settingsPatch,
+        addResourcesPatch,
+    )
 
-    private const val FILTER_CLASS_DESCRIPTOR =
-        "Lapp/revanced/integrations/youtube/patches/components/PlayerFlyoutMenuItemsFilter;"
+    compatibleWith(
+        "com.google.android.youtube"(
+            "18.32.39",
+            "18.37.36",
+            "18.38.44",
+            "18.43.45",
+            "18.44.41",
+            "18.45.43",
+            "18.48.39",
+            "18.49.37",
+            "19.01.34",
+            "19.02.39",
+            "19.03.36",
+            "19.04.38",
+            "19.05.36",
+            "19.06.39",
+            "19.07.40",
+            "19.08.36",
+            "19.09.38",
+            "19.10.39",
+            "19.11.43",
+        ),
+    )
 
-    override fun execute(context: ResourceContext) {
-        AddResourcesPatch(this::class)
+    execute {
+        val filterClassDescriptor = "Lapp/revanced/integrations/youtube/patches/components/PlayerFlyoutMenuItemsFilter;"
 
-        SettingsPatch.PreferenceScreen.PLAYER.addPreferences(
+        addResources("youtube", "layout.hide.player.flyoutmenupanel.HidePlayerFlyoutMenuPatch")
+
+        PreferenceScreen.PLAYER.addPreferences(
             PreferenceScreenPreference(
                 key = "revanced_hide_player_flyout",
                 preferences = setOf(
@@ -75,6 +71,6 @@ object HidePlayerFlyoutMenuPatch : ResourcePatch() {
             ),
         )
 
-        LithoFilterPatch.addFilter(FILTER_CLASS_DESCRIPTOR)
+        addFilter(filterClassDescriptor)
     }
 }
