98b9e27b14ea86fedc9a4f42547d5efdcdcffcf5
oSumAtrIX <johan.melkonyan1@web.de>
2024-05-23 01:14:40 +0200

    fix(YouTube - Client spoof): Spoof iOS client model to fix various side effects
    This commit also changes the default client spoof to iOS as the only side-effect known now, is HDR not working.

--- src/main/kotlin/app/revanced/patches/youtube/misc/fix/playback/SpoofClientPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/fix/playback/SpoofClientPatch.kt
@@ -19,6 +19,7 @@ import app.revanced.patches.youtube.misc.fix.playback.fingerprints.*
 import app.revanced.patches.youtube.misc.settings.SettingsPatch
 import app.revanced.patches.youtube.video.playerresponse.PlayerResponseMethodHookPatch
 import app.revanced.util.getReference
+import app.revanced.util.indexOfFirstInstruction
 import app.revanced.util.resultOrThrow
 import com.android.tools.smali.dexlib2.AccessFlags
 import com.android.tools.smali.dexlib2.Opcode
@@ -74,6 +75,7 @@ object SpoofClientPatch : BytecodePatch(
         BuildPlayerRequestURIFingerprint,
         SetPlayerRequestClientTypeFingerprint,
         CreatePlayerRequestBodyFingerprint,
+        CreatePlayerRequestBodyWithModelFingerprint,
 
         // Storyboard spoof.
         StoryboardRendererSpecFingerprint,
@@ -168,6 +170,22 @@ object SpoofClientPatch : BytecodePatch(
                 Triple(clientInfoField, clientInfoClientTypeField, clientInfoClientVersionField)
             }
 
+        val clientInfoClientModelField = CreatePlayerRequestBodyWithModelFingerprint.resultOrThrow().mutableMethod.let {
+            val instructions = it.getInstructions()
+
+            val getClientModelIndex = it.indexOfFirstInstruction {
+                getReference<FieldReference>().toString() == "Landroid/os/Build;->MODEL:Ljava/lang/String;"
+            }
+
+            // The next IPUT_OBJECT instruction after getting the client model is setting the client model field.
+            instructions.subList(
+                getClientModelIndex,
+                instructions.lastIndex,
+            ).first { instruction ->
+                instruction.opcode == Opcode.IPUT_OBJECT
+            }.getReference<FieldReference>()
+        }
+
         // endregion
 
         // region Spoof client type for /player requests.
@@ -216,12 +234,17 @@ object SpoofClientPatch : BytecodePatch(
                             move-result v1
                             iput v1, v0, $clientInfoClientTypeField
                             
+                            # Set client model to the spoofed value.
+                            iget-object v1, v0, $clientInfoClientModelField
+                            invoke-static { v1 }, $INTEGRATIONS_CLASS_DESCRIPTOR->getClientModel(Ljava/lang/String;)Ljava/lang/String;
+                            move-result-object v1
+                            iput-object v1, v0, $clientInfoClientModelField
+
                             # Set client version to the spoofed value.
                             iget-object v1, v0, $clientInfoClientVersionField
                             invoke-static { v1 }, $INTEGRATIONS_CLASS_DESCRIPTOR->getClientVersion(Ljava/lang/String;)Ljava/lang/String;
                             move-result-object v1
                             iput-object v1, v0, $clientInfoClientVersionField
-                            
                             :disabled
                             return-void
                         """,
