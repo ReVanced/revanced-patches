0560e3568135a158c7ecd66a677ee1a529e18171
oSumAtrIX <johan.melkonyan1@web.de>
2024-05-27 17:04:59 +0200

    shared fingerprints

--- src/main/kotlin/app/revanced/patches/youtube/shared/fingerprints/LayoutConstructorFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/youtube/shared/fingerprints/LayoutConstructorFingerprint.kt
@@ -1,12 +1,11 @@
 package app.revanced.patches.youtube.shared.fingerprints
 
-import app.revanced.patcher.extensions.or
-import app.revanced.patcher.fingerprint.MethodFingerprint
+import app.revanced.patcher.fingerprint.methodFingerprint
 import com.android.tools.smali.dexlib2.AccessFlags
 
-internal object LayoutConstructorFingerprint : MethodFingerprint(
-    returnType = "V",
-    accessFlags = AccessFlags.PUBLIC or AccessFlags.FINAL,
-    parameters = emptyList(),
-    strings = listOf("1.0x")
-)
+internal val layoutConstructorFingerprint = methodFingerprint {
+    accessFlags(AccessFlags.PUBLIC, AccessFlags.FINAL)
+    returns("V")
+    parameters()
+    strings("1.0x")
+}
