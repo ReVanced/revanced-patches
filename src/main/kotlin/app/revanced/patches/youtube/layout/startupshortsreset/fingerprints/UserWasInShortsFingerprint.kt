26404f6941d88cf3c3d5c1024e5977041af5d50e
oSumAtrIX <johan.melkonyan1@web.de>
2024-05-28 00:15:03 +0200

    Disable resuming Shorts on startup

--- src/main/kotlin/app/revanced/patches/youtube/layout/startupshortsreset/fingerprints/UserWasInShortsFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/startupshortsreset/fingerprints/UserWasInShortsFingerprint.kt
@@ -1,12 +1,11 @@
 package app.revanced.patches.youtube.layout.startupshortsreset.fingerprints
 
-import app.revanced.patcher.extensions.or
-import app.revanced.patcher.fingerprint.MethodFingerprint
+import app.revanced.patcher.fingerprint.methodFingerprint
 import com.android.tools.smali.dexlib2.AccessFlags
 
-object UserWasInShortsFingerprint : MethodFingerprint(
-    returnType = "V",
-    accessFlags = AccessFlags.PUBLIC or AccessFlags.FINAL,
-    parameters = listOf("Ljava/lang/Object;"),
-    strings = listOf("Failed to read user_was_in_shorts proto after successful warmup"),
-)
+internal val userWasInShortsFingerprint = methodFingerprint {
+    returns("V")
+    accessFlags(AccessFlags.PUBLIC, AccessFlags.FINAL)
+    parameters("Ljava/lang/Object;")
+    strings("Failed to read user_was_in_shorts proto after successful warmup")
+}
