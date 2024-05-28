228527c7b794af39c5434361ff0aecf6473c9f7e
oSumAtrIX <johan.melkonyan1@web.de>
2024-05-29 00:45:12 +0200

    spoof device dimensions

--- src/main/kotlin/app/revanced/patches/youtube/misc/dimensions/spoof/fingerprints/DeviceDimensionsModelToStringFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/dimensions/spoof/fingerprints/DeviceDimensionsModelToStringFingerprint.kt
@@ -1,8 +1,8 @@
 package app.revanced.patches.youtube.misc.dimensions.spoof.fingerprints
 
-import app.revanced.patcher.fingerprint.MethodFingerprint
+import app.revanced.patcher.fingerprint.methodFingerprint
 
-internal object DeviceDimensionsModelToStringFingerprint : MethodFingerprint(
-    returnType = "L",
-    strings = listOf("minh.", ";maxh.")
-)
+internal val deviceDimensionsModelToStringFingerprint = methodFingerprint {
+    returns("L")
+    strings("minh.", ";maxh.")
+}
