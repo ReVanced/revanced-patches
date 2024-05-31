ea4fd68df089324826008c266b7061a5f00f28b7
oSumAtrIX <johan.melkonyan1@web.de>
2024-06-01 00:36:20 +0200

    remove tracking query param

--- src/main/kotlin/app/revanced/patches/youtube/misc/privacy/fingerprints/YouTubeShareSheetFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/privacy/fingerprints/YouTubeShareSheetFingerprint.kt
@@ -1,18 +1,17 @@
 package app.revanced.patches.youtube.misc.privacy.fingerprints
 
-import app.revanced.patcher.extensions.or
-import app.revanced.patcher.fingerprint.MethodFingerprint
+import app.revanced.patcher.fingerprint.methodFingerprint
 import com.android.tools.smali.dexlib2.AccessFlags
 import com.android.tools.smali.dexlib2.Opcode
 
-internal object YouTubeShareSheetFingerprint : MethodFingerprint(
-    returnType = "V",
-    accessFlags = AccessFlags.PUBLIC or AccessFlags.FINAL,
-    parameters = listOf("L", "Ljava/util/Map;"),
-    opcodes = listOf(
+internal val youtubeShareSheetFingerprint = methodFingerprint {
+    accessFlags(AccessFlags.PUBLIC, AccessFlags.FINAL)
+    returns("V")
+    parameters("L", "Ljava/util/Map;")
+    opcodes(
         Opcode.CHECK_CAST,
         Opcode.GOTO,
         Opcode.MOVE_OBJECT,
         Opcode.INVOKE_VIRTUAL,
     )
-)
+}
