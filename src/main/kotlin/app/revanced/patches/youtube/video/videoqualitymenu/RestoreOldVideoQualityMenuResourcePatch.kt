8471b9afb14a5fb742151d60c91796ac80b7526f
oSumAtrIX <johan.melkonyan1@web.de>
2024-06-01 03:26:28 +0200

    restore old video quality menu

--- src/main/kotlin/app/revanced/patches/youtube/video/videoqualitymenu/RestoreOldVideoQualityMenuResourcePatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/video/videoqualitymenu/RestoreOldVideoQualityMenuResourcePatch.kt
@@ -1,36 +1,43 @@
 package app.revanced.patches.youtube.video.videoqualitymenu
 
-import app.revanced.patcher.data.ResourceContext
-import app.revanced.patcher.patch.ResourcePatch
-import app.revanced.patcher.patch.annotation.Patch
-import app.revanced.patches.all.misc.resources.AddResourcesPatch
-import app.revanced.patches.shared.misc.mapping.ResourceMappingPatch
+import app.revanced.patcher.patch.resourcePatch
+import app.revanced.patches.all.misc.resources.addResources
+import app.revanced.patches.all.misc.resources.addResourcesPatch
+import app.revanced.patches.shared.misc.mapping.get
+import app.revanced.patches.shared.misc.mapping.resourceMappingPatch
+import app.revanced.patches.shared.misc.mapping.resourceMappings
 import app.revanced.patches.shared.misc.settings.preference.SwitchPreference
-import app.revanced.patches.youtube.misc.settings.SettingsPatch
+import app.revanced.patches.youtube.misc.settings.PreferenceScreen
+import app.revanced.patches.youtube.misc.settings.settingsPatch
 
-@Patch(
-    dependencies = [SettingsPatch::class, ResourceMappingPatch::class, AddResourcesPatch::class],
-)
-object RestoreOldVideoQualityMenuResourcePatch : ResourcePatch() {
-    internal var videoQualityBottomSheetListFragmentTitle = -1L
-    internal var videoQualityQuickMenuAdvancedMenuDescription = -1L
+internal var videoQualityBottomSheetListFragmentTitle = -1L
+    private set
+internal var videoQualityQuickMenuAdvancedMenuDescription = -1L
+    private set
 
-    override fun execute(context: ResourceContext) {
-        AddResourcesPatch(this::class)
+internal val restoreOldVideoQualityMenuResourcePatch = resourcePatch {
+    dependsOn(
+        settingsPatch,
+        resourceMappingPatch,
+        addResourcesPatch,
+    )
 
-        SettingsPatch.PreferenceScreen.VIDEO.addPreferences(
+    execute {
+        addResources("youtube", "video.videoqualitymenu.RestoreOldVideoQualityMenuResourcePatch")
+
+        PreferenceScreen.VIDEO.addPreferences(
             SwitchPreference("revanced_restore_old_video_quality_menu"),
         )
 
         // Used for the old type of the video quality menu.
-        videoQualityBottomSheetListFragmentTitle = ResourceMappingPatch[
+        videoQualityBottomSheetListFragmentTitle = resourceMappings[
             "layout",
             "video_quality_bottom_sheet_list_fragment_title",
         ]
 
-        videoQualityQuickMenuAdvancedMenuDescription = ResourceMappingPatch[
+        videoQualityQuickMenuAdvancedMenuDescription = resourceMappings[
             "string",
-            "video_quality_quick_menu_advanced_menu_description"
+            "video_quality_quick_menu_advanced_menu_description",
         ]
     }
 }
--- src/main/kotlin/app/revanced/patches/youtube/video/videoqualitymenu/fingerprints/VideoQualityMenuOptionsFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/youtube/video/videoqualitymenu/fingerprints/VideoQualityMenuOptionsFingerprint.kt
@@ -1,22 +1,23 @@
 package app.revanced.patches.youtube.video.videoqualitymenu.fingerprints
 
-import app.revanced.patches.youtube.video.videoqualitymenu.RestoreOldVideoQualityMenuResourcePatch
-import app.revanced.util.patch.LiteralValueFingerprint
+import app.revanced.patches.youtube.video.videoqualitymenu.videoQualityQuickMenuAdvancedMenuDescription
+import app.revanced.util.patch.literalValueFingerprint
 import com.android.tools.smali.dexlib2.AccessFlags
 import com.android.tools.smali.dexlib2.Opcode
 
-internal object VideoQualityMenuOptionsFingerprint : LiteralValueFingerprint(
-    accessFlags = AccessFlags.STATIC.value,
-    parameters = listOf("Landroid/content/Context", "L", "L"),
-    returnType = "[L",
-    opcodes = listOf(
+internal val videoQualityMenuOptionsFingerprint = literalValueFingerprint(
+    literalSupplier = { videoQualityQuickMenuAdvancedMenuDescription },
+) {
+    accessFlags(AccessFlags.STATIC)
+    returns("[L")
+    parameters("Landroid/content/Context", "L", "L")
+    opcodes(
         Opcode.IF_EQZ, // Check if advanced menu should be shown.
         Opcode.NEW_ARRAY,
         Opcode.APUT_OBJECT,
         Opcode.APUT_OBJECT,
         Opcode.APUT_OBJECT,
         Opcode.RETURN_OBJECT,
-        Opcode.CONST_4 // Advanced menu code path.
-    ),
-    literalSupplier = { RestoreOldVideoQualityMenuResourcePatch.videoQualityQuickMenuAdvancedMenuDescription }
-)
+        Opcode.CONST_4, // Advanced menu code path.
+    )
+}
