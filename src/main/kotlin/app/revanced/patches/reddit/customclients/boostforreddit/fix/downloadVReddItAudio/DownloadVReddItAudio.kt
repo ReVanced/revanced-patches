ee928b20a5f544c8771927e4d323db873d6ca549
OctoNezd <me@octonezd.me>
2024-06-02 22:12:11 +0300

    Use scanResult to find instruction indexes

--- src/main/kotlin/app/revanced/patches/reddit/customclients/boostforreddit/fix/downloadVReddItAudio/DownloadVReddItAudio.kt
+++ src/main/kotlin/app/revanced/patches/reddit/customclients/boostforreddit/fix/downloadVReddItAudio/DownloadVReddItAudio.kt
@@ -7,23 +7,37 @@ import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.annotation.CompatiblePackage
 import app.revanced.patcher.patch.annotation.Patch
 import app.revanced.patches.reddit.customclients.boostforreddit.fix.downloadVReddItAudio.fingerprints.DownloaderSelectVReddItAudioLink
+import com.android.tools.smali.dexlib2.builder.instruction.BuilderInstruction21c
 import com.android.tools.smali.dexlib2.iface.instruction.formats.Instruction21c
+import com.android.tools.smali.dexlib2.immutable.reference.ImmutableStringReference
 
 @Patch(
-    name="Fix downloads from v.redd.it",
+    name = "Fix downloads from v.redd.it",
     description = "Fixes missing audio in videos downloaded from v.redd.it",
     compatiblePackages = [CompatiblePackage("com.rubenmayayo.reddit")]
 )
 object FixDownloadedVReddItAudio : BytecodePatch(setOf(DownloaderSelectVReddItAudioLink)) {
     private val INDEXES = setOf(
-        Pair(17, "/DASH_AUDIO_128.mp4"),
-        Pair(34, "/DASH_AUDIO_64.mp4")
+        Pair("/DASH_audio.mp4", "/DASH_AUDIO_128.mp4"),
+        Pair("/audio", "/DASH_AUDIO_64.mp4")
     )
+
     override fun execute(context: BytecodeContext) {
-        DownloaderSelectVReddItAudioLink.result?.mutableMethod?.apply {
-            INDEXES.forEach {
-                val instruction = getInstruction(it.first) as Instruction21c
-                replaceInstruction(it.first, "const-string v${instruction.registerA}, \"${it.second}\"")
+        val searchResult = DownloaderSelectVReddItAudioLink.result
+        searchResult?.mutableMethod?.apply {
+            INDEXES.forEach { endpointPair ->
+                val instructionIndex = searchResult.scanResult.stringsScanResult!!.matches.find {
+                    it.string == endpointPair.first
+                }!!.index
+                val instruction = getInstruction(instructionIndex) as Instruction21c
+                replaceInstruction(
+                    instructionIndex,
+                    BuilderInstruction21c(
+                        instruction.opcode,
+                        instruction.registerA,
+                        ImmutableStringReference(endpointPair.second)
+                    )
+                )
             }
         }
     }
