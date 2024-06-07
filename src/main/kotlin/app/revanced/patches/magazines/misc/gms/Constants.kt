498eb4aa8f469f857cfe86f20099c16e4fd1aa60
oSumAtrIX <johan.melkonyan1@web.de>
2024-06-08 01:05:04 +0200

    refactor

--- src/main/kotlin/app/revanced/patches/magazines/misc/gms/Constants.kt
+++
@@ -1,6 +0,0 @@
-package app.revanced.patches.magazines.misc.gms
-
-internal object Constants {
-    const val MAGAZINES_PACKAGE_NAME = "com.google.android.apps.magazines"
-    const val REVANCED_MAGAZINES_PACKAGE_NAME = "app.revanced.android.magazines"
-}
--- src/main/kotlin/app/revanced/patches/magazines/misc/gms/fingerprints/MagazinesActivityOnCreateFingerprint.kt
+++
@@ -1,9 +0,0 @@
-package app.revanced.patches.magazines.misc.gms.fingerprints
-
-import app.revanced.patcher.fingerprint.MethodFingerprint
-
-internal object MagazinesActivityOnCreateFingerprint : MethodFingerprint(
-    customFingerprint = { methodDef, classDef ->
-       classDef.type.endsWith("/StartActivity;") && methodDef.name == "onCreate"
-    }
-)
