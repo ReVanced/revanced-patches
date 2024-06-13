25bf6316427917ef1fa55749af430eb7d9f64d51
oSumAtrIX <johan.melkonyan1@web.de>
2024-06-13 21:58:05 +0200

    cleanup

--- src/main/kotlin/app/revanced/patches/all/misc/versioncode/ChangeVersionCodePatch.kt
+++ src/main/kotlin/app/revanced/patches/all/misc/versioncode/ChangeVersionCodePatch.kt
@@ -1,39 +1,39 @@
 package app.revanced.patches.all.misc.versioncode
 
-
 import app.revanced.patcher.data.ResourceContext
 import app.revanced.patcher.patch.ResourcePatch
 import app.revanced.patcher.patch.annotation.Patch
 import app.revanced.patcher.patch.options.PatchOption.PatchExtensions.intPatchOption
+import app.revanced.util.getNode
 import org.w3c.dom.Element
-
 
 @Patch(
     name = "Change version code",
-    description = "Sets the version code to the max value by default, which enables app downgrading and hides available updates from app stores like Google Play",
-    use = false
+    description = "Changes the version code of the app. By default the highest version code is set. " +
+        "This allows older versions of an app to be installed " +
+        "if their version code is set to the same or a higher value, or preventing app stores to update the app.",
+    use = false,
 )
 @Suppress("unused")
-object ChangeVersionCode : ResourcePatch() {
-    private val versionCodeOption by intPatchOption(
+object ChangeVersionCodePatch : ResourcePatch() {
+    private val versionCode by intPatchOption(
         key = "versionCode",
-        // max allowed by Google Play
         default = Int.MAX_VALUE,
         values = mapOf(
-            "Minimum" to 1, "Maximum" to Int.MAX_VALUE
+            "Lowest" to 1,
+            "Highest" to Int.MAX_VALUE,
         ),
         title = "Version code",
         description = "The version code to use",
         required = true,
     ) {
-        it != null && it >= 1
+        it!! >= 1
     }
 
     override fun execute(context: ResourceContext) {
         context.document["AndroidManifest.xml"].use { document ->
-            val manifest = document.getElementsByTagName("manifest").item(0) as Element
-            manifest.setAttribute("android:versionCode", "$versionCodeOption")
+            val manifestElement = document.getNode("manifest") as Element
+            manifestElement.setAttribute("android:versionCode", "$versionCode")
         }
     }
 }
-
