6fba3367a1a22aff33b3bcc05a51d9861b4e2bae
oSumAtrIX <johan.melkonyan1@web.de>
2024-06-14 17:45:59 +0200

    Apply suggestions from code review

--- src/main/kotlin/app/revanced/patches/all/misc/versioncode/ChangeVersionCodePatch.kt
+++ src/main/kotlin/app/revanced/patches/all/misc/versioncode/ChangeVersionCodePatch.kt
@@ -11,7 +11,7 @@ import org.w3c.dom.Element
     name = "Change version code",
     description = "Changes the version code of the app. By default the highest version code is set. " +
         "This allows older versions of an app to be installed " +
-        "if their version code is set to the same or a higher value, or preventing app stores to update the app.",
+        "if their version code is set to the same or a higher value and can stop app stores to update the app.",
     use = false,
 )
 @Suppress("unused")
