04b8c65d254fc3a9144f2ee6ab6e250526f163c8
Edgar Pireyn <edgar@pireyn.be>
2024-06-20 19:01:35 +0200

    Remove offers tab and offer stories

---
+++ src/main/kotlin/app/revanced/patches/stocard/HideOfferTabPatch.kt
@@ -0,0 +1,18 @@
+package app.revanced.patches.stocard
+
+import app.revanced.patcher.data.ResourceContext
+import app.revanced.patcher.patch.ResourcePatch
+import app.revanced.patcher.patch.annotation.CompatiblePackage
+import app.revanced.patcher.patch.annotation.Patch
+import app.revanced.util.childElementsSequence
+
+@Patch(name = "Hide offers tab", compatiblePackages = [CompatiblePackage("de.stocard.stocard", ["10.50.0"])])
+object HideOfferTabPatch : ResourcePatch() {
+    override fun execute(context: ResourceContext) {
+        context.document["res/menu/bottom_navigation_menu.xml"].use { document ->
+            val menu = document.getElementsByTagName("menu").item(0)
+            menu.removeChild(menu.childElementsSequence()
+                .first { it.attributes.getNamedItem("android:id")?.nodeValue?.contains("offer") ?: false })
+        }
+    }
+}
---
+++ src/main/kotlin/app/revanced/patches/stocard/HideStoriesPatch.kt
@@ -0,0 +1,18 @@
+package app.revanced.patches.stocard
+
+import app.revanced.patcher.data.ResourceContext
+import app.revanced.patcher.patch.ResourcePatch
+import app.revanced.patcher.patch.annotation.CompatiblePackage
+import app.revanced.patcher.patch.annotation.Patch
+
+@Patch(name = "Hide story bubbles", compatiblePackages = [CompatiblePackage("de.stocard.stocard", ["10.50.0"])])
+object HideStoriesPatch : ResourcePatch() {
+    override fun execute(context: ResourceContext) {
+        context.document["res/layout/rv_story_bubbles_list.xml"].use { document ->
+            val view = document.getElementsByTagName("androidx.recyclerview.widget.RecyclerView").item(0)
+            view.attributes.getNamedItem("android:layout_width").nodeValue = "0dp"
+            view.attributes.getNamedItem("android:layout_height").nodeValue = "0dp"
+        }
+    }
+
+}
