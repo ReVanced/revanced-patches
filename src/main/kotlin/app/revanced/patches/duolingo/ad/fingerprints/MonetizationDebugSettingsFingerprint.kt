3ecee1443e2923cce8e4eaaf2d719421008e1d5e
Joshua May <git@hotdog.horse>
2024-07-09 00:15:21 +0200

    feat(Duolingo): Add Duolingo patch to skip ads

---
+++ src/main/kotlin/app/revanced/patches/duolingo/ad/fingerprints/MonetizationDebugSettingsFingerprint.kt
@@ -0,0 +1,17 @@
+package app.revanced.patches.duolingo.ad.fingerprints
+
+import app.revanced.patcher.extensions.or
+import app.revanced.patcher.fingerprint.MethodFingerprint
+import com.android.tools.smali.dexlib2.AccessFlags
+
+internal object MonetizationDebugSettingsFingerprint : MethodFingerprint(
+    returnType = "V",
+    accessFlags = AccessFlags.PUBLIC or AccessFlags.CONSTRUCTOR,
+    parameters = listOf(
+        "Z", // disableAds
+        "Z", // useDebugBilling
+        "Z", // showManageSubscriptions
+        "Z", // alwaysShowSuperAds
+        "Lcom/duolingo/debug/FamilyQuestOverride;",
+    )
+)
