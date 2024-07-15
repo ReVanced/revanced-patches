fab40b85ffadf2641cd055c35f9ea7969397674a
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2024-07-15 18:16:39 +0400

    fix(YouTube - Disable auto captions): Do not break Shorts captions menu

--- integrations/java/app/revanced/integrations/youtube/patches/DisableAutoCaptionsPatch.java
+++ integrations/java/app/revanced/integrations/youtube/patches/DisableAutoCaptionsPatch.java
@@ -1,6 +1,7 @@
 package app.revanced.integrations.youtube.patches;
 
 import app.revanced.integrations.youtube.settings.Settings;
+import app.revanced.integrations.youtube.shared.PlayerType;
 
 @SuppressWarnings("unused")
 public class DisableAutoCaptionsPatch {
@@ -11,7 +12,9 @@ public class DisableAutoCaptionsPatch {
     public static boolean captionsButtonDisabled;
 
     public static boolean autoCaptionsEnabled() {
-        return Settings.AUTO_CAPTIONS.get();
+        return Settings.AUTO_CAPTIONS.get()
+                // Do not use auto captions for Shorts.
+                && !PlayerType.getCurrent().isNoneHiddenOrSlidingMinimized();
     }
 
 }
