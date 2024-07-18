f2bf2da9a5fe649abc955a7f106d71246dc3b255
Tim <tymmesyde@gmail.com>
2024-07-19 01:50:03 +0200

    fix(Instagram - Hide ads):  Restore compatibility with latest version by fixing fingerprint (#3455)

--- src/main/kotlin/app/revanced/patches/instagram/patches/ad/fingerprints/AdInjectorFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/instagram/patches/ad/fingerprints/AdInjectorFingerprint.kt
@@ -2,16 +2,13 @@ package app.revanced.patches.instagram.patches.ad.fingerprints
 
 import app.revanced.patcher.fingerprint.MethodFingerprint
 import com.android.tools.smali.dexlib2.AccessFlags
-import com.android.tools.smali.dexlib2.Opcode
 
 internal object AdInjectorFingerprint : MethodFingerprint(
     returnType = "Z",
     accessFlags = AccessFlags.PRIVATE.value,
     parameters = listOf("L", "L"),
-    opcodes = listOf(
-        Opcode.IGET,
-        Opcode.INVOKE_INTERFACE,
-        Opcode.MOVE_RESULT_OBJECT,
+    strings = listOf(
+        "SponsoredContentController.insertItem",
+        "SponsoredContentController::Delivery",
     ),
-    strings = listOf("SponsoredContentController::Delivery"),
 )
