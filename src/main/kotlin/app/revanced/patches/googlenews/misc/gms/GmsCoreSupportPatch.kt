deff5846e56e5f38a61cd53bbe470974f73e0230
oSumAtrIX <johan.melkonyan1@web.de>
2024-07-20 04:54:07 +0200

    add comment [skip ci]

--- src/main/kotlin/app/revanced/patches/googlenews/misc/gms/GmsCoreSupportPatch.kt
+++ src/main/kotlin/app/revanced/patches/googlenews/misc/gms/GmsCoreSupportPatch.kt
@@ -17,6 +17,8 @@ object GmsCoreSupportPatch : BaseGmsCoreSupportPatch(
     mainActivityOnCreateFingerprint = MagazinesActivityOnCreateFingerprint,
     integrationsPatchDependency = IntegrationsPatch::class,
     gmsCoreSupportResourcePatch = GmsCoreSupportResourcePatch,
+    // Remove version constraint,
+    // once https://github.com/ReVanced/revanced-patches/pull/3111#issuecomment-2240877277 is resolved.
     compatiblePackages = setOf(CompatiblePackage(MAGAZINES_PACKAGE_NAME, setOf("5.108.0.644447823"))),
     fingerprints = setOf(ServiceCheckFingerprint),
 ) {
