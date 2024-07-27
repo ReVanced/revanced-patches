4fe8e09f5bfc44c52594d0b1a8f4af4fcafa2b82
Cedric <cedric-kell@gmx.ch>
2024-07-27 12:06:38 -0600

    Update CheckIntegrityFingerprint.kt
    Remove OpCodes as it still matches the fingerprint

--- src/main/kotlin/app/revanced/patches/swissid/integritycheck/fingerprints/CheckIntegrityFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/swissid/integritycheck/fingerprints/CheckIntegrityFingerprint.kt
@@ -7,31 +7,5 @@ import com.android.tools.smali.dexlib2.Opcode
 internal object CheckIntegrityFingerprint : MethodFingerprint(
     returnType = "V",
     parameters = listOf("Lcom/swisssign/deviceintegrity/model/DeviceIntegrityResult;"),
-    strings = listOf("it", "result"),
-    opcodes = listOf(
-        Opcode.CONST_STRING,
-        Opcode.INVOKE_STATIC,
-        Opcode.INSTANCE_OF,
-        Opcode.IF_EQZ,
-        Opcode.NEW_INSTANCE,
-        Opcode.INVOKE_DIRECT,
-        Opcode.CHECK_CAST,
-        Opcode.INVOKE_VIRTUAL,
-        Opcode.MOVE_RESULT_OBJECT,
-        Opcode.CONST_STRING,
-        Opcode.INVOKE_VIRTUAL,
-        Opcode.IGET_OBJECT,
-        Opcode.INVOKE_VIRTUAL,
-        Opcode.MOVE_RESULT_OBJECT,
-        Opcode.INVOKE_INTERFACE,
-        Opcode.GOTO,
-        Opcode.INSTANCE_OF,
-        Opcode.IF_EQZ,
-        Opcode.IGET_OBJECT,
-        Opcode.CHECK_CAST,
-        Opcode.INVOKE_VIRTUAL,
-        Opcode.MOVE_RESULT_OBJECT,
-        Opcode.INVOKE_INTERFACE,
-        Opcode.RETURN_VOID
-    ),
+    strings = listOf("it", "result")
 )
