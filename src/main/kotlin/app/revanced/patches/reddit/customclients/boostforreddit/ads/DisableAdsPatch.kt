2ba3bad7042dea752133046045db0a36e026f414
oSumAtrIX <johan.melkonyan1@web.de>
2024-07-28 16:09:26 +0200

    refactor

--- src/main/kotlin/app/revanced/patches/reddit/customclients/boostforreddit/ads/DisableAdsPatch.kt
+++ src/main/kotlin/app/revanced/patches/reddit/customclients/boostforreddit/ads/DisableAdsPatch.kt
@@ -1,20 +1,23 @@
 package app.revanced.patches.reddit.customclients.boostforreddit.ads
 
-import app.revanced.patcher.patch.PatchException
 import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patches.reddit.customclients.boostforreddit.ads.fingerprints.*
 import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
+import app.revanced.patcher.patch.annotation.CompatiblePackage
+import app.revanced.patcher.patch.annotation.Patch
 import app.revanced.util.resultOrThrow
 
+@Patch(
+    name = "Disable ads",
+    compatiblePackages = [CompatiblePackage("com.rubenmayayo.reddit")],
+)
 @Suppress("unused")
 object DisableAdsPatch : BytecodePatch(
-    name = "Disable ads",
-    compatiblePackages = setOf(CompatiblePackage("com.rubenmayayo.reddit")),
-    fingerprints = setOf(MaxMediationFingerprint, AdmobMediationFingerprint),
+    setOf(MaxMediationFingerprint, AdmobMediationFingerprint),
 ) {
-    override fun execute(context: BytecodeContext) {
-        MaxMediationFingerprint.resultOrThrow().mutableMethod.addInstructions(0, "return-void")
-        AdmobMediationFingerprint.resultOrThrow().mutableMethod.addInstructions(0, "return-void")
-    }
+    override fun execute(context: BytecodeContext) =
+        arrayOf(MaxMediationFingerprint, AdmobMediationFingerprint).forEach {
+            it.resultOrThrow().mutableMethod.addInstructions(0, "return-void")
+        }
 }
