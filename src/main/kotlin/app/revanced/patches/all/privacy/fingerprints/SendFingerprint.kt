a02dd1819ded2ed5ec32ec532494479bd8986614
patchink0 <173717366+patchink0@users.noreply.github.com>
2024-08-01 00:16:23 +0200

    Updated FirebaseTransport patch to resist obfuscation

--- src/main/kotlin/app/revanced/patches/all/privacy/fingerprints/SendFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/all/privacy/fingerprints/SendFingerprint.kt
@@ -2,11 +2,20 @@ package app.revanced.patches.all.privacy.fingerprints
 
 import app.revanced.patcher.fingerprint.MethodFingerprint
 import com.android.tools.smali.dexlib2.AccessFlags
+import com.android.tools.smali.dexlib2.Opcode
 
 internal object SendFingerprint : MethodFingerprint(
     returnType = "V",
     accessFlags = AccessFlags.PUBLIC.value,
-    customFingerprint = { methodDef, classDef ->
-        classDef.sourceFile == "TransportRuntime.java" && methodDef.name == "send"
+    parameters = listOf("L", "L"),
+    opcodes = listOf(
+        Opcode.IGET_OBJECT,
+        Opcode.INVOKE_VIRTUAL,
+        Opcode.MOVE_RESULT_OBJECT,
+        Opcode.INVOKE_VIRTUAL,
+        Opcode.MOVE_RESULT_OBJECT
+    ),
+    customFingerprint = { _, classDef ->
+        classDef.sourceFile == "TransportRuntime.java"
     }
 )
