becaad4a9876392b567478cdfe773956655901f0
ILoveOpenSourceApplications <117499019+ILoveOpenSourceApplications@users.noreply.github.com>
2024-08-04 13:39:48 +0530

    refactor(YouTube Music): Rename `Hide music video ads` to `Hide video ads` and add patch description (#3494)
    Co-authored-by: ILoveOpenSourceApplications <ILoveOpenSourceApplications@users.noreply.github.com>
    Co-authored-by: LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>

--- src/main/kotlin/app/revanced/patches/music/ad/video/HideVideoAds.kt
+++ src/main/kotlin/app/revanced/patches/music/ad/video/HideVideoAds.kt
@@ -6,11 +6,12 @@ import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.annotation.CompatiblePackage
 import app.revanced.patcher.patch.annotation.Patch
 import app.revanced.patcher.util.proxy.mutableTypes.MutableMethod
-import app.revanced.patches.music.ad.video.fingerprints.ShowMusicVideoAdsParentFingerprint
+import app.revanced.patches.music.ad.video.fingerprints.ShowVideoAdsParentFingerprint
 import app.revanced.util.exception
 
 @Patch(
-    name = "Hide music video ads",
+    name = "Hide video ads",
+    description = "Hides ads that appear while listening to or streaming music videos, podcasts, or songs.",
     compatiblePackages = [
         CompatiblePackage(
             "com.google.android.apps.youtube.music",
@@ -25,21 +26,29 @@ import app.revanced.util.exception
     ],
 )
 @Suppress("unused")
-object HideMusicVideoAds : BytecodePatch(
-    setOf(ShowMusicVideoAdsParentFingerprint),
+object HideVideoAds : BytecodePatch(
+    setOf(ShowVideoAdsParentFingerprint),
 ) {
     override fun execute(context: BytecodeContext) {
-        ShowMusicVideoAdsParentFingerprint.result?.let {
-            val showMusicVideoAdsMethod = context
+        ShowVideoAdsParentFingerprint.result?.let {
+            val showVideoAdsMethod = context
                 .toMethodWalker(it.mutableMethod)
                 .nextMethod(it.scanResult.patternScanResult!!.startIndex + 1, true).getMethod() as MutableMethod
 
-            showMusicVideoAdsMethod.addInstruction(0, "const/4 p1, 0x0")
-        } ?: throw ShowMusicVideoAdsParentFingerprint.exception
+            showVideoAdsMethod.addInstruction(0, "const/4 p1, 0x0")
+        } ?: throw ShowVideoAdsParentFingerprint.exception
     }
 }
 
-@Deprecated("This patch class has been renamed to HideMusicVideoAds.")
+@Deprecated("This patch class has been renamed to HideVideoAds.")
+object HideMusicVideoAds : BytecodePatch(
+    dependencies = setOf(HideVideoAds::class)
+) {
+    override fun execute(context: BytecodeContext) {
+    }
+}
+
+@Deprecated("This patch class has been renamed to HideVideoAds.")
 object MusicVideoAdsPatch : BytecodePatch(
     dependencies = setOf(HideMusicVideoAds::class),
 ) {
