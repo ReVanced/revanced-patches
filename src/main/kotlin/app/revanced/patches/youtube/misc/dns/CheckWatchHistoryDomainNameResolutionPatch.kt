d219d04f406168c03cafe207966a094a571ef5c7
oSumAtrIX <johan.melkonyan1@web.de>
2024-08-15 07:49:40 +0200

    add dependency

--- src/main/kotlin/app/revanced/patches/youtube/misc/dns/CheckWatchHistoryDomainNameResolutionPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/dns/CheckWatchHistoryDomainNameResolutionPatch.kt
@@ -5,15 +5,17 @@ import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
 import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.annotation.Patch
 import app.revanced.patches.all.misc.resources.AddResourcesPatch
+import app.revanced.patches.youtube.misc.integrations.IntegrationsPatch
 import app.revanced.patches.youtube.shared.fingerprints.MainActivityOnCreateFingerprint
 import app.revanced.util.resultOrThrow
 
 @Patch(
     description = "Checks, if the endpoint 's.youtube.com' to track watch history  is unreachable.",
+    dependencies = [IntegrationsPatch::class],
 )
 @Suppress("unused")
 internal object CheckWatchHistoryDomainNameResolutionPatch : BytecodePatch(
-    setOf(MainActivityOnCreateFingerprint)
+    setOf(MainActivityOnCreateFingerprint),
 ) {
     private const val INTEGRATIONS_CLASS_DESCRIPTOR =
         "Lapp/revanced/integrations/youtube/patches/CheckWatchHistoryDomainNameResolutionPatch;"
@@ -22,10 +24,13 @@ internal object CheckWatchHistoryDomainNameResolutionPatch : BytecodePatch(
         AddResourcesPatch(this::class)
 
         MainActivityOnCreateFingerprint.resultOrThrow().mutableMethod.addInstructions(
-            // Insert index must be great than the insert index used by GmsCoreSupport,
-            // as both patch the same method and GmsCore check should be first.
+            // FIXME: Insert index must be greater than the insert index used by GmsCoreSupport,
+            //  as both patch the same method and GmsCoreSupport check should be first,
+            //  but the patch does not depend on GmsCoreSupport, so it should not be possible to enforce this
+            //  unless a third patch is added that this patch and GmsCoreSupport depend on to manage
+            //  the order of the patches.
             1,
-            "invoke-static/range { p0 .. p0 }, $INTEGRATIONS_CLASS_DESCRIPTOR->checkDnsResolver(Landroid/app/Activity;)V"
+            "invoke-static/range { p0 .. p0 }, $INTEGRATIONS_CLASS_DESCRIPTOR->checkDnsResolver(Landroid/app/Activity;)V",
         )
     }
 }
