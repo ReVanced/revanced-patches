1a49d1f3c2a343d05d0abc07c143add486246fd0
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2024-08-22 13:48:49 -0400

    feat(YouTube - Spoof client): Allow forcing AVC codec with iOS (#3570)

--- src/main/kotlin/app/revanced/patches/youtube/misc/fix/playback/SpoofClientPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/fix/playback/SpoofClientPatch.kt
@@ -14,6 +14,8 @@ import app.revanced.patcher.patch.annotation.Patch
 import app.revanced.patcher.util.proxy.mutableTypes.MutableMethod
 import app.revanced.patcher.util.proxy.mutableTypes.MutableMethod.Companion.toMutable
 import app.revanced.patches.all.misc.resources.AddResourcesPatch
+import app.revanced.patches.shared.misc.settings.preference.ListPreference
+import app.revanced.patches.shared.misc.settings.preference.NonInteractivePreference
 import app.revanced.patches.shared.misc.settings.preference.PreferenceScreen
 import app.revanced.patches.shared.misc.settings.preference.SwitchPreference
 import app.revanced.patches.youtube.misc.backgroundplayback.BackgroundPlaybackPatch
@@ -118,9 +120,16 @@ object SpoofClientPatch : BytecodePatch(
                 sorting = PreferenceScreen.Sorting.UNSORTED,
                 preferences = setOf(
                     SwitchPreference("revanced_spoof_client"),
-                    SwitchPreference("revanced_spoof_client_use_ios"),
-                ),
-            ),
+                    ListPreference("revanced_spoof_client_type",
+                        summaryKey = null,
+                        entriesKey = "revanced_spoof_client_type_entries",
+                        entryValuesKey = "revanced_spoof_client_type_entry_values"
+                    ),
+                    SwitchPreference("revanced_spoof_client_ios_force_avc"),
+                    NonInteractivePreference("revanced_spoof_client_about_android_ios"),
+                    NonInteractivePreference("revanced_spoof_client_about_android_vr")
+                )
+            )
         )
 
         // region Block /initplayback requests to fall back to /get_watch requests.
