af9fc807be1762002689611a09c442b9015bb2b1
swakwork <swakwork+git@gmail.com>
2024-09-02 15:42:41 +0530

    upd(Instagram): Code correction for `Enable developer menu` patch

--- src/main/kotlin/app/revanced/patches/instagram/patches/developerMenu/EnableDeveloperMenuPatch.kt
+++ src/main/kotlin/app/revanced/patches/instagram/patches/developerMenu/EnableDeveloperMenuPatch.kt
@@ -16,7 +16,6 @@ import com.android.tools.smali.dexlib2.dexbacked.reference.DexBackedMethodRefere
 @Patch(
     name = "Enable developer menu",
     compatiblePackages = [CompatiblePackage("com.instagram.android")],
-    use = false,
 )
 @Suppress("unused")
 object EnableDeveloperMenuPatch : BytecodePatch(
@@ -25,23 +24,23 @@ object EnableDeveloperMenuPatch : BytecodePatch(
     override fun execute(context: BytecodeContext) {
         ShouldAddPrefTTLFingerprint.resultOrThrow().let { methodFingerprintResult ->
             methodFingerprintResult.mutableMethod.apply {
-                // finding the method which calls the developer flag check method.
+                // Finding the method which calls the developer flag check method.
                 val developerMethodCallIndex = getInstructions().first { it.opcode == Opcode.INVOKE_STATIC }.location.index
                 val developerMethodReference = (getInstruction<BuilderInstruction35c>(developerMethodCallIndex).reference as DexBackedMethodReference)
-                // the class and method name were the developer flag check happens.
+                // The class and method name were the developer flag check happens.
                 val className = developerMethodReference.definingClass
                 val methodName = developerMethodReference.name
 
-                // locating the developer flag check method.
+                // Locating the developer flag check method.
                 val developerFlagCheckMethod = context.findClass(className)!!.mutableClass.methods.first { it.name == methodName }
 
-                // make the method return true always.
+                // Make the method return true always.
                 developerFlagCheckMethod.addInstructions(
                     0,
                     """
-                    const v0, 0x1
-                    return v0
-                    """.trimIndent(),
+                        const v0, 0x1
+                        return v0
+                    """,
                 )
             }
         }
