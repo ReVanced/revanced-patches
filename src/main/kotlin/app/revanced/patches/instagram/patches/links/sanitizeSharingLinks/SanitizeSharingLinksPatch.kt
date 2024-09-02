7ab600371153fb3f277d535b7d3a0df91b105132
swakwork <swakwork+git@gmail.com>
2024-09-02 16:03:51 +0530

    upd(Instagram): Code correction for `Sanitize sharing links` patch

--- src/main/kotlin/app/revanced/patches/instagram/patches/links/sanitizeSharingLinks/SanitizeSharingLinksPatch.kt
+++ src/main/kotlin/app/revanced/patches/instagram/patches/links/sanitizeSharingLinks/SanitizeSharingLinksPatch.kt
@@ -4,6 +4,7 @@ import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
 import app.revanced.patcher.extensions.InstructionExtensions.getInstruction
 import app.revanced.patcher.extensions.InstructionExtensions.getInstructions
+import app.revanced.patcher.fingerprint.MethodFingerprint
 import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.annotation.CompatiblePackage
 import app.revanced.patcher.patch.annotation.Patch
@@ -21,43 +22,46 @@ import com.android.tools.smali.dexlib2.iface.instruction.OneRegisterInstruction
     name = "Sanitize sharing links",
     description = "Removes the tracking query parameters from links before they are shared.",
     compatiblePackages = [CompatiblePackage("com.instagram.android")],
-    use = false,
     requiresIntegrations = true,
 )
 @Suppress("unused")
 object SanitizeSharingLinksPatch : BytecodePatch(
     setOf(StoryShareUrlFingerprint, LiveShareUrlFingerprint, PostShareClassFinderFingerprint),
 ) {
-    private const val FUNC_CALL = "Lapp/revanced/integrations/instagram/links/ShareLink;->sanitizeUrl(Ljava/lang/String;)Ljava/lang/String;"
+    private const val INVOKE_INTEGRATIONS_METHOD_INSTRUCTION = "Lapp/revanced/integrations/instagram/links/ShareLink;->sanitizeUrl(Ljava/lang/String;)Ljava/lang/String;"
 
     override fun execute(context: BytecodeContext) {
-        fun addFunction(method: MutableMethod) {
+        fun addCode(method: MutableMethod) {
             val index = method.getInstructions().first { it.opcode == Opcode.IPUT_OBJECT }.location.index - 2
-            // link register.
+            // Register where the link is present.
             val register = method.getInstruction<OneRegisterInstruction>(index).registerA
 
             method.addInstructions(
                 index + 1,
                 """
-            invoke-static{v$register}, $FUNC_CALL
-            move-result-object v$register
-                """.trimIndent(),
+                    invoke-static{v$register}, $INVOKE_INTEGRATIONS_METHOD_INSTRUCTION
+                    move-result-object v$register
+                """,
             )
         }
 
-        // sanitize share link of stories.
-        addFunction(StoryShareUrlFingerprint.resultOrThrow().mutableMethod)
-        // sanitize share link of live.
-        addFunction(LiveShareUrlFingerprint.resultOrThrow().mutableMethod)
+        fun addMethod(fingerprint: MethodFingerprint) {
+            addCode(fingerprint.resultOrThrow().mutableMethod)
+        }
 
-        // finding post share url class.
+        // Sanitize share link of stories.
+        addMethod(StoryShareUrlFingerprint)
+        // Sanitize share link of live.
+        addMethod(LiveShareUrlFingerprint)
+
+        // Finding post share url class.
         PostShareClassFinderFingerprint.resultOrThrow().let { it ->
             it.mutableMethod.apply {
                 val classIndex = getInstructions().last { it.opcode == Opcode.CONST_CLASS }.location.index
                 val className = (getInstruction<BuilderInstruction21c>(classIndex).reference as DexBackedTypeReference).type
                 val parseJsonMethod = context.findClass(className)!!.mutableClass.methods.first { it.name == "parseFromJson" }
-                // sanitize share link of posts & reels.
-                addFunction(parseJsonMethod)
+                // Sanitize share link of posts & reels.
+                addCode(parseJsonMethod)
             }
         }
     }
