b6e2e8a63a1ef3ca30b457aabba32c257adc55be
swakwork <swakwork+git@gmail.com>
2024-09-02 16:57:23 +0530

    upd(Instagram): Code correction for `Max media quality` patch

--- src/main/kotlin/app/revanced/patches/instagram/patches/maxQuality/MaxMediaQualityPatch.kt
+++ src/main/kotlin/app/revanced/patches/instagram/patches/maxQuality/MaxMediaQualityPatch.kt
@@ -20,38 +20,36 @@ import com.android.tools.smali.dexlib2.iface.instruction.TwoRegisterInstruction
     name = "Max media quality",
     description = "Sets the images/videos/stories quality to max",
     compatiblePackages = [CompatiblePackage("com.instagram.android")],
-    use = false,
 )
 @Suppress("unused")
 object MaxMediaQualityPatch : BytecodePatch(
     setOf(DisplayMetricsFingerprint, MediaSizeFingerprint, StoryMediaBitrateFingerprint, VideoEncoderConfigFingerprint),
 ) {
     override fun execute(context: BytecodeContext) {
-        val maxPostSize = "0x800" // dec = 2048. Maximum post size.
-        val maxBitRate = "0x989680" // dec = 10000000. Maximum bit rate possible (found in code).
+        val maxPostSize = "0x800" // Decimal value = 2048. Maximum post size.
+        val maxBitRate = "0x989680" // Decimal value = 10000000. Maximum bit rate possible (found in code).
 
-        // Images.
-        // display metrics.
+        // Improve quality of images.
         // Instagram tend to reduce/compress the image resolution to user's device height and width.
         // This section of code removes that restriction and sets the resolution to 2048x2048 (max possible).
         DisplayMetricsFingerprint.resultOrThrow().let { it ->
             it.mutableMethod.apply {
                 val displayMetInstructions = getInstructions().filter { it.opcode == Opcode.IGET }
 
-                // there are 3 iget instances.
+                // There are 3 iget instances.
                 // 1.dpi 2.width 3.height.
-                // we don't need to change dpi, we just need to change height and width.
+                // We don't need to change dpi, we just need to change height and width.
                 displayMetInstructions.drop(1).forEach { instruction ->
                     val index = instruction.location.index
                     val register = getInstruction<TwoRegisterInstruction>(index).registerA
 
-                    // set height and width to 2048.
+                    // Set height and width to 2048.
                     addInstruction(index + 1, "const v$register, $maxPostSize")
                 }
             }
         }
 
-        // Media size.
+        // Yet another method where the image resolution is compressed.
         MediaSizeFingerprint.resultOrThrow().let { it ->
             it.mutableClass.apply {
                 val mediaSetMethod = methods.first { it.returnType == "Lcom/instagram/model/mediasize/ExtendedImageUrl;" }
@@ -62,13 +60,13 @@ object MaxMediaQualityPatch : BytecodePatch(
                     val index = instruction.location.index + 1
                     val register = mediaSetMethod.getInstruction<OneRegisterInstruction>(index).registerA
 
-                    // set height and width to 2048
+                    // Set height and width to 2048.
                     mediaSetMethod.addInstruction(index + 1, "const v$register, $maxPostSize")
                 }
             }
         }
 
-        // Stories.
+        // Improve quality of stories.
         // This section of code sets the bitrate of the stories to the max possible.
         StoryMediaBitrateFingerprint.resultOrThrow().let { it ->
             it.mutableMethod.apply {
@@ -76,25 +74,25 @@ object MaxMediaQualityPatch : BytecodePatch(
 
                 val bitRateRegister = getInstruction<OneRegisterInstruction>(ifLezIndex).registerA
 
-                // set bitrate to maximum possible.
+                // Set the bitrate to maximum possible.
                 addInstruction(ifLezIndex + 1, "const v$bitRateRegister, $maxBitRate")
             }
         }
 
-        // Reels
-        // In general Instagram ted to set the minimum bitrate between max possible and compressed video's bitrate
+        // Improve quality of reels.
+        // In general Instagram tend to set the minimum bitrate between max possible and compressed video's bitrate
         // This section of code sets the bitrate of the reels to the max possible.
         VideoEncoderConfigFingerprint.resultOrThrow().let { it ->
             it.mutableClass.apply {
-                // get the constructor.
+                // Get the constructor.
                 val videoEncoderConfigConstructor = methods.first()
 
                 val lastMoveResIndex = videoEncoderConfigConstructor.getInstructions().last { it.opcode == Opcode.MOVE_RESULT }.location.index
 
-                // finding the register the bitrate is stored.
+                // Finding the register were the bitrate is stored.
                 val bitRateRegister = videoEncoderConfigConstructor.getInstruction<OneRegisterInstruction>(lastMoveResIndex).registerA
 
-                // set bitrate to maximum possible.
+                // Set bitrate to maximum possible.
                 videoEncoderConfigConstructor.addInstruction(lastMoveResIndex + 1, "const v$bitRateRegister, $maxBitRate")
             }
         }
