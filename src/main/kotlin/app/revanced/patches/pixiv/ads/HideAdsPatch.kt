724788cc20a2efb8fe0a2e2dd644d29fc5667bdd
Itroublve <115026399+Anajrim01@users.noreply.github.com>
2024-09-03 20:38:43 +0200

    drop support for old fingerprint

--- src/main/kotlin/app/revanced/patches/pixiv/ads/HideAdsPatch.kt
+++ src/main/kotlin/app/revanced/patches/pixiv/ads/HideAdsPatch.kt
@@ -6,7 +6,6 @@ import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
 import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.annotation.CompatiblePackage
 import app.revanced.patcher.patch.annotation.Patch
-import app.revanced.patches.pixiv.ads.fingerprints.IsNotPremiumFingerprint
 import app.revanced.patches.pixiv.ads.fingerprints.ShouldShowAdsFingerprint
 
 @Patch(
@@ -14,21 +13,17 @@ import app.revanced.patches.pixiv.ads.fingerprints.ShouldShowAdsFingerprint
     compatiblePackages = [CompatiblePackage("jp.pxv.android")]
 )
 @Suppress("unused")
-object HideAdsPatch : BytecodePatch(setOf(IsNotPremiumFingerprint, ShouldShowAdsFingerprint)) {
+object HideAdsPatch : BytecodePatch(setOf(ShouldShowAdsFingerprint)) {
     // Always return false in the "isNotPremium" or "shouldShowAds" method which normally returns !this.accountManager.isPremium.
     // However, this is not the method that controls the user's premium status.
     // Instead, this method is used to determine whether ads should be shown.
     override fun execute(context: BytecodeContext) {
-        val method = IsNotPremiumFingerprint.result?.mutableClass?.virtualMethods?.first()
-            ?: ShouldShowAdsFingerprint.result?.mutableMethod
-            ?: throw ShouldShowAdsFingerprint.exception
-
-        method.addInstructions(
+        ShouldShowAdsFingerprint.result?.mutableMethod.addInstructions(
             0,
             """
                 const/4 v0, 0x0
                 return v0
             """
-        )
+        ) ?: throw ShouldShowAdsFingerprint.exception
     }
 }
