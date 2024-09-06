90b004483971af8b671431e2ed84c0896def1857
PantlessCoding <pantless-coding@proton.me>
2024-09-06 14:35:33 -0400

    Better implementation

--- src/main/kotlin/app/revanced/patches/facebook/ads/mainfeed/HideSponsoredStoriesPatch.kt
+++ src/main/kotlin/app/revanced/patches/facebook/ads/mainfeed/HideSponsoredStoriesPatch.kt
@@ -3,6 +3,7 @@ package app.revanced.patches.facebook.ads.mainfeed
 import app.revanced.util.exception
 import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.extensions.InstructionExtensions.addInstruction
+import app.revanced.patcher.extensions.InstructionExtensions.addInstructionsWithLabels
 import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.annotation.CompatiblePackage
 import app.revanced.patcher.patch.annotation.Patch
@@ -20,26 +21,24 @@ object HideSponsoredStoriesPatch : BytecodePatch(setOf(GetStoryVisibilityFingerp
     override fun execute(context: BytecodeContext) {
         GetStoryVisibilityFingerprint.result?.apply {
 
-            // TODO: Resolve class and method by fingerprint
-            // Get the class name
+            // Get the sponsored data model getter
             val sponsoredDataModelGetter = GraphQLStorySponsoredDataGetterFingerprint.result ?: throw GraphQLStorySponsoredDataGetterFingerprint.exception;
 
             // Hide stories with sponsored data defined
             // Check if param type is GraphQLStory
-            // If so calling the sponsoredDataModelGetter, only sponsored content has non null data
-            // Sponsored Stories gets their visibility forced to StoryVisibility.GONE (== 3)
-            mutableMethod.addInstruction(0, """
+            // If so calling the sponsoredDataModelGetter, only sponsored content has non-null data
+            // Sponsored Stories gets their visibility forced to StoryVisibility.GONE
+            mutableMethod.addInstructionsWithLabels(scanResult.patternScanResult!!.startIndex, """
                     instance-of v0, p0, Lcom/facebook/graphql/model/GraphQLStory;
                     if-eqz v0, :resume_normal
                     invoke-virtual {p0}, Lcom/facebook/graphql/model/GraphQLStory;->${sponsoredDataModelGetter.method.name}()${sponsoredDataModelGetter.method.returnType}
                     move-result-object v0 
                     if-eqz v0, :resume_normal
-                    const/4 v0, 0x3
-                    invoke-static {v0}, Ljava/lang/Integer;->valueOf(I)Ljava/lang/Integer;
-                    move-result-object p0 
-                    return-object p0
-            """.trimIndent())
-
+                    const-string v0, "GONE"
+                    return-object v0
+                    :resume_normal
+                    nop
+            """)
         } ?: throw GetStoryVisibilityFingerprint.exception
     }
 }
