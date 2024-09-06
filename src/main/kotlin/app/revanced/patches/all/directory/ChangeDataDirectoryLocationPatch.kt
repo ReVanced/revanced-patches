fcdf8593ed7bd37031b259f850a3fe9504bd0166
oSumAtrIX <johan.melkonyan1@web.de>
2024-09-06 11:12:17 +0400

    fix some small styling issues/refactor

--- src/main/kotlin/app/revanced/patches/all/directory/ChangeDataDirectoryLocationPatch.kt
+++ src/main/kotlin/app/revanced/patches/all/directory/ChangeDataDirectoryLocationPatch.kt
@@ -5,70 +5,69 @@ import app.revanced.patcher.extensions.InstructionExtensions.replaceInstruction
 import app.revanced.patcher.patch.annotation.Patch
 import app.revanced.patcher.util.proxy.mutableTypes.MutableMethod
 import app.revanced.patches.all.misc.transformation.BaseTransformInstructionsPatch
+import app.revanced.util.getReference
 import com.android.tools.smali.dexlib2.iface.ClassDef
 import com.android.tools.smali.dexlib2.iface.Method
 import com.android.tools.smali.dexlib2.iface.instruction.Instruction
-import com.android.tools.smali.dexlib2.iface.instruction.OneRegisterInstruction
-import com.android.tools.smali.dexlib2.iface.instruction.ReferenceInstruction
-import com.android.tools.smali.dexlib2.iface.reference.MethodReference
 import com.android.tools.smali.dexlib2.iface.instruction.formats.Instruction35c
+import com.android.tools.smali.dexlib2.iface.reference.MethodReference
 import com.android.tools.smali.dexlib2.immutable.reference.ImmutableMethodReference
 import com.android.tools.smali.dexlib2.util.MethodUtil
 
 @Patch(
     name = "Change data directory location",
-    description = "Changes the data directory in the application from /data/user/0 to /sdcard/android/data for root-less devices. Using this patch can cause unexpected issues with some apps.",
+    description = "Changes the data directory in the application from " +
+        "the app internal storage directory to /sdcard/android/data accessible by root-less devices." +
+        "Using this patch can cause unexpected issues with some apps.",
     use = false,
 )
 @Suppress("unused")
 object ChangeDataDirectoryLocationPatch : BaseTransformInstructionsPatch<Int>() {
-
     override fun filterMap(
         classDef: ClassDef,
         method: Method,
         instruction: Instruction,
-        instructionIndex: Int
+        instructionIndex: Int,
     ): Int? {
-        if (instruction !is ReferenceInstruction) return null
+        val reference = instruction.getReference<MethodReference>() ?: return null
 
-        val reference = instruction.reference as? MethodReference ?: return null
-
-        val match = MethodCall.GetDir.reference.takeIf { 
-            MethodUtil.methodSignaturesMatch(reference, it) 
-        } ?: return null
+        if (!MethodUtil.methodSignaturesMatch(reference, MethodCall.GetDir.reference)) {
+            return null
+        }
 
         return instructionIndex
     }
 
     override fun transform(
         mutableMethod: MutableMethod,
-        entry: Int
+        entry: Int,
     ) = transformMethodCall(entry, mutableMethod)
 
     private fun transformMethodCall(
-    instructionIndex: Int,
-    mutableMethod: MutableMethod
+        instructionIndex: Int,
+        mutableMethod: MutableMethod,
     ) {
-        val contextRegister = mutableMethod.getInstruction<Instruction35c>(instructionIndex).registerC
-        val dataRegister = mutableMethod.getInstruction<Instruction35c>(instructionIndex).registerD
+        val getDirInstruction = mutableMethod.getInstruction<Instruction35c>(instructionIndex)
+        val contextRegister = getDirInstruction.registerC
+        val dataRegister = getDirInstruction.registerD
 
         mutableMethod.replaceInstruction(
             instructionIndex,
-            "invoke-virtual {v$contextRegister, v$dataRegister}, Landroid/content/Context;->getExternalFilesDir(Ljava/lang/String;)Ljava/io/File;"
+            "invoke-virtual { v$contextRegister, v$dataRegister }, " +
+                "Landroid/content/Context;->getExternalFilesDir(Ljava/lang/String;)Ljava/io/File;",
         )
     }
 
-
     private enum class MethodCall(
-        val reference: MethodReference
+        val reference: MethodReference,
     ) {
         GetDir(
             ImmutableMethodReference(
                 "Landroid/content/Context;",
                 "getDir",
                 listOf("Ljava/lang/String;", "I"),
-                "Ljava/io/File;"
-            )
-        )
+                "Ljava/io/File;",
+            ),
+        ),
     }
 }
