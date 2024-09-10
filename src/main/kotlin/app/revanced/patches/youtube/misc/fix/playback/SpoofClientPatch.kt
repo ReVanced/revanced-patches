67b2664827b360c3742c89e7dd3c6848239b72d5
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2024-09-10 00:52:01 -0400

    fix: Use helper method, simplify fetch request hook

--- src/main/kotlin/app/revanced/patches/youtube/misc/fix/playback/SpoofClientPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/fix/playback/SpoofClientPatch.kt
@@ -6,11 +6,12 @@ import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
 import app.revanced.patcher.extensions.InstructionExtensions.addInstructionsWithLabels
 import app.revanced.patcher.extensions.InstructionExtensions.getInstruction
 import app.revanced.patcher.extensions.InstructionExtensions.getInstructions
-import app.revanced.patcher.extensions.InstructionExtensions.replaceInstruction
+import app.revanced.patcher.extensions.or
 import app.revanced.patcher.patch.BytecodePatch
+import app.revanced.patcher.patch.PatchException
 import app.revanced.patcher.patch.annotation.CompatiblePackage
 import app.revanced.patcher.patch.annotation.Patch
-import app.revanced.patcher.util.smali.ExternalLabel
+import app.revanced.patcher.util.proxy.mutableTypes.MutableMethod.Companion.toMutable
 import app.revanced.patches.all.misc.resources.AddResourcesPatch
 import app.revanced.patches.shared.misc.settings.preference.ListPreference
 import app.revanced.patches.shared.misc.settings.preference.NonInteractivePreference
@@ -24,10 +25,18 @@ import app.revanced.patches.youtube.misc.fix.playback.fingerprints.CreateStreami
 import app.revanced.patches.youtube.misc.fix.playback.fingerprints.ProtobufClassParseByteBufferFingerprint
 import app.revanced.patches.youtube.misc.settings.SettingsPatch
 import app.revanced.util.getReference
+import app.revanced.util.indexOfFirstInstructionOrThrow
 import app.revanced.util.resultOrThrow
+import com.android.tools.smali.dexlib2.AccessFlags
+import com.android.tools.smali.dexlib2.Opcode
+import com.android.tools.smali.dexlib2.builder.MutableMethodImplementation
 import com.android.tools.smali.dexlib2.iface.instruction.FiveRegisterInstruction
 import com.android.tools.smali.dexlib2.iface.instruction.OneRegisterInstruction
+import com.android.tools.smali.dexlib2.iface.instruction.TwoRegisterInstruction
 import com.android.tools.smali.dexlib2.iface.reference.FieldReference
+import com.android.tools.smali.dexlib2.iface.reference.MethodReference
+import com.android.tools.smali.dexlib2.immutable.ImmutableMethod
+import com.android.tools.smali.dexlib2.immutable.ImmutableMethodParameter
 
 @Patch(
     name = "Spoof client",
@@ -78,14 +87,7 @@ object SpoofClientPatch : BytecodePatch(
         ProtobufClassParseByteBufferFingerprint
     )
 ) {
-    private const val INTEGRATIONS_CLASS_DESCRIPTOR =
-        "Lapp/revanced/integrations/youtube/patches/spoof/SpoofClientPatch;"
-    private const val REQUEST_CLASS_DESCRIPTOR =
-        "Lorg/chromium/net/UrlRequest;"
-    private const val REQUEST_BUILDER_CLASS_DESCRIPTOR =
-        "Lorg/chromium/net/UrlRequest\$Builder;"
-    private const val STREAMING_DATA_CLASS_DESCRIPTOR =
-        "Lcom/google/protos/youtube/api/innertube/StreamingDataOuterClass\$StreamingData;"
+    private const val INTEGRATIONS_CLASS_DESCRIPTOR = "Lapp/revanced/integrations/youtube/patches/spoof/SpoofClientPatch;"
 
     override fun execute(context: BytecodeContext) {
         AddResourcesPatch(this::class)
@@ -150,26 +152,21 @@ object SpoofClientPatch : BytecodePatch(
 
         // region Fetch replacement streams.
 
-        BuildRequestFingerprint.resultOrThrow().let { result ->
-            result.mutableMethod.apply {
-                val buildRequestIndex = getInstructions().lastIndex - 2
-                val requestBuilderRegister = getInstruction<FiveRegisterInstruction>(buildRequestIndex).registerC
-
-                val newRequestBuilderIndex = result.scanResult.patternScanResult!!.endIndex
-                val urlRegister = getInstruction<FiveRegisterInstruction>(newRequestBuilderIndex).registerD
-
-                // Replace "requestBuilder.build()" with integrations call.
-                replaceInstruction(
-                    buildRequestIndex,
-                    "invoke-static { v$requestBuilderRegister, v$urlRegister, v${urlRegister + 1} }, " +
-                            "$INTEGRATIONS_CLASS_DESCRIPTOR->" +
-                            "buildRequest(${REQUEST_BUILDER_CLASS_DESCRIPTOR}Ljava/lang/String;Ljava/util/Map;)" +
-                            REQUEST_CLASS_DESCRIPTOR
-                )
-                
-                // Copy request headers for streaming data fetch.
-                addInstruction(newRequestBuilderIndex + 2, "move-object v${urlRegister + 1}, p1")
+        BuildRequestFingerprint.resultOrThrow().mutableMethod.apply {
+            val newRequestBuilderIndex = indexOfFirstInstructionOrThrow {
+                opcode == Opcode.INVOKE_VIRTUAL
+                        && getReference<MethodReference>()?.name == "newUrlRequestBuilder"
             }
+            val urlRegister = getInstruction<FiveRegisterInstruction>(newRequestBuilderIndex).registerD
+            val freeRegister = getInstruction<OneRegisterInstruction>(newRequestBuilderIndex + 1).registerA
+
+            addInstructions(
+                newRequestBuilderIndex,
+                """
+                    move-object v$freeRegister, p1
+                    invoke-static { v$urlRegister, v$freeRegister }, $INTEGRATIONS_CLASS_DESCRIPTOR->fetchStreams(Ljava/lang/String;Ljava/util/Map;)V
+                """
+            )
         }
 
         // endregion
@@ -178,43 +175,70 @@ object SpoofClientPatch : BytecodePatch(
 
         CreateStreamingDataFingerprint.resultOrThrow().let { result ->
             result.mutableMethod.apply {
+                val setStreamDataMethodName = "patch_setStreamingData"
+                val resultMethodType = result.mutableClass.type
                 val videoDetailsIndex = result.scanResult.patternScanResult!!.endIndex
+                val videoDetailsRegister = getInstruction<TwoRegisterInstruction>(videoDetailsIndex).registerA
                 val videoDetailsClass = getInstruction(videoDetailsIndex).getReference<FieldReference>()!!.type
+
+                addInstruction(
+                    videoDetailsIndex + 1,
+                    "invoke-direct { p0, v$videoDetailsRegister }, $resultMethodType->$setStreamDataMethodName($videoDetailsClass)V"
+                )
+
+                val protobufClass = ProtobufClassParseByteBufferFingerprint.resultOrThrow().mutableMethod.definingClass
+
                 val setStreamingDataIndex = result.scanResult.patternScanResult!!.startIndex
                 val playerProtoClass = getInstruction(setStreamingDataIndex + 1)
                     .getReference<FieldReference>()!!.definingClass
-                val protobufClass = ProtobufClassParseByteBufferFingerprint.resultOrThrow().mutableMethod.definingClass
+                val setStreamingDataField = getInstruction(setStreamingDataIndex).getReference<FieldReference>()
+                val getStreamingDataField = getInstructions().find { instruction ->
+                    instruction.opcode == Opcode.IGET_OBJECT &&
+                            instruction.getReference<FieldReference>()?.definingClass == playerProtoClass
+                }?.getReference<FieldReference>() ?: throw PatchException("Could not find getStreamingDataField")
 
-                addInstructionsWithLabels(
-                    videoDetailsIndex + 1,
-                    """
-                        # Registers is free at this index.
-
-                        invoke-static { }, $INTEGRATIONS_CLASS_DESCRIPTOR->isSpoofingEnabled()Z
-                        move-result v1
-                        if-eqz v1, :disabled
-
-                        # Get video id.
-                        iget-object v1, v0, $videoDetailsClass->c:Ljava/lang/String;
-                        if-eqz v1, :disabled
-
-                        # Get streaming data.
-                        invoke-static { v1 }, $INTEGRATIONS_CLASS_DESCRIPTOR->getStreamingData(Ljava/lang/String;)Ljava/nio/ByteBuffer;
-                        move-result-object v1
-                        if-eqz v1, :disabled
-
-                        # Parse streaming data.
-                        sget-object v0, $playerProtoClass->a:$playerProtoClass
-                        invoke-static { v0, v1 }, $protobufClass->parseFrom(${protobufClass}Ljava/nio/ByteBuffer;)$protobufClass
-                        move-result-object v1
-                        check-cast v1, $playerProtoClass
-
-                        # Set streaming data.
-                        iget-object v0, v1, $playerProtoClass->h:$STREAMING_DATA_CLASS_DESCRIPTOR
-                        if-eqz v0, :disabled
-                        iput-object v0, p0, $definingClass->a:$STREAMING_DATA_CLASS_DESCRIPTOR
-                    """,
-                    ExternalLabel("disabled", getInstruction(videoDetailsIndex + 1))
+                // Use a helper method to avoid the need of picking out multiple free registers from the hooked code.
+                result.mutableClass.methods.add(
+                    ImmutableMethod(
+                        resultMethodType,
+                        setStreamDataMethodName,
+                        listOf(ImmutableMethodParameter(videoDetailsClass, null, "videoDetails")),
+                        "V",
+                        AccessFlags.PRIVATE or AccessFlags.FINAL,
+                        null,
+                        null,
+                        MutableMethodImplementation(9),
+                    ).toMutable().apply {
+                        addInstructionsWithLabels(0, """
+                            invoke-static { }, $INTEGRATIONS_CLASS_DESCRIPTOR->isSpoofingEnabled()Z
+                            move-result v0
+                            if-eqz v0, :disabled
+    
+                            # Get video id.
+                            iget-object v2, p1, $videoDetailsClass->c:Ljava/lang/String;
+                            if-eqz v2, :disabled
+    
+                            # Get streaming data.
+                            invoke-static { v2 }, $INTEGRATIONS_CLASS_DESCRIPTOR->getStreamingData(Ljava/lang/String;)Ljava/nio/ByteBuffer;
+                            move-result-object v3
+                            if-eqz v3, :disabled
+    
+                            # Parse streaming data.
+                            sget-object v4, $playerProtoClass->a:$playerProtoClass
+                            invoke-static { v4, v3 }, $protobufClass->parseFrom(${protobufClass}Ljava/nio/ByteBuffer;)$protobufClass
+                            move-result-object v5
+                            check-cast v5, $playerProtoClass
+    
+                            # Set streaming data.
+                            iget-object v6, v5, $getStreamingDataField
+                            if-eqz v6, :disabled
+                            iput-object v6, p0, $setStreamingDataField
+                            
+                            :disabled
+                            return-void
+                        """
+                        )
+                    }
                 )
             }
         }
@@ -228,12 +252,15 @@ object SpoofClientPatch : BytecodePatch(
             it.mutableMethod.apply {
                 val targetIndex = getInstructions().lastIndex
 
+                // Instructions are added just before the method returns,
+                // so there's no concern of clobbering in-use registers.
                 addInstructions(
                     targetIndex,
                     """
                         # Field a: Stream uri.
                         # Field c: Http method.
                         # Field d: Post data.
+                        move-object v0, p0  # method has over 15 registers and must copy p0 to a lower register.
                         iget-object v1, v0, $definingClass->a:Landroid/net/Uri;
                         iget v2, v0, $definingClass->c:I
                         iget-object v3, v0, $definingClass->d:[B
