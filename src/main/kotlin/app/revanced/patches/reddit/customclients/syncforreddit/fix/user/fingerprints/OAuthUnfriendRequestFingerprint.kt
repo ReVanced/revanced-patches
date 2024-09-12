46d11f3530fcdae9ed08b7e93aac235638a92dff
oSumAtrIX <johan.melkonyan1@web.de>
2024-09-12 20:18:22 +0200

    feat(Sync for Reddit): Add `Fix /user/ endpoint` patch

---
+++ src/main/kotlin/app/revanced/patches/reddit/customclients/syncforreddit/fix/user/fingerprints/OAuthUnfriendRequestFingerprint.kt
@@ -0,0 +1,3 @@
+package app.revanced.patches.reddit.customclients.syncforreddit.fix.user.fingerprints
+
+internal object OAuthUnfriendRequestFingerprint : BaseUserEndpointFingerprint("OAuthUnfriendRequest.java")
---
+++ src/main/kotlin/app/revanced/patches/reddit/customclients/syncforreddit/fix/user/fingerprints/OAuthUserIdRequestFingerprint.kt
@@ -0,0 +1,3 @@
+package app.revanced.patches.reddit.customclients.syncforreddit.fix.user.fingerprints
+
+internal object OAuthUserIdRequestFingerprint : BaseUserEndpointFingerprint("OAuthUserIdRequest.java")
