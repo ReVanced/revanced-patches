44270af390c9a6c9ec4e1c90c845de501287cdf4
zainarbani <zaintsyariev@gmail.com>
2024-09-13 15:51:13 +0700

    feat: Unlock sleep timer menu

---
+++ src/main/kotlin/app/revanced/patches/youtube/layout/buttons/player/flyout/fingerprints/SleepTimerFeatureFingerprint.kt
@@ -0,0 +1,7 @@
+package app.revanced.patches.youtube.layout.buttons.player.flyout.fingerprints
+
+import app.revanced.util.patch.LiteralValueFingerprint
+
+internal object SleepTimerFeatureFingerprint : LiteralValueFingerprint(
+    literalSupplier = { 45630421 }
+)
