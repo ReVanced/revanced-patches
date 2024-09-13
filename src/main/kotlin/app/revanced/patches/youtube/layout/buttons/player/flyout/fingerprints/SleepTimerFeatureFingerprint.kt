e0ff21be4b86ce391260e8cb3e11e2238dd07164
zainarbani <zaintsyariev@gmail.com>
2024-09-13 16:28:22 +0700

    Revert "feat: Unlock sleep timer menu"
    This reverts commit 44270af390c9a6c9ec4e1c90c845de501287cdf4.

--- src/main/kotlin/app/revanced/patches/youtube/layout/buttons/player/flyout/fingerprints/SleepTimerFeatureFingerprint.kt
+++
@@ -1,7 +0,0 @@
-package app.revanced.patches.youtube.layout.buttons.player.flyout.fingerprints
-
-import app.revanced.util.patch.LiteralValueFingerprint
-
-internal object SleepTimerFeatureFingerprint : LiteralValueFingerprint(
-    literalSupplier = { 45630421 }
-)
