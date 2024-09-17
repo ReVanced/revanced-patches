eec88407f66d5196cf112efaa392d4b2bbf6c131
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2024-09-17 16:33:15 -0400

    refactor: Rename to `Spoof video streams`

--- src/main/kotlin/app/revanced/patches/youtube/misc/fix/playback/SpoofSignaturePatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/fix/playback/SpoofSignaturePatch.kt
@@ -3,9 +3,9 @@ package app.revanced.patches.youtube.misc.fix.playback
 import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.patch.BytecodePatch
 
-@Deprecated("This patch is obsolete.", replaceWith = ReplaceWith("SpoofClientPatch"))
+@Deprecated("This patch is obsolete.", replaceWith = ReplaceWith("SpoofVideoStreamsPatch"))
 object SpoofSignaturePatch : BytecodePatch(
-    dependencies = setOf(SpoofClientPatch::class),
+    dependencies = setOf(SpoofVideoStreamsPatch::class),
 ) {
     override fun execute(context: BytecodeContext) {}
 }
