8ba8642fb75b4e405a447067e1168f85de2f62af
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2024-09-21 12:58:44 -0400

    fix: Do not use Cairo settings as too much stuff is broken.
    Code adapted from: https://github.com/inotia00/revanced-patches/blob/revanced-extended/src/main/kotlin/app/revanced/patches/youtube/utils/fix/cairo/CairoSettingsPatch.kt

---
+++ src/main/kotlin/app/revanced/patches/youtube/misc/fix/cairo/CairoSettingsPatch.kt
@@ -0,0 +1,49 @@
+package app.revanced.patches.youtube.misc.fix.cairo
+
+import app.revanced.patcher.data.BytecodeContext
+import app.revanced.patcher.extensions.InstructionExtensions.addInstruction
+import app.revanced.patcher.extensions.InstructionExtensions.getInstruction
+import app.revanced.patcher.patch.BytecodePatch
+import app.revanced.patcher.patch.annotation.Patch
+import app.revanced.patches.youtube.misc.backgroundplayback.BackgroundPlaybackPatch
+import app.revanced.patches.youtube.misc.fix.cairo.fingerprints.CarioFragmentConfigFingerprint
+import app.revanced.util.indexOfFirstInstructionOrThrow
+import app.revanced.util.indexOfFirstWideLiteralInstructionValueOrThrow
+import app.revanced.util.resultOrThrow
+import com.android.tools.smali.dexlib2.Opcode
+import com.android.tools.smali.dexlib2.iface.instruction.OneRegisterInstruction
+
+@Patch(
+    description = "Prevents Cairo Fragment from being used."
+)
+internal object CairoSettingsPatch : BytecodePatch(
+    setOf(CarioFragmentConfigFingerprint)
+) {
+    override fun execute(context: BytecodeContext) {
+
+        /**
+         * <pre>
+         * Cairo Fragment was added since YouTube v19.04.38.
+         *
+         * Disable this for the following reasons:
+         * 1. [BackgroundPlaybackPatch] does not activate the Minimized playback setting of Cairo Fragment.
+         * 2. Some patches do not yet support Cairo Fragments (ie: custom Seekbar color).
+         * 3. Settings preferences added by ReVanced are missing.
+         *
+         * Screenshots of the Cairo Fragment:
+         * <a href="https://github.com/qnblackcat/uYouPlus/issues/1468">uYouPlus#1468</a>.
+         */
+        CarioFragmentConfigFingerprint.resultOrThrow().mutableMethod.apply {
+            val literalIndex = indexOfFirstWideLiteralInstructionValueOrThrow(
+                CarioFragmentConfigFingerprint.CAIRO_CONFIG_LITERAL_VALUE
+            )
+            val targetIndex = indexOfFirstInstructionOrThrow(literalIndex, Opcode.MOVE_RESULT)
+            val targetRegister = getInstruction<OneRegisterInstruction>(targetIndex).registerA
+
+            addInstruction(
+                targetIndex + 1,
+                "const/16 v$targetRegister, 0x0"
+            )
+        }
+    }
+}
