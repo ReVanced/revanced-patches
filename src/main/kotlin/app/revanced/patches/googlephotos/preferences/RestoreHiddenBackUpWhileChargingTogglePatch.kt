4063a07b611f2b1c377ecf94d10bdd98876c8d4a
xob0t <5348245@gmail.com>
2024-09-23 16:11:37 +0300

    requested fixes

--- src/main/kotlin/app/revanced/patches/googlephotos/preferences/RestoreHiddenBackUpWhileChargingTogglePatch.kt
+++ src/main/kotlin/app/revanced/patches/googlephotos/preferences/RestoreHiddenBackUpWhileChargingTogglePatch.kt
@@ -1,6 +1,5 @@
 package app.revanced.patches.googlephotos.preferences
 
-import app.revanced.util.exception
 import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.extensions.InstructionExtensions.addInstruction
 import app.revanced.patcher.extensions.InstructionExtensions.getInstruction
@@ -11,21 +10,22 @@ import app.revanced.patches.googlephotos.preferences.fingerprints.BackupPreferen
 import com.android.tools.smali.dexlib2.iface.instruction.OneRegisterInstruction
 
 @Patch(
-    name = "Show hidden 'Back up while charging' toggle",
-    description = "This toggle is hidden by default, when enabled, backups run only when the device is charging",
-    compatiblePackages = [CompatiblePackage("com.google.android.apps.photos")]
+    name = "Restore hidden 'Back up while charging' toggle",
+    description = "Restores a hidden toggle to only run backups when the device is charging.",
+    compatiblePackages = [CompatiblePackage("com.google.android.apps.photos")],
 )
 @Suppress("unused")
-object BackupPreferencesFingerprint : BytecodePatch(
-    setOf(BackupPreferencesFingerprint)
+object RestoreHiddenBackUpWhileChargingTogglePatch : BytecodePatch(
+    setOf(BackupPreferencesFingerprint),
 ) {
     override fun execute(context: BytecodeContext) {
-        // patches 'backup_prefs_had_backup_only_when_charging_enabled' to always be true
+        // Patches 'backup_prefs_had_backup_only_when_charging_enabled' to always be true.
         BackupPreferencesFingerprint.result?.let {
-            val chargingPrefStringIndex = it.scanResult.stringsScanResult!!.matches.first().index    
+            val chargingPrefStringIndex = it.scanResult.stringsScanResult!!.matches.first().index
             it.mutableMethod.apply {
-                val resultRegister = getInstruction<OneRegisterInstruction>(chargingPrefStringIndex+2).registerA
-                // override result as true
+                // Get the register of move-result.
+                val resultRegister = getInstruction<OneRegisterInstruction>(chargingPrefStringIndex + 2).registerA
+                // Insert const after move-result to override register as true.
                 addInstruction(chargingPrefStringIndex + 3, "const/4 v$resultRegister, 0x1")
             }
         } ?: throw Exception("BackupPreferencesFingerprint result not found")
--- src/main/kotlin/app/revanced/patches/googlephotos/preferences/fingerprints/BackupPreferencesFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/googlephotos/preferences/fingerprints/BackupPreferencesFingerprint.kt
@@ -5,6 +5,6 @@ import app.revanced.patcher.fingerprint.MethodFingerprint
 internal object BackupPreferencesFingerprint : MethodFingerprint(
     returnType = "Lcom/google/android/apps/photos/backup/data/BackupPreferences;",
     strings = listOf(
-                "backup_prefs_had_backup_only_when_charging_enabled",
-                ),
+        "backup_prefs_had_backup_only_when_charging_enabled",
+    ),
 )
