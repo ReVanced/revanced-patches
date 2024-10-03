b654370ec80f76d56137591c76bf2bb8b36362f2
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2024-10-03 15:34:57 -0400

    refactor

--- src/main/kotlin/app/revanced/patches/youtube/misc/playservice/VersionCheckPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/playservice/VersionCheckPatch.kt
@@ -1,12 +1,10 @@
 package app.revanced.patches.youtube.misc.playservice
 
 import app.revanced.patcher.data.ResourceContext
-import app.revanced.patcher.patch.PatchException
 import app.revanced.patcher.patch.ResourcePatch
 import app.revanced.patcher.patch.annotation.Patch
 import app.revanced.patches.shared.misc.mapping.ResourceMappingPatch
-import org.w3c.dom.Element
-import org.w3c.dom.Node
+import app.revanced.util.findElementByAttributeValueOrThrow
 import kotlin.properties.Delegates
 
 /**
@@ -20,19 +18,15 @@ internal object VersionCheckPatch : ResourcePatch() {
 
     var is_19_03_or_greater by Delegates.notNull<Boolean>()
     var is_19_04_or_greater by Delegates.notNull<Boolean>()
-    var is_19_15_or_greater by Delegates.notNull<Boolean>()
     var is_19_16_or_greater by Delegates.notNull<Boolean>()
     var is_19_17_or_greater by Delegates.notNull<Boolean>()
     var is_19_18_or_greater by Delegates.notNull<Boolean>()
-    var is_19_19_or_greater by Delegates.notNull<Boolean>()
     var is_19_23_or_greater by Delegates.notNull<Boolean>()
-    var is_19_24_or_greater by Delegates.notNull<Boolean>()
     var is_19_25_or_greater by Delegates.notNull<Boolean>()
     var is_19_26_or_greater by Delegates.notNull<Boolean>()
     var is_19_29_or_greater by Delegates.notNull<Boolean>()
     var is_19_32_or_greater by Delegates.notNull<Boolean>()
     var is_19_33_or_greater by Delegates.notNull<Boolean>()
-    var is_19_35_or_greater by Delegates.notNull<Boolean>()
     var is_19_36_or_greater by Delegates.notNull<Boolean>()
 
     override fun execute(context: ResourceContext) {
@@ -45,38 +39,26 @@ internal object VersionCheckPatch : ResourcePatch() {
             // The app version is missing from the decompiled manifest,
             // so instead use the Google Play services version and compare against specific releases.
             context.document["res/values/integers.xml"].use { document ->
-                val nodeList = document.documentElement.childNodes
-                for (i in 0 until nodeList.length) {
-                    val node = nodeList.item(i)
-                    if (node.nodeType == Node.ELEMENT_NODE) {
-                        val element = node as Element
-                        if (element.getAttribute("name") == "google_play_services_version") {
-                            return element.textContent.toInt()
-                        }
-                    }
-                }
+                return document.documentElement.childNodes.findElementByAttributeValueOrThrow(
+                    "name",
+                    "google_play_services_version"
+                ).textContent.toInt()
             }
-
-            throw PatchException("integers.xml does not contain a Google Play services version")
         }
 
         playStoreServicesVersion = getPlayServicesVersion(context)
 
         is_19_03_or_greater = 240402000 <= playStoreServicesVersion
         is_19_04_or_greater = 240502000 <= playStoreServicesVersion
-        is_19_15_or_greater = 241602000 <= playStoreServicesVersion
         is_19_16_or_greater = 241702000 <= playStoreServicesVersion
         is_19_17_or_greater = 241802000 <= playStoreServicesVersion
         is_19_18_or_greater = 241902000 <= playStoreServicesVersion
-        is_19_19_or_greater = 241999000 <= playStoreServicesVersion
         is_19_23_or_greater = 242402000 <= playStoreServicesVersion
-        is_19_24_or_greater = 242505000 <= playStoreServicesVersion
         is_19_25_or_greater = 242599000 <= playStoreServicesVersion
         is_19_26_or_greater = 242705000 <= playStoreServicesVersion
         is_19_29_or_greater = 243005000 <= playStoreServicesVersion
         is_19_32_or_greater = 243199000 <= playStoreServicesVersion
         is_19_33_or_greater = 243405000 <= playStoreServicesVersion
-        is_19_35_or_greater = 243605000 <= playStoreServicesVersion
         is_19_36_or_greater = 243705000 <= playStoreServicesVersion
     }
 }
