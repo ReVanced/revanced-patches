3efe59dd911fb67bc4f3cd18d09aeb5e0154a7aa
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2024-10-03 01:56:40 -0400

    fix: Remove obsolete fingerprints removed in 19.40

--- src/main/kotlin/app/revanced/patches/youtube/misc/integrations/IntegrationsPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/integrations/IntegrationsPatch.kt
@@ -3,14 +3,10 @@ package app.revanced.patches.youtube.misc.integrations
 import app.revanced.patcher.patch.annotation.Patch
 import app.revanced.patches.shared.misc.integrations.BaseIntegrationsPatch
 import app.revanced.patches.youtube.misc.integrations.fingerprints.ApplicationInitFingerprint
-import app.revanced.patches.youtube.misc.integrations.fingerprints.RemoteEmbedFragmentFingerprint
-import app.revanced.patches.youtube.misc.integrations.fingerprints.RemoteEmbeddedPlayerFingerprint
 
 @Patch(requiresIntegrations = true)
 object IntegrationsPatch : BaseIntegrationsPatch(
     setOf(
         ApplicationInitFingerprint,
-        RemoteEmbeddedPlayerFingerprint,
-        RemoteEmbedFragmentFingerprint,
     ),
 )
