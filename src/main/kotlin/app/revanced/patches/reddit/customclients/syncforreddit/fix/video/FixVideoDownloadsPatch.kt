84a3f4f8991433b8a0bcef3dbcca7921b244ed44
1fexd <PGFvy85nC@protonmail.com>
2024-10-08 07:33:17 +0200

    Dynamically get registers

--- src/main/kotlin/app/revanced/patches/reddit/customclients/syncforreddit/fix/video/FixVideoDownloadsPatch.kt
+++ src/main/kotlin/app/revanced/patches/reddit/customclients/syncforreddit/fix/video/FixVideoDownloadsPatch.kt
@@ -2,16 +2,13 @@ package app.revanced.patches.reddit.customclients.syncforreddit.fix.video
 
 import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
-import app.revanced.patcher.extensions.InstructionExtensions.removeInstructions
+import app.revanced.patcher.extensions.InstructionExtensions.getInstruction
 import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.annotation.CompatiblePackage
 import app.revanced.patcher.patch.annotation.Patch
 import app.revanced.patches.reddit.customclients.syncforreddit.fix.video.fingerprints.ParseRedditVideoNetworkResponseFingerprint
-import app.revanced.util.getReference
-import app.revanced.util.indexOfFirstInstructionOrThrow
 import app.revanced.util.resultOrThrow
-import com.android.tools.smali.dexlib2.Opcode
-import com.android.tools.smali.dexlib2.iface.reference.TypeReference
+import com.android.tools.smali.dexlib2.iface.instruction.formats.Instruction35c
 
 @Patch(
     name = "Fix video downloads",
@@ -33,39 +30,32 @@ object FixVideoDownloadsPatch : BytecodePatch(
     private const val GET_LINKS_METHOD = "getLinks([B)[Ljava/lang/String;"
 
     override fun execute(context: BytecodeContext) {
-        ParseRedditVideoNetworkResponseFingerprint.resultOrThrow().mutableMethod.apply {
+        val fingerprint = ParseRedditVideoNetworkResponseFingerprint.resultOrThrow()
+        val scanResult = fingerprint.scanResult.patternScanResult!!
+        val newInstanceIndex = scanResult.startIndex
+        val invokeDirectIndex = scanResult.endIndex - 1
 
-            // Find the instruction which creates the result data class.
-            val newInstanceIndex = indexOfFirstInstructionOrThrow {
-                opcode == Opcode.NEW_INSTANCE && getReference<TypeReference>().toString() == "Lo8/h;"
-            }
+        fingerprint.mutableMethod.apply {
+            val responseBuilderInstruction = getInstruction<Instruction35c>(invokeDirectIndex)
 
-            // Remove all instructions up to that point, because they are not needed anymore.
-            removeInstructions(0, newInstanceIndex)
-
-            // new-instance instruction from above is now at index 0, add the patch directly after it.
-            addInstructions(
-                1,
-                """
-                    # Get byte array from response.
-                    iget-object v2, p1, Lcom/android/volley/NetworkResponse;->data:[B
+            addInstructions(newInstanceIndex + 1, """
+                # Get byte array from response.
+                iget-object v2, p1, Lcom/android/volley/NetworkResponse;->data:[B
                     
-                    # Parse the videoUrl and audioUrl from the byte array.
-                    invoke-static { v2 }, $INTEGRATIONS_CLASS_DESCRIPTOR->$GET_LINKS_METHOD
-                    move-result-object v2
+                # Parse the videoUrl and audioUrl from the byte array.
+                invoke-static { v2 }, $INTEGRATIONS_CLASS_DESCRIPTOR->$GET_LINKS_METHOD
+                move-result-object v2
         
-                    # Get videoUrl (Index 0) and move it into v3.
-                    const/4 v5, 0x0
-                    aget-object v3, v2, v5
+                # Get videoUrl (Index 0) and move it into register (v3).
+                const/4 v5, 0x0
+                aget-object v${responseBuilderInstruction.registerE}, v2, v5
         
-                    # Get audioUrl (Index 1) and move it into v4.
-                    const/4 v6, 0x1
-                    aget-object v4, v2, v6
-                    
-                    # v3 and v4 is used to build the response after this.
-                """
-            )
+                # Get audioUrl (Index 1) and move it into register (v4).
+                const/4 v6, 0x1
+                aget-object v${responseBuilderInstruction.registerF}, v2, v6
+
+                # v3 and v4 are used to build the response after this.
+            """)
         }
-
     }
 }
