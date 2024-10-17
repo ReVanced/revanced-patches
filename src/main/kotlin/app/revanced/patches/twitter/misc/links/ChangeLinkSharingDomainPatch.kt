3088bffefb0640fe2aa19ac927a11c80736a136f
oSumAtrIX <johan.melkonyan1@web.de>
2024-10-17 17:22:36 +0200

    slight refactor

--- src/main/kotlin/app/revanced/patches/twitter/misc/links/ChangeLinkSharingDomainPatch.kt
+++ src/main/kotlin/app/revanced/patches/twitter/misc/links/ChangeLinkSharingDomainPatch.kt
@@ -14,16 +14,14 @@ import app.revanced.patches.twitter.misc.links.fingerprints.LinkSharingDomainFin
 import app.revanced.util.exception
 import app.revanced.util.getReference
 import app.revanced.util.indexOfFirstInstructionOrThrow
-import com.android.tools.smali.dexlib2.Opcode
-import com.android.tools.smali.dexlib2.builder.instruction.BuilderInstruction35c
 import com.android.tools.smali.dexlib2.iface.instruction.OneRegisterInstruction
+import com.android.tools.smali.dexlib2.iface.instruction.formats.Instruction35c
 import com.android.tools.smali.dexlib2.iface.reference.StringReference
-
 
 @Patch(
     name = "Change link sharing domain",
     description = "Replaces the domain name of Twitter links when sharing them.",
-    compatiblePackages = [CompatiblePackage("com.twitter.android")]
+    compatiblePackages = [CompatiblePackage("com.twitter.android")],
 )
 @Suppress("unused")
 object ChangeLinkSharingDomainPatch : BytecodePatch(
@@ -31,34 +29,33 @@ object ChangeLinkSharingDomainPatch : BytecodePatch(
         LinkBuilderFingerprint,
         LinkResourceGetterFingerprint,
         LinkSharingDomainFingerprint,
-    )
+    ),
 ) {
     private var domainName by stringPatchOption(
         key = "domainName",
         default = "fxtwitter.com",
         title = "Domain name",
-        description = "The domain to use when sharing links.",
-        required = true
+        description = "The domain name to use when sharing links.",
+        required = true,
     )
 
     // This method is used to build the link that is shared when the "Share via..." button is pressed.
     private const val FORMAT_METHOD_RESOURCE_REFERENCE =
         "Lapp/revanced/integrations/twitter/patches/links/ChangeLinkSharingDomainPatch;->" +
-                "formatResourceLink([Ljava/lang/Object;)Ljava/lang/String;"
+            "formatResourceLink([Ljava/lang/Object;)Ljava/lang/String;"
 
     // This method is used to build the link that is shared when the "Copy link" button is pressed.
     private const val FORMAT_METHOD_REFERENCE =
         "Lapp/revanced/integrations/twitter/patches/links/ChangeLinkSharingDomainPatch;->" +
-                "formatLink(JLjava/lang/String;)Ljava/lang/String;"
+            "formatLink(JLjava/lang/String;)Ljava/lang/String;"
 
     override fun execute(context: BytecodeContext) {
-
         LinkSharingDomainFingerprint.result?.let {
             val replacementIndex = it.scanResult.stringsScanResult!!.matches.first().index
             val domainRegister = it.mutableMethod.getInstruction<OneRegisterInstruction>(replacementIndex).registerA
             it.mutableMethod.replaceInstruction(
                 replacementIndex,
-                "const-string v$domainRegister, \"https://$domainName\""
+                "const-string v$domainRegister, \"https://$domainName\"",
             )
         } ?: throw LinkSharingDomainFingerprint.exception
 
@@ -71,7 +68,7 @@ object ChangeLinkSharingDomainPatch : BytecodePatch(
                         invoke-static { p0, p1, p2 }, $FORMAT_METHOD_REFERENCE
                         move-result-object p0
                         return-object p0
-                    """
+                    """,
                 )
             }
         } ?: throw LinkBuilderFingerprint.exception
@@ -79,21 +76,17 @@ object ChangeLinkSharingDomainPatch : BytecodePatch(
         // Used in the Share via... dialog.
         LinkResourceGetterFingerprint.result?.mutableMethod?.apply {
             val constWithParameterName = indexOfFirstInstructionOrThrow {
-                val reference = getReference<StringReference>()
-                opcode == Opcode.CONST_STRING &&
-                        (reference?.string?.contains("id.toString()") == true)
+                getReference<StringReference>()?.string?.contains("id.toString()") == true
             }
 
-            // Format the link with the new domain name call address. (2 instructions above the const-string)
+            // Format the link with the new domain name register (2 instructions above the const-string).
             val formatLinkCallIndex = constWithParameterName - 2
-
-            val formatLinkCall = getInstruction<BuilderInstruction35c>(formatLinkCallIndex)
-            if (formatLinkCall.registerCount != 3) throw LinkResourceGetterFingerprint.exception
+            val formatLinkCall = getInstruction<Instruction35c>(formatLinkCallIndex)
 
             // Replace the original method call with the new method call.
             replaceInstruction(
                 formatLinkCallIndex,
-                "invoke-static { v${formatLinkCall.registerE} }, $FORMAT_METHOD_RESOURCE_REFERENCE"
+                "invoke-static { v${formatLinkCall.registerE} }, $FORMAT_METHOD_RESOURCE_REFERENCE",
             )
         } ?: throw LinkResourceGetterFingerprint.exception
     }
