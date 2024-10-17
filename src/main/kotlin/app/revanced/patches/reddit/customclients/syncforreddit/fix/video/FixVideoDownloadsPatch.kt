15a338ba5ec34dc486d924130bba3c3324683d3d
oSumAtrIX <johan.melkonyan1@web.de>
2024-10-17 17:27:53 +0200

    slight refactor

--- src/main/kotlin/app/revanced/patches/reddit/customclients/syncforreddit/fix/video/FixVideoDownloadsPatch.kt
+++ src/main/kotlin/app/revanced/patches/reddit/customclients/syncforreddit/fix/video/FixVideoDownloadsPatch.kt
@@ -18,7 +18,7 @@ import com.android.tools.smali.dexlib2.iface.instruction.formats.Instruction35c
         CompatiblePackage("com.laurencedawson.reddit_sync.pro"),
         CompatiblePackage("com.laurencedawson.reddit_sync.dev"),
     ],
-    requiresIntegrations = true
+    requiresIntegrations = true,
 )
 @Suppress("unused")
 object FixVideoDownloadsPatch : BytecodePatch(
@@ -29,32 +29,34 @@ object FixVideoDownloadsPatch : BytecodePatch(
     private const val GET_LINKS_METHOD = "getLinks([B)[Ljava/lang/String;"
 
     override fun execute(context: BytecodeContext) {
-        val fingerprint = ParseRedditVideoNetworkResponseFingerprint.resultOrThrow()
-        val scanResult = fingerprint.scanResult.patternScanResult!!
-        val newInstanceIndex = scanResult.startIndex
-        val invokeDirectIndex = scanResult.endIndex - 1
+        ParseRedditVideoNetworkResponseFingerprint.resultOrThrow().let {
+            val scanResult = it.scanResult.patternScanResult!!
+            val newInstanceIndex = scanResult.startIndex
+            val invokeDirectIndex = scanResult.endIndex - 1
 
-        fingerprint.mutableMethod.apply {
-            val responseBuilderInstruction = getInstruction<Instruction35c>(invokeDirectIndex)
+            val buildResponseInstruction = it.mutableMethod.getInstruction<Instruction35c>(invokeDirectIndex)
 
-            addInstructions(newInstanceIndex + 1, """
-                # Get byte array from response.
-                iget-object v2, p1, Lcom/android/volley/NetworkResponse;->data:[B
-                    
-                # Parse the videoUrl and audioUrl from the byte array.
-                invoke-static { v2 }, $INTEGRATIONS_CLASS_DESCRIPTOR->$GET_LINKS_METHOD
-                move-result-object v2
-        
-                # Get videoUrl (Index 0) and move it into register (v3).
-                const/4 v5, 0x0
-                aget-object v${responseBuilderInstruction.registerE}, v2, v5
-        
-                # Get audioUrl (Index 1) and move it into register (v4).
-                const/4 v6, 0x1
-                aget-object v${responseBuilderInstruction.registerF}, v2, v6
-
-                # v3 and v4 are used to build the response after this.
-            """)
+            it.mutableMethod.addInstructions(
+                newInstanceIndex + 1,
+                """
+                    # Get byte array from response.
+                    iget-object v2, p1, Lcom/android/volley/NetworkResponse;->data:[B
+                        
+                    # Parse the videoUrl and audioUrl from the byte array.
+                    invoke-static { v2 }, $INTEGRATIONS_CLASS_DESCRIPTOR->$GET_LINKS_METHOD
+                    move-result-object v2
+            
+                    # Get videoUrl (Index 0).
+                    const/4 v5, 0x0
+                    aget-object v${buildResponseInstruction.registerE}, v2, v5
+            
+                    # Get audioUrl (Index 1).
+                    const/4 v6, 0x1
+                    aget-object v${buildResponseInstruction.registerF}, v2, v6
+    
+                    # Register E and F are used to build the response.
+                """,
+            )
         }
     }
 }
