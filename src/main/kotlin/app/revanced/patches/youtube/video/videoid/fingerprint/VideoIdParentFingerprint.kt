d1b23acab06b86b2d4d3d2779171ee7e26d2727e
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2024-10-19 04:00:55 -0400

    fix video id hook with 19.41+

--- src/main/kotlin/app/revanced/patches/youtube/video/videoid/fingerprint/VideoIdParentFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/youtube/video/videoid/fingerprint/VideoIdParentFingerprint.kt
@@ -1,9 +1,12 @@
 package app.revanced.patches.youtube.video.videoid.fingerprint
 
-import app.revanced.patcher.fingerprint.MethodFingerprint
+import app.revanced.patcher.extensions.or
+import app.revanced.util.patch.LiteralValueFingerprint
+import com.android.tools.smali.dexlib2.AccessFlags
 
-internal object VideoIdParentFingerprint : MethodFingerprint(
-    returnType = "V",
-    parameters = listOf("Ljava/lang/Object;", "Ljava/lang/Exception;"),
-    strings = listOf("error retrieving subtitle"),
+internal object VideoIdParentFingerprint : LiteralValueFingerprint(
+    accessFlags = AccessFlags.PUBLIC or AccessFlags.FINAL,
+    returnType = "[L",
+    parameters = listOf("L"),
+    literalSupplier = { 524288L }
 )
