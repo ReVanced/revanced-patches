132d925d5c38c7d3ac5ad83899e6829675c7c504
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2024-10-21 02:53:09 -0400

    feat(YouTube): Add `Shorts autoplay` patch (#3794)

--- src/main/kotlin/app/revanced/patches/youtube/layout/hide/shorts/HideShortsComponentsResourcePatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/hide/shorts/HideShortsComponentsResourcePatch.kt
@@ -5,6 +5,8 @@ import app.revanced.patcher.patch.ResourcePatch
 import app.revanced.patcher.patch.annotation.Patch
 import app.revanced.patches.all.misc.resources.AddResourcesPatch
 import app.revanced.patches.shared.misc.mapping.ResourceMappingPatch
+import app.revanced.patches.shared.misc.settings.preference.PreferenceScreen
+import app.revanced.patches.shared.misc.settings.preference.PreferenceScreen.Sorting
 import app.revanced.patches.shared.misc.settings.preference.SwitchPreference
 import app.revanced.patches.youtube.layout.hide.shorts.HideShortsComponentsPatch.hideShortsAppShortcut
 import app.revanced.patches.youtube.layout.hide.shorts.HideShortsComponentsPatch.hideShortsWidget
@@ -34,40 +36,49 @@ object HideShortsComponentsResourcePatch : ResourcePatch() {
             SwitchPreference("revanced_hide_shorts_subscriptions"),
             SwitchPreference("revanced_hide_shorts_search"),
 
-            // Shorts player components.
-            // Ideally each group should be ordered similar to how they appear in the UI
-            // since this Setting menu currently uses the ordering used here.
+            PreferenceScreen(
+                key = "revanced_shorts_player_screen",
+                sorting = Sorting.UNSORTED,
+                preferences = setOf(
+                    // Shorts player components.
+                    // Ideally each group should be ordered similar to how they appear in the UI
 
-            // Vertical row of buttons on right side of the screen.
-            SwitchPreference("revanced_hide_shorts_like_fountain"),
-            SwitchPreference("revanced_hide_shorts_like_button"),
-            SwitchPreference("revanced_hide_shorts_dislike_button"),
-            SwitchPreference("revanced_hide_shorts_comments_button"),
-            SwitchPreference("revanced_hide_shorts_share_button"),
-            SwitchPreference("revanced_hide_shorts_remix_button"),
-            SwitchPreference("revanced_hide_shorts_sound_button"),
+                    // Vertical row of buttons on right side of the screen.
+                    SwitchPreference("revanced_hide_shorts_like_fountain"),
+                    SwitchPreference("revanced_hide_shorts_like_button"),
+                    SwitchPreference("revanced_hide_shorts_dislike_button"),
+                    SwitchPreference("revanced_hide_shorts_comments_button"),
+                    SwitchPreference("revanced_hide_shorts_share_button"),
+                    SwitchPreference("revanced_hide_shorts_remix_button"),
+                    SwitchPreference("revanced_hide_shorts_sound_button"),
 
-            // Everything else.
-            SwitchPreference("revanced_hide_shorts_join_button"),
-            SwitchPreference("revanced_hide_shorts_subscribe_button"),
-            SwitchPreference("revanced_hide_shorts_paused_overlay_buttons"),
-            SwitchPreference("revanced_hide_shorts_save_sound_button"),
-            SwitchPreference("revanced_hide_shorts_use_template_button"),
-            SwitchPreference("revanced_hide_shorts_upcoming_button"),
-            SwitchPreference("revanced_hide_shorts_green_screen_button"),
-            SwitchPreference("revanced_hide_shorts_hashtag_button"),
-            SwitchPreference("revanced_hide_shorts_shop_button"),
-            SwitchPreference("revanced_hide_shorts_tagged_products"),
-            SwitchPreference("revanced_hide_shorts_stickers"),
-            SwitchPreference("revanced_hide_shorts_search_suggestions"),
-            SwitchPreference("revanced_hide_shorts_super_thanks_button"),
-            SwitchPreference("revanced_hide_shorts_location_label"),
-            SwitchPreference("revanced_hide_shorts_channel_bar"),
-            SwitchPreference("revanced_hide_shorts_info_panel"),
-            SwitchPreference("revanced_hide_shorts_full_video_link_label"),
-            SwitchPreference("revanced_hide_shorts_video_title"),
-            SwitchPreference("revanced_hide_shorts_sound_metadata_label"),
-            SwitchPreference("revanced_hide_shorts_navigation_bar"),
+                    // Upper and middle area of the player.
+                    SwitchPreference("revanced_hide_shorts_join_button"),
+                    SwitchPreference("revanced_hide_shorts_subscribe_button"),
+                    SwitchPreference("revanced_hide_shorts_paused_overlay_buttons"),
+
+                    // Suggested actions.
+                    SwitchPreference("revanced_hide_shorts_save_sound_button"),
+                    SwitchPreference("revanced_hide_shorts_use_template_button"),
+                    SwitchPreference("revanced_hide_shorts_upcoming_button"),
+                    SwitchPreference("revanced_hide_shorts_green_screen_button"),
+                    SwitchPreference("revanced_hide_shorts_hashtag_button"),
+                    SwitchPreference("revanced_hide_shorts_shop_button"),
+                    SwitchPreference("revanced_hide_shorts_tagged_products"),
+                    SwitchPreference("revanced_hide_shorts_search_suggestions"),
+                    SwitchPreference("revanced_hide_shorts_super_thanks_button"),
+                    SwitchPreference("revanced_hide_shorts_stickers"),
+
+                    // Bottom of the screen.
+                    SwitchPreference("revanced_hide_shorts_location_label"),
+                    SwitchPreference("revanced_hide_shorts_channel_bar"),
+                    SwitchPreference("revanced_hide_shorts_info_panel"),
+                    SwitchPreference("revanced_hide_shorts_full_video_link_label"),
+                    SwitchPreference("revanced_hide_shorts_video_title"),
+                    SwitchPreference("revanced_hide_shorts_sound_metadata_label"),
+                    SwitchPreference("revanced_hide_shorts_navigation_bar"),
+                )
+            )
         )
 
         if (hideShortsAppShortcut == true) {
