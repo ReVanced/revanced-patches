24d612d0d13d03daa712266cbb23a3329e2f9edf
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2024-10-22 18:29:23 -0400

    feat(YouTube): Merge multiple layout patches into `Hide Layout Components` (#3799)

--- src/main/kotlin/app/revanced/patches/youtube/layout/hide/crowdfundingbox/CrowdfundingBoxPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/hide/crowdfundingbox/CrowdfundingBoxPatch.kt
@@ -1,51 +1,13 @@
 package app.revanced.patches.youtube.layout.hide.crowdfundingbox
 
-import app.revanced.util.exception
 import app.revanced.patcher.data.BytecodeContext
-import app.revanced.patcher.extensions.InstructionExtensions.addInstruction
-import app.revanced.patcher.extensions.InstructionExtensions.getInstruction
 import app.revanced.patcher.patch.BytecodePatch
-import app.revanced.patcher.patch.annotation.CompatiblePackage
-import app.revanced.patcher.patch.annotation.Patch
-import app.revanced.patches.youtube.layout.hide.crowdfundingbox.fingerprints.CrowdfundingBoxFingerprint
-import app.revanced.patches.youtube.misc.integrations.IntegrationsPatch
-import com.android.tools.smali.dexlib2.iface.instruction.TwoRegisterInstruction
+import app.revanced.patches.youtube.layout.hide.general.HideLayoutComponentsPatch
 
-@Patch(
-    name = "Hide crowdfunding box",
-    description = "Adds an option to hide the crowdfunding box between the player and video description.",
-    dependencies = [
-        IntegrationsPatch::class,
-        CrowdfundingBoxResourcePatch::class
-    ],
-    compatiblePackages = [
-        CompatiblePackage(
-            "com.google.android.youtube",
-            [
-                "18.38.44",
-                "18.49.37",
-                "19.16.39",
-                "19.25.37",
-                "19.34.42",
-            ]
-        )
-    ]
-)
+@Deprecated("This patch has been merged to HideLayoutComponentsPatch.")
 @Suppress("unused")
 object CrowdfundingBoxPatch : BytecodePatch(
-    setOf(CrowdfundingBoxFingerprint)
+    dependencies = setOf(HideLayoutComponentsPatch::class),
 ) {
-    private const val INTEGRATIONS_METHOD_DESCRIPTOR =
-        "Lapp/revanced/integrations/youtube/patches/HideCrowdfundingBoxPatch;->hideCrowdfundingBox(Landroid/view/View;)V"
-
-    override fun execute(context: BytecodeContext) {
-        CrowdfundingBoxFingerprint.result?.let {
-            it.mutableMethod.apply {
-                val insertIndex = it.scanResult.patternScanResult!!.endIndex
-                val objectRegister = getInstruction<TwoRegisterInstruction>(insertIndex).registerA
-
-                addInstruction(insertIndex, "invoke-static {v$objectRegister}, $INTEGRATIONS_METHOD_DESCRIPTOR")
-            }
-        } ?: throw CrowdfundingBoxFingerprint.exception
-    }
+    override fun execute(context: BytecodeContext) { }
 }
