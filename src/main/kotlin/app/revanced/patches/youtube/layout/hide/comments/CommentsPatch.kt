d7f01a3f1b77853d5907814e523755cd1e4266b9
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2024-10-22 04:38:02 -0400

    fix compatibility

--- src/main/kotlin/app/revanced/patches/youtube/layout/hide/comments/CommentsPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/hide/comments/CommentsPatch.kt
@@ -1,13 +1,13 @@
 package app.revanced.patches.youtube.layout.hide.comments
 
-import app.revanced.patcher.data.BytecodeContext
-import app.revanced.patcher.patch.BytecodePatch
+import app.revanced.patcher.data.ResourceContext
+import app.revanced.patcher.patch.ResourcePatch
 import app.revanced.patches.youtube.layout.hide.general.HideLayoutComponentsPatch
 
 @Deprecated("This patch has been merged to HideLayoutComponentsPatch.")
 @Suppress("unused")
-object CommentsPatch : BytecodePatch(
+object CommentsPatch : ResourcePatch(
     dependencies = setOf(HideLayoutComponentsPatch::class),
 ) {
-    override fun execute(context: BytecodeContext) { }
+    override fun execute(context: ResourceContext) { }
 }
