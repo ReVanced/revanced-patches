c3a5e14a0a24973a0f9956845c9e0f99c1301d42
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2024-10-24 07:47:01 -0400

    fix(YouTube - Playback speed): Remember playback speed with new speed menu (#3810)

--- src/main/kotlin/app/revanced/patches/youtube/shared/fingerprints/NewVideoQualityChangedFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/youtube/shared/fingerprints/NewVideoQualityChangedFingerprint.kt
@@ -1,4 +1,4 @@
-package app.revanced.patches.youtube.video.quality.fingerprints
+package app.revanced.patches.youtube.shared.fingerprints
 
 import app.revanced.patcher.extensions.or
 import app.revanced.patcher.fingerprint.MethodFingerprint
