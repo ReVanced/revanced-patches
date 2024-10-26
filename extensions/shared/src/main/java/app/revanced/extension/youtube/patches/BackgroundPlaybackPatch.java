3ee78b275580dbcd0a81ef1618fe248845aa7827
Zain <zaintsyariev@gmail.com>
2024-10-27 03:57:43 +0700

    fix(YouTube - Remove background playback restrictions): Enable for Shorts as well (#3671)

--- extensions/shared/src/main/java/app/revanced/extension/youtube/patches/BackgroundPlaybackPatch.java
+++ extensions/shared/src/main/java/app/revanced/extension/youtube/patches/BackgroundPlaybackPatch.java
@@ -1,5 +1,6 @@
 package app.revanced.extension.youtube.patches;
 
+import app.revanced.extension.youtube.settings.Settings;
 import app.revanced.extension.youtube.shared.PlayerType;
 
 @SuppressWarnings("unused")
@@ -8,10 +9,10 @@ public class BackgroundPlaybackPatch {
     /**
      * Injection point.
      */
-    public static boolean allowBackgroundPlayback(boolean original) {
+    public static boolean isBackgroundPlaybackAllowed(boolean original) {
         if (original) return true;
 
-        // Steps to verify most edge cases:
+        // Steps to verify most edge cases (with Shorts background playback set to off):
         // 1. Open a regular video
         // 2. Minimize app (PIP should appear)
         // 3. Reopen app
@@ -22,13 +23,13 @@ public class BackgroundPlaybackPatch {
         // 7. Close the Short
         // 8. Resume playing the regular video
         // 9. Minimize the app (PIP should appear)
-
         if (!VideoInformation.lastVideoIdIsShort()) {
             return true; // Definitely is not a Short.
         }
 
-        // Might be a Short, or might be a prior regular video on screen again after a Short was closed.
-        // This incorrectly prevents PIP if player is in WATCH_WHILE_MINIMIZED after closing a Short,
+        // TODO: Add better hook.
+        // Might be a Shorts, or might be a prior regular video on screen again after a Shorts was closed.
+        // This incorrectly prevents PIP if player is in WATCH_WHILE_MINIMIZED after closing a Shorts,
         // But there's no way around this unless an additional hook is added to definitively detect
         // the Shorts player is on screen. This use case is unusual anyways so it's not a huge concern.
         return !PlayerType.getCurrent().isNoneHiddenOrMinimized();
@@ -37,10 +38,7 @@ public class BackgroundPlaybackPatch {
     /**
      * Injection point.
      */
-    public static boolean overrideBackgroundPlaybackAvailable() {
-        // This could be done entirely in the patch,
-        // but having a unique method to search for makes manually inspecting the patched apk much easier.
-        return true;
+    public static boolean isBackgroundShortsPlaybackAllowed(boolean original) {
+        return !Settings.DISABLE_SHORTS_BACKGROUND_PLAYBACK.get();
     }
-
 }
