8e5116197c73cf0c0406d5616b911834990c20ac
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2024-10-28 05:21:12 -0400

    chore: Fix merge fix for 19.34 target

--- extensions/shared/src/main/java/app/revanced/extension/youtube/patches/SlideToSeekPatch.java
+++ extensions/shared/src/main/java/app/revanced/extension/youtube/patches/SlideToSeekPatch.java
@@ -4,10 +4,10 @@ import app.revanced.extension.youtube.settings.Settings;
 
 @SuppressWarnings("unused")
 public final class SlideToSeekPatch {
-    private static final Boolean SLIDE_TO_SEEK_DISABLED = !Settings.SLIDE_TO_SEEK.get();
+    private static final boolean SLIDE_TO_SEEK_DISABLED = !Settings.SLIDE_TO_SEEK.get();
 
     public static boolean isSlideToSeekDisabled(boolean isDisabled) {
-        if (!isDisabled) return isDisabled;
+        if (!isDisabled) return false;
 
         return SLIDE_TO_SEEK_DISABLED;
     }
