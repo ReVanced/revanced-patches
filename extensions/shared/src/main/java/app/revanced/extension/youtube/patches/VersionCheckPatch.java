042515a4f044ad8fd1b3e43321a639222ecf4379
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2024-11-04 19:22:41 -0400

    fix(YouTube): Merge `Restore old seekbar thumbnails` into `Seekbar thumbnails` (#3860)
    Co-authored-by: oSumAtrIX <johan.melkonyan1@web.de>

--- extensions/shared/src/main/java/app/revanced/extension/youtube/patches/VersionCheckPatch.java
+++ extensions/shared/src/main/java/app/revanced/extension/youtube/patches/VersionCheckPatch.java
@@ -3,6 +3,7 @@ package app.revanced.extension.youtube.patches;
 import app.revanced.extension.shared.Utils;
 
 public class VersionCheckPatch {
+    public static final boolean IS_19_17_OR_GREATER = Utils.getAppVersionName().compareTo("19.17.00") >= 0;
     public static final boolean IS_19_20_OR_GREATER = Utils.getAppVersionName().compareTo("19.20.00") >= 0;
     public static final boolean IS_19_21_OR_GREATER = Utils.getAppVersionName().compareTo("19.21.00") >= 0;
     public static final boolean IS_19_26_OR_GREATER = Utils.getAppVersionName().compareTo("19.26.00") >= 0;
