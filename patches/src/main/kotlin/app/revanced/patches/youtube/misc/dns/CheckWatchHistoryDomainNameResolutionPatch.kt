327ebd364996a0ce54608c6473be4a51918153c2
oSumAtrIX <johan.melkonyan1@web.de>
2024-11-09 05:16:56 +0100

    fix: Add missing dependency to patch

--- patches/src/main/kotlin/app/revanced/patches/youtube/misc/dns/CheckWatchHistoryDomainNameResolutionPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/youtube/misc/dns/CheckWatchHistoryDomainNameResolutionPatch.kt
@@ -3,6 +3,7 @@ package app.revanced.patches.youtube.misc.dns
 import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
 import app.revanced.patcher.patch.bytecodePatch
 import app.revanced.patches.all.misc.resources.addResources
+import app.revanced.patches.all.misc.resources.addResourcesPatch
 import app.revanced.patches.youtube.shared.mainActivityOnCreateFingerprint
 
 private const val EXTENSION_CLASS_DESCRIPTOR =
@@ -13,6 +14,8 @@ val checkWatchHistoryDomainNameResolutionPatch = bytecodePatch(
     name = "Check watch history domain name resolution",
     description = "Checks if the device DNS server is preventing user watch history from being saved.",
 ) {
+    dependsOn(addResourcesPatch)
+
     compatibleWith(
         "com.google.android.youtube"(
             "18.38.44",
