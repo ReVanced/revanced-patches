38a4bad5b890e3906d77d22efeabd8f38653508b
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2024-12-07 01:14:22 +0400

    fix(YouTube Music - GmsCore support): Resolve patching errors (#4056)

--- patches/src/main/kotlin/app/revanced/patches/music/misc/extension/SharedExtensionPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/music/misc/extension/SharedExtensionPatch.kt
@@ -4,6 +4,5 @@ import app.revanced.patches.music.misc.extension.hooks.applicationInitHook
 import app.revanced.patches.shared.misc.extension.sharedExtensionPatch
 
 val sharedExtensionPatch = sharedExtensionPatch(
-    "music",
     applicationInitHook,
 )
