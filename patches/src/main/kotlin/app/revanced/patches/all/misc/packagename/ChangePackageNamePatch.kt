3dc93a1da7ceaee680ed54b4e4236048f5de6458
oSumAtrIX <johan.melkonyan1@web.de>
2024-12-09 07:46:24 +0100

    refactor

--- patches/src/main/kotlin/app/revanced/patches/all/misc/packagename/ChangePackageNamePatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/all/misc/packagename/ChangePackageNamePatch.kt
@@ -1,15 +1,10 @@
 package app.revanced.patches.all.misc.packagename
 
-import app.revanced.patcher.patch.Option
-import app.revanced.patcher.patch.booleanOption
-import app.revanced.patcher.patch.resourcePatch
-import app.revanced.patcher.patch.stringOption
+import app.revanced.patcher.patch.*
 import app.revanced.util.asSequence
 import org.w3c.dom.Element
 
 lateinit var packageNameOption: Option<String>
-lateinit var updatePermissions: Option<Boolean>
-lateinit var updateProviders: Option<Boolean>
 
 /**
  * Set the package name to use.
@@ -29,11 +24,10 @@ fun setOrGetFallbackPackageName(fallbackPackageName: String): String {
     }
 }
 
-private const val RECEIVER_NOT_EXPORTED = "DYNAMIC_RECEIVER_NOT_EXPORTED_PERMISSION"
-
 val changePackageNamePatch = resourcePatch(
     name = "Change package name",
-    description = "Appends \".revanced\" to the package name by default. Changing the package name of the app can lead to unexpected issues.",
+    description = "Appends \".revanced\" to the package name by default. " +
+        "Changing the package name of the app can lead to unexpected issues.",
     use = false,
 ) {
     packageNameOption = stringOption(
@@ -47,20 +41,20 @@ val changePackageNamePatch = resourcePatch(
         it == "Default" || it!!.matches(Regex("^[a-z]\\w*(\\.[a-z]\\w*)+\$"))
     }
 
-    updatePermissions = booleanOption(
+    val updatePermissions by booleanOption(
         key = "updatePermissions",
         default = false,
         title = "Update permissions",
-        description = "Update compatibility receiver permissions. Enabling this can fix installation errors, but this can also break features in certain apps.",
-        required = false
+        description = "Update compatibility receiver permissions. " +
+            "Enabling this can fix installation errors, but this can also break features in certain apps.",
     )
 
-    updateProviders = booleanOption(
+    val updateProviders by booleanOption(
         key = "updateProviders",
         default = false,
         title = "Update providers",
-        description = "Update provider names declared by the app. Enabling this can fix installation errors, but this can also break features in certain apps.",
-        required = false
+        description = "Update provider names declared by the app. " +
+            "Enabling this can fix installation errors, but this can also break features in certain apps.",
     )
 
     finalize {
@@ -73,23 +67,24 @@ val changePackageNamePatch = resourcePatch(
             val newPackageName = if (replacementPackageName != packageNameOption.default) {
                 replacementPackageName!!
             } else {
-                "${packageName}.revanced"
+                "$packageName.revanced"
             }
 
             manifest.setAttribute("package", newPackageName)
 
-
-            if (updatePermissions.value == true) {
+            if (updatePermissions == true) {
                 val permissions = manifest.getElementsByTagName("permission").asSequence()
                 val usesPermissions = manifest.getElementsByTagName("uses-permission").asSequence()
 
+                val receiverNotExported = "DYNAMIC_RECEIVER_NOT_EXPORTED_PERMISSION"
+
                 (permissions + usesPermissions)
                     .map { it as Element }
-                    .filter { it.getAttribute("android:name") == "$packageName.$RECEIVER_NOT_EXPORTED" }
-                    .forEach { it.setAttribute("android:name", "$newPackageName.$RECEIVER_NOT_EXPORTED") }
+                    .filter { it.getAttribute("android:name") == "$packageName.$receiverNotExported" }
+                    .forEach { it.setAttribute("android:name", "$newPackageName.$receiverNotExported") }
             }
 
-            if(updateProviders.value == true){
+            if (updateProviders == true) {
                 val providers = manifest.getElementsByTagName("provider").asSequence()
 
                 for (node in providers) {
@@ -104,4 +99,3 @@ val changePackageNamePatch = resourcePatch(
         }
     }
 }
-
