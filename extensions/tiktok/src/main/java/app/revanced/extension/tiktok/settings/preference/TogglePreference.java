ca58c9590cac6f8d2281cd90809444cc3121a3c9
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2024-12-10 00:24:11 +0400

    fix(TikTok - Settings): Use correct colors for dark mode

--- extensions/tiktok/src/main/java/app/revanced/extension/tiktok/settings/preference/TogglePreference.java
+++ extensions/tiktok/src/main/java/app/revanced/extension/tiktok/settings/preference/TogglePreference.java
@@ -2,16 +2,26 @@ package app.revanced.extension.tiktok.settings.preference;
 
 import android.content.Context;
 import android.preference.SwitchPreference;
+import android.view.View;
 
 import app.revanced.extension.shared.settings.BooleanSetting;
+import app.revanced.extension.tiktok.Utils;
 
 @SuppressWarnings("deprecation")
 public class TogglePreference extends SwitchPreference {
+
     public TogglePreference(Context context, String title, String summary, BooleanSetting setting) {
         super(context);
-        this.setTitle(title);
-        this.setSummary(summary);
-        this.setKey(setting.key);
-        this.setChecked(setting.get());
+        setTitle(title);
+        setSummary(summary);
+        setKey(setting.key);
+        setChecked(setting.get());
+    }
+
+    @Override
+    protected void onBindView(View view) {
+        super.onBindView(view);
+
+        Utils.setTitleAndSummaryColor(getContext(), view);
     }
 }
