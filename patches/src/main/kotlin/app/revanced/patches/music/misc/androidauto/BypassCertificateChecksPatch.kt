11216cd9424ec9893758debb683c712658dfd53f
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2024-12-12 00:00:32 +0400

    fix(YouTube Music - Bypass certificate checks): Add a recommended target version (#4104)

--- patches/src/main/kotlin/app/revanced/patches/music/misc/androidauto/BypassCertificateChecksPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/music/misc/androidauto/BypassCertificateChecksPatch.kt
@@ -1,22 +1,16 @@
 package app.revanced.patches.music.misc.androidauto
 
-import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
 import app.revanced.patcher.patch.bytecodePatch
+import app.revanced.util.returnEarly
 
 @Suppress("unused")
 val bypassCertificateChecksPatch = bytecodePatch(
     name = "Bypass certificate checks",
     description = "Bypasses certificate checks which prevent YouTube Music from working on Android Auto.",
 ) {
-    compatibleWith("com.google.android.apps.youtube.music")
+    compatibleWith("com.google.android.apps.youtube.music"("7.29.52"))
 
     execute {
-        checkCertificateFingerprint.method.addInstructions(
-            0,
-            """
-                const/4 v0, 0x1
-                return v0
-            """,
-        )
+        checkCertificateFingerprint.method.returnEarly(true)
     }
 }
