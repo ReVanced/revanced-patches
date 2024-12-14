dbccff0fdcc8379952701dfe0a7cc0338cf850d3
oSumAtrIX <johan.melkonyan1@web.de>
2024-12-14 05:53:00 +0100

    fix(YouTube Music - Spoof video streams): Use `IOS_MUSIC` client to fix playback issues

--- extensions/shared/library/src/main/java/app/revanced/extension/shared/spoof/ClientType.java
+++ extensions/shared/library/src/main/java/app/revanced/extension/shared/spoof/ClientType.java
@@ -45,61 +45,48 @@ public enum ClientType {
      * Android VR with no language code.
      * Used for age restricted videos and YouTube Music to disable stable volume.
      */
-    ANDROID_VR_NO_HL(
-            ANDROID_VR.id,
-            ANDROID_VR.clientName,
-            ANDROID_VR.deviceModel,
-            ANDROID_VR.osVersion,
-            ANDROID_VR.userAgent,
-            ANDROID_VR.androidSdkVersion,
-            ANDROID_VR.clientVersion,
-            ANDROID_VR.canLogin,
+    IOS_MUSIC(26,
+            "IOS_MUSIC",
+            "iPhone16,2",
+            "17.5.1.21F90",
+            "com.google.ios.youtubemusic/7.08.2 (iPhone16,2; U; CPU iOS 17_5_1 like Mac OS X;)",
+            null,
+            "7.08.2",
+            true,
             false);
-
-    private static boolean forceAVC() {
-        return BaseSettings.SPOOF_VIDEO_STREAMS_IOS_FORCE_AVC.get();
-    }
 
     /**
      * YouTube
      * <a href="https://github.com/zerodytrash/YouTube-Internal-Clients?tab=readme-ov-file#clients">client type</a>
      */
     public final int id;
-
     public final String clientName;
-
     /**
      * Device model, equivalent to {@link Build#MODEL} (System property: ro.product.model)
      */
     public final String deviceModel;
-
     /**
      * Device OS version.
      */
     public final String osVersion;
-
     /**
      * Player user-agent.
      */
     public final String userAgent;
-
     /**
      * Android SDK version, equivalent to {@link Build.VERSION#SDK} (System property: ro.build.version.sdk)
      * Field is null if not applicable.
      */
     @Nullable
     public final String androidSdkVersion;
-
     /**
      * App version.
      */
     public final String clientVersion;
-
     /**
      * If the client can access the API logged in.
      */
     public final boolean canLogin;
-
     /**
      * If a language code should be used.
      */
@@ -123,5 +110,9 @@ public enum ClientType {
         this.clientVersion = clientVersion;
         this.canLogin = canLogin;
         this.useLanguageCode = useLanguageCode;
+    }
+
+    private static boolean forceAVC() {
+        return BaseSettings.SPOOF_VIDEO_STREAMS_IOS_FORCE_AVC.get();
     }
 }
