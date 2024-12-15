68304fd96ace53fe5c3dcd401a82b032d7559aed
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2024-12-15 16:51:34 +0400

    feat(YouTube): Add `Force original audio` patch (#4122)

---
+++ extensions/youtube/src/main/java/app/revanced/extension/youtube/patches/ForceOriginalAudioPatch.java
@@ -0,0 +1,41 @@
+package app.revanced.extension.youtube.patches;
+
+import app.revanced.extension.shared.Logger;
+import app.revanced.extension.youtube.settings.Settings;
+
+@SuppressWarnings("unused")
+public class ForceOriginalAudioPatch {
+
+    private static final String DEFAULT_AUDIO_TRACKS_IDENTIFIER = "original";
+
+    /**
+     * Injection point.
+     */
+    public static boolean isDefaultAudioStream(boolean isDefault, String audioTrackId, String audioTrackDisplayName) {
+        try {
+            if (!Settings.FORCE_ORIGINAL_AUDIO.get()) {
+                return isDefault;
+            }
+
+            if (audioTrackDisplayName.isEmpty()) {
+                // Older app targets can have empty audio tracks and these might be placeholders.
+                // The real audio tracks are called after these.
+                return isDefault;
+            }
+
+            Logger.printDebug(() -> "default: " + String.format("%-5s", isDefault) + " id: "
+                    + String.format("%-8s", audioTrackId) + " name:" + audioTrackDisplayName);
+
+            final boolean isOriginal = audioTrackDisplayName.contains(DEFAULT_AUDIO_TRACKS_IDENTIFIER);
+            if (isOriginal) {
+                Logger.printDebug(() -> "Using audio: " + audioTrackId);
+            }
+
+            return isOriginal;
+        } catch (Exception ex) {
+            Logger.printException(() -> "isDefaultAudioStream failure", ex);
+        }
+
+        return isDefault;
+    }
+}
--- patches/src/main/kotlin/app/revanced/patches/music/misc/spoof/SpoofVideoStreamsPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/music/misc/spoof/SpoofVideoStreamsPatch.kt
@@ -1,15 +1,7 @@
 package app.revanced.patches.music.misc.spoof
 
-import app.revanced.patcher.extensions.InstructionExtensions.addInstruction
-import app.revanced.patches.music.misc.gms.musicActivityOnCreateFingerprint
-import app.revanced.patches.shared.misc.spoof.EXTENSION_CLASS_DESCRIPTOR
 import app.revanced.patches.shared.misc.spoof.spoofVideoStreamsPatch
 
 val spoofVideoStreamsPatch = spoofVideoStreamsPatch({
     compatibleWith("com.google.android.apps.youtube.music")
-}, {
-    musicActivityOnCreateFingerprint.method.addInstruction(
-        0,
-        "invoke-static { }, $EXTENSION_CLASS_DESCRIPTOR->setClientTypeToAndroidVrNoHl()V"
-    )
 })
