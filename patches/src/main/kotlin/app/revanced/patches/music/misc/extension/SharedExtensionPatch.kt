ccb6a7f161a4368f7ecd942236a28a5916860c72
oSumAtrIX <johan.melkonyan1@web.de>
2024-12-16 19:07:37 +0100

    fix(YouTube Music): Add `Spoof client patch` to fix playback (#4132)

--- patches/src/main/kotlin/app/revanced/patches/music/misc/extension/SharedExtensionPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/music/misc/extension/SharedExtensionPatch.kt
@@ -4,5 +4,6 @@ import app.revanced.patches.music.misc.extension.hooks.applicationInitHook
 import app.revanced.patches.shared.misc.extension.sharedExtensionPatch
 
 val sharedExtensionPatch = sharedExtensionPatch(
+    "music",
     applicationInitHook,
 )
--- patches/src/main/kotlin/app/revanced/patches/music/misc/spoof/SpoofVideoStreamsPatch.kt
+++
@@ -1,7 +0,0 @@
-package app.revanced.patches.music.misc.spoof
-
-import app.revanced.patches.shared.misc.spoof.spoofVideoStreamsPatch
-
-val spoofVideoStreamsPatch = spoofVideoStreamsPatch({
-    compatibleWith("com.google.android.apps.youtube.music")
-})
