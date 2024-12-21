2089e613d36c45352db7d852aaee0087b1c3e1a4
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2024-12-21 19:06:51 +0400

    feat(YouTube): Support version `19.47.53` (#4182)

--- patches/src/main/kotlin/app/revanced/patches/youtube/interaction/copyvideourl/CopyVideoUrlPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/youtube/interaction/copyvideourl/CopyVideoUrlPatch.kt
@@ -61,6 +61,7 @@ val copyVideoUrlPatch = bytecodePatch(
             "19.43.41",
             "19.45.38",
             "19.46.42",
+            "19.47.53",
         ),
     )
 
--- patches/src/main/kotlin/app/revanced/patches/youtube/layout/buttons/navigation/NavigationButtonsPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/youtube/layout/buttons/navigation/NavigationButtonsPatch.kt
@@ -48,6 +48,7 @@ val navigationButtonsPatch = bytecodePatch(
             "19.43.41",
             "19.45.38",
             "19.46.42",
+            "19.47.53",
         ),
     )
 
