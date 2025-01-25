a06bf3c2acd40e8db5636358113c709283a2e441
ILoveOpenSourceApplications <117499019+ILoveOpenSourceApplications@users.noreply.github.com>
2025-01-25 22:25:13 +0530

    Update patch naming

--- extensions/youtube/src/main/java/app/revanced/extension/youtube/settings/Settings.java
+++ extensions/youtube/src/main/java/app/revanced/extension/youtube/settings/Settings.java
@@ -61,7 +61,7 @@ public class Settings extends BaseSettings {
 
     // Ads
     public static final BooleanSetting HIDE_BUTTONED_ADS = new BooleanSetting("revanced_hide_buttoned_ads", TRUE);
-    public static final BooleanSetting HIDE_END_SCREEN_STORE_AD = new BooleanSetting("revanced_hide_end_screen_store_ad", FALSE);
+    public static final BooleanSetting HIDE_END_SCREEN_STORE_BANNER = new BooleanSetting("revanced_hide_end_screen_store_banner", FALSE);
     public static final BooleanSetting HIDE_FULLSCREEN_ADS = new BooleanSetting("revanced_hide_fullscreen_ads", TRUE);
     public static final BooleanSetting HIDE_GENERAL_ADS = new BooleanSetting("revanced_hide_general_ads", TRUE);
     public static final BooleanSetting HIDE_GET_PREMIUM = new BooleanSetting("revanced_hide_get_premium", TRUE);
