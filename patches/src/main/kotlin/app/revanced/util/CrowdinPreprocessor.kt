70bd0ec4d6412cda07f998da6535b12212916d62
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2025-01-30 10:57:17 +0200

    comments

--- patches/src/main/kotlin/app/revanced/util/CrowdinPreprocessor.kt
+++ patches/src/main/kotlin/app/revanced/util/CrowdinPreprocessor.kt
@@ -2,6 +2,13 @@ package app.revanced.util
 
 import java.io.File
 
+/**
+ * Comments out the non-standard <app> and <patch> tags.
+ *
+ * Previously this was done on Crowdin after pushing.
+ * But Crowdin preprocessing has randomly failed but still used the unmodified
+ * strings.xml file, which effectively deletes all patch strings from Crowdin.
+ */
 internal fun main(args: Array<String>) {
     if (args.size != 2) {
         throw RuntimeException("Exactly two arguments are required: <input_file> <output_file>")
@@ -10,10 +17,14 @@ internal fun main(args: Array<String>) {
     val inputFilePath = args[0]
     val inputFile = File(inputFilePath)
     if (!inputFile.exists()) {
-        throw RuntimeException("File not found at path: $inputFilePath")
+        throw RuntimeException(
+            "Input file not found: $inputFilePath  currentDirectory: " + File(".").canonicalPath
+        )
     }
 
-    // Comment out the non standard <app> and <patch> tags.
+    // Comment out the non-standard tags. Otherwise Crowdin interprets the file
+    // not as Android but instead a generic xml file where strings are
+    // identified by xml position and not key.
     val content = inputFile.readText()
     val tagRegex = """((<app\s+.*>)|(</app>)|(<patch\s+.*>)|(</patch>))""".toRegex()
     val modifiedContent = content.replace(tagRegex, """<!-- $1 -->""")
