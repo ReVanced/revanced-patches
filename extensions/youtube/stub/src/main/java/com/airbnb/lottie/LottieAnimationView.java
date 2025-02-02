f81b658fb7d82b4298293b1e6768be43a62e31f0
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2025-02-02 11:10:57 +0200

    fix(YouTube - Theme): Use custom seekbar color for cairo startup animation (#4399)

---
+++ extensions/youtube/stub/src/main/java/com/airbnb/lottie/LottieAnimationView.java
@@ -0,0 +1,15 @@
+package com.airbnb.lottie;
+
+import java.io.InputStream;
+
+@SuppressWarnings("unused")
+public class LottieAnimationView {
+
+    public void patch_setAnimation(InputStream stream, String cacheKey) {
+        throw new RuntimeException("stub");
+    }
+
+    public final void patch_setAnimation(int rawResInt) {
+        throw new RuntimeException("stub");
+    }
+}
