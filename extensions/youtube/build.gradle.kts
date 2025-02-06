1d52b7478d34e699d8c629eeaa9fdbb470b7d5c8
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2025-02-06 14:24:40 +0200

    fix: Fix broken `Remove screen capture restriction`,  `Remove screenshot restriction`, `Spoof Wi-Fi connection`, and `Export internal data documents provider` patch (#4405)

--- extensions/youtube/build.gradle.kts
+++ extensions/youtube/build.gradle.kts
@@ -1,8 +1,13 @@
-//noinspection GradleDependency
-android.compileSdk = 33
-
 dependencies {
     compileOnly(project(":extensions:shared:library"))
     compileOnly(project(":extensions:youtube:stub"))
     compileOnly(libs.annotation)
+}
+
+android {
+    compileSdk = 33 // TODO: Update Swipe controls code to allow updating this to the latest sdk.
+
+    defaultConfig {
+        minSdk = 26
+    }
 }
