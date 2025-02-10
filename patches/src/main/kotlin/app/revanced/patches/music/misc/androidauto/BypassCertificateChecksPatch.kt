6265b1ca44749d004d35cf764fd148972508b40b
Alberto Ponces <ponces26@gmail.com>
2025-02-10 18:01:22 +0000

    fix(YouTube Music): update `Bypass certificate checks` patch to be more reliable across multiple versions

--- patches/src/main/kotlin/app/revanced/patches/music/misc/androidauto/BypassCertificateChecksPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/music/misc/androidauto/BypassCertificateChecksPatch.kt
@@ -8,7 +8,7 @@ val bypassCertificateChecksPatch = bytecodePatch(
     name = "Bypass certificate checks",
     description = "Bypasses certificate checks which prevent YouTube Music from working on Android Auto.",
 ) {
-    compatibleWith("com.google.android.apps.youtube.music"("7.29.52"))
+    compatibleWith("com.google.android.apps.youtube.music"("8.05.50"))
 
     execute {
         checkCertificateFingerprint.method.returnEarly(true)
--- patches/src/main/kotlin/app/revanced/patches/music/misc/androidauto/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/music/misc/androidauto/Fingerprints.kt
@@ -4,8 +4,7 @@ import com.android.tools.smali.dexlib2.AccessFlags
 import app.revanced.patcher.fingerprint
 
 internal val checkCertificateFingerprint = fingerprint {
-    accessFlags(AccessFlags.PUBLIC, AccessFlags.FINAL)
     returns("Z")
     parameters("Ljava/lang/String;")
-    strings("X509", "Failed to get certificate.")
+    strings("X509", "Failed to get certificate")
 }
