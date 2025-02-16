128441e78bc0d096c3fc2f57782ab90c39c3ae4b
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2025-02-16 18:31:52 +0200

    feat(YouTube Music): Support version `8.05.51`

--- patches/src/main/kotlin/app/revanced/patches/music/misc/spoof/SpoofClientPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/music/misc/spoof/SpoofClientPatch.kt
@@ -28,7 +28,7 @@ val spoofClientPatch = bytecodePatch(
     compatibleWith(
         "com.google.android.apps.youtube.music"(
             "7.16.53",
-            "8.05.50"
+            "8.05.51"
         )
     )
 
