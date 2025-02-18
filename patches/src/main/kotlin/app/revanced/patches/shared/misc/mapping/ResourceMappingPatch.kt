b31b0eb39f1abdf7794fdaf97912c0b8ec9c4056
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2025-02-18 22:11:50 +0200

    refactor: Use map instead of list to lookup resource ids

--- patches/src/main/kotlin/app/revanced/patches/shared/misc/mapping/ResourceMappingPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/shared/misc/mapping/ResourceMappingPatch.kt
@@ -7,13 +7,17 @@ import app.revanced.patcher.patch.PatchException
 import app.revanced.patcher.patch.resourcePatch
 import org.w3c.dom.Element
 import java.lang.Runtime
-import java.util.Collections
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
 
-// TODO: Probably renaming the patch/this is a good idea.
-lateinit var resourceMappings: List<ResourceElement>
-    private set
+private lateinit var resourceMappings: Map<String, ResourceElement>
+
+/**
+ * @return A resource id of the given resource type and name.
+ * @throws PatchException if the resource is not found.
+ */
+fun getResourceId(type: String, name: String) = resourceMappings[type + name]?.id
+    ?: throw PatchException("Could not find resource type: $type name: $name")
 
 /**
  * Identical to [LiteralFilter] except uses a decoded resource literal value.
@@ -25,30 +29,34 @@ fun resourceLiteral(
     type: String,
     name: String,
     maxBefore: Int = METHOD_MAX_INSTRUCTIONS,
-) = literal({ resourceMappings[type, name] }, null, maxBefore)
+) = literal({ getResourceId(type, name) }, null, maxBefore)
 
 
 val resourceMappingPatch = resourcePatch {
-    val resourceMappings = Collections.synchronizedList(mutableListOf<ResourceElement>())
-
     execute {
+        val mappings : Map<String, ResourceElement>
         val threadCount = Runtime.getRuntime().availableProcessors()
         val threadPoolExecutor = Executors.newFixedThreadPool(threadCount)
 
         // Save the file in memory to concurrently read from it.
         val resourceXmlFile = get("res/values/public.xml").readBytes()
 
-        for (threadIndex in 0 until threadCount) {
-            threadPoolExecutor.execute thread@{
-                document(resourceXmlFile.inputStream()).use { document ->
+        document(resourceXmlFile.inputStream()).use { document ->
+            // Need to synchronize while building the map, but after it's built
+            // no synchronization is needed. Don't use a synchronized map and
+            // instead only synchronize while building.
+            val lock = Object()
+            val resources = document.documentElement.childNodes
+            val resourcesLength = resources.length
+            val jobSize = resourcesLength / threadCount
+            mappings = HashMap<String, ResourceElement>(2 * resourcesLength)
 
-                    val resources = document.documentElement.childNodes
-                    val resourcesLength = resources.length
-                    val jobSize = resourcesLength / threadCount
-
+            for (threadIndex in 0 until threadCount) {
+                threadPoolExecutor.execute thread@{
                     val batchStart = jobSize * threadIndex
                     val batchEnd = jobSize * (threadIndex + 1)
-                    element@ for (i in batchStart until batchEnd) {
+
+                    for (i in batchStart until batchEnd) {
                         // Prevent out of bounds.
                         if (i >= resourcesLength) return@thread
 
@@ -62,7 +70,11 @@ val resourceMappingPatch = resourcePatch {
 
                         val id = node.getAttribute("id").substring(2).toLong(16)
 
-                        resourceMappings.add(ResourceElement(typeAttribute, nameAttribute, id))
+                        val resourceElement = ResourceElement(typeAttribute, nameAttribute, id)
+
+                        synchronized(lock) {
+                            mappings[typeAttribute + nameAttribute] = resourceElement
+                        }
                     }
                 }
             }
@@ -70,12 +82,8 @@ val resourceMappingPatch = resourcePatch {
 
         threadPoolExecutor.also { it.shutdown() }.awaitTermination(Long.MAX_VALUE, TimeUnit.SECONDS)
 
-        app.revanced.patches.shared.misc.mapping.resourceMappings = resourceMappings
+        resourceMappings = mappings
     }
 }
-
-operator fun List<ResourceElement>.get(type: String, name: String) = resourceMappings.firstOrNull {
-    it.type == type && it.name == name
-}?.id ?: throw PatchException("Could not find resource type: $type name: $name")
 
 data class ResourceElement internal constructor(val type: String, val name: String, val id: Long)
