da6109c9d4a0340a94d5a9bae52d1e3d3664a6d3
Jamie McCullough <92225779+AoifeMcCull@users.noreply.github.com>
2025-03-01 08:03:58 -0600

    Compatibility and formatting

--- patches/src/main/kotlin/app/revanced/patches/protonmail/signature/RemoveSentFromSignaturePatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/protonmail/signature/RemoveSentFromSignaturePatch.kt
@@ -1,16 +1,16 @@
-package app.revanced.patches.protonmail.footer
+package app.revanced.patches.protonmail.signature
 
 import app.revanced.patcher.patch.resourcePatch
 import org.w3c.dom.Element
 import app.revanced.util.findElementByAttributeValueOrThrow
 
 @Suppress("unused")
-val removeMobileFooterPatch = resourcePatch(
-    name = "Remove 'sent from' signature",
+val removeSentFromSignaturePatch = resourcePatch(
+    name = "Remove \"Sent from\" signature",
     description = "Removes the 'Sent from Proton Mail mobile' signature from emails.",
 ) {
+    compatibleWith("ch.protonmail.android"("4.7.2"))
     execute {
-
         //TODO: This goes out of date if Proton Mail adds additional languages.
         //It could be improved by dynamically iterating through all directories in /res/ .
         val directories = listOf(
@@ -47,7 +47,6 @@ val removeMobileFooterPatch = resourcePatch(
             "res/values-zh-rCN",
             "res/values-zh-rTW",
         )
-
         directories.forEach { directory ->
             document("$directory/strings.xml").use { document ->
                 document.documentElement.childNodes.findElementByAttributeValueOrThrow("name", "mail_settings_identity_mobile_footer_default_free").textContent = ""
