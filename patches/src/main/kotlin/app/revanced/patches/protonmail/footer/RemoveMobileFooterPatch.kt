49e405e0bdc45db530ef5c04ddecc01c36a5befc
Jamie McCullough <92225779+AoifeMcCull@users.noreply.github.com>
2025-02-28 20:40:12 -0600

    Improved to support all current languages supported by Proton Mail.

--- patches/src/main/kotlin/app/revanced/patches/protonmail/footer/RemoveMobileFooterPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/protonmail/footer/RemoveMobileFooterPatch.kt
@@ -10,8 +10,49 @@ val removeMobileFooterPatch = resourcePatch(
     description = "Removes the 'Sent from Proton Mail mobile' signature from emails.",
 ) {
     execute {
-        document("res/values/strings.xml").use { document ->
-            document.documentElement.childNodes.findElementByAttributeValueOrThrow("name", "mail_settings_identity_mobile_footer_default_free").textContent = ""
+
+        //TODO: This breaks if Proton Mail removes a language and goes out of date if they add one.
+        //It could be improved by dynamically iterating through all directories in /res/ .
+        val directories = listOf(
+            "res/values",
+            "res/values-b+es+419",
+            "res/values-be",
+            "res/values-ca",
+            "res/values-cs",
+            "res/values-da",
+            "res/values-de",
+            "res/values-el",
+            "res/values-es-rES",
+            "res/values-fr",
+            "res/values-hr",
+            "res/values-hu",
+            "res/values-in",
+            "res/values-it",
+            "res/values-ja",
+            "res/values-ka",
+            "res/values-kab",
+            "res/values-ko",
+            "res/values-nb-rNO",
+            "res/values-nl",
+            "res/values-pl",
+            "res/values-pt-rBR",
+            "res/values-pt-rPT",
+            "res/values-ro",
+            "res/values-ru",
+            "res/values-sk",
+            "res/values-sl",
+            "res/values-sv-rSE",
+            "res/values-tr",
+            "res/values-uk",
+            "res/values-zh-rCN",
+            "res/values-zh-rTW",
+            "res/testbreak"
+        )
+
+        directories.forEach { directory ->
+            document("$directory/strings.xml").use { document ->
+                document.documentElement.childNodes.findElementByAttributeValueOrThrow("name", "mail_settings_identity_mobile_footer_default_free").textContent = ""
+            }
         }
     }
 }
