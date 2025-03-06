08be0e1c5fadf1a07396129b5947c13fd38b2472
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2025-03-06 09:16:04 +0200

    fix(YouTube): Merge `Restore old video quality menu` and `Remember video quality` into a single patch `Video quality`

---
+++ patches/src/main/kotlin/app/revanced/patches/youtube/video/quality/AdvancedVideoQualityMenuPatch.kt
@@ -0,0 +1,114 @@
+package app.revanced.patches.youtube.video.quality
+
+import app.revanced.patcher.extensions.InstructionExtensions.addInstruction
+import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
+import app.revanced.patcher.extensions.InstructionExtensions.getInstruction
+import app.revanced.patcher.patch.PatchException
+import app.revanced.patcher.patch.bytecodePatch
+import app.revanced.patcher.patch.resourcePatch
+import app.revanced.patches.all.misc.resources.addResources
+import app.revanced.patches.all.misc.resources.addResourcesPatch
+import app.revanced.patches.shared.misc.mapping.get
+import app.revanced.patches.shared.misc.mapping.resourceMappingPatch
+import app.revanced.patches.shared.misc.mapping.resourceMappings
+import app.revanced.patches.shared.misc.settings.preference.SwitchPreference
+import app.revanced.patches.youtube.misc.extension.sharedExtensionPatch
+import app.revanced.patches.youtube.misc.litho.filter.addLithoFilter
+import app.revanced.patches.youtube.misc.litho.filter.lithoFilterPatch
+import app.revanced.patches.youtube.misc.recyclerviewtree.hook.addRecyclerViewTreeHook
+import app.revanced.patches.youtube.misc.recyclerviewtree.hook.recyclerViewTreeHookPatch
+import app.revanced.patches.youtube.misc.settings.settingsPatch
+import com.android.tools.smali.dexlib2.iface.instruction.OneRegisterInstruction
+
+internal var videoQualityBottomSheetListFragmentTitle = -1L
+    private set
+internal var videoQualityQuickMenuAdvancedMenuDescription = -1L
+    private set
+
+private val advancedVideoQualityMenuResourcePatch = resourcePatch {
+    dependsOn(resourceMappingPatch)
+
+    execute {
+        // Used for the old type of the video quality menu.
+        videoQualityBottomSheetListFragmentTitle = resourceMappings[
+            "layout",
+            "video_quality_bottom_sheet_list_fragment_title",
+        ]
+
+        videoQualityQuickMenuAdvancedMenuDescription = resourceMappings[
+            "string",
+            "video_quality_quick_menu_advanced_menu_description",
+        ]
+    }
+}
+
+private const val EXTENSION_CLASS_DESCRIPTOR =
+    "Lapp/revanced/extension/youtube/patches/playback/quality/AdvancedVideoQualityMenuPatch;"
+
+private const val FILTER_CLASS_DESCRIPTOR =
+    "Lapp/revanced/extension/youtube/patches/components/AdvancedVideoQualityMenuFilter;"
+
+internal val advancedVideoQualityMenuPatch = bytecodePatch {
+    dependsOn(
+        advancedVideoQualityMenuResourcePatch,
+        sharedExtensionPatch,
+        settingsPatch,
+        addResourcesPatch,
+        lithoFilterPatch,
+        recyclerViewTreeHookPatch,
+    )
+
+    execute {
+        addResources("youtube", "video.quality.advancedVideoQualityMenuPatch")
+
+        settingsMenuVideoQualityGroup.add(
+            SwitchPreference("revanced_advanced_video_quality_menu")
+        )
+
+        // region Patch for the old type of the video quality menu.
+        // Used for regular videos when spoofing to old app version,
+        // and for the Shorts quality flyout on newer app versions.
+
+        videoQualityMenuViewInflateFingerprint.method.apply {
+            val checkCastIndex = videoQualityMenuViewInflateFingerprint.patternMatch!!.endIndex
+            val listViewRegister = getInstruction<OneRegisterInstruction>(checkCastIndex).registerA
+
+            addInstruction(
+                checkCastIndex + 1,
+                "invoke-static { v$listViewRegister }, $EXTENSION_CLASS_DESCRIPTOR->" +
+                    "showAdvancedVideoQualityMenu(Landroid/widget/ListView;)V",
+            )
+        }
+
+        // Force YT to add the 'advanced' quality menu for Shorts.
+        val patternMatch = videoQualityMenuOptionsFingerprint.patternMatch!!
+        val startIndex = patternMatch.startIndex
+        if (startIndex != 0) throw PatchException("Unexpected opcode start index: $startIndex")
+        val insertIndex = patternMatch.endIndex
+
+        videoQualityMenuOptionsFingerprint.method.apply {
+            val register = getInstruction<OneRegisterInstruction>(insertIndex).registerA
+
+            // A condition controls whether to show the three or four items quality menu.
+            // Force the four items quality menu to make the "Advanced" item visible, necessary for the patch.
+            addInstructions(
+                insertIndex,
+                """
+                    invoke-static { v$register }, $EXTENSION_CLASS_DESCRIPTOR->forceAdvancedVideoQualityMenuCreation(Z)Z
+                    move-result v$register
+                """,
+            )
+        }
+
+        // endregion
+
+        // region Patch for the new type of the video quality menu.
+
+        addRecyclerViewTreeHook(EXTENSION_CLASS_DESCRIPTOR)
+
+        // Required to check if the video quality menu is currently shown in order to click on the "Advanced" item.
+        addLithoFilter(FILTER_CLASS_DESCRIPTOR)
+
+        // endregion
+    }
+}
---
+++ patches/src/main/kotlin/app/revanced/patches/youtube/video/quality/VideoQualityPatch.kt
@@ -0,0 +1,48 @@
+package app.revanced.patches.youtube.video.quality
+
+import app.revanced.patcher.patch.bytecodePatch
+import app.revanced.patches.shared.misc.settings.preference.BasePreference
+import app.revanced.patches.shared.misc.settings.preference.PreferenceCategory
+import app.revanced.patches.shared.misc.settings.preference.PreferenceScreenPreference.Sorting
+import app.revanced.patches.youtube.misc.settings.PreferenceScreen
+
+/**
+ * Video quality settings.  Used to organize all speed related settings together.
+ */
+internal val settingsMenuVideoQualityGroup = mutableSetOf<BasePreference>()
+
+@Suppress("unused")
+val videoQualityPatch = bytecodePatch(
+    name = "Video quality",
+    description = "Adds options to use the advanced video quality menu and set default video qualities."
+) {
+    dependsOn(
+        rememberVideoQualityPatch,
+        advancedVideoQualityMenuPatch,
+    )
+
+    compatibleWith(
+        "com.google.android.youtube"(
+            "19.16.39",
+            "19.25.37",
+            "19.34.42",
+            "19.43.41",
+            "19.45.38",
+            "19.46.42",
+            "19.47.53",
+        )
+    )
+
+    finalize {
+        PreferenceScreen.VIDEO.addPreferences(
+            // Keep the preferences organized together.
+            PreferenceCategory(
+                key = "revanced_01_video_key", // Dummy key to force the quality preferences first.
+                titleKey = null,
+                sorting = Sorting.UNSORTED,
+                tag = "app.revanced.extension.shared.settings.preference.NoTitlePreferenceCategory",
+                preferences = settingsMenuVideoQualityGroup.toSet()
+            )
+        )
+    }
+}
