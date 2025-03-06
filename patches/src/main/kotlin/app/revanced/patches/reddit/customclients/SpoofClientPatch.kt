466d4e7dc35366fee90bbeb142ce9848f1778b77
Eric Ahn <wchill1337@gmail.com>
2025-03-06 00:44:35 -0800

    Create common interface for other reddit clients

--- patches/src/main/kotlin/app/revanced/patches/reddit/customclients/SpoofClientPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/reddit/customclients/SpoofClientPatch.kt
@@ -32,3 +32,51 @@ fun spoofClientPatch(
         ),
     )
 }
+
+/**
+ * Base class for patches that spoof the Reddit client.
+ *
+ * @param block The patch block. It is called with the client ID option, redirect URI
+ * option and user agent option.
+ */
+fun spoofClientPatch(
+    block: BytecodePatchBuilder.(
+        clientIdOption: Option<String>,
+        redirectUriOption: Option<String>,
+        userAgentOption: Option<String>,
+    ) -> Unit,
+) = bytecodePatch(
+    name = "Spoof client",
+    description = "Restores functionality of the app by using custom client ID.",
+) {
+    block(
+        stringOption(
+            "client-id",
+            null,
+            null,
+            "OAuth client ID",
+            "The Reddit OAuth client ID. " +
+                "You can get your client ID from https://www.reddit.com/prefs/apps. " +
+                "The application type has to be \"Installed app\" and the redirect " +
+                "URI has to match the value provided for the \"Redirect URI\" option.",
+            true,
+        ),
+        stringOption(
+            "redirect-uri",
+            "http://127.0.0.1:8080",
+            null,
+            "Redirect URI",
+            "The Reddit OAuth redirect URI. Should be a valid URI.",
+            true,
+        ),
+        stringOption(
+            "user-agent",
+            null,
+            null,
+            "User agent",
+            "The app's user agent. User agent should be in the format " +
+                    "\"<platform>:<app id>:<version> (by /u/<username>)\".",
+            true,
+        )
+    )
+}
--- patches/src/main/kotlin/app/revanced/patches/reddit/customclients/boostforreddit/api/SpoofClientPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/reddit/customclients/boostforreddit/api/SpoofClientPatch.kt
@@ -5,61 +5,18 @@ import app.revanced.patcher.Match
 import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
 import app.revanced.patcher.extensions.InstructionExtensions.getInstruction
 import app.revanced.patcher.extensions.InstructionExtensions.replaceInstruction
-import app.revanced.patcher.patch.BytecodePatchBuilder
-import app.revanced.patcher.patch.Option
-import app.revanced.patcher.patch.bytecodePatch
-import app.revanced.patcher.patch.stringOption
+import app.revanced.patches.reddit.customclients.spoofClientPatch
 import com.android.tools.smali.dexlib2.iface.instruction.OneRegisterInstruction
 
-@Suppress("unused")
-val spoofClientPatch = bytecodePatch(
-    name = "Spoof client",
-) {
+val spoofClientPatch = spoofClientPatch() { clientIdOption, redirectUriOption, userAgentOption ->
     compatibleWith("com.rubenmayayo.reddit")
-
-    val redirectUri by stringOption(
-        "redirect-uri",
-        "http://127.0.0.1:8080",
-        null,
-        "Redirect URI",
-        "The Reddit OAuth redirect URI. " +
-                "If you don't know what this means, don't change it. " +
-                "Make sure you update the \"Installed app\" you created at " +
-                "https://www.reddit.com/prefs/apps to have the same redirect URI " +
-                "as what you put here.",
-        true,
-    )
-
-    val clientId by stringOption(
-        "client-id",
-        null,
-        null,
-        "OAuth client ID",
-        "The Reddit OAuth client ID. " +
-                "You can get a client ID by creating an \"Installed app\" at " +
-                "https://www.reddit.com/prefs/apps. The redirect URI should be set to " +
-                "\"http://127.0.0.1:8080\" or whatever is specified for the redirect URI " +
-                "option.",
-        true,
-    )
-
-    val userAgent by stringOption(
-        "user-agent",
-        null,
-        null,
-        "User agent",
-        "The app's user agent. User agent should be in the format " +
-                "\"<platform>:<app id>:<version> (by /u/<username)\".",
-        true,
-    )
-
     execute {
         // region Patch client id.
 
         getClientIdFingerprint.method.addInstructions(
             0,
             """
-                 const-string v0, "$clientId"
+                 const-string v0, "$clientIdOption"
                  return-object v0
             """,
         )
@@ -70,8 +27,8 @@ val spoofClientPatch = bytecodePatch(
 
         val stringReplacements = mapOf(
             // Take advantage of the fact that String.format() will ignore extraneous parameters.
-            "%s:%s:%s (by /u/%s)" to userAgent,
-            "http://rubenmayayo.com" to redirectUri
+            "%s:%s:%s (by /u/%s)" to userAgentOption,
+            "http://rubenmayayo.com" to redirectUriOption
         )
 
         val replaceStrings = fun(methodFingerprint: Fingerprint) : Unit {
