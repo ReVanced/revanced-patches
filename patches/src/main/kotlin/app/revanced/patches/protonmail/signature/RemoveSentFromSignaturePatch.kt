84c53e21713659132e7015e80ece63edca3fcf93
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2025-03-06 11:32:33 +0200

    refactor: format code, use same naming convention as other patches

--- patches/src/main/kotlin/app/revanced/patches/protonmail/signature/RemoveSentFromSignaturePatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/protonmail/signature/RemoveSentFromSignaturePatch.kt
@@ -1,18 +1,18 @@
 package app.revanced.patches.protonmail.signature
 
 import app.revanced.patcher.patch.resourcePatch
-import org.w3c.dom.Element
 import app.revanced.util.findElementByAttributeValueOrThrow
 
 @Suppress("unused")
 val removeSentFromSignaturePatch = resourcePatch(
-    name = "Remove \"Sent from\" signature",
+    name = "Remove 'Sent from' signature",
     description = "Removes the 'Sent from Proton Mail mobile' signature from emails.",
 ) {
     compatibleWith("ch.protonmail.android")
+
     execute {
-        //TODO: This goes out of date if Proton Mail adds additional languages.
-        //It could be improved by dynamically iterating through all directories in /res/ .
+        //TODO: If Proton Mail adds additional languages then those languages will be ignored.
+        // This could be improved by dynamically iterating through all directories in /res
         val directories = listOf(
             "res/values",
             "res/values-b+es+419",
@@ -47,9 +47,13 @@ val removeSentFromSignaturePatch = resourcePatch(
             "res/values-zh-rCN",
             "res/values-zh-rTW",
         )
+
         directories.forEach { directory ->
             document("$directory/strings.xml").use { document ->
-                document.documentElement.childNodes.findElementByAttributeValueOrThrow("name", "mail_settings_identity_mobile_footer_default_free").textContent = ""
+                document.documentElement.childNodes.findElementByAttributeValueOrThrow(
+                    "name",
+                    "mail_settings_identity_mobile_footer_default_free"
+                ).textContent = ""
             }
         }
     }
