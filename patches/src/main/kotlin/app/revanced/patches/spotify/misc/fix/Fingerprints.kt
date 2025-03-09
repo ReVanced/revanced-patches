b013694a4413dafea5e6cf183f18276c62087d48
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2025-03-09 14:59:39 +0200

    refactor(Spotify): Use instruction filters

--- patches/src/main/kotlin/app/revanced/patches/spotify/misc/fix/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/spotify/misc/fix/Fingerprints.kt
@@ -1,5 +1,15 @@
 package app.revanced.patches.spotify.misc.fix
 
 import app.revanced.patcher.fingerprint
+import app.revanced.patcher.methodCall
+import app.revanced.patcher.opcode
+import app.revanced.patcher.string
+import com.android.tools.smali.dexlib2.Opcode
 
-internal val getAppSignatureFingerprint = fingerprint { strings("Failed to get the application signatures") }
+internal val getAppSignatureFingerprint by fingerprint {
+    instructions(
+        methodCall(parameters = listOf("Ljava/util/Collection;"), returnType = "Ljava/lang/String;"),
+        opcode(Opcode.MOVE_RESULT_OBJECT, maxAfter = 0),
+        string("Failed to get the application signatures", maxAfter = 10)
+    )
+}
