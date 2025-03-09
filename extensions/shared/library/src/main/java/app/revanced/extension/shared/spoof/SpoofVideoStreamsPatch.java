235ab7d759e7ddc3e955a77c53427e3960c9395a
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2025-03-09 16:06:12 +0200

    fix: Turn off 20.03+ playback feature flag that breaks stream spoofing
    Flag found by @0xrxL

--- extensions/shared/library/src/main/java/app/revanced/extension/shared/spoof/SpoofVideoStreamsPatch.java
+++ extensions/shared/library/src/main/java/app/revanced/extension/shared/spoof/SpoofVideoStreamsPatch.java
@@ -124,6 +124,21 @@ public class SpoofVideoStreamsPatch {
 
     /**
      * Injection point.
+     * Turns off a feature flag that interferes with video playback.
+     */
+    public static boolean usePlaybackStartFeatureFlag(boolean original) {
+        if (original) {
+            Logger.printDebug(() -> "usePlaybackStartFeatureFlag is set on");
+        }
+
+        if (!SPOOF_STREAMING_DATA) {
+            return original;
+        }
+        return false;
+    }
+
+    /**
+     * Injection point.
      */
     public static void fetchStreams(String url, Map<String, String> requestHeaders) {
         if (SPOOF_STREAMING_DATA) {
--- patches/src/main/kotlin/app/revanced/patches/shared/misc/spoof/SpoofVideoStreamsPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/shared/misc/spoof/SpoofVideoStreamsPatch.kt
@@ -30,7 +30,8 @@ internal const val EXTENSION_CLASS_DESCRIPTOR =
 
 fun spoofVideoStreamsPatch(
     block: BytecodePatchBuilder.() -> Unit = {},
-    applyMediaFetchHotConfigChanges: BytecodePatchBuilder.() -> Boolean = { false },
+    fixMediaFetchHotConfigChanges: BytecodePatchBuilder.() -> Boolean = { false },
+    fixParsePlaybackResponseFeatureFlag: BytecodePatchBuilder.() -> Boolean = { false },
     executeBlock: BytecodePatchContext.() -> Unit = {},
 ) = bytecodePatch(
     name = "Spoof video streams",
@@ -49,18 +50,20 @@ fun spoofVideoStreamsPatch(
 
         // region Block /initplayback requests to fall back to /get_watch requests.
 
-        val moveUriStringIndex = buildInitPlaybackRequestFingerprint.instructionMatches.first().index
 
-        buildInitPlaybackRequestFingerprint.method.apply {
-            val targetRegister = getInstruction<OneRegisterInstruction>(moveUriStringIndex).registerA
+        buildInitPlaybackRequestFingerprint.let {
+            it.method.apply {
+                val moveUriStringIndex = it.instructionMatches.first().index
+                val targetRegister = getInstruction<OneRegisterInstruction>(moveUriStringIndex).registerA
 
-            addInstructions(
-                moveUriStringIndex + 1,
-                """
-                    invoke-static { v$targetRegister }, $EXTENSION_CLASS_DESCRIPTOR->blockInitPlaybackRequest(Ljava/lang/String;)Ljava/lang/String;
-                    move-result-object v$targetRegister
-                """,
-            )
+                addInstructions(
+                    moveUriStringIndex + 1,
+                    """
+                        invoke-static { v$targetRegister }, $EXTENSION_CLASS_DESCRIPTOR->blockInitPlaybackRequest(Ljava/lang/String;)Ljava/lang/String;
+                        move-result-object v$targetRegister
+                    """
+                )
+            }
         }
 
         // endregion
@@ -68,8 +71,8 @@ fun spoofVideoStreamsPatch(
         // region Block /get_watch requests to fall back to /player requests.
 
         buildPlayerRequestURIFingerprint.let {
-            val invokeToStringIndex = it.instructionMatches.first().index
             it.method.apply {
+                val invokeToStringIndex = it.instructionMatches.first().index
                 val uriRegister = getInstruction<FiveRegisterInstruction>(invokeToStringIndex).registerC
 
                 addInstructions(
@@ -242,11 +245,20 @@ fun spoofVideoStreamsPatch(
 
         // region turn off stream config replacement feature flag.
 
-        if (applyMediaFetchHotConfigChanges()) {
+        if (fixMediaFetchHotConfigChanges()) {
             mediaFetchHotConfigFingerprint.let {
                 it.method.insertFeatureFlagBooleanOverride(
                     it.instructionMatches.first().index,
                     "$EXTENSION_CLASS_DESCRIPTOR->useMediaFetchHotConfigReplacement(Z)Z"
+                )
+            }
+        }
+
+        if (fixParsePlaybackResponseFeatureFlag()) {
+            playbackStartDescriptorFeatureFlagFingerprint.let {
+                it.method.insertFeatureFlagBooleanOverride(
+                    it.instructionMatches.first().index,
+                    "$EXTENSION_CLASS_DESCRIPTOR->usePlaybackStartFeatureFlag(Z)Z"
                 )
             }
         }
