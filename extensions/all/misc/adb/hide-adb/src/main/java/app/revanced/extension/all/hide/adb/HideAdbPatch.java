99c1c5f77d0f10864fc1b95daa6f6f990acc71e9
1fexd <PGFvy85nC@protonmail.com>
2025-03-14 20:03:38 +0100

    chore: Slight cleanup (2)

--- extensions/all/misc/adb/hide-adb/src/main/java/app/revanced/extension/all/hide/adb/HideAdbPatch.java
+++ extensions/all/misc/adb/hide-adb/src/main/java/app/revanced/extension/all/hide/adb/HideAdbPatch.java
@@ -3,21 +3,15 @@ package app.revanced.extension.all.hide.adb;
 import android.content.ContentResolver;
 import android.provider.Settings;
 
+import java.util.Arrays;
+import java.util.List;
+
 @SuppressWarnings("unused")
 public final class HideAdbPatch {
-    private static final String[] SPOOF_SETTINGS = new String[]{"adb_enabled", "adb_wifi_enabled", "development_settings_enabled"};
-
-    private static boolean shouldSpoof(String name) {
-        for (String spoofSetting : SPOOF_SETTINGS) {
-            if (!spoofSetting.equals(name)) continue;
-            return true;
-        }
-
-        return false;
-    }
+    private static final List<String> SPOOF_SETTINGS = Arrays.asList("adb_enabled", "adb_wifi_enabled", "development_settings_enabled");
 
     public static int getInt(ContentResolver cr, String name) throws Settings.SettingNotFoundException {
-        if (shouldSpoof(name)) {
+        if (SPOOF_SETTINGS.contains(name)) {
             return 0;
         }
 
@@ -25,7 +19,7 @@ public final class HideAdbPatch {
     }
 
     public static int getInt(ContentResolver cr, String name, int def) {
-        if (shouldSpoof(name)) {
+        if (SPOOF_SETTINGS.contains(name)) {
             return 0;
         }
 
