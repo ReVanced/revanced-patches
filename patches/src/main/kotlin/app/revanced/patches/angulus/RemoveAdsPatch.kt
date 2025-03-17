d82d4090492958c4e4e29c95c88cbfa052b450ba
Denis Kerner <denis.kerner@vodafone.com>
2025-03-17 20:32:57 +0100

    feat(Angulus): Add ad removal patch with daily measurement override

---
+++ patches/src/main/kotlin/app/revanced/patches/angulus/RemoveAdsPatch.kt
@@ -0,0 +1,24 @@
+package app.revanced.patches.angulus
+
+import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
+import app.revanced.patcher.patch.bytecodePatch
+
+@Suppress("unused")
+val angulusPatch = bytecodePatch(
+    name = "Angulus Ad removal patch",
+    description = "Unlimited daily measurements.",
+) {
+    compatibleWith("com.drinkplusplus.angulus"("5.0.20"))
+
+    execute {
+        // Always returns 1 as the daily measurement count
+        angulusAdsFingerprint.method.addInstructions(
+            0, 
+            """
+                const/4 v0, 0x1
+                return v0
+            """,
+            )
+    }
+
+}
