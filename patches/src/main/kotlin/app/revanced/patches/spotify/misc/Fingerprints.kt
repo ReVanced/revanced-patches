f048c50e56fc1f5a5c607860be4206ef83b528fe
oSumAtrIX <johan.melkonyan1@web.de>
2025-03-26 04:24:47 +0100

    feat(Spotify): Add `Unlock premium` patch (#4644)
    Co-authored-by: Nuckyz <61953774+Nuckyz@users.noreply.github.com>
    Co-authored-by: Brosssh <tiabroch@gmail.com>

---
+++ patches/src/main/kotlin/app/revanced/patches/spotify/misc/Fingerprints.kt
@@ -0,0 +1,18 @@
+package app.revanced.patches.spotify.misc
+
+import app.revanced.patcher.fingerprint
+
+internal val accountAttributeFingerprint = fingerprint {
+    custom { _, c -> c.endsWith("internal/AccountAttribute;") }
+}
+
+internal val productStateProtoFingerprint = fingerprint {
+    returns("Ljava/util/Map;")
+    custom { _, classDef ->
+        classDef.endsWith("ProductStateProto;")
+    }
+}
+
+internal val buildQueryParametersFingerprint = fingerprint {
+    strings("trackRows", "device_type:tablet")
+}
