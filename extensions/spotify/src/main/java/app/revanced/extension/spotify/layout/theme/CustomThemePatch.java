0442c1d5ef454d3145171339e30c984ece043365
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2025-03-29 23:39:41 +0100

    fix: Catch invalid color options

--- extensions/spotify/src/main/java/app/revanced/extension/spotify/layout/theme/CustomThemePatch.java
+++ extensions/spotify/src/main/java/app/revanced/extension/spotify/layout/theme/CustomThemePatch.java
@@ -2,14 +2,24 @@ package app.revanced.extension.spotify.layout.theme;
 
 import android.graphics.Color;
 
+import app.revanced.extension.shared.Logger;
 import app.revanced.extension.shared.Utils;
 
 @SuppressWarnings("unused")
 public final class CustomThemePatch {
+
+    /**
+     * Injection point.
+     */
     public static long getColorInt(String colorString) {
-        if (colorString.startsWith("#")) {
-            return Long.valueOf(Color.parseColor(colorString));
+        try {
+            if (colorString.startsWith("#")) {
+                return Color.parseColor(colorString);
+            }
+            return Utils.getResourceColor(colorString);
+        } catch (Exception ex) {
+            Logger.printException(() -> "Invalid custom color: " + colorString, ex);
+            return Color.BLACK;
         }
-        return Long.valueOf(Utils.getResourceColor(colorString));
     }
 }
