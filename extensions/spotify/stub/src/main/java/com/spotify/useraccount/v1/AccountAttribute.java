c850062700a29a9e87d176db386a614f7dca692d
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2025-04-08 15:17:26 +0200

    refactor

--- extensions/spotify/stub/src/main/java/com/spotify/useraccount/v1/AccountAttribute.java
+++ extensions/spotify/stub/src/main/java/com/spotify/useraccount/v1/AccountAttribute.java
@@ -1,7 +1,7 @@
 package com.spotify.useraccount.v1;
 
 /**
- * Used for target 8.6.98.900.  Class is still present in newer app targets.
+ * Used for target 8.6.98.900. Class is still present in newer app targets.
  */
 public class AccountAttribute {
     public Object value_;
--- patches/src/main/kotlin/app/revanced/patches/spotify/misc/extension/Hooks.kt
+++ patches/src/main/kotlin/app/revanced/patches/spotify/misc/extension/Hooks.kt
@@ -2,6 +2,8 @@ package app.revanced.patches.spotify.misc.extension
 
 import app.revanced.patches.shared.misc.extension.extensionHook
 
+private const val SPOTIFY_MAIN_ACTIVITY = "Lcom/spotify/music/SpotifyMainActivity;"
+
 /**
  * Main activity of target 8.6.98.900.
  */
@@ -9,7 +11,7 @@ internal const val SPOTIFY_MAIN_ACTIVITY_LEGACY = "Lcom/spotify/music/MainActivi
 
 internal val spotifyMainActivityOnCreate = extensionHook {
     custom { method, classDef ->
-        method.name == "onCreate" && (classDef.type == "Lcom/spotify/music/SpotifyMainActivity;"
+        method.name == "onCreate" && (classDef.type == SPOTIFY_MAIN_ACTIVITY
                 || classDef.type == SPOTIFY_MAIN_ACTIVITY_LEGACY)
     }
 }
