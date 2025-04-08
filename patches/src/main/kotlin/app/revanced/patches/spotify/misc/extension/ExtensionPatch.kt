dec46f6474fb9e5b314d67a9dc1ae8a0e9af3a73
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2025-04-08 15:20:42 +0200

    comments

--- patches/src/main/kotlin/app/revanced/patches/spotify/misc/extension/ExtensionPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/spotify/misc/extension/ExtensionPatch.kt
@@ -4,7 +4,9 @@ import app.revanced.patcher.patch.bytecodePatch
 import app.revanced.patches.shared.misc.extension.sharedExtensionPatch
 
 /**
- * If patching 8.6.98.900.
+ * If patching a legacy 8.x target. This may also be set if patching slightly older/newer app targets,
+ * but the only legacy target of interest is 8.6.98.900 as it's the last version that
+ * supports Spotify integration on Kenwood/Pioneer car stereos.
  */
 internal var IS_SPOTIFY_LEGACY_APP_TARGET = false
 
