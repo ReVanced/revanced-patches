671fc7999773ed776818958cc9324ac5c26df8e5
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2025-04-11 13:37:20 +0200

    fix(Google Photos): Add warning if patching using ReVanced manager

--- patches/src/main/kotlin/app/revanced/patches/googlephotos/misc/gms/GmsCoreSupportPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/googlephotos/misc/gms/GmsCoreSupportPatch.kt
@@ -5,6 +5,8 @@ import app.revanced.patches.googlephotos.misc.extension.extensionPatch
 import app.revanced.patches.googlephotos.misc.gms.Constants.PHOTOS_PACKAGE_NAME
 import app.revanced.patches.googlephotos.misc.gms.Constants.REVANCED_PHOTOS_PACKAGE_NAME
 import app.revanced.patches.shared.misc.gms.gmsCoreSupportPatch
+import app.revanced.util.isAndroidRuntime
+import java.util.logging.Logger
 
 @Suppress("unused")
 val gmsCoreSupportPatch = gmsCoreSupportPatch(
@@ -13,6 +15,27 @@ val gmsCoreSupportPatch = gmsCoreSupportPatch(
     mainActivityOnCreateFingerprint = homeActivityOnCreateFingerprint,
     extensionPatch = extensionPatch,
     gmsCoreSupportResourcePatchFactory = ::gmsCoreSupportResourcePatch,
+    executeBlock = {
+        // Resource compilation fails if patching inside ReVanced Manager.
+        // This check is only needed for GmsCore support since all other patches
+        // are bytecode and should work if using root install without GmsCore.
+        // TODO: Remove this logic after Manager resource compilation is fixed.
+        if (isAndroidRuntime()) {
+            Logger.getLogger(this::class.java.name).severe(
+                """
+                    !!! Patching in this environment is not yet supported.
+                    !!!
+                    !!! Google Photos cannot be patched using ReVanced Manager.
+                    !!!
+                    !!! To Patch Google Photos, please patch using command prompt and ReVanced CLI tools.
+                    !!! 
+                """.trimIndent()
+            )
+
+            // Add a delay, so the user is more likely to read the warning.
+            Thread.sleep(30_000)
+        }
+    }
 ) {
     compatibleWith(PHOTOS_PACKAGE_NAME)
 }
--- patches/src/main/kotlin/app/revanced/util/Utils.kt
+++ patches/src/main/kotlin/app/revanced/util/Utils.kt
@@ -6,3 +6,15 @@ internal object Utils {
             .joinToString("\n") { it.trimIndent() } // Remove the leading whitespace from each line.
             .trimIndent() // Remove the leading newline.
 }
+
+/**
+ * @return If the current VM environment is an Android device. This will return false
+ *         if running Termux or any other virtualized environment inside Android.
+ */
+internal fun isAndroidRuntime() =
+    try {
+        Class.forName("android.os.Build")
+        true
+    } catch (_: ClassNotFoundException) {
+        false
+    }
