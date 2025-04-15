0f687ecfd34ac6d1e791eb648afbd0d1abecfddb
oSumAtrIX <johan.melkonyan1@web.de>
2025-04-15 12:53:26 +0200

    fix(Spotify): Fix login by replacing `Spoof signature` patch with new `Spoof package info` patch (#4794)
    Co-authored-by: LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>

--- patches/src/main/kotlin/app/revanced/patches/spotify/misc/fix/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/spotify/misc/fix/Fingerprints.kt
@@ -2,4 +2,16 @@ package app.revanced.patches.spotify.misc.fix
 
 import app.revanced.patcher.fingerprint
 
-internal val getAppSignatureFingerprint = fingerprint { strings("Failed to get the application signatures") }
+internal val getPackageInfoFingerprint = fingerprint {
+    strings(
+        "Failed to get the application signatures",
+        "Failed to get installer package"
+    )
+}
+
+internal val getPackageInfoLegacyFingerprint = fingerprint {
+    strings(
+        "Failed to get the application signatures"
+    )
+}
+
