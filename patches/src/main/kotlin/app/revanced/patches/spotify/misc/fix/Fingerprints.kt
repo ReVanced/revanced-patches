7b2bae79df591da0da70accb5c23ec9a1a62f156
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2025-04-15 13:18:47 +0200

    refactor(Spotify - Spoof package info): Replace installer package name of legacy target

--- patches/src/main/kotlin/app/revanced/patches/spotify/misc/fix/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/spotify/misc/fix/Fingerprints.kt
@@ -4,14 +4,6 @@ import app.revanced.patcher.fingerprint
 
 internal val getPackageInfoFingerprint = fingerprint {
     strings(
-        "Failed to get the application signatures",
-        "Failed to get installer package"
-    )
-}
-
-internal val getPackageInfoLegacyFingerprint = fingerprint {
-    strings(
         "Failed to get the application signatures"
     )
 }
-
