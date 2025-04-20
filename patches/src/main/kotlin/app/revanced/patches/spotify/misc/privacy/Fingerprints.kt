05ca46dbb4d1b60421fe3515df8d2e1fea8c5aa6
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2025-04-20 20:09:33 +0200

    work in progress: Modify share url while preserving logging

--- patches/src/main/kotlin/app/revanced/patches/spotify/misc/privacy/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/spotify/misc/privacy/Fingerprints.kt
@@ -1,15 +1,21 @@
 package app.revanced.patches.spotify.misc.privacy
 
 import app.revanced.patcher.fingerprint
+import app.revanced.util.literal
 import com.android.tools.smali.dexlib2.AccessFlags
 
-internal val shareUrlToStringFingerprint = fingerprint {
-    strings("ShareUrl", "shareId") // partial matches
-    custom { method, _ ->
-        method.name == "toString"
+internal val androidShareSheetUrlFormatterFingerprint = fingerprint {
+    accessFlags(AccessFlags.PUBLIC, AccessFlags.STATIC)
+    returns("Ljava/lang/String;")
+    parameters("L", "Ljava/lang/String;")
+    literal {
+        '\n'.code.toLong()
     }
 }
 
-internal val shareUrlConstructorFingerprint = fingerprint {
-    accessFlags(AccessFlags.PUBLIC, AccessFlags.CONSTRUCTOR)
+internal val copyUrlFormatterFingerprint = fingerprint {
+    accessFlags(AccessFlags.PUBLIC, AccessFlags.FINAL)
+    returns("Ljava/lang/Object;")
+    parameters("Ljava/lang/Object;")
+    strings("clipboard", "Spotify Link")
 }
--- patches/src/main/kotlin/app/revanced/patches/spotify/misc/privacy/SanitizeSharingLinksPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/spotify/misc/privacy/SanitizeSharingLinksPatch.kt
@@ -1,8 +1,13 @@
 package app.revanced.patches.spotify.misc.privacy
 
 import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
+import app.revanced.patcher.extensions.InstructionExtensions.getInstruction
 import app.revanced.patcher.patch.bytecodePatch
 import app.revanced.patches.spotify.misc.extension.sharedExtensionPatch
+import app.revanced.util.getReference
+import app.revanced.util.indexOfFirstInstructionOrThrow
+import com.android.tools.smali.dexlib2.iface.instruction.FiveRegisterInstruction
+import com.android.tools.smali.dexlib2.iface.reference.MethodReference
 
 private const val EXTENSION_CLASS_DESCRIPTOR =
     "Lapp/revanced/extension/spotify/misc/privacy/SanitizeSharingLinksPatch;"
@@ -17,25 +22,33 @@ val sanitizeSharingLinksPatch = bytecodePatch(
     dependsOn(sharedExtensionPatch)
 
     execute {
-        shareUrlConstructorFingerprint.match(shareUrlToStringFingerprint.originalClassDef).method.apply {
+        val extensionMethodDescriptor = "$EXTENSION_CLASS_DESCRIPTOR->" +
+                "sanitizeUrl(Ljava/lang/String;)Ljava/lang/String;"
+
+        androidShareSheetUrlFormatterFingerprint.method.addInstructions(
+            0,
+            """
+                invoke-static { p1 }, $extensionMethodDescriptor
+                move-result-object p1
+            """
+        )
+
+        copyUrlFormatterFingerprint.method.apply {
+            val index = indexOfFirstInstructionOrThrow {
+                val reference = getReference<MethodReference>()
+                reference?.name == "newPlainText"
+            }
+            val register = getInstruction<FiveRegisterInstruction>(index).registerD
+
             addInstructions(
-                0,
+                index,
                 """
-                    invoke-static {p1}, $EXTENSION_CLASS_DESCRIPTOR->sanitizeUrl(Ljava/lang/String;)Ljava/lang/String;
-                    
-                    move-result-object p1
+                    invoke-static { v$register }, $extensionMethodDescriptor
+                    move-result-object v$register
                 """
             )
-            if (parameters.count() > 3) { // sanitize fullUrl (not present in legacy app version)
-                addInstructions(
-                    0,
-                    """
-                        invoke-static {p4}, $EXTENSION_CLASS_DESCRIPTOR->sanitizeUrl(Ljava/lang/String;)Ljava/lang/String;
-                        
-                        move-result-object p4
-                    """
-                )
-            }
         }
+
+        // TODO: Patch the sharing of music to X/Twitter and patch the multiple TikTok share sheets.
     }
 }
