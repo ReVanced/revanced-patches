573c1d893c83b8b7a88d1364a8ec75a0b0ee3988
hoodles <207470673+hoo-dles@users.noreply.github.com>
2025-04-29 14:36:47 -0700

    Wrap extension in try block, and additional comments

--- extensions/primevideo/src/main/java/app/revanced/extension/primevideo/ads/SkipAdsPatch.java
+++ extensions/primevideo/src/main/java/app/revanced/extension/primevideo/ads/SkipAdsPatch.java
@@ -10,16 +10,25 @@ import com.amazon.avod.media.ads.internal.state.ServerInsertedAdBreakState;
 @SuppressWarnings("unused")
 public final class SkipAdsPatch {
     public static void enterServerInsertedAdBreakState(ServerInsertedAdBreakState state, AdBreakTrigger trigger, VideoPlayer player) {
-        AdBreak adBreak = trigger.getBreak();
+        try {
+            AdBreak adBreak = trigger.getBreak();
 
-        if (trigger.getSeekStartPosition() != null)
-            // If scrubbing over an ad, seek straight to it.
-            player.seekTo(trigger.getSeekTarget().getTotalMilliseconds());
-        else
-            // If naturally entering an ad, seek to end of the ad break.
-            player.seekTo(player.getCurrentPosition() + adBreak.getDurationExcludingAux().getTotalMilliseconds());
+            // There are two scenarios when entering the original method:
+            //  1. Player naturally entered an ad break while watching a video.
+            //  2. User is skipped/scrubbed to a position on the timeline. If seek position is past an ad break,
+            //     user is forced to watch an ad before continuing.
+            //
+            // Scenario 2 is indicated by trigger.getSeekStartPosition() != null, so skip directly to the scrubbing
+            // target. Otherwise, just calculate when the ad break should end and skip to there.
+            if (trigger.getSeekStartPosition() != null)
+                player.seekTo(trigger.getSeekTarget().getTotalMilliseconds());
+            else
+                player.seekTo(player.getCurrentPosition() + adBreak.getDurationExcludingAux().getTotalMilliseconds());
 
-        // Send "end of ads" trigger to state machine so everything doesn't get whacky.
-        state.doTrigger(new SimpleTrigger(AdEnabledPlayerTriggerType.NO_MORE_ADS_SKIP_TRANSITION));
+            // Send "end of ads" trigger to state machine so everything doesn't get whacky.
+            state.doTrigger(new SimpleTrigger(AdEnabledPlayerTriggerType.NO_MORE_ADS_SKIP_TRANSITION));
+        } catch (Exception ex) {
+            Logger.printException(() -> "Failed skipping ads", ex);
+        }
     }
 }
