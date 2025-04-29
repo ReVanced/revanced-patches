f71e5c88ced06380d69fa6ca3da23941feaf07f5
Nuckyz <61953774+Nuckyz@users.noreply.github.com>
2025-04-29 11:23:21 -0300

    Refactor

--- extensions/pandora/src/main/kotlin/app/revanced/extension/pandora/misc/hook/BaseJsonHook.kt
+++
@@ -1,9 +0,0 @@
-package app.revanced.extension.pandora.misc.hook
-
-import org.json.JSONObject
-
-abstract class BaseJsonHook : JsonHook {
-    abstract fun apply(json: JSONObject)
-
-    override fun transform(json: JSONObject) = json.apply { apply(json) }
-}
