bd53955df738bb7b819eb91a3e776e9d2ca5c74a
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2025-05-05 15:25:25 +0400

    fix(YouTube): Simplify litho filtering patch (#4910)

--- extensions/youtube/src/main/java/app/revanced/extension/youtube/patches/components/LithoFilterPatch.java
+++ extensions/youtube/src/main/java/app/revanced/extension/youtube/patches/components/LithoFilterPatch.java
@@ -87,6 +87,10 @@ public final class LithoFilterPatch {
      * the buffer is saved to a ThreadLocal so each calling thread does not interfere with other threads.
      */
     private static final ThreadLocal<ByteBuffer> bufferThreadLocal = new ThreadLocal<>();
+    /**
+     * Results of calling {@link #filter(String, StringBuilder)}.
+     */
+    private static final ThreadLocal<Boolean> filterResult = new ThreadLocal<>();
 
     static {
         for (Filter filter : filters) {
@@ -141,10 +145,21 @@ public final class LithoFilterPatch {
     }
 
     /**
+     * Injection point.
+     */
+    public static boolean shouldFilter() {
+        Boolean shouldFilter = filterResult.get();
+        return shouldFilter != null && shouldFilter;
+    }
+
+    /**
      * Injection point.  Called off the main thread, and commonly called by multiple threads at the same time.
      */
-    @SuppressWarnings("unused")
-    public static boolean filter(@Nullable String lithoIdentifier, @NonNull StringBuilder pathBuilder) {
+    public static void filter(@Nullable String lithoIdentifier, StringBuilder pathBuilder) {
+        filterResult.set(handleFiltering(lithoIdentifier, pathBuilder));
+    }
+
+    private static boolean handleFiltering(@Nullable String lithoIdentifier, StringBuilder pathBuilder) {
         try {
             if (pathBuilder.length() == 0) {
                 return false;
