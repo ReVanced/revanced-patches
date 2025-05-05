c9988cc216d5fd8ef05b46d0ffb81ef85426a31e
oSumAtrIX <johan.melkonyan1@web.de>
2025-05-05 15:30:54 +0200

    Apply suggestions from code review

--- patches/src/main/kotlin/app/revanced/patches/instagram/misc/signature/SignatureCheckPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/instagram/misc/signature/SignatureCheckPatch.kt
@@ -17,11 +17,11 @@ val signatureCheckPatch = bytecodePatch(
     // Patching method is inspired by:
     // https://github.com/mamiiblt/instafel/blob/032c6a714a4a862462cd4bcd106083f640b13219/instafel.patcher/src/main/java/me/mamiiblt/instafel/patcher/patches/fix/FixSecureCtxCrash.java
     //
-    // Logic has been adapted to rely less on garbled method names that are likely to change. Instagram's code is
-    // highly obfuscated, so any comments on code flow are best guesses.
+    // Logic has been adapted to rely less on garbled method names that are likely to change.
+    // Instagram's code is highly obfuscated, so any comments on code flow are best guesses.
     execute {
-        // Main activity insertion point from NotificationAction receiver bypasses IgSecureContext check. Get the
-        // method it calls, and use it to replace the method called by the Launcher.
+        // Main activity insertion point from NotificationAction receiver bypasses IgSecureContext check.
+        // Get the method it calls, and use it to replace the method called by the launcher.
         val safeMethod = onReceiveNotificationFingerprint.let { it ->
             navigate(it.method)
                 .to(it.patternMatch!!.startIndex) // navigate into invoke-static
