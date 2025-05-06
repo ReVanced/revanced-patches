19cc2aff94375a9205846896f2e7bc2decb7efb5
hoodles <207470673+hoo-dles@users.noreply.github.com>
2025-05-05 22:35:24 -0700

    Updating patch to be simpler without convoluted logic

--- patches/src/main/kotlin/app/revanced/patches/instagram/misc/signature/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/instagram/misc/signature/Fingerprints.kt
@@ -1,27 +1,15 @@
 package app.revanced.patches.instagram.misc.signature
 
 import app.revanced.patcher.fingerprint
+import com.android.tools.smali.dexlib2.AccessFlags
 import com.android.tools.smali.dexlib2.Opcode
 
-internal val launcherFingerprint = fingerprint {
-    opcodes(
-        Opcode.INVOKE_STATIC,
-        Opcode.INVOKE_VIRTUAL,
-        Opcode.RETURN_VOID
-    )
-    strings(
-        "com.instagram.mainactivity.InstagramMainActivity",
-        "redirect_from_launcher_activity"
-    )
+internal val isValidSignatureClassFingerprint = fingerprint {
+    strings("The provider for uri '", "' is not trusted: ")
 }
 
-internal val onReceiveNotificationFingerprint = fingerprint {
-    opcodes(
-        Opcode.INVOKE_STATIC,
-        Opcode.CONST,
-        Opcode.GOTO
-    )
-    custom { method, classDef ->
-        method.name == "onReceive" && classDef.endsWith("/NotificationActionReceiver;")
-    }
+internal val isValidSignatureMethodFingerprint = fingerprint {
+    accessFlags(AccessFlags.PUBLIC, AccessFlags.FINAL)
+    parameters("L", "Z")
+    returns("Z")
 }
