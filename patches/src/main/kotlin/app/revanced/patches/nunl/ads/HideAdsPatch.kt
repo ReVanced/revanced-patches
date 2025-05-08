1104c4522b3a7c0ad117eefec169337282dc6292
jabbink <git@abb.ink>
2025-05-08 18:15:21 +0200

    feat(NU.nl): Support version 11.3.0

--- patches/src/main/kotlin/app/revanced/patches/nunl/ads/HideAdsPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/nunl/ads/HideAdsPatch.kt
@@ -2,6 +2,7 @@ package app.revanced.patches.nunl.ads
 
 import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
 import app.revanced.patcher.extensions.InstructionExtensions.getInstruction
+import app.revanced.patcher.extensions.InstructionExtensions.removeInstructions
 import app.revanced.patcher.patch.bytecodePatch
 import app.revanced.patches.shared.misc.extension.sharedExtensionPatch
 import com.android.tools.smali.dexlib2.iface.instruction.OneRegisterInstruction
@@ -11,23 +12,14 @@ val hideAdsPatch = bytecodePatch(
     name = "Hide ads",
     description = "Hide ads and sponsored articles in list pages and remove pre-roll ads on videos.",
 ) {
-    compatibleWith("nl.sanomamedia.android.nu"("11.0.0", "11.0.1", "11.1.0"))
+    compatibleWith("nl.sanomamedia.android.nu"("11.3.0"))
 
     dependsOn(sharedExtensionPatch("nunl", mainActivityOnCreateHook))
 
     execute {
         // Disable video pre-roll ads.
-        // Whenever the app tries to create an ad via JWUtils.createAdvertising, don't actually tell the underlying JWPlayer library to do so => JWPlayer will not display ads.
-        jwUtilCreateAdvertisementFingerprint.method.addInstructions(
-            0,
-            """
-                new-instance v0, Lcom/jwplayer/pub/api/configuration/ads/VastAdvertisingConfig${'$'}Builder;
-                invoke-direct { v0 }, Lcom/jwplayer/pub/api/configuration/ads/VastAdvertisingConfig${'$'}Builder;-><init>()V
-                invoke-virtual { v0 }, Lcom/jwplayer/pub/api/configuration/ads/VastAdvertisingConfig${'$'}Builder;->build()Lcom/jwplayer/pub/api/configuration/ads/VastAdvertisingConfig;
-                move-result-object v0
-                return-object v0
-            """,
-        )
+        // Whenever the app tries to define the advertising config for JWPlayer, don't set the advertising config and directly return.
+        jwPlayerConfigFingerprint.method.removeInstructions(1)
 
         // Filter injected content from API calls out of lists.
         arrayOf(screenMapperFingerprint, nextPageRepositoryImplFingerprint).forEach {
