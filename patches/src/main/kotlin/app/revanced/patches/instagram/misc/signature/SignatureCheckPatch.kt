784f1affb88500ee9f80f517ed5f4865bdd14dc4
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2025-05-11 22:24:25 +0400

    Update patches/src/main/kotlin/app/revanced/patches/instagram/misc/signature/SignatureCheckPatch.kt
    Co-authored-by: Nuckyz <61953774+Nuckyz@users.noreply.github.com>

--- patches/src/main/kotlin/app/revanced/patches/instagram/misc/signature/SignatureCheckPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/instagram/misc/signature/SignatureCheckPatch.kt
@@ -6,7 +6,7 @@ import app.revanced.util.returnEarly
 @Suppress("unused")
 val signatureCheckPatch = bytecodePatch(
     name = "Disable signature check",
-    description = "Disables the signature check that causes any modified app to crash on startup."
+    description = "Disables the signature check that causes the app to crash on startup."
 ) {
     compatibleWith("com.instagram.android"("378.0.0.52.68"))
 
