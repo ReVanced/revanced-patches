7b26ce51eb3f0d9d7a53155e3c162ca8f1b786ff
brosssh <tiabroch@gmail.com>
2025-05-12 08:44:26 +0200

    Cleanup

--- patches/src/main/kotlin/app/revanced/patches/spotify/misc/lyrics/CustomLyricsPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/spotify/misc/lyrics/CustomLyricsPatch.kt
@@ -1,7 +1,5 @@
 package app.revanced.patches.spotify.misc.lyrics
 
-import app.revanced.patcher.Fingerprint
-import app.revanced.patcher.extensions.InstructionExtensions.addInstruction
 import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
 import app.revanced.patcher.extensions.InstructionExtensions.getInstruction
 import app.revanced.patcher.extensions.InstructionExtensions.instructions
@@ -9,13 +7,10 @@ import app.revanced.patcher.extensions.InstructionExtensions.replaceInstruction
 import app.revanced.patcher.patch.bytecodePatch
 import app.revanced.patcher.patch.stringOption
 import app.revanced.patcher.util.proxy.mutableTypes.MutableMethod.Companion.toMutable
-import app.revanced.util.indexOfFirstInstructionOrThrow
 import app.revanced.util.indexOfFirstInstructionReversedOrThrow
 import com.android.tools.smali.dexlib2.Opcode
 import com.android.tools.smali.dexlib2.builder.MutableMethodImplementation
-import com.android.tools.smali.dexlib2.builder.instruction.BuilderInstruction21t
 import com.android.tools.smali.dexlib2.builder.instruction.BuilderInstruction35c
-import com.android.tools.smali.dexlib2.iface.instruction.FiveRegisterInstruction
 import com.android.tools.smali.dexlib2.iface.instruction.OneRegisterInstruction
 import com.android.tools.smali.dexlib2.immutable.ImmutableMethod
 import com.android.tools.smali.dexlib2.immutable.reference.ImmutableMethodReference
@@ -23,95 +18,75 @@ import com.android.tools.smali.dexlib2.immutable.reference.ImmutableMethodRefere
 
 @Suppress("unused")
 val customLyricsPatch = bytecodePatch(
-    name = "Custom lyrics provider",
-    description = "Make Spotify use a custom lyrics provider.",
+    name = "Change lyrics provider",
+    description = "Changes the lyrics provider to a custom one.",
 ) {
     compatibleWith("com.spotify.music")
 
-    val lyricsProviderUrlOption = stringOption(
+    var lyricsUrlHost by stringOption(
         key = "lyricsProviderUrl",
-        title = "Lyrics provider URL",
-        description = "The custom lyrics provider URL.",
-        required = true,
+        title = "Lyrics provider URL/hostname",
+        description = "The URL or the hostname to a custom lyrics provider, " +
+                "such as SpotifyMobileLyricsAPI: https://github.com/Natoune/SpotifyMobileLyricsAPI.",
+        required = true
     )
 
     execute {
-        val lyricsProviderUrl by lyricsProviderUrlOption()
+        lyricsUrlHost = lyricsUrlHost!!
+            .removePrefix("http://")
+            .removePrefix("https://")
+            .substringBefore("/")
+
         var patchedClientMethod: ImmutableMethod?
 
-        fun addLyricsProviderToValidHosts(fingerprint: Fingerprint) {
-            with(fingerprint) {
-                val validUrlAssignmentIndex = method.indexOfFirstInstructionOrThrow(
-                    stringMatches!!.first().index, Opcode.CONST_STRING
-                )
-
-                val urlComparisonIndex = method.indexOfFirstInstructionOrThrow(
-                    validUrlAssignmentIndex,
-                    Opcode.INVOKE_VIRTUAL
-                )
-
-                val ifValidUrlIndex = method.indexOfFirstInstructionOrThrow(
-                    urlComparisonIndex,
-                    Opcode.IF_NEZ
-                )
-
-                val label = method.getInstruction<BuilderInstruction21t>(ifValidUrlIndex).target
-
-                val (stringRegister, compareToRegister) = method.getInstruction<FiveRegisterInstruction>(urlComparisonIndex).let { it.registerC to it.registerD }
-
-                method.addInstructions(ifValidUrlIndex + 1,
-                    """
-                    const-string v$stringRegister, "$lyricsProviderUrl"
-                    
-                    invoke-virtual {v$compareToRegister, v$stringRegister}, Ljava/lang/Object;->equals(Ljava/lang/Object;)Z
-                    
-                    move-result v$stringRegister
-                """
-                )
-
-                method.addInstruction(ifValidUrlIndex + 1 + 3,
-                    BuilderInstruction21t(Opcode.IF_NEZ, stringRegister, label))
-            }
-        }
-
-        addLyricsProviderToValidHosts(oauthHostnameCheckFingerprint)
-        addLyricsProviderToValidHosts(webgateHostnameCheckFingerprint)
-
-        val clientBuilderMethodName = "getCustomLyricsProviderClient"
-
-        with(clientBuilderFingerprint) {
-            val clientStringAssignmentIndex = method.indexOfFirstInstructionOrThrow(
-                stringMatches!!.first().index,
-                Opcode.CONST_STRING,
-            )
-
-            val urlRegister = method.getInstruction<OneRegisterInstruction>(clientStringAssignmentIndex).registerA
-
+        clientBuilderFingerprint.method.apply {
+            /**
+             * Copy the method definition of the HTTP client builder for a valid hostname.
+             */
             patchedClientMethod = ImmutableMethod(
-                method.definingClass,
-                clientBuilderMethodName,
-                method.parameters,
-                method.returnType,
-                method.accessFlags,
+                definingClass,
+                "getCustomLyricsProviderClient",
+                parameters,
+                returnType,
+                accessFlags,
                 null,
                 null,
                 MutableMethodImplementation(6),
             )
 
-            classDef.methods.add(
-                patchedClientMethod!!.toMutable().apply {
-                    addInstructions(method.instructions)
+            val urlRegister = getInstruction<OneRegisterInstruction>(
+                clientBuilderFingerprint.stringMatches!!.first().index,
+            ).registerA
 
-                    replaceInstruction(clientStringAssignmentIndex, "const-string v$urlRegister, \"$lyricsProviderUrl\"")
+            /**
+             * Copy the instructions into the new method and replace the assigned hostname with our host.
+             */
+            clientBuilderFingerprint.classDef.methods.add(
+                patchedClientMethod!!.toMutable().apply {
+                    addInstructions(clientBuilderFingerprint.method.instructions)
+
+                    replaceInstruction(
+                        clientBuilderFingerprint.stringMatches!!.first().index,
+                        "const-string v$urlRegister, \"$lyricsUrlHost\"")
                 }
             )
         }
 
-        with(executeFingerprint.method) {
-            val getLyricsClientIndex = indexOfFirstInstructionReversedOrThrow(Opcode.INVOKE_STATIC)
-            val getLyricsClientInstruction = getInstruction<BuilderInstruction35c>(getLyricsClientIndex)
+        /**
+         * Adjust the lyrics HTTP builder method name to the method defined above.
+         * In this way the copied method is called rather than the stock one, returning the patched HTTP builder.
+         */
+        executeFingerprint.method.apply {
+            val getLyricsClientIndex = indexOfFirstInstructionReversedOrThrow(
+                Opcode.INVOKE_STATIC
+            )
 
-            replaceInstruction(getLyricsClientIndex,
+            val getLyricsClientInstruction = getInstruction<BuilderInstruction35c>(
+                getLyricsClientIndex
+            )
+
+            replaceInstruction(
+                getLyricsClientIndex,
                 BuilderInstruction35c(
                     Opcode.INVOKE_STATIC,
                     getLyricsClientInstruction.registerCount,
@@ -122,7 +97,7 @@ val customLyricsPatch = bytecodePatch(
                     getLyricsClientInstruction.registerG,
                     ImmutableMethodReference(
                         patchedClientMethod!!.definingClass,
-                        clientBuilderMethodName,
+                        patchedClientMethod!!.name, //This is the only thing that will be different.
                         patchedClientMethod!!.parameters,
                         patchedClientMethod!!.returnType
                     )
--- patches/src/main/kotlin/app/revanced/patches/spotify/misc/lyrics/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/spotify/misc/lyrics/Fingerprints.kt
@@ -5,21 +5,18 @@ import app.revanced.patcher.fingerprint
 import app.revanced.patcher.patch.BytecodePatchContext
 import com.android.tools.smali.dexlib2.Opcode
 
+/**
+ * This method builds and returns an HTTP client for a hardcoded host (declared in this method).
+ */
 internal val clientBuilderFingerprint = fingerprint {
     strings("spclient.wg.spotify.com")
     parameters("Lokhttp3/OkHttpClient;", "Lcom/fasterxml/jackson/databind/ObjectMapper;", "L", "Lio/reactivex/rxjava3/core/Scheduler;")
 }
 
-internal val oauthHostnameCheckFingerprint = fingerprint {
-    strings("spclient.wg.spotify.com")
-    parameters("Lokhttp3/Request;")
-    returns("Z")
-}
-
-internal val webgateHostnameCheckFingerprint = fingerprint {
-    strings("spclient.wg.spotify.com", "Authorization")
-}
-
+/**
+ * This method is where the HTTP client for lyrics is defined.
+ * This patch will replace this HTTP client with a patched HTTP client for the required custom lyrics host.
+ */
 context(BytecodePatchContext)
 internal val executeFingerprint: Fingerprint
     get() = fingerprint {
