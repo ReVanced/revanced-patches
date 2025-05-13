27214c181384ee056f70b8bd56b55c8447c98c0a
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2025-05-13 12:38:18 +0400

    remove unused variable

--- patches/src/main/kotlin/app/revanced/patches/spotify/misc/lyrics/ChangeLyricsProviderPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/spotify/misc/lyrics/ChangeLyricsProviderPatch.kt
@@ -81,7 +81,7 @@ val changeLyricsProviderPatch = bytecodePatch(
         //endregion
 
         // Replace the call to the HTTP client builder method used exclusively for lyrics by the modified one.
-        val buildLyricsHttpClientFingerprint = fingerprint {
+        fingerprint {
             returns(httpClientBuilderFingerprint.originalMethod.returnType)
             parameters()
             custom { method, _ ->
