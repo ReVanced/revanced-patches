b30b5938bb63e7a68be8c841fd65b2a24dbfa7e2
Dawid Krajcarz <80264606+drobotk@users.noreply.github.com>
2025-05-13 10:40:24 +0200

    Update RemoveMetaAIPatch.java

--- extensions/messenger/src/main/java/app/revanced/extension/messenger/metaai/RemoveMetaAIPatch.java
+++ extensions/messenger/src/main/java/app/revanced/extension/messenger/metaai/RemoveMetaAIPatch.java
@@ -3,11 +3,11 @@ package app.revanced.extension.messenger.metaai;
 @SuppressWarnings("unused")
 public class RemoveMetaAIPatch {
     public static boolean overrideConfigBool(long id, boolean value) {
-        // it seems like all configs starting with 363219 are related to Meta AI
-        // a list of specific ones that need disabling would probably be better,
-        // but these config numbers seem to change slightly with each update
-        // the starting digits don't, and seems to work perfectly fine
-        if (String.valueOf(id).startsWith("363219"))
+        // It seems like all configs starting with 363219 are related to Meta AI. 
+        // A list of specific ones that need disabling would probably be better,
+        // but these config numbers seem to change slightly with each update.
+        // These first 6 digits don't though.
+        if (Long.toString(id).startsWith("363219"))
             return false;
 
         return value;
