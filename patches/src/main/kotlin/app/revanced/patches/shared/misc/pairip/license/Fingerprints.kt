46dfe09986d1da87430c18f9a348ecbc21bc0c53
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2025-05-22 12:16:21 +0200

    finish merge

--- patches/src/main/kotlin/app/revanced/patches/shared/misc/pairip/license/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/shared/misc/pairip/license/Fingerprints.kt
@@ -2,14 +2,14 @@ package app.revanced.patches.shared.misc.pairip.license
 
 import app.revanced.patcher.fingerprint
 
-internal val processLicenseResponseFingerprint = fingerprint {
+internal val processLicenseResponseFingerprint by fingerprint {
     custom { method, classDef ->
         classDef.type == "Lcom/pairip/licensecheck/LicenseClient;" &&
                 method.name == "processResponse"
     }
 }
 
-internal val validateLicenseResponseFingerprint = fingerprint {
+internal val validateLicenseResponseFingerprint by fingerprint {
     custom { method, classDef ->
         classDef.type == "Lcom/pairip/licensecheck/ResponseValidator;" &&
                 method.name == "validateResponse"
