6b14e2240e8a381288e2cc9ba9da91974c3a019b
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2025-05-26 13:11:38 +0200

    maintain binary compatibility

---
+++ patches/src/main/kotlin/app/revanced/patches/instagram/ads/HideAdsPatch.kt
@@ -0,0 +1,9 @@
+package app.revanced.patches.instagram.ads
+
+import app.revanced.patcher.patch.bytecodePatch
+
+@Deprecated("Patch was moved to different package: app.revanced.patches.meta.ads.hideAdsPatch")
+@Suppress("unused")
+val hideAdsPatch = bytecodePatch{
+    dependsOn(app.revanced.patches.meta.ads.hideAdsPatch)
+}
