f2f3b6f5ba72d4dd0720d3b442e27c7bb734192e
brosssh <tiabroch@gmail.com>
2025-05-28 20:58:10 +0200

    Added support to latest version

--- patches/src/main/kotlin/app/revanced/patches/spotify/misc/lyrics/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/spotify/misc/lyrics/Fingerprints.kt
@@ -4,5 +4,5 @@ import app.revanced.patcher.fingerprint
 
 // This method builds and returns an HTTP client with a hardcoded host (declared in this method).
 internal val httpClientBuilderFingerprint = fingerprint {
-    strings("client == null")
+    strings("client == null", "scheduler == null")
 }
