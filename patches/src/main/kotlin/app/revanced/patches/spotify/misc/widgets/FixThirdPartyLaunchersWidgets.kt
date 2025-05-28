625ad9d7366b5bce45b5724d8600465fbff0b687
Nuckyz <61953774+Nuckyz@users.noreply.github.com>
2025-05-28 17:20:20 -0300

    Fix `Fix third party launchers widgets` erroring for legacy versions

--- patches/src/main/kotlin/app/revanced/patches/spotify/misc/widgets/FixThirdPartyLaunchersWidgets.kt
+++ patches/src/main/kotlin/app/revanced/patches/spotify/misc/widgets/FixThirdPartyLaunchersWidgets.kt
@@ -1,6 +1,7 @@
 package app.revanced.patches.spotify.misc.widgets
 
 import app.revanced.patcher.patch.bytecodePatch
+import app.revanced.patches.spotify.misc.extension.IS_SPOTIFY_LEGACY_APP_TARGET
 import app.revanced.util.returnEarly
 
 @Suppress("unused")
@@ -11,6 +12,11 @@ val fixThirdPartyLaunchersWidgets = bytecodePatch(
     compatibleWith("com.spotify.music")
 
     execute {
+        if (IS_SPOTIFY_LEGACY_APP_TARGET)  {
+            // The permission check does not exist on legacy versions. No patches are needed.
+            return@execute
+        }
+
         // Only system app launchers are granted the BIND_APPWIDGET permission.
         // Override the method that checks for it to always return true, as this permission is not actually required
         // for the widgets to work.
