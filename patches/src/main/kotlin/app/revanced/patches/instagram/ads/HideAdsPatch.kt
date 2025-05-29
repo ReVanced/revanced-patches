793090299121c2a8fea5d14a04490ab2922ad038
scrazzz <scruzism@mail.com>
2025-05-29 09:19:15 +0530

    feat(Threads): Hide Ads

--- patches/src/main/kotlin/app/revanced/patches/instagram/ads/HideAdsPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/instagram/ads/HideAdsPatch.kt
@@ -1,23 +1,8 @@
 package app.revanced.patches.instagram.ads
 
-import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
 import app.revanced.patcher.patch.bytecodePatch
 
 @Suppress("unused")
-val hideAdsPatch = bytecodePatch(
-    name = "Hide ads",
-    description = "Hides ads in stories, discover, profile, etc. " +
-        "An ad can still appear once when refreshing the home feed.",
-) {
-    compatibleWith("com.instagram.android")
-
-    execute {
-        adInjectorFingerprint.method.addInstructions(
-            0,
-            """
-                const/4 v0, 0x0
-                return v0
-            """,
-        )
-    }
+val hideAdsPatch = bytecodePatch {
+    dependsOn(app.revanced.patches.meta.ads.hideAdsPatch)
 }
