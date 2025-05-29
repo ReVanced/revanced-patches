d9d3c040267fe277ff7029a30d105e02ce5d1507
scrazzz <scruzism@mail.com>
2025-05-29 12:20:26 +0530

    Add deprecated messge

--- patches/src/main/kotlin/app/revanced/patches/instagram/ads/HideAdsPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/instagram/ads/HideAdsPatch.kt
@@ -2,6 +2,7 @@ package app.revanced.patches.instagram.ads
 
 import app.revanced.patcher.patch.bytecodePatch
 
+@Deprecated("Patch was moved to different package: app.revanced.patches.meta.ads.hideAdsPatch")
 @Suppress("unused")
 val hideAdsPatch = bytecodePatch {
     dependsOn(app.revanced.patches.meta.ads.hideAdsPatch)
