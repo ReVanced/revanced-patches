49ae0df224f42bc511d3e4d748c6b94f6273b44c
ByteEVM <155392541+ByteEVM@users.noreply.github.com>
2025-05-30 22:51:12 +0200

    feat(Proton Mail): Add `Remove free accounts limit` patch (#4970)
    Co-authored-by: Nuckyz <61953774+Nuckyz@users.noreply.github.com>
    Co-authored-by: oSumAtrIX <johan.melkonyan1@web.de>

---
+++ patches/src/main/kotlin/app/revanced/patches/protonmail/account/RemoveFreeAccountsLimitPatch.kt
@@ -0,0 +1,21 @@
+package app.revanced.patches.protonmail.account
+
+import app.revanced.patcher.patch.resourcePatch
+import app.revanced.util.findElementByAttributeValueOrThrow
+
+@Suppress("unused")
+val removeFreeAccountsLimitPatch = resourcePatch(
+    name = "Remove free accounts limit",
+    description = "Removes the limit for maximum free accounts logged in.",
+) {
+    compatibleWith("ch.protonmail.android")
+
+    execute {
+        document("res/values/integers.xml").use { document ->
+            document.documentElement.childNodes.findElementByAttributeValueOrThrow(
+                "name",
+                "core_feature_auth_user_check_max_free_user_count",
+            ).textContent = Int.MAX_VALUE.toString()
+        }
+    }
+}
