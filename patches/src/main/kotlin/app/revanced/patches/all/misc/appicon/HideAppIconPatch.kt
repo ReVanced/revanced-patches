0df9d2d21663a1d44970f4a2acf274ac3b0826c8
ILoveOpenSouceApplications <em29072chinmayjagalchandrababu@gmail.com>
2025-05-31 14:01:43 +0530

    Use `childElementSequence()`

--- patches/src/main/kotlin/app/revanced/patches/all/misc/appicon/HideAppIconPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/all/misc/appicon/HideAppIconPatch.kt
@@ -1,6 +1,7 @@
 package app.revanced.patches.all.misc.appicon
 
 import app.revanced.patcher.patch.resourcePatch
+import app.revanced.util.childElementsSequence
 import org.w3c.dom.Element
 import java.util.logging.Logger
 
@@ -17,15 +18,18 @@ val hideAppIconPatch = resourcePatch(
             val intentFilters = document.getElementsByTagName("intent-filter")
             for (i in 0 until intentFilters.length) {
                 val node = intentFilters.item(i) as? Element ?: continue
+
                 var hasMainAction = false
                 var launcherCategory: Element? = null
 
-                val children = node.childNodes
-                for (j in 0 until children.length) {
-                    val child = children.item(j) as? Element ?: continue
+                for (child in node.childElementsSequence()) {
                     when (child.tagName) {
-                        "action" -> if (child.getAttribute("android:name") == "android.intent.action.MAIN") hasMainAction = true
-                        "category" -> if (child.getAttribute("android:name") == "android.intent.category.LAUNCHER") launcherCategory = child
+                        "action" -> if (child.getAttribute("android:name") == "android.intent.action.MAIN") {
+                            hasMainAction = true
+                        }
+                        "category" -> if (child.getAttribute("android:name") == "android.intent.category.LAUNCHER") {
+                            launcherCategory = child
+                        }
                     }
                 }
 
@@ -36,8 +40,10 @@ val hideAppIconPatch = resourcePatch(
             }
 
             if (!changed) {
-                Logger.getLogger(this::class.java.name).warning("No changes made: Did not find any launcher intent-filters to change.")
+                Logger.getLogger(this::class.java.name)
+                    .warning("No changes made: Did not find any launcher intent-filters to change.")
             }
         }
     }
 }
+
