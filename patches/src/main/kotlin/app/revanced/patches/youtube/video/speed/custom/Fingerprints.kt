9a1e6ca178d9833ee2c681fb130b9290a4e89cd8
MarcaD <152095496+MarcaDian@users.noreply.github.com>
2025-06-01 12:12:56 +0300

    feat(YouTube - Playback Speed): Use modern custom speed dialog (#5069)
    Co-authored-by: LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>

--- patches/src/main/kotlin/app/revanced/patches/youtube/video/speed/custom/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/youtube/video/speed/custom/Fingerprints.kt
@@ -1,29 +1,8 @@
 package app.revanced.patches.youtube.video.speed.custom
 
 import app.revanced.patcher.fingerprint
-import app.revanced.util.literal
 import com.android.tools.smali.dexlib2.AccessFlags
 import com.android.tools.smali.dexlib2.Opcode
-
-internal val getOldPlaybackSpeedsFingerprint = fingerprint {
-    parameters("[L", "I")
-    strings("menu_item_playback_speed")
-}
-
-internal val showOldPlaybackSpeedMenuFingerprint = fingerprint {
-    literal { speedUnavailableId }
-}
-
-internal val showOldPlaybackSpeedMenuExtensionFingerprint = fingerprint {
-    custom { method, _ -> method.name == "showOldPlaybackSpeedMenu" }
-}
-
-internal val speedArrayGeneratorFingerprint = fingerprint {
-    accessFlags(AccessFlags.PUBLIC, AccessFlags.STATIC)
-    returns("[L")
-    parameters("Lcom/google/android/libraries/youtube/innertube/model/player/PlayerResponseModel;")
-    strings("0.0#")
-}
 
 internal val speedLimiterFingerprint = fingerprint {
     accessFlags(AccessFlags.PUBLIC, AccessFlags.FINAL)
