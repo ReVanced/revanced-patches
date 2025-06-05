bef0dacac54caf1ca9511d7bc19b19140ccb4eaf
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2025-06-05 11:21:05 +0200

    fix(YouTube - Hide Shorts components): Disable A/B player flags that prevents hiding buttons

--- patches/src/main/kotlin/app/revanced/patches/youtube/layout/hide/shorts/HideShortsComponentsPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/youtube/layout/hide/shorts/HideShortsComponentsPatch.kt
@@ -267,12 +267,12 @@ val hideShortsComponentsPatch = bytecodePatch(
             // Since the buttons are native components and not Litho, it should be possible to
             // fix the RYD Shorts loading delay by asynchronously loading RYD and updating
             // the button text after RYD has loaded.
-            shortsExperimentalPlayerFeatureFlagFingerprint.method.returnLate(true)
+            shortsExperimentalPlayerFeatureFlagFingerprint.method.returnLate(false)
 
             // Experimental UI renderer must also be disabled since it requires the
             // experimental Shorts player.  If this is enabled but Shorts player
             // is disabled then the app crashes when the Shorts player is opened.
-            renderNextUIFeatureFlagFingerprint.method.returnLate(true)
+            renderNextUIFeatureFlagFingerprint.method.returnLate(false)
         }
 
         // endregion
