a29c4e48b13b74d6ef367f2c142952f9e06def86
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2025-06-06 21:53:15 +0200

    refactor

--- patches/src/main/kotlin/app/revanced/util/BytecodeUtils.kt
+++ patches/src/main/kotlin/app/revanced/util/BytecodeUtils.kt
@@ -92,7 +92,8 @@ fun Method.findFreeRegister(startIndex: Int, vararg registersToExclude: Int): In
                 return bestFreeRegisterFound
             }
             // This method is simple and does not follow branching.
-            throw IllegalArgumentException("Encountered a branch statement before a free register could be found")
+            throw IllegalArgumentException("Encountered a branch statement before" +
+                    "a free register could be found from startIndex: $startIndex")
         }
 
         if (instruction.isReturnInstruction) {
