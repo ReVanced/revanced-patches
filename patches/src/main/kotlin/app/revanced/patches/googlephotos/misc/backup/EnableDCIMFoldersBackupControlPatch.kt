eeb4028970402d4c3f5ea2eaae08d23c2fbb82a5
Nuckyz <61953774+Nuckyz@users.noreply.github.com>
2025-06-07 20:52:05 -0300

    Improve patch description

--- patches/src/main/kotlin/app/revanced/patches/googlephotos/misc/backup/EnableDCIMFoldersBackupControlPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/googlephotos/misc/backup/EnableDCIMFoldersBackupControlPatch.kt
@@ -6,9 +6,8 @@ import app.revanced.util.returnEarly
 @Suppress("unused")
 val enableDCIMFoldersBackupControlPatch = bytecodePatch(
     name = "Enable DCIM folders backup control",
-    description = "Allows controlling whether backup is enabled for each DCIM folder. " +
-        "This will by make the app default to having no folders backed up, as it allows controlling " +
-        "the backup even for the Camera folder, which is normally always backed up.",
+    description = "Disables always on backup for the Camera and other DCIM folders, allowing you to control backup " +
+            "for each folder individually. This will make the app default to having no folders backed up.",
     use = false,
 ) {
     compatibleWith("com.google.android.apps.photos")
