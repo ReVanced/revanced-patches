8c51cedbae8d20282350c883ab56f7a346cbbe59
Nuckyz <61953774+Nuckyz@users.noreply.github.com>
2025-06-07 23:11:01 -0300

    Explain logic better

--- extensions/messenger/src/main/java/app/revanced/extension/messenger/metaai/RemoveMetaAIPatch.java
+++ extensions/messenger/src/main/java/app/revanced/extension/messenger/metaai/RemoveMetaAIPatch.java
@@ -3,10 +3,12 @@ package app.revanced.extension.messenger.metaai;
 @SuppressWarnings("unused")
 public class RemoveMetaAIPatch {
     public static boolean overrideBooleanFlag(long id, boolean value) {
-        // A list of specific flags that need disabling would probably be better,
-        // but these IDs change slightly with each app update.
-        if ((id & 0x7FFFFFC000000000L) == 0x810a8000000000L)
+        // This catches all flag ids related to Meta AI.
+        // A list of specific ones that need disabling would probably be better,
+        // but these ids seem to change slightly with each update.
+        if ((id & 0x7FFFFFC000000000L) == 0x810A8000000000L) {
             return false;
+        }
 
         return value;
     }
