1f539b1396526b2c767d77a804bd0308ee4a42ec
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2025-06-23 12:19:07 +0400

    fix(YouTube): Always use single threaded layout to resolve layout bugs in unpatched YouTube (#5226)

--- extensions/youtube/src/main/java/app/revanced/extension/youtube/patches/components/LithoFilterPatch.java
+++ extensions/youtube/src/main/java/app/revanced/extension/youtube/patches/components/LithoFilterPatch.java
@@ -74,14 +74,35 @@ public final class LithoFilterPatch {
         }
     }
 
+    /**
+     * Litho layout fixed thread pool size override.
+     * <p>
+     * Unpatched YouTube uses a layout fixed thread pool between 1 and 3 threads:
+     * <pre>
+     * 1 thread - > Device has less than 6 cores
+     * 2 threads -> Device has over 6 cores and less than 6GB of memory
+     * 3 threads -> Device has over 6 cores and more than 6GB of memory
+     * </pre>
+     *
+     * Using more than 1 thread causes layout issues such as the You tab watch/playlist shelf
+     * that is sometimes incorrectly hidden (ReVanced is not hiding it), and seems to
+     * fix a race issue if using the active navigation tab status with litho filtering.
+     */
+    private static final int LITHO_LAYOUT_THREAD_POOL_SIZE = 1;
+
+    private static final byte[] EMPTY_BYTE_ARRAY = new byte[0];
+
+    /**
+     * Placeholder for actual filters.
+     */
+    private static final class DummyFilter extends Filter { }
+
     private static final Filter[] filters = new Filter[] {
             new DummyFilter() // Replaced by patch.
     };
 
     private static final StringTrieSearch pathSearchTree = new StringTrieSearch();
     private static final StringTrieSearch identifierSearchTree = new StringTrieSearch();
-
-    private static final byte[] EMPTY_BYTE_ARRAY = new byte[0];
 
     /**
      * Because litho filtering is multi-threaded and the buffer is passed in from a different injection point,
@@ -213,9 +234,28 @@ public final class LithoFilterPatch {
 
         return false;
     }
-}
 
-/**
- * Placeholder for actual filters.
- */
-final class DummyFilter extends Filter { }
+    /**
+     * Injection point.
+     */
+    public static int getExecutorCorePoolSize(int originalCorePoolSize) {
+        if (originalCorePoolSize != LITHO_LAYOUT_THREAD_POOL_SIZE) {
+            Logger.printDebug(() -> "Overriding core thread pool size from: " + originalCorePoolSize
+                    + " to: " + LITHO_LAYOUT_THREAD_POOL_SIZE);
+        }
+
+        return LITHO_LAYOUT_THREAD_POOL_SIZE;
+    }
+
+    /**
+     * Injection point.
+     */
+    public static int getExecutorMaxThreads(int originalMaxThreads) {
+        if (originalMaxThreads != LITHO_LAYOUT_THREAD_POOL_SIZE) {
+            Logger.printDebug(() -> "Overriding max thread pool size from: " + originalMaxThreads
+                    + " to: " + LITHO_LAYOUT_THREAD_POOL_SIZE);
+        }
+
+        return LITHO_LAYOUT_THREAD_POOL_SIZE;
+    }
+}
