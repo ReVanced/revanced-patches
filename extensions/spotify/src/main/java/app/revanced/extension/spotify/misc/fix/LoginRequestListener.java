b7b75bb9d8d5fd505121e752b8a20e61ff28d1b2
oSumAtrIX <johan.melkonyan1@web.de>
2025-06-26 19:51:18 +0200

    fix(Spotify): Add `Spoof client` patch to fix various issues by using a web platform access token (#5173)
    Co-authored-by: Nuckyz <61953774+Nuckyz@users.noreply.github.com>
    Co-authored-by: brosssh <tiabroch@gmail.com>
    Co-authored-by: Dawid Krajcarz <80264606+drobotk@users.noreply.github.com>
    Co-authored-by: LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>

---
+++ extensions/spotify/src/main/java/app/revanced/extension/spotify/misc/fix/LoginRequestListener.java
@@ -0,0 +1,153 @@
+package app.revanced.extension.spotify.misc.fix;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import app.revanced.extension.shared.Logger;
+import app.revanced.extension.spotify.login5.v4.proto.Login5.*;
+import com.google.protobuf.ByteString;
+import com.google.protobuf.MessageLite;
+import fi.iki.elonen.NanoHTTPD;
+
+import java.io.ByteArrayInputStream;
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
+
+import static fi.iki.elonen.NanoHTTPD.Response.Status.INTERNAL_ERROR;
+
+class LoginRequestListener extends NanoHTTPD {
+    LoginRequestListener(int port) {
+        super(port);
+    }
+
+    @NonNull
+    @Override
+    public Response serve(IHTTPSession request) {
+        Logger.printInfo(() -> "Serving request for URI: " + request.getUri());
+
+        InputStream requestBodyInputStream = getRequestBodyInputStream(request);
+
+        LoginRequest loginRequest;
+        try {
+            loginRequest = LoginRequest.parseFrom(requestBodyInputStream);
+        } catch (IOException e) {
+            Logger.printException(() -> "Failed to parse LoginRequest", e);
+            return newResponse(INTERNAL_ERROR);
+        }
+
+        MessageLite loginResponse;
+
+        // A request may be made concurrently by Spotify,
+        // however a webview can only handle one request at a time due to singleton cookie manager.
+        // Therefore, synchronize to ensure that only one webview handles the request at a time.
+        synchronized (this) {
+            loginResponse = getLoginResponse(loginRequest);
+        }
+
+        if (loginResponse != null) {
+            return newResponse(Response.Status.OK, loginResponse);
+        }
+
+        return newResponse(INTERNAL_ERROR);
+    }
+
+
+    @Nullable
+    private static LoginResponse getLoginResponse(@NonNull LoginRequest loginRequest) {
+        Session session;
+
+        boolean isInitialLogin = !loginRequest.hasStoredCredential();
+        if (isInitialLogin) {
+            Logger.printInfo(() -> "Received request for initial login");
+            session = WebApp.currentSession; // Session obtained from WebApp.login.
+        } else {
+            Logger.printInfo(() -> "Received request to restore saved session");
+            session = Session.read(loginRequest.getStoredCredential().getUsername());
+        }
+
+        return toLoginResponse(session, isInitialLogin);
+    }
+
+
+    private static LoginResponse toLoginResponse(Session session, boolean isInitialLogin) {
+        LoginResponse.Builder builder = LoginResponse.newBuilder();
+
+        if (session == null) {
+            if (isInitialLogin) {
+                Logger.printInfo(() -> "Session is null, returning try again later error for initial login");
+                builder.setError(LoginError.TRY_AGAIN_LATER);
+            } else {
+                Logger.printInfo(() -> "Session is null, returning invalid credentials error for stored credential login");
+                builder.setError(LoginError.INVALID_CREDENTIALS);
+            }
+        } else if (session.username == null) {
+            Logger.printInfo(() -> "Session username is null, returning invalid credentials error");
+            builder.setError(LoginError.INVALID_CREDENTIALS);
+        } else if (session.accessTokenExpired()) {
+            Logger.printInfo(() -> "Access token has expired, renewing session");
+            WebApp.renewSession(session.cookies);
+            return toLoginResponse(WebApp.currentSession, isInitialLogin);
+        } else {
+            session.save();
+            Logger.printInfo(() -> "Returning session for username: " + session.username);
+            builder.setOk(LoginOk.newBuilder()
+                    .setUsername(session.username)
+                    .setAccessToken(session.accessToken)
+                    .setStoredCredential(ByteString.fromHex("00")) // Placeholder, as it cannot be null or empty.
+                    .setAccessTokenExpiresIn(session.accessTokenExpiresInSeconds())
+                    .build());
+        }
+
+        return builder.build();
+    }
+
+    @NonNull
+    private static InputStream limitedInputStream(InputStream inputStream, long contentLength) {
+        return new FilterInputStream(inputStream) {
+            private long remaining = contentLength;
+
+            @Override
+            public int read() throws IOException {
+                if (remaining <= 0) return -1;
+                int result = super.read();
+                if (result != -1) remaining--;
+                return result;
+            }
+
+            @Override
+            public int read(byte[] b, int off, int len) throws IOException {
+                if (remaining <= 0) return -1;
+                len = (int) Math.min(len, remaining);
+                int result = super.read(b, off, len);
+                if (result != -1) remaining -= result;
+                return result;
+            }
+        };
+    }
+
+    @NonNull
+    private static InputStream getRequestBodyInputStream(@NonNull IHTTPSession request) {
+        long requestContentLength =
+                Long.parseLong(Objects.requireNonNull(request.getHeaders().get("content-length")));
+        return limitedInputStream(request.getInputStream(), requestContentLength);
+    }
+
+
+    @SuppressWarnings("SameParameterValue")
+    @NonNull
+    private static Response newResponse(Response.Status status) {
+        return newResponse(status, null);
+    }
+
+    @NonNull
+    private static Response newResponse(Response.IStatus status, MessageLite messageLite) {
+        if (messageLite == null) {
+            return newFixedLengthResponse(status, "application/x-protobuf", null);
+        }
+
+        byte[] messageBytes = messageLite.toByteArray();
+        InputStream stream = new ByteArrayInputStream(messageBytes);
+        return newFixedLengthResponse(status, "application/x-protobuf", stream, messageBytes.length);
+    }
+}
---
+++ patches/src/main/kotlin/app/revanced/patches/spotify/misc/fix/SpoofClientPatch.kt
@@ -0,0 +1,122 @@
+package app.revanced.patches.spotify.misc.fix
+
+import app.revanced.patcher.extensions.InstructionExtensions.addInstruction
+import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
+import app.revanced.patcher.extensions.InstructionExtensions.getInstruction
+import app.revanced.patcher.extensions.InstructionExtensions.replaceInstruction
+import app.revanced.patcher.patch.bytecodePatch
+import app.revanced.patcher.patch.intOption
+import app.revanced.patches.shared.misc.hex.HexPatchBuilder
+import app.revanced.patches.shared.misc.hex.hexPatch
+import app.revanced.patches.spotify.misc.extension.sharedExtensionPatch
+import app.revanced.util.findInstructionIndicesReversedOrThrow
+import app.revanced.util.getReference
+import app.revanced.util.indexOfFirstInstructionReversedOrThrow
+import com.android.tools.smali.dexlib2.Opcode
+import com.android.tools.smali.dexlib2.iface.instruction.OneRegisterInstruction
+import com.android.tools.smali.dexlib2.iface.reference.MethodReference
+
+internal const val EXTENSION_CLASS_DESCRIPTOR = "Lapp/revanced/extension/spotify/misc/fix/SpoofClientPatch;"
+
+@Suppress("unused")
+val spoofClientPatch = bytecodePatch(
+    name = "Spoof client",
+    description = "Spoofs the client to fix various functions of the app.",
+) {
+    val port by intOption(
+        key = "port",
+        default = 4345,
+        title = " Login request listener port",
+        description = "The port to use for the listener that intercepts and handles login requests. " +
+                "Port must be between 0 and 65535.",
+        required = true,
+        validator = {
+            it!!
+            !(it < 0 || it > 65535)
+        }
+    )
+
+    dependsOn(
+        sharedExtensionPatch,
+        hexPatch(ignoreMissingTargetFiles = true, block = fun HexPatchBuilder.() {
+            listOf(
+                "arm64-v8a",
+                "armeabi-v7a",
+                "x86",
+                "x86_64"
+            ).forEach { architecture ->
+                "https://login5.spotify.com/v3/login" to "http://127.0.0.1:$port/v3/login" inFile
+                        "lib/$architecture/liborbit-jni-spotify.so"
+
+                "https://login5.spotify.com/v4/login" to "http://127.0.0.1:$port/v4/login" inFile
+                        "lib/$architecture/liborbit-jni-spotify.so"
+            }
+        })
+    )
+
+    compatibleWith("com.spotify.music")
+
+    execute {
+        getPackageInfoFingerprint.method.apply {
+            // region Spoof signature.
+
+            val failedToGetSignaturesStringIndex =
+                getPackageInfoFingerprint.stringMatches!!.first().index
+
+            val concatSignaturesIndex = indexOfFirstInstructionReversedOrThrow(
+                failedToGetSignaturesStringIndex,
+                Opcode.MOVE_RESULT_OBJECT,
+            )
+
+            val signatureRegister = getInstruction<OneRegisterInstruction>(concatSignaturesIndex).registerA
+            val expectedSignature = "d6a6dced4a85f24204bf9505ccc1fce114cadb32"
+
+            replaceInstruction(concatSignaturesIndex, "const-string v$signatureRegister, \"$expectedSignature\"")
+
+            // endregion
+
+            // region Spoof installer name.
+
+            val expectedInstallerName = "com.android.vending"
+
+            findInstructionIndicesReversedOrThrow {
+                val reference = getReference<MethodReference>()
+                reference?.name == "getInstallerPackageName" || reference?.name == "getInstallingPackageName"
+            }.forEach { index ->
+                val returnObjectIndex = index + 1
+
+                val installerPackageNameRegister = getInstruction<OneRegisterInstruction>(
+                    returnObjectIndex
+                ).registerA
+
+                addInstruction(
+                    returnObjectIndex + 1,
+                    "const-string v$installerPackageNameRegister, \"$expectedInstallerName\""
+                )
+            }
+
+            // endregion
+        }
+
+        startLiborbitFingerprint.method.addInstructions(
+            0,
+            """
+                const/16 v0, $port
+                invoke-static { v0 }, $EXTENSION_CLASS_DESCRIPTOR->listen(I)V
+            """
+        )
+
+        startupPageLayoutInflateFingerprint.method.apply {
+            val openLoginWebViewDescriptor =
+                "$EXTENSION_CLASS_DESCRIPTOR->login(Landroid/view/LayoutInflater;)V"
+
+            addInstructions(
+                0,
+                """
+                    move-object/from16 v3, p1
+                    invoke-static { v3 }, $openLoginWebViewDescriptor
+                """
+            )
+        }
+    }
+}
