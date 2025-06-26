1a8aacdff601e90ec47daf1c54c47815062ae4ce
oSumAtrIX <johan.melkonyan1@web.de>
2025-06-26 19:51:18 +0200

    fix(Spotify): Add `Spoof client` patch to fix various issues by using a web platform access token (#5173)
    Co-authored-by: Nuckyz <61953774+Nuckyz@users.noreply.github.com>
    Co-authored-by: brosssh <tiabroch@gmail.com>
    Co-authored-by: Dawid Krajcarz <80264606+drobotk@users.noreply.github.com>
    Co-authored-by: LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>

--- patches/src/main/kotlin/app/revanced/patches/spotify/misc/fix/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/spotify/misc/fix/Fingerprints.kt
@@ -1,9 +1,21 @@
 package app.revanced.patches.spotify.misc.fix
 
 import app.revanced.patcher.fingerprint
+import com.android.tools.smali.dexlib2.AccessFlags
 
 internal val getPackageInfoFingerprint = fingerprint {
     strings(
         "Failed to get the application signatures"
     )
+}
+
+internal val startLiborbitFingerprint = fingerprint {
+    strings("/liborbit-jni-spotify.so")
+}
+
+internal val startupPageLayoutInflateFingerprint = fingerprint {
+    accessFlags(AccessFlags.PUBLIC, AccessFlags.FINAL)
+    returns("Landroid/view/View;")
+    parameters("Landroid/view/LayoutInflater;", "Landroid/view/ViewGroup;", "Landroid/os/Bundle;")
+    strings("blueprintContainer", "gradient", "valuePropositionTextView")
 }
