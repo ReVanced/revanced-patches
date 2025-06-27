8777d036cc1b71528a4249e2bc934fcacfe65f6b
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2025-06-27 11:04:13 +0400

    fix build

--- patches/src/main/kotlin/app/revanced/patches/cricbuzz/ads/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/cricbuzz/ads/Fingerprints.kt
@@ -3,7 +3,7 @@ package app.revanced.patches.cricbuzz.ads
 import app.revanced.patcher.fingerprint
 import com.android.tools.smali.dexlib2.Opcode
 
-internal val userStateSwitchFingerprint = fingerprint {
+internal val userStateSwitchFingerprint by fingerprint {
     strings("key.user.state", "NA")
     opcodes(Opcode.SPARSE_SWITCH)
 }
