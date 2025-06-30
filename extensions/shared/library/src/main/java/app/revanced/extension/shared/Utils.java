6ee94f8532a073d720f5f2cc6862c78c2e06aee6
MarcaD <152095496+MarcaDian@users.noreply.github.com>
2025-06-30 09:50:52 +0300

    feat(YouTube - SponsorBlock): Add "Undo automatic skip toast" (#5277)
    Co-authored-by: LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>

--- extensions/shared/library/src/main/java/app/revanced/extension/shared/Utils.java
+++ extensions/shared/library/src/main/java/app/revanced/extension/shared/Utils.java
@@ -311,6 +311,10 @@ public class Utils {
         return getContext().getResources().getDimension(getResourceIdentifier(resourceIdentifierName, "dimen"));
     }
 
+    public static String[] getResourceStringArray(String resourceIdentifierName) throws Resources.NotFoundException {
+        return getContext().getResources().getStringArray(getResourceIdentifier(resourceIdentifierName, "array"));
+    }
+
     public interface MatchFilter<T> {
         boolean matches(T object);
     }
@@ -579,7 +583,7 @@ public class Utils {
             Context currentContext = context;
 
             if (currentContext == null) {
-                Logger.printException(() -> "Cannot show toast (context is null): " + messageToToast, null);
+                Logger.printException(() -> "Cannot show toast (context is null): " + messageToToast);
             } else {
                 Logger.printDebug(() -> "Showing toast: " + messageToToast);
                 Toast.makeText(currentContext, messageToToast, toastDuration).show();
@@ -809,7 +813,7 @@ public class Utils {
 
         // Create content container (message/EditText) inside a ScrollView only if message or editText is provided.
         ScrollView contentScrollView = null;
-        LinearLayout contentContainer = null;
+        LinearLayout contentContainer;
         if (message != null || editText != null) {
             contentScrollView = new ScrollView(context);
             contentScrollView.setVerticalScrollBarEnabled(false); // Disable the vertical scrollbar.
@@ -833,7 +837,7 @@ public class Utils {
             contentScrollView.addView(contentContainer);
 
             // Message (if not replaced by EditText).
-            if (editText == null && message != null) {
+            if (editText == null) {
                 TextView messageView = new TextView(context);
                 messageView.setText(message); // Supports Spanned (HTML).
                 messageView.setTextSize(16);
