a3d47e72e3841a959d88ae3a0e7d5320a781c28e
hoodles <207470673+hoo-dles@users.noreply.github.com>
2025-07-09 10:47:10 -0700

    feat(Cricbuzz - Hide ads): Hide Cricbuzz11 UI elements (#5381)

--- patches/src/main/kotlin/app/revanced/patches/cricbuzz/ads/DisableAdsPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/cricbuzz/ads/DisableAdsPatch.kt
@@ -3,24 +3,38 @@ package app.revanced.patches.cricbuzz.ads
 import app.revanced.patcher.extensions.InstructionExtensions.addInstruction
 import app.revanced.patcher.extensions.InstructionExtensions.getInstruction
 import app.revanced.patcher.patch.bytecodePatch
+import app.revanced.patches.cricbuzz.misc.extension.sharedExtensionPatch
+import app.revanced.util.getReference
 import app.revanced.util.indexOfFirstInstructionOrThrow
+import app.revanced.util.returnEarly
 import com.android.tools.smali.dexlib2.Opcode
-import com.android.tools.smali.dexlib2.iface.instruction.OneRegisterInstruction
+import com.android.tools.smali.dexlib2.iface.instruction.TwoRegisterInstruction
+import com.android.tools.smali.dexlib2.iface.reference.FieldReference
+
+private const val EXTENSION_CLASS_DESCRIPTOR =
+    "Lapp/revanced/extension/cricbuzz/ads/HideAdsPatch;"
 
 @Suppress("unused")
 val disableAdsPatch = bytecodePatch (
     name = "Hide ads",
 ) {
-    compatibleWith("com.cricbuzz.android"("6.23.02"))
+    compatibleWith("com.cricbuzz.android"("6.24.01"))
+
+    dependsOn(sharedExtensionPatch)
 
     execute {
-        userStateSwitchFingerprint.method.apply {
-            val opcodeIndex = indexOfFirstInstructionOrThrow(Opcode.MOVE_RESULT_OBJECT)
-            val register = getInstruction<OneRegisterInstruction>(opcodeIndex).registerA
+        userStateSwitchFingerprint.method.returnEarly(true)
 
-            addInstruction(
-                opcodeIndex + 1,
-                "const-string v$register, \"ACTIVE\""
+        // Remove region-specific Cricbuzz11 elements.
+        cb11ConstructorFingerprint.method.addInstruction(0, "const/4 p7, 0x0")
+        getBottomBarFingerprint.method.apply {
+            val getIndex = indexOfFirstInstructionOrThrow() {
+                opcode == Opcode.IGET_OBJECT && getReference<FieldReference>()?.name == "bottomBar"
+            }
+            val getRegister = getInstruction<TwoRegisterInstruction>(getIndex).registerA
+
+            addInstruction(getIndex + 1,
+                "invoke-static { v$getRegister }, $EXTENSION_CLASS_DESCRIPTOR->filterCb11(Ljava/util/List;)V"
             )
         }
     }
