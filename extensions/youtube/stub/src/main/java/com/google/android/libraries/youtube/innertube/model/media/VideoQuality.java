bd3ace0bd04ccd0369adb49d63aa0cf986402346
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2025-07-31 14:27:17 -0400

    fix(YouTube - Video quality): Use 1080p enhanced bitrate for Premium users (#5565)

---
+++ extensions/youtube/stub/src/main/java/com/google/android/libraries/youtube/innertube/model/media/VideoQuality.java
@@ -0,0 +1,12 @@
+package com.google.android.libraries.youtube.innertube.model.media;
+
+public class VideoQuality {
+    public final String patch_getQualityName() {
+        throw new UnsupportedOperationException("Stub");
+    }
+
+    public final int patch_getResolution() {
+        throw new UnsupportedOperationException("Stub");
+    }
+}
+
