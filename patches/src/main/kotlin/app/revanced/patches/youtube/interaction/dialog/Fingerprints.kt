31ed198fcdb2f601c46089c8007d969744faa8c5
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2025-08-03 10:57:17 -0400

    fix patching 20.21

--- patches/src/main/kotlin/app/revanced/patches/youtube/interaction/dialog/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/youtube/interaction/dialog/Fingerprints.kt
@@ -1,14 +1,12 @@
 package app.revanced.patches.youtube.interaction.dialog
 
 import app.revanced.patcher.fingerprint
-import app.revanced.patcher.literal
 import app.revanced.patcher.methodCall
 
 internal val createDialogFingerprint by fingerprint {
     returns("V")
     parameters("L", "L", "Ljava/lang/String;")
     instructions(
-        literal(0x7f140232),
         methodCall(smali = "Landroid/app/AlertDialog\$Builder;->setNegativeButton(ILandroid/content/DialogInterface\$OnClickListener;)Landroid/app/AlertDialog\$Builder;"),
         methodCall(smali = "Landroid/app/AlertDialog\$Builder;->setOnCancelListener(Landroid/content/DialogInterface\$OnCancelListener;)Landroid/app/AlertDialog\$Builder;"),
         methodCall(smali = "Landroid/app/AlertDialog\$Builder;->create()Landroid/app/AlertDialog;"),
