9f13fc216305ae24a312bf7e433615fcae5bc7ac
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2025-08-07 14:57:17 -0400

    refactor

--- patches/src/main/kotlin/app/revanced/patches/youtube/video/audio/ForceOriginalAudioPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/youtube/video/audio/ForceOriginalAudioPatch.kt
@@ -70,87 +70,84 @@ val forceOriginalAudioPatch = bytecodePatch(
             )
         }
 
-        val isDefaultAudioTrackMethod = formatStreamModelToStringFingerprint.originalMethod
-            .findMethodFromToString("isDefaultAudioTrack=")
-        val audioTrackDisplayNameMethod = formatStreamModelToStringFingerprint.originalMethod
-            .findMethodFromToString("audioTrackDisplayName=")
-        val audioTrackIdMethod = formatStreamModelToStringFingerprint.originalMethod
-            .findMethodFromToString("audioTrackId=")
+        formatStreamModelToStringFingerprint.let {
+            val isDefaultAudioTrackMethod = it.originalMethod.findMethodFromToString("isDefaultAudioTrack=")
+            val audioTrackDisplayNameMethod = it.originalMethod.findMethodFromToString("audioTrackDisplayName=")
+            val audioTrackIdMethod = it.originalMethod.findMethodFromToString("audioTrackId=")
 
-        proxy(classes.first {
-            it.type == audioTrackIdMethod.definingClass
-        }).mutableClass.apply {
-            // Add a new field to store the override.
-            val helperFieldName = "isDefaultAudioTrackOverride"
-            fields.add(
-                ImmutableField(
-                    type,
-                    helperFieldName,
-                    "Ljava/lang/Boolean;",
-                    // Boolean is a 100% immutable class (all fields are final)
-                    // and safe to write to a shared field without volatile/synchronization,
-                    // but without volatile the field can show stale data
-                    // and the same field is calculated more than once by different threads.
-                    AccessFlags.PRIVATE.value or AccessFlags.VOLATILE.value,
+            it.classDef.apply {
+                // Add a new field to store the override.
+                val helperFieldName = "isDefaultAudioTrackOverride"
+                fields.add(
+                    ImmutableField(
+                        type,
+                        helperFieldName,
+                        "Ljava/lang/Boolean;",
+                        // Boolean is a 100% immutable class (all fields are final)
+                        // and safe to write to a shared field without volatile/synchronization,
+                        // but without volatile the field can show stale data
+                        // and the same field is calculated more than once by different threads.
+                        AccessFlags.PRIVATE.value or AccessFlags.VOLATILE.value,
+                        null,
+                        null,
+                        null
+                    ).toMutable()
+                )
+
+                // Add a helper method because the isDefaultAudioTrack() has only 2 registers and 3 are needed.
+                val helperMethodClass = type
+                val helperMethodName = "patch_isDefaultAudioTrack"
+                val helperMethod = ImmutableMethod(
+                    helperMethodClass,
+                    helperMethodName,
+                    listOf(ImmutableMethodParameter("Z", null, null)),
+                    "Z",
+                    AccessFlags.PRIVATE.value,
                     null,
                     null,
-                    null
-                ).toMutable()
-            )
+                    MutableMethodImplementation(6),
+                ).toMutable().apply {
+                    addInstructionsWithLabels(
+                        0,
+                        """
+                            iget-object v0, p0, $helperMethodClass->$helperFieldName:Ljava/lang/Boolean;
+                            if-eqz v0, :call_extension            
+                            invoke-virtual { v0 }, Ljava/lang/Boolean;->booleanValue()Z
+                            move-result v3
+                            return v3
+                            
+                            :call_extension
+                            invoke-virtual { p0 }, $audioTrackIdMethod
+                            move-result-object v1
+                            
+                            invoke-virtual { p0 }, $audioTrackDisplayNameMethod
+                            move-result-object v2
+        
+                            invoke-static { p1, v1, v2 }, $EXTENSION_CLASS_DESCRIPTOR->isDefaultAudioStream(ZLjava/lang/String;Ljava/lang/String;)Z
+                            move-result v3
+                            
+                            invoke-static { v3 }, Ljava/lang/Boolean;->valueOf(Z)Ljava/lang/Boolean;
+                            move-result-object v0
+                            iput-object v0, p0, $helperMethodClass->$helperFieldName:Ljava/lang/Boolean;
+                            return v3
+                        """
+                    )
+                }
+                methods.add(helperMethod)
 
-            // Add a helper method because the isDefaultAudioTrack() has only 2 registers and 3 are needed.
-            val helperMethodClass = type
-            val helperMethodName = "patch_isDefaultAudioTrack"
-            val helperMethod = ImmutableMethod(
-                helperMethodClass,
-                helperMethodName,
-                listOf(ImmutableMethodParameter("Z", null, null)),
-                "Z",
-                AccessFlags.PRIVATE.value,
-                null,
-                null,
-                MutableMethodImplementation(6),
-            ).toMutable().apply {
-                addInstructionsWithLabels(
-                    0,
-                    """
-                        iget-object v0, p0, $helperMethodClass->$helperFieldName:Ljava/lang/Boolean;
-                        if-eqz v0, :call_extension            
-                        invoke-virtual { v0 }, Ljava/lang/Boolean;->booleanValue()Z
-                        move-result v3
-                        return v3
-                        
-                        :call_extension
-                        invoke-virtual { p0 }, $audioTrackIdMethod
-                        move-result-object v1
-                        
-                        invoke-virtual { p0 }, $audioTrackDisplayNameMethod
-                        move-result-object v2
-    
-                        invoke-static { p1, v1, v2 }, $EXTENSION_CLASS_DESCRIPTOR->isDefaultAudioStream(ZLjava/lang/String;Ljava/lang/String;)Z
-                        move-result v3
-                        
-                        invoke-static { v3 }, Ljava/lang/Boolean;->valueOf(Z)Ljava/lang/Boolean;
-                        move-result-object v0
-                        iput-object v0, p0, $helperMethodClass->$helperFieldName:Ljava/lang/Boolean;
-                        return v3
-                    """
-                )
-            }
-            methods.add(helperMethod)
+                // Modify isDefaultAudioTrack() to call extension helper method.
+                isDefaultAudioTrackMethod.apply {
+                    val index = indexOfFirstInstructionOrThrow(Opcode.RETURN)
+                    val register = getInstruction<OneRegisterInstruction>(index).registerA
 
-            // Modify isDefaultAudioTrack() to call extension helper method.
-            isDefaultAudioTrackMethod.apply {
-                val index = indexOfFirstInstructionOrThrow(Opcode.RETURN)
-                val register = getInstruction<OneRegisterInstruction>(index).registerA
-
-                addInstructions(
-                    index,
-                    """
-                        invoke-direct { p0, v$register }, $helperMethodClass->$helperMethodName(Z)Z
-                        move-result v$register
-                    """
-                )
+                    addInstructions(
+                        index,
+                        """
+                            invoke-direct { p0, v$register }, $helperMethodClass->$helperMethodName(Z)Z
+                            move-result v$register
+                        """
+                    )
+                }
             }
         }
     }
