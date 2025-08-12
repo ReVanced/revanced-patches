6dcf28ee558b3588387055797e20c2a52f699197
AndnixSH <40742924+AndnixSH@users.noreply.github.com>
2025-08-12 18:00:18 +0200

    Update the fingerprint to be more specific

--- patches/src/main/kotlin/app/revanced/patches/youtube/layout/hide/signintotvpopup/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/youtube/layout/hide/signintotvpopup/Fingerprints.kt
@@ -2,9 +2,27 @@ package app.revanced.patches.youtube.layout.hide.signintotvpopup
 
 import app.revanced.patcher.fingerprint
 import com.android.tools.smali.dexlib2.AccessFlags
+import com.android.tools.smali.dexlib2.Opcode
 
 internal val signInToTvPopupFingerprint = fingerprint {
     accessFlags(AccessFlags.PUBLIC, AccessFlags.FINAL)
     returns("Z")
     parameters("Ljava/lang/String;", "Z", "L")
+    opcodes(
+        Opcode.CONST, //mdx_seamless_tv_sign_in_drawer_fragment_title
+        Opcode.INVOKE_VIRTUAL,
+        Opcode.MOVE_RESULT_OBJECT,
+        Opcode.INVOKE_INTERFACE,
+        Opcode.MOVE_RESULT_OBJECT,
+        Opcode.CHECK_CAST,
+        Opcode.CONST, //mdx_seamless_tv_sign_in_drawer_fragment_subtitle
+        Opcode.INVOKE_VIRTUAL,
+        Opcode.MOVE_RESULT_OBJECT,
+        Opcode.INVOKE_STATIC,
+        Opcode.MOVE_RESULT_OBJECT,
+        Opcode.MOVE,
+        Opcode.MOVE_OBJECT,
+        Opcode.INVOKE_VIRTUAL_RANGE,
+        Opcode.MOVE_RESULT,
+    )
 }
