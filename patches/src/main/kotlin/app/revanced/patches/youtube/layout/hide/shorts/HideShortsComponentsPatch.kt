2274eeb6e69b8442f145198f85de2ddc1e6c880d
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2025-08-12 00:02:09 -0400

    Disable Shorts hide buttons that currently cannot be filtered with 20.22+

--- patches/src/main/kotlin/app/revanced/patches/youtube/layout/hide/shorts/HideShortsComponentsPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/youtube/layout/hide/shorts/HideShortsComponentsPatch.kt
@@ -19,6 +19,7 @@ import app.revanced.patches.youtube.misc.litho.filter.lithoFilterPatch
 import app.revanced.patches.youtube.misc.navigation.navigationBarHookPatch
 import app.revanced.patches.youtube.misc.playservice.is_19_41_or_greater
 import app.revanced.patches.youtube.misc.playservice.is_20_07_or_greater
+import app.revanced.patches.youtube.misc.playservice.is_20_22_or_greater
 import app.revanced.patches.youtube.misc.playservice.versionCheckPatch
 import app.revanced.patches.youtube.misc.settings.PreferenceScreen
 import app.revanced.patches.youtube.misc.settings.settingsPatch
@@ -58,6 +59,62 @@ private val hideShortsComponentsResourcePatch = resourcePatch {
 
         addResources("youtube", "layout.hide.shorts.hideShortsComponentsResourcePatch")
 
+        val preferences = mutableSetOf(
+            // Shorts player components.
+            // Ideally each group should be ordered similar to how they appear in the UI
+
+            // Vertical row of buttons on right side of the screen.
+            SwitchPreference("revanced_hide_shorts_like_fountain"),
+            SwitchPreference("revanced_hide_shorts_like_button"),
+            SwitchPreference("revanced_hide_shorts_dislike_button"),
+        )
+
+        if (!is_20_22_or_greater) {
+            // FIXME: The buffer is very different for 20.22+ and these current cannot be hidden.
+            preferences.addAll(
+                listOf(
+                    SwitchPreference("revanced_hide_shorts_comments_button"),
+                    SwitchPreference("revanced_hide_shorts_share_button"),
+                    SwitchPreference("revanced_hide_shorts_remix_button"),
+                    SwitchPreference("revanced_hide_shorts_sound_button")
+                )
+            )
+        }
+
+        preferences.addAll(
+            listOf(
+                // Upper and middle area of the player.
+                SwitchPreference("revanced_hide_shorts_join_button"),
+                SwitchPreference("revanced_hide_shorts_subscribe_button"),
+                SwitchPreference("revanced_hide_shorts_paused_overlay_buttons"),
+
+                // Suggested actions.
+                SwitchPreference("revanced_hide_shorts_preview_comment"),
+                SwitchPreference("revanced_hide_shorts_save_sound_button"),
+                SwitchPreference("revanced_hide_shorts_use_sound_button"),
+                SwitchPreference("revanced_hide_shorts_use_template_button"),
+                SwitchPreference("revanced_hide_shorts_upcoming_button"),
+                SwitchPreference("revanced_hide_shorts_effect_button"),
+                SwitchPreference("revanced_hide_shorts_green_screen_button"),
+                SwitchPreference("revanced_hide_shorts_hashtag_button"),
+                SwitchPreference("revanced_hide_shorts_new_posts_button"),
+                SwitchPreference("revanced_hide_shorts_shop_button"),
+                SwitchPreference("revanced_hide_shorts_tagged_products"),
+                SwitchPreference("revanced_hide_shorts_search_suggestions"),
+                SwitchPreference("revanced_hide_shorts_super_thanks_button"),
+                SwitchPreference("revanced_hide_shorts_stickers"),
+
+                // Bottom of the screen.
+                SwitchPreference("revanced_hide_shorts_location_label"),
+                SwitchPreference("revanced_hide_shorts_channel_bar"),
+                SwitchPreference("revanced_hide_shorts_info_panel"),
+                SwitchPreference("revanced_hide_shorts_full_video_link_label"),
+                SwitchPreference("revanced_hide_shorts_video_title"),
+                SwitchPreference("revanced_hide_shorts_sound_metadata_label"),
+                SwitchPreference("revanced_hide_shorts_navigation_bar"),
+            )
+        )
+
         PreferenceScreen.SHORTS.addPreferences(
             SwitchPreference("revanced_hide_shorts_home"),
             SwitchPreference("revanced_hide_shorts_search"),
@@ -67,50 +124,8 @@ private val hideShortsComponentsResourcePatch = resourcePatch {
             PreferenceScreenPreference(
                 key = "revanced_shorts_player_screen",
                 sorting = PreferenceScreenPreference.Sorting.UNSORTED,
-                preferences = setOf(
-                    // Shorts player components.
-                    // Ideally each group should be ordered similar to how they appear in the UI
-
-                    // Vertical row of buttons on right side of the screen.
-                    SwitchPreference("revanced_hide_shorts_like_fountain"),
-                    SwitchPreference("revanced_hide_shorts_like_button"),
-                    SwitchPreference("revanced_hide_shorts_dislike_button"),
-                    SwitchPreference("revanced_hide_shorts_comments_button"),
-                    SwitchPreference("revanced_hide_shorts_share_button"),
-                    SwitchPreference("revanced_hide_shorts_remix_button"),
-                    SwitchPreference("revanced_hide_shorts_sound_button"),
-
-                    // Upper and middle area of the player.
-                    SwitchPreference("revanced_hide_shorts_join_button"),
-                    SwitchPreference("revanced_hide_shorts_subscribe_button"),
-                    SwitchPreference("revanced_hide_shorts_paused_overlay_buttons"),
-
-                    // Suggested actions.
-                    SwitchPreference("revanced_hide_shorts_preview_comment"),
-                    SwitchPreference("revanced_hide_shorts_save_sound_button"),
-                    SwitchPreference("revanced_hide_shorts_use_sound_button"),
-                    SwitchPreference("revanced_hide_shorts_use_template_button"),
-                    SwitchPreference("revanced_hide_shorts_upcoming_button"),
-                    SwitchPreference("revanced_hide_shorts_effect_button"),
-                    SwitchPreference("revanced_hide_shorts_green_screen_button"),
-                    SwitchPreference("revanced_hide_shorts_hashtag_button"),
-                    SwitchPreference("revanced_hide_shorts_new_posts_button"),
-                    SwitchPreference("revanced_hide_shorts_shop_button"),
-                    SwitchPreference("revanced_hide_shorts_tagged_products"),
-                    SwitchPreference("revanced_hide_shorts_search_suggestions"),
-                    SwitchPreference("revanced_hide_shorts_super_thanks_button"),
-                    SwitchPreference("revanced_hide_shorts_stickers"),
-
-                    // Bottom of the screen.
-                    SwitchPreference("revanced_hide_shorts_location_label"),
-                    SwitchPreference("revanced_hide_shorts_channel_bar"),
-                    SwitchPreference("revanced_hide_shorts_info_panel"),
-                    SwitchPreference("revanced_hide_shorts_full_video_link_label"),
-                    SwitchPreference("revanced_hide_shorts_video_title"),
-                    SwitchPreference("revanced_hide_shorts_sound_metadata_label"),
-                    SwitchPreference("revanced_hide_shorts_navigation_bar"),
-                ),
-            ),
+                preferences = preferences,
+            )
         )
 
         // Verify the file has the expected node, even if the patch option is off.
