616768e5f335eb13549adb327208416f7cee8f66
emiferpro <emiferpro@gmail.com>
2025-08-18 16:28:24 -0300

    feat(Spotify): refactor ForceLocalPlaybackPatch.kt, and its corresponding Fingerprints.kt

--- patches/src/main/kotlin/app/revanced/patches/spotify/misc/volume/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/spotify/misc/volume/Fingerprints.kt
@@ -2,20 +2,24 @@ package app.revanced.patches.spotify.misc.volume
 
 import app.revanced.patcher.fingerprint
 import app.revanced.util.getReference
-import app.revanced.util.indexOfFirstInstruction
+import app.revanced.util.indexOfFirstInstructionOrThrow
 import com.android.tools.smali.dexlib2.AccessFlags
 import com.android.tools.smali.dexlib2.iface.reference.*;
 
 internal val forceLocalPlaybackFingerprint = fingerprint {
     accessFlags(AccessFlags.PUBLIC, AccessFlags.FINAL)
     returns("V")
-    parameters("Lp/vgc;")
 
     strings("mediaSessionCompat")
 
+    // This custom check is NECESSARY for accuracy. It pinpoints the exact
+    // method by finding the unique call to "setPlaybackToRemote".
+    // The main reason is that there are 22 matches to the "mediaSessionCompat" string.
     custom { method, _ ->
-        method.indexOfFirstInstruction {
+        method.indexOfFirstInstructionOrThrow {
             getReference<MethodReference>()?.name == "setPlaybackToRemote"
         } >= 0
+
+        method.parameters.isNotEmpty()
     }
 }
