4a8a3e69bccc2d9a8d8e6af7c9f636a336552751
PainfulPaladins <painfulpaladins@gmail.com>
2025-08-20 19:27:54 +0300

    added: improvements suggested

--- patches/src/main/kotlin/app/revanced/patches/instagram/hide/reels_navbar_button/hideReelsButton.kt
+++ patches/src/main/kotlin/app/revanced/patches/instagram/hide/reels_navbar_button/hideReelsButton.kt
@@ -10,18 +10,22 @@ import app.revanced.patcher.util.smali.ExternalLabel
 val hideReelsNavbarButton = bytecodePatch(
     name = "Hide Reels button on navbar",
 ) {
-    compatibleWith("com.instagram.android"("393.1.0.50.76"))
-
     execute {
 
         bytecodePatch {
-            val endIndex = tabCreateButtonsFingerprint.patternMatch!!.endIndex
+            tabCreateButtonsFingerprint.let {
+                val endIndex = it.patternMatch!!.endIndex
+                val instruction = it.method.getInstruction(endIndex + 1)
 
-            val instruction = tabCreateButtonsFingerprint.method.getInstruction(endIndex+1)
-            tabCreateButtonsFingerprint.method.addInstructionsWithLabels(endIndex-1,"""
+                tabCreateButtonsFingerprint.method.addInstructionsWithLabels(endIndex-1,"""
                 const v1, 0x3
                 if-eq v1,v8, :skipAdd
             """, ExternalLabel("skipAdd", instruction))
+            }
+
+
+
+
         }
     }
 }
