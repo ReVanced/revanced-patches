df1946a2515d88488614f4667a110a051c3f3745
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2025-08-20 13:11:34 -0400

    refactor

--- patches/src/main/kotlin/app/revanced/patches/instagram/hide/reels_navbar_button/hideReelsButton.kt
+++ patches/src/main/kotlin/app/revanced/patches/instagram/hide/reels_navbar_button/hideReelsButton.kt
@@ -1,33 +1,27 @@
 package app.revanced.patches.instagram.hide.reels_navbar_button
 
 import app.revanced.patcher.extensions.InstructionExtensions.addInstructionsWithLabels
-import app.revanced.patcher.extensions.InstructionExtensions.getInstruction
 import app.revanced.patcher.patch.bytecodePatch
-import app.revanced.patcher.util.smali.ExternalLabel
-
 
 @Suppress("unused")
 val hideReelsNavbarButton = bytecodePatch(
     name = "Hide navigation bar Reels button",
 ) {
-    execute {
-
     compatibleWith("com.instagram.android")
 
-        bytecodePatch {
-            tabCreateButtonsFingerprint.let {
-                val endIndex = it.patternMatch!!.endIndex
-                val instruction = it.method.getInstruction(endIndex + 1)
+    execute {
+        tabCreateButtonsFingerprint.let {
+            val endIndex = it.patternMatch!!.endIndex
 
-                it.method.addInstructionsWithLabels(endIndex-1,"""
-                const v1, 0x3
-                if-eq v1,v8, :skipAdd
-            """, ExternalLabel("skipAdd", instruction))
-            }
-
-
-
-
+            it.method.addInstructionsWithLabels(
+                endIndex - 1,
+                """
+                    const v1, 0x3
+                    if-eq v1,v8, :skipButton
+                    :skipButton
+                    nop
+                """
+            )
         }
     }
 }
