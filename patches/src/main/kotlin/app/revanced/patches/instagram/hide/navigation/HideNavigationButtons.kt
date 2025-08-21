2ce004148ee7a50a489129a0437d6f7d0565b5b0
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2025-08-21 05:55:10 -0400

    Update patches/src/main/kotlin/app/revanced/patches/instagram/hide/navigation/HideNavigationButtons.kt
    Co-authored-by: oSumAtrIX <johan.melkonyan1@web.de>

--- patches/src/main/kotlin/app/revanced/patches/instagram/hide/navigation/HideNavigationButtons.kt
+++ patches/src/main/kotlin/app/revanced/patches/instagram/hide/navigation/HideNavigationButtons.kt
@@ -12,7 +12,7 @@ import java.util.logging.Logger
 @Suppress("unused")
 val hideNavigationButtonsPatch = bytecodePatch(
     name = "Hide navigation buttons",
-    description = "Hides navigation bar buttons, such as Reels and the create button.",
+    description = "Hides navigation bar buttons, such as Reels and the Create button.",
     use = false
 ) {
     compatibleWith("com.instagram.android")
