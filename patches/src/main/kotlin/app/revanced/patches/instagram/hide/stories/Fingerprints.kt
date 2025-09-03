42936a8bea910c40cc14a4089472b4ed4d101c41
PainfulPaladins <painfulpaladins@gmail.com>
2025-09-03 19:06:09 +0300

    feat(Instagram): Remove Stories from Home Page

---
+++ patches/src/main/kotlin/app/revanced/patches/instagram/hide/stories/Fingerprints.kt
@@ -0,0 +1,15 @@
+package app.revanced.patches.instagram.hide.stories
+import app.revanced.patcher.fingerprint
+import com.android.tools.smali.dexlib2.Opcode
+
+
+internal val addStoryButton = fingerprint {
+        custom { method, classDef ->
+            classDef.toString() == "Lcom/instagram/reels/ui/views/reelavatar/RecyclerReelAvatarView;"
+        }
+        opcodes(
+            Opcode.INVOKE_VIRTUAL,
+            Opcode.IPUT_OBJECT,
+            Opcode.INVOKE_VIRTUAL // Add View (Story)
+        )
+    }
---
+++ patches/src/main/kotlin/app/revanced/patches/instagram/hide/stories/HideStories.kt
@@ -0,0 +1,24 @@
+package app.revanced.patches.instagram.hide.stories
+import app.revanced.patcher.extensions.InstructionExtensions.addInstructionsWithLabels
+import app.revanced.patcher.extensions.InstructionExtensions.getInstruction
+import app.revanced.patcher.extensions.InstructionExtensions.removeInstruction
+import app.revanced.patcher.extensions.InstructionExtensions.replaceInstructions
+import app.revanced.patcher.patch.booleanOption
+import app.revanced.patcher.patch.bytecodePatch
+import com.android.tools.smali.dexlib2.iface.instruction.TwoRegisterInstruction
+
+@Suppress("unused")
+val hideReelsPatch = bytecodePatch(
+    name = "Hide Stories",
+    description = "Hides Stories from the main page.",
+    use = false
+) {
+    compatibleWith("com.instagram.android")
+    execute {
+        val addStoryMethod = addStoryButton.method
+        val addStoryEndIndex = addStoryButton.patternMatch!!.endIndex
+
+        // Remove addView of Story.
+        addStoryMethod.removeInstruction(addStoryEndIndex)
+    }
+}
