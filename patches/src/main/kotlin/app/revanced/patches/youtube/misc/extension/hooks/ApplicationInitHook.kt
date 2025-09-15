4fba8bf715b82a0fe7a0c1824073a7d2655a8bc2
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2025-09-15 20:56:53 +0400

    Add second context hook just in case

--- patches/src/main/kotlin/app/revanced/patches/youtube/misc/extension/hooks/ApplicationInitHook.kt
+++ patches/src/main/kotlin/app/revanced/patches/youtube/misc/extension/hooks/ApplicationInitHook.kt
@@ -8,9 +8,16 @@ import app.revanced.patches.youtube.shared.YOUTUBE_MAIN_ACTIVITY_CLASS_TYPE
  */
 // Extension context is the Activity itself.
 internal val applicationInitHook = extensionHook {
+    // Does _not_ resolve to the YouTube main activity.
+    // Required as some hooked code runs before the main activity is launched.
+    strings("Application creation", "Application.onCreate")
+}
+
+internal val applicationInitOnCrateHook = extensionHook {
     returns("V")
     parameters("Landroid/os/Bundle;")
     custom { method, classDef ->
         method.name == "onCreate" && classDef.type == YOUTUBE_MAIN_ACTIVITY_CLASS_TYPE
     }
 }
+
