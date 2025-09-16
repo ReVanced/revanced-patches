fc4eec994a19b31ed24a2c504c9461ec38d391cc
samolego <34912839+samolego@users.noreply.github.com>
2025-09-16 07:42:24 +0200

    improve code readability

--- patches/src/main/kotlin/app/revanced/patches/viber/ads/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/viber/ads/Fingerprints.kt
@@ -4,18 +4,6 @@ import app.revanced.patcher.fingerprint
 
 const val ADS_FREE_STR = "viber_plus_debug_ads_free_flag"
 
-internal val adsFreeFingerprint = { className: String ->
-    fingerprint {
-        returns("I")
-        parameters()
-        custom { method, classDef ->
-            classDef.type == className &&
-            classDef.superclass != null &&
-            classDef.methods.count() == 1
-        }
-    }
-}
-
 internal val findAdStringFingerprint = fingerprint {
     strings(ADS_FREE_STR)
 }
--- patches/src/main/kotlin/app/revanced/patches/viber/ads/HideAdsPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/viber/ads/HideAdsPatch.kt
@@ -1,11 +1,14 @@
 package app.revanced.patches.viber.ads
 
+import app.revanced.patcher.extensions.InstructionExtensions.getInstruction
+import app.revanced.patcher.fingerprint
 import app.revanced.patcher.patch.PatchException
 import app.revanced.patcher.patch.bytecodePatch
+import app.revanced.util.indexOfFirstInstructionReversedOrThrow
 import app.revanced.util.returnEarly
+import com.android.tools.smali.dexlib2.iface.instruction.ReferenceInstruction
 import com.android.tools.smali.dexlib2.iface.instruction.formats.Instruction21c
 import com.android.tools.smali.dexlib2.iface.reference.TypeReference
-import com.android.tools.smali.dexlib2.iface.reference.StringReference
 
 @Suppress("unused")
 val hideAdsPatch = bytecodePatch(
@@ -15,24 +18,29 @@ val hideAdsPatch = bytecodePatch(
     compatibleWith("com.viber.voip"("25.9.2.0", "26.1.2.0"))
 
     execute {
-        val instructions = findAdStringFingerprint.method.implementation!!.instructions.toList()
+        val method = findAdStringFingerprint.method
+
+        val instructions = method.implementation!!.instructions.toList()
 
         // Find the ads free string index
-        val stringIndex = instructions.indexOfFirst { instruction ->
-            instruction is Instruction21c &&
-            (instruction.reference as? StringReference)?.string == ADS_FREE_STR
+        val stringIndex = findAdStringFingerprint.stringMatches!!.indexOfFirst { it.string == ADS_FREE_STR }
+
+        // Search backwards from the string to find the `new-instance` (TypeReference) instruction
+        val typeRefIndex = method.indexOfFirstInstructionReversedOrThrow(stringIndex) {
+            this is Instruction21c && this.reference is TypeReference
         }
 
-        // Find the last TypeReference before the string
-        // to get the class (object type) of the no ads field
-        val targetClassName = instructions.take(stringIndex)
-            .filterIsInstance<Instruction21c>()
-            .lastOrNull { it.reference is TypeReference }
-            ?.let { (it.reference as TypeReference).type }
-            ?: throw PatchException("Could not find obfuscated class for $ADS_FREE_STR")
-
+        // Get the class name from the TypeReference
+        val targetClass = method.getInstruction<ReferenceInstruction>(typeRefIndex).reference as TypeReference
 
         // Patch the ads-free method to always return true
-        adsFreeFingerprint(targetClassName).method.returnEarly(1)
+        fingerprint {
+            returns("I")
+            parameters()
+            custom { method, classDef ->
+                classDef == targetClass &&
+                classDef.superclass != null
+            }
+        }.method.returnEarly(1)
     }
 }
