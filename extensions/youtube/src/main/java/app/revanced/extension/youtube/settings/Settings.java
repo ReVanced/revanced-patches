cb318dd1672cb49366ff3e6a7d2992d0f8e1162b
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2025-09-20 19:22:56 +0400

    Handle using newer settings with old install

--- extensions/youtube/src/main/java/app/revanced/extension/youtube/settings/Settings.java
+++ extensions/youtube/src/main/java/app/revanced/extension/youtube/settings/Settings.java
@@ -32,6 +32,7 @@ import static app.revanced.extension.youtube.sponsorblock.objects.CategoryBehavi
 import android.graphics.Color;
 
 import app.revanced.extension.shared.Logger;
+import app.revanced.extension.shared.Utils;
 import app.revanced.extension.shared.settings.BaseSettings;
 import app.revanced.extension.shared.settings.BooleanSetting;
 import app.revanced.extension.shared.settings.EnumSetting;
@@ -514,10 +515,14 @@ public class Settings extends BaseSettings {
             DEPRECATED_SWIPE_OVERLAY_BACKGROUND_ALPHA.resetToDefault();
         }
 
-        // Old spoof versions that no longer work.
-        if (SPOOF_APP_VERSION_TARGET.get().compareTo(SPOOF_APP_VERSION_TARGET.defaultValue) < 0) {
-            Logger.printInfo(() -> "Resetting spoof app version target");
+        // Old spoof versions that no longer work,
+        // or is spoofing to a version the same or newer than this app.
+        if (!SPOOF_APP_VERSION_TARGET.isSetToDefault() &&
+                (SPOOF_APP_VERSION_TARGET.get().compareTo(SPOOF_APP_VERSION_TARGET.defaultValue) < 0
+                || (Utils.getAppVersionName().compareTo(SPOOF_APP_VERSION_TARGET.get()) <= 0))) {
+            Logger.printInfo(() -> "Resetting spoof app version");
             SPOOF_APP_VERSION_TARGET.resetToDefault();
+            SPOOF_APP_VERSION.resetToDefault();
         }
 
         // RYD requires manually migrating old settings since the lack of
