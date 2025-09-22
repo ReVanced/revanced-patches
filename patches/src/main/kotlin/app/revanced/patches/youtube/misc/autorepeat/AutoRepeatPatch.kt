7e010d38cc60abf964af812c00e7233cebf99824
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2025-09-22 22:05:54 +0400

    delete deprecated dummy files

--- patches/src/main/kotlin/app/revanced/patches/youtube/misc/autorepeat/AutoRepeatPatch.kt
+++
@@ -1,9 +0,0 @@
-package app.revanced.patches.youtube.misc.autorepeat
-
-import app.revanced.patcher.patch.bytecodePatch
-import app.revanced.patches.youtube.misc.loopvideo.loopVideoPatch
-
-@Deprecated("Patch was renamed", ReplaceWith("looVideoPatch"))
-val autoRepeatPatch = bytecodePatch {
-    dependsOn(loopVideoPatch)
-}
