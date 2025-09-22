dfb5407e67222e80e23c8935e04b6dbf1a43d757
MarcaD <152095496+MarcaDian@users.noreply.github.com>
2025-09-22 11:54:09 +0300

    feat(YouTube - Loop video): Add player button to change loop video state (#5961)

--- patches/src/main/kotlin/app/revanced/patches/youtube/misc/autorepeat/AutoRepeatPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/youtube/misc/autorepeat/AutoRepeatPatch.kt
@@ -1,62 +1,9 @@
 package app.revanced.patches.youtube.misc.autorepeat
 
-import app.revanced.patcher.extensions.InstructionExtensions.addInstructionsWithLabels
-import app.revanced.patcher.extensions.InstructionExtensions.instructions
-import app.revanced.patcher.extensions.InstructionExtensions.removeInstruction
 import app.revanced.patcher.patch.bytecodePatch
-import app.revanced.patches.all.misc.resources.addResources
-import app.revanced.patches.all.misc.resources.addResourcesPatch
-import app.revanced.patches.shared.misc.settings.preference.SwitchPreference
-import app.revanced.patches.youtube.misc.extension.sharedExtensionPatch
-import app.revanced.patches.youtube.misc.settings.PreferenceScreen
-import app.revanced.patches.youtube.shared.autoRepeatFingerprint
-import app.revanced.patches.youtube.shared.autoRepeatParentFingerprint
+import app.revanced.patches.youtube.misc.loopvideo.loopVideoPatch
 
-// TODO: Rename this patch to AlwaysRepeatPatch (as well as strings and references in the extension).
-val autoRepeatPatch = bytecodePatch(
-    name = "Always repeat",
-    description = "Adds an option to always repeat videos when they end.",
-) {
-    dependsOn(
-        sharedExtensionPatch,
-        addResourcesPatch,
-    )
-
-    compatibleWith(
-        "com.google.android.youtube"(
-            "19.34.42",
-            "19.43.41",
-            "20.07.39",
-            "20.13.41",
-            "20.14.43",
-        )
-    )
-
-    execute {
-        addResources("youtube", "misc.autorepeat.autoRepeatPatch")
-
-        PreferenceScreen.MISC.addPreferences(
-            SwitchPreference("revanced_auto_repeat"),
-        )
-
-        autoRepeatFingerprint.match(autoRepeatParentFingerprint.originalClassDef).method.apply {
-            val playMethod = autoRepeatParentFingerprint.method
-            val index = instructions.lastIndex
-
-            // Remove return-void.
-            removeInstruction(index)
-            // Add own instructions there.
-            addInstructionsWithLabels(
-                index,
-                """
-                    invoke-static {}, Lapp/revanced/extension/youtube/patches/AutoRepeatPatch;->shouldAutoRepeat()Z
-                    move-result v0
-                    if-eqz v0, :noautorepeat
-                    invoke-virtual { p0 }, $playMethod
-                    :noautorepeat
-                    return-void
-                """,
-            )
-        }
-    }
+@Deprecated("Patch was renamed", ReplaceWith("looVideoPatch"))
+val autoRepeatPatch = bytecodePatch {
+    dependsOn(loopVideoPatch)
 }
