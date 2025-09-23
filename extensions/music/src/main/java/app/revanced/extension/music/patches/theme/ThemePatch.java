3ccfd7389a4dfc3f69f294aa6509fa107cc8c478
MarcaDian <152095496+MarcaDian@users.noreply.github.com>
2025-09-23 16:26:59 +0300

    move to abstract

--- extensions/music/src/main/java/app/revanced/extension/music/patches/theme/ThemePatch.java
+++ extensions/music/src/main/java/app/revanced/extension/music/patches/theme/ThemePatch.java
@@ -1,9 +1,9 @@
 package app.revanced.extension.music.patches.theme;
 
-import app.revanced.extension.shared.Utils;
+import app.revanced.extension.shared.theme.BaseThemePatch;
 
 @SuppressWarnings("unused")
-public class ThemePatch {
+public class ThemePatch extends BaseThemePatch {
     // Color constants used in relation with litho components.
     private static final int[] DARK_VALUES = {
             0xFF212121, // Comments box background.
@@ -11,29 +11,16 @@ public class ThemePatch {
             0xFF000000, // Button container background in playlist.
     };
 
-    // Background colors
-    private static final int BLACK_COLOR = Utils.getResourceColor("yt_black1");
-
-     /**
+    /**
      * Injection point.
      * <p>
      * Change the color of Litho components.
-     * If the color of the component matches one of the values, return the background color .
+     * If the color of the component matches one of the values, return the background color.
      *
      * @param originalValue The original color value.
-     * @return The new or original color value
+     * @return The new or original color value.
      */
     public static int getValue(int originalValue) {
-        if (Utils.isDarkModeEnabled()) {
-            if (anyEquals(originalValue, DARK_VALUES)) return BLACK_COLOR;
-        }
-
-        return originalValue;
-    }
-
-    private static boolean anyEquals(int value, int... of) {
-        for (int v : of) if (value == v) return true;
-
-        return false;
+        return processColorValue(originalValue, DARK_VALUES, null);
     }
 }
