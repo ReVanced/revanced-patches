160003c67459a6955af3e8a9e079620cb4bfc8c4
MarcaDian <152095496+MarcaDian@users.noreply.github.com>
2025-09-23 13:45:51 +0300

    fix?

--- patches/src/main/kotlin/app/revanced/patches/music/layout/theme/ThemePatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/music/layout/theme/ThemePatch.kt
@@ -15,7 +15,7 @@ import org.w3c.dom.Element
 
 private const val EXTENSION_CLASS_DESCRIPTOR = "Lapp/revanced/extension/music/patches/theme/ThemePatch;"
 
-private val musicThemeBytecodePatch = bytecodePatch(
+val musicThemeBytecodePatch = bytecodePatch(
     name = "Theme",
     description = "Adds options for theming and applies a custom background theme (defaults to pure black).",
 ) {
