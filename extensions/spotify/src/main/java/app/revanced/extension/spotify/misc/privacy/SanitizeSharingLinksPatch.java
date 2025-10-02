31d17d105796c945997f399eb240730601bc52a9
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2025-10-02 11:29:22 +0400

    Use shared class for YouTube and YT Music

--- extensions/spotify/src/main/java/app/revanced/extension/spotify/misc/privacy/SanitizeSharingLinksPatch.java
+++ extensions/spotify/src/main/java/app/revanced/extension/spotify/misc/privacy/SanitizeSharingLinksPatch.java
@@ -4,6 +4,7 @@ import app.revanced.extension.shared.privacy.LinkSanitizer;
 
 @SuppressWarnings("unused")
 public final class SanitizeSharingLinksPatch {
+
     private static final LinkSanitizer sanitizer = new LinkSanitizer(
             "si", // Share tracking parameter.
             "utm_source" // Share source, such as "copy-link".
