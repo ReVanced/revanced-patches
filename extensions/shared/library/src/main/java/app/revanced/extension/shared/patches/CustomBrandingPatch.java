921b0d1e3f75ab37a7801c5461e9e9dcb70682b5
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2025-10-06 00:50:55 +0400

    fix app won't startup if bad settings name index is imported

--- extensions/shared/library/src/main/java/app/revanced/extension/shared/patches/CustomBrandingPatch.java
+++ extensions/shared/library/src/main/java/app/revanced/extension/shared/patches/CustomBrandingPatch.java
@@ -4,6 +4,9 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.pm.PackageManager;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import app.revanced.extension.shared.Logger;
 import app.revanced.extension.shared.Utils;
 import app.revanced.extension.shared.settings.BaseSettings;
@@ -15,6 +18,7 @@ import app.revanced.extension.shared.settings.BaseSettings;
 public class CustomBrandingPatch {
 
     public enum BrandingTheme {
+        // Original must be first.
         ORIGINAL("revanced_original"),
         ROUNDED("revanced_rounded"),
         MINIMAL("revanced_minimal"),
@@ -45,6 +49,11 @@ public class CustomBrandingPatch {
         throw new IllegalStateException();
     }
 
+    private static ComponentName createComponentName(BrandingTheme theme, String packageName, int index) {
+        return new ComponentName(packageName,
+                packageName + '.' + theme.themeAlias + '_' + index);
+    }
+
     /**
      * Injection point.
      */
@@ -54,51 +63,74 @@ public class CustomBrandingPatch {
             PackageManager pm = context.getPackageManager();
             String packageName = context.getPackageName();
 
+            final int numberOfCustomNames = numberOfCustomNames();
             BrandingTheme selectedBranding = BaseSettings.CUSTOM_BRANDING_ICON.get();
-            final int selectedNameIndex = BaseSettings.CUSTOM_BRANDING_NAME.get();
+            int selectedNameIndex = BaseSettings.CUSTOM_BRANDING_NAME.get();
+            if (selectedNameIndex <= 0 || selectedNameIndex > numberOfCustomNames) {
+                // User imported a bad app name index value. Either the imported data
+                // was corrupted, or they previously had custom name enabled and the app
+                // no longer has a custom name specified.
+                Utils.showToastLong("Custom branding reset");
+                selectedNameIndex = BaseSettings.CUSTOM_BRANDING_NAME.resetToDefault();
+            }
+
             final boolean customIconIncluded = customIconIncluded();
             ComponentName componentToEnable = null;
+            ComponentName defaultComponent = null;
+            List<ComponentName> componentsToDisable = new ArrayList<>();
 
-            for (int index = 1, maxIndex = numberOfCustomNames(); index <= maxIndex; index++) {
-                for (BrandingTheme theme : BrandingTheme.values()) {
+            for (BrandingTheme theme : BrandingTheme.values()) {
+                // App name indices starts at 1.
+                for (int index = 1; index <= numberOfCustomNames; index++) {
                     if (!customIconIncluded && theme == BrandingTheme.CUSTOM) {
                         continue;
                     }
 
                     String aliasClass = packageName + '.' + theme.themeAlias + '_' + index;
                     ComponentName component = new ComponentName(packageName, aliasClass);
-
-                    final boolean aliasMatchesSettings = theme == selectedBranding && index == selectedNameIndex;
-                    if (aliasMatchesSettings) {
-                        componentToEnable = component;
+                    if (defaultComponent == null) {
+                        // Default is always the first alias.
+                        defaultComponent = component;
                     }
 
-                    final int desiredState = aliasMatchesSettings
-                            ? PackageManager.COMPONENT_ENABLED_STATE_ENABLED
-                            : PackageManager.COMPONENT_ENABLED_STATE_DISABLED;
-                    final int currentState = pm.getComponentEnabledSetting(component);
-                    if (currentState != desiredState) {
-                        // First turn off all aliases, then turn on what is needed.
-                        // This is required otherwise the app can shutdown instead of restarting
-                        // leaving the aliases in a corrupted and conflicting state.
-                        if (desiredState == PackageManager.COMPONENT_ENABLED_STATE_DISABLED) {
-                            // Don't allow the app to be killed.
-                            pm.setComponentEnabledSetting(component, desiredState, PackageManager.DONT_KILL_APP);
+                    if (index == selectedNameIndex && theme == selectedBranding) {
+                        if (componentToEnable != null) {
+                            // Should never happen.
+                            ComponentName componentToEnableFinal = componentToEnable;
+                            Logger.printException(() -> "Found duplicate alias: "
+                                    + componentToEnableFinal.getClassName());
+                            componentsToDisable.add(componentToEnable);
                         }
+                        componentToEnable = component;
+                    } else {
+                        componentsToDisable.add(component);
                     }
                 }
             }
 
-            if (componentToEnable != null) {
-                pm.setComponentEnabledSetting(componentToEnable,
-                        PackageManager.COMPONENT_ENABLED_STATE_ENABLED, 0);
-            } else {
-                // Settings are for custom user icons but no user icons are present.
-                Utils.showToastLong("Resetting to default custom branding");
+            if (componentToEnable == null) {
+                // Should never be reached, because the branding icon enum
+                // resets itself the saved enum type is invalid or no longer exists,
+                // and a bad name index is already handled above.
+                Logger.printException(() -> "Could not find alias to enable");
                 BaseSettings.CUSTOM_BRANDING_ICON.resetToDefault();
                 BaseSettings.CUSTOM_BRANDING_NAME.resetToDefault();
-                setBrandingIcon();
+
+                componentToEnable = defaultComponent;
+                componentsToDisable.remove(defaultComponent);
             }
+
+            for (ComponentName disable : componentsToDisable) {
+                // Use info logging because if the aliases state become corrupt the app cannot launch.
+                Logger.printInfo(() -> "Disabling: " + disable.getClassName());
+                pm.setComponentEnabledSetting(disable,
+                        PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
+            }
+
+            ComponentName componentToEnableFinal = componentToEnable;
+            Logger.printInfo(() -> "Enabling:  " + componentToEnableFinal.getClassName());
+            pm.setComponentEnabledSetting(componentToEnable,
+                    PackageManager.COMPONENT_ENABLED_STATE_ENABLED, 0);
         } catch (Exception ex) {
             Logger.printException(() -> "setBrandingIcon failure", ex);
         }
