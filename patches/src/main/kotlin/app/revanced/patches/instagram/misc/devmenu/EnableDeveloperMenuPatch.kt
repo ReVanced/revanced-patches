2154d89242fd8d7f7460145d5d35a4f1986944a3
brosssh <44944126+brosssh@users.noreply.github.com>
2025-10-07 14:37:20 +0200

    feat(Instagram): Add `Enable developer menu` patch (#6043)

---
+++ patches/src/main/kotlin/app/revanced/patches/instagram/misc/devmenu/EnableDeveloperMenuPatch.kt
@@ -0,0 +1,37 @@
+package app.revanced.patches.instagram.misc.devmenu
+
+import app.revanced.patcher.patch.bytecodePatch
+import app.revanced.util.Utils.trimIndentMultiline
+import app.revanced.util.getReference
+import app.revanced.util.indexOfFirstInstructionReversedOrThrow
+import app.revanced.util.returnEarly
+import com.android.tools.smali.dexlib2.Opcode
+import com.android.tools.smali.dexlib2.iface.reference.MethodReference
+
+@Suppress("unused")
+val enableDeveloperMenuPatch = bytecodePatch(
+    name = "Enable developer menu",
+    description = """
+        Enables the developer menu, which can be found at the bottom of settings menu with name 'Internal Settings'.
+        
+        It is recommended to use this patch with an alpha/beta Instagram release. Patching a stable release works, but the developer menu shows the developer flags as numbers and does not show a human readable description.
+    """.trimIndentMultiline(),
+    use = false
+) {
+    compatibleWith("com.instagram.android")
+
+    execute {
+        with(clearNotificationReceiverFingerprint.method) {
+            indexOfFirstInstructionReversedOrThrow(clearNotificationReceiverFingerprint.stringMatches!!.first().index) {
+                val reference = getReference<MethodReference>()
+                Opcode.INVOKE_STATIC == opcode &&
+                        reference?.parameterTypes?.size == 1 &&
+                        reference.parameterTypes.first() == "Lcom/instagram/common/session/UserSession;" &&
+                        reference.returnType == "Z"
+            }.let { index ->
+                navigate(this).to(index).stop().returnEarly(true)
+            }
+        }
+    }
+}
+
