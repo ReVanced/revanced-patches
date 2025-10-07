b2e274c58ad19e72cec8349cb633bc61bdc20c35
brosssh <tiabroch@gmail.com>
2025-10-07 11:03:30 +0200

    Using shared names

--- patches/src/main/kotlin/app/revanced/patches/instagram/misc/share/privacy/SanitizeSharingLinksPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/instagram/misc/share/privacy/SanitizeSharingLinksPatch.kt
@@ -4,14 +4,16 @@ import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
 import app.revanced.patcher.patch.bytecodePatch
 import app.revanced.patches.instagram.misc.extension.sharedExtensionPatch
 import app.revanced.patches.instagram.misc.share.editShareLinksPatch
+import app.revanced.patches.shared.PATCH_DESCRIPTION_SANITIZE_SHARING_LINKS
+import app.revanced.patches.shared.PATCH_NAME_SANITIZE_SHARING_LINKS
 
 private const val EXTENSION_CLASS_DESCRIPTOR =
     "Lapp/revanced/extension/instagram/misc/share/privacy/SanitizeSharingLinksPatch;"
 
 @Suppress("unused")
 val sanitizeSharingLinksPatch = bytecodePatch(
-    name = "Sanitize sharing links",
-    description = "Removes the tracking query parameters from shared links.",
+    name = PATCH_NAME_SANITIZE_SHARING_LINKS,
+    description = PATCH_DESCRIPTION_SANITIZE_SHARING_LINKS,
 ) {
     compatibleWith("com.instagram.android")
 
