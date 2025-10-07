d7d768f4e2f798e6205b6067bc824a86cddf1691
brosssh <tiabroch@gmail.com>
2025-10-07 13:56:05 +0200

    Added description

--- patches/src/main/kotlin/app/revanced/patches/instagram/misc/devmenu/EnableDeveloperMenuPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/instagram/misc/devmenu/EnableDeveloperMenuPatch.kt
@@ -1,6 +1,7 @@
 package app.revanced.patches.instagram.misc.devmenu
 
 import app.revanced.patcher.patch.bytecodePatch
+import app.revanced.util.Utils.trimIndentMultiline
 import app.revanced.util.getReference
 import app.revanced.util.indexOfFirstInstructionReversedOrThrow
 import app.revanced.util.returnEarly
@@ -10,7 +11,12 @@ import com.android.tools.smali.dexlib2.iface.reference.MethodReference
 @Suppress("unused")
 val enableDeveloperMenuPatch = bytecodePatch(
     name = "Enable developer menu",
-    description = "Enables the developer menu, which can be found at the bottom of settings menu with name \"Internal Settings\".",
+    description = """
+        Enables the developer menu, which can be found at the bottom of settings menu with name "Internal Settings".
+        
+        This patch is suggested to be applied on Alpha/Beta builds. Applying the patch on a Stable build will still
+        enable the menu, however the flags will be displayed as numbers without a human readable description.
+    """.trimIndentMultiline(),
     use = false
 ) {
     compatibleWith("com.instagram.android")
