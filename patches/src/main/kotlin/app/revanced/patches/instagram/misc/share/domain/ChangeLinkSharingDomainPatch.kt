93159daea42befbaf7f8d5084ca1bdf62287d219
brosssh <tiabroch@gmail.com>
2025-10-09 08:01:39 +0200

    fix(Instagram - Change sharing domain): Display patch options

--- patches/src/main/kotlin/app/revanced/patches/instagram/misc/share/domain/ChangeLinkSharingDomainPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/instagram/misc/share/domain/ChangeLinkSharingDomainPatch.kt
@@ -19,14 +19,14 @@ val changeLinkSharingDomainPatch = bytecodePatch(
 
     dependsOn(sharedExtensionPatch)
 
-    execute {
-        val customDomainHost by stringOption(
-            key = "domainName",
-            default = "imginn.com",
-            title = "Domain name",
-            description = "The domain name to use when sharing links."
-        )
+    val customDomainHost by stringOption(
+        key = "domainName",
+        default = "imginn.com",
+        title = "Domain name",
+        description = "The domain name to use when sharing links."
+    )
 
+    execute {
         getCustomShareDomainFingerprint.method.returnEarly(customDomainHost!!)
 
         editShareLinksPatch { index, register ->
