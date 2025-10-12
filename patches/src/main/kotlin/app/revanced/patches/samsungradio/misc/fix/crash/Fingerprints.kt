ec84f689be44a93a809bcc2ba5178aaccfa1f43a
rospino74 <34315725+rospino74@users.noreply.github.com>
2025-10-12 17:59:12 +0200

    feat(Samsung Radio): Add crash protections

---
+++ patches/src/main/kotlin/app/revanced/patches/samsungradio/misc/fix/crash/Fingerprints.kt
@@ -0,0 +1,21 @@
+@file:Suppress("unused")
+
+package app.revanced.patches.samsungradio.misc.fix.crash
+
+import app.revanced.patcher.fingerprint
+import app.revanced.patches.all.misc.transformation.IMethodCall
+import app.revanced.patches.all.misc.transformation.fromMethodReference
+import app.revanced.util.getReference
+import com.android.tools.smali.dexlib2.iface.reference.MethodReference
+
+internal val checkCallStateFingerprint = fingerprint {
+    returns("Z")
+    strings("phone", "isOnCall. callState: ")
+}
+internal val permissionRequestListFingerprint = fingerprint {
+    strings(
+        "android.permission.POST_NOTIFICATIONS",
+        "android.permission.READ_MEDIA_AUDIO",
+        "android.permission.RECORD_AUDIO"
+    )
+}
---
+++ patches/src/main/kotlin/app/revanced/patches/samsungradio/misc/fix/crash/FixCrashPatch.kt
@@ -0,0 +1,57 @@
+@file:Suppress("unused")
+
+package app.revanced.patches.samsungradio.misc.fix.crash
+
+import app.revanced.patcher.extensions.InstructionExtensions.addInstruction
+import app.revanced.patcher.extensions.InstructionExtensions.getInstruction
+import app.revanced.patcher.extensions.InstructionExtensions.replaceInstruction
+import app.revanced.patcher.patch.bytecodePatch
+import app.revanced.patches.samsungradio.restrictions.device.bypassDeviceChecksPatch
+import app.revanced.util.findFreeRegister
+import app.revanced.util.getReference
+import app.revanced.util.indexOfFirstInstructionOrThrow
+import com.android.tools.smali.dexlib2.Opcode
+import com.android.tools.smali.dexlib2.iface.instruction.OneRegisterInstruction
+import com.android.tools.smali.dexlib2.iface.reference.StringReference
+
+
+val fixCrashPatch = bytecodePatch(
+    name = "Fix Crashes",
+    description = "Stops the app from crashing because of missing system permissions.",
+) {
+    dependsOn(addManifestPermissionsPatch)
+    dependsOn(bypassDeviceChecksPatch)
+    compatibleWith("com.sec.android.app.fm")
+
+    execute {
+        permissionRequestListFingerprint.method.apply {
+            // Search for the "android.permission.POST_NOTIFICATIONS" string and get its register
+            val notificationPermStrIndex = indexOfFirstInstructionOrThrow {
+                opcode == Opcode.CONST_STRING && getReference<StringReference>()?.string == "android.permission.POST_NOTIFICATIONS"
+            }
+            val notificationPermRegister = getInstruction<OneRegisterInstruction>(notificationPermStrIndex).registerA
+
+            // Find a free register to load our new permission string
+            val phoneStatePermRegister = findFreeRegister(notificationPermStrIndex + 1) // +1 to avoid messing with the previous instruction
+            addInstruction(
+                notificationPermStrIndex,
+                "const-string v$phoneStatePermRegister, \"android.permission.READ_PHONE_STATE\""
+            )
+
+            // Replace the next "filled-new-array" instruction with our new arrray
+            val filledNewArrayIndex = indexOfFirstInstructionOrThrow(notificationPermStrIndex) {
+                opcode == Opcode.FILLED_NEW_ARRAY
+            }
+            replaceInstruction(
+                filledNewArrayIndex,
+                "filled-new-array {v$notificationPermRegister, v$phoneStatePermRegister}, [Ljava/lang/String;"
+            )
+        }
+
+        checkCallStateFingerprint.method.apply {
+            // Return false = The user is not on a call
+            addInstruction(0, "const/4 v0, 0x0")
+            addInstruction(1, "return v0")
+        }
+    }
+}
