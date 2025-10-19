f8c093021182cec9a25311f7107df253c26fa2b8
rospino74 <34315725+rospino74@users.noreply.github.com>
2025-10-19 13:19:45 +0200

    refactor(Samsung Radio): Use indexOfFirstInstructionOrThrow for better error handling

--- patches/src/main/kotlin/app/revanced/patches/samsung/radio/restrictions/device/BypassDeviceChecksPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/samsung/radio/restrictions/device/BypassDeviceChecksPatch.kt
@@ -3,10 +3,9 @@ package app.revanced.patches.samsung.radio.restrictions.device
 import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
 import app.revanced.patcher.extensions.InstructionExtensions.removeInstructions
 import app.revanced.patcher.patch.bytecodePatch
-import app.revanced.patches.shared.misc.extension.sharedExtensionPatch
 import app.revanced.util.findFreeRegister
 import app.revanced.util.getReference
-import app.revanced.util.indexOfFirstInstruction
+import app.revanced.util.indexOfFirstInstructionOrThrow
 import com.android.tools.smali.dexlib2.Opcode
 import com.android.tools.smali.dexlib2.iface.reference.StringReference
 
@@ -26,13 +25,13 @@ val bypassDeviceChecksPatch = bytecodePatch(
         // Return false = The device is not blacklisted
         checkDeviceFingerprint.method.apply {
             // Find the first string that start with "SM-", that's the list of incompatible devices
-            val firstStringIndex = indexOfFirstInstruction {
+            val firstStringIndex = indexOfFirstInstructionOrThrow {
                 opcode == Opcode.CONST_STRING &&
                         getReference<StringReference>()?.string?.startsWith("SM-") == true
             }
 
             // Find the following filled-new-array (or filled-new-array/range) instruction
-            val filledNewArrayIndex = indexOfFirstInstruction(firstStringIndex + 1) {
+            val filledNewArrayIndex = indexOfFirstInstructionOrThrow(firstStringIndex + 1) {
                 opcode == Opcode.FILLED_NEW_ARRAY || opcode == Opcode.FILLED_NEW_ARRAY_RANGE
             }
 
@@ -40,12 +39,14 @@ val bypassDeviceChecksPatch = bytecodePatch(
             val resultRegister = findFreeRegister(filledNewArrayIndex + 1)
 
             // Store the array there and invoke the method that we added to the class earlier
-            addInstructions(filledNewArrayIndex + 1, """
+            addInstructions(
+                filledNewArrayIndex + 1, """
                 move-result-object v$resultRegister
                 invoke-static {v$resultRegister}, ${EXTENSION_CLASS_DESCRIPTOR}->checkIfDeviceIsIncompatible([Ljava/lang/String;)Z
                 move-result v$resultRegister
                 return v$resultRegister
-            """.trimIndent())
+            """.trimIndent()
+            )
 
             // Remove the instructions before our strings
             removeInstructions(0, firstStringIndex)
