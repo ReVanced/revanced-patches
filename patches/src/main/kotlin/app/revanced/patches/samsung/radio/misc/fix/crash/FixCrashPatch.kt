ef31ca352ae592ecdbe59a514d31735129acc827
rospino74 <34315725+rospino74@users.noreply.github.com>
2025-10-19 13:15:34 +0200

    refactor(Samsung Radio): Use findInstructionIndicesReversedOrThrow instead of while loop

--- patches/src/main/kotlin/app/revanced/patches/samsung/radio/misc/fix/crash/FixCrashPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/samsung/radio/misc/fix/crash/FixCrashPatch.kt
@@ -4,10 +4,8 @@ package app.revanced.patches.samsung.radio.misc.fix.crash
 
 import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
 import app.revanced.patcher.extensions.InstructionExtensions.getInstruction
-import app.revanced.patcher.extensions.InstructionExtensions.instructions
 import app.revanced.patcher.patch.bytecodePatch
 import app.revanced.patches.samsung.radio.restrictions.device.bypassDeviceChecksPatch
-import app.revanced.patches.shared.misc.extension.sharedExtensionPatch
 import app.revanced.util.findInstructionIndicesReversedOrThrow
 import app.revanced.util.indexOfFirstInstruction
 import com.android.tools.smali.dexlib2.Opcode
@@ -24,28 +22,20 @@ val fixCrashPatch = bytecodePatch(
 
     execute {
         permissionRequestListFingerprint.method.apply {
-            findInstructionIndicesReversedOrThrow(Opcode.FILLED_NEW_ARRAY).forEach {filledNewArrayIndex -> }
-            var searchIndex = 0
-            while (searchIndex < instructions.size) {
-                // We search for each "filled-new-array" instruction followed, even not immediately, by "move-result-object", from which we take the index
-                val filledNewArrayIndex = indexOfFirstInstruction(searchIndex, Opcode.FILLED_NEW_ARRAY)
-                if (filledNewArrayIndex == -1) break // No more filled-new-array instructions found
-
+            findInstructionIndicesReversedOrThrow(Opcode.FILLED_NEW_ARRAY).forEach { filledNewArrayIndex ->
                 val moveResultIndex = indexOfFirstInstruction(filledNewArrayIndex, Opcode.MOVE_RESULT_OBJECT)
-                if (moveResultIndex == -1) break // No move-result-object found after the filled-new-array
+                if (moveResultIndex == -1) return@forEach // No move-result-object found after the filled-new-array
 
                 // Get the register where the array is saved
                 val arrayRegister = getInstruction<OneRegisterInstruction>(moveResultIndex).registerA
 
-                // Invoke the method that we added to the class earlier
+                // Invoke the method from the extension
                 addInstructions(
                     moveResultIndex + 1, """
                         invoke-static {v$arrayRegister}, ${EXTENSION_CLASS_DESCRIPTOR}->fixPermissionRequestList([Ljava/lang/String;)[Ljava/lang/String;
                         move-result-object v$arrayRegister
                     """.trimIndent()
                 )
-
-                searchIndex = moveResultIndex + 1 + 2 // 2 = number of instructions we added
             }
         }
     }
