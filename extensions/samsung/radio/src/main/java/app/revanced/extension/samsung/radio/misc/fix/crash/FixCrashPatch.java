462b50042137735c69ce72ee80267a91685cbaf7
rospino74 <34315725+rospino74@users.noreply.github.com>
2025-10-19 14:00:26 +0200

    refactor(Samsung Radio): Use ArrayList to modify arrays

--- extensions/samsung/radio/src/main/java/app/revanced/extension/samsung/radio/misc/fix/crash/FixCrashPatch.java
+++ extensions/samsung/radio/src/main/java/app/revanced/extension/samsung/radio/misc/fix/crash/FixCrashPatch.java
@@ -1,5 +1,6 @@
 package app.revanced.extension.samsung.radio.misc.fix.crash;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -11,7 +12,7 @@ public final class FixCrashPatch {
      * Add the required permissions to the request list to avoid crashes on API 34+.
      **/
     public static final String[] fixPermissionRequestList(String[] perms) {
-        List<String> permsList = Arrays.asList(perms);
+        List<String> permsList = new ArrayList<>(Arrays.asList(perms));
         if (permsList.contains("android.permission.POST_NOTIFICATIONS")) {
             permsList.addAll(Arrays.asList("android.permission.RECORD_AUDIO", "android.permission.READ_PHONE_STATE", "android.permission.FOREGROUND_SERVICE_MICROPHONE"));
         }
