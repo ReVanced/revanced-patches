e30e21273175c477449a95e7348e70309a981847
oSumAtrIX <johan.melkonyan1@web.de>
2025-11-08 16:51:36 +0100

    Apply suggestion from @oSumAtrIX

--- patches/src/main/kotlin/app/revanced/patches/instagram/story/flipping/DisableStoryFlippingPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/instagram/story/flipping/DisableStoryFlippingPatch.kt
@@ -6,7 +6,7 @@ import app.revanced.util.returnEarly
 @Suppress("unused")
 val disableStoryFlippingPatch = bytecodePatch(
     name = "Disable story flipping",
-    description = "Disable stories automatically flipping/skipping after some seconds.",
+    description = "Disable. stories automatically flipping/skipping after some seconds.",
     use = false
 ) {
     compatibleWith("com.instagram.android")
