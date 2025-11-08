f531096bde30d88b8c3529f2428599d65e590f30
brosssh <tiabroch@gmail.com>
2025-11-08 16:41:54 +0100

    feat(Instagram): Add `Disable story flipping` patch

---
+++ patches/src/main/kotlin/app/revanced/patches/instagram/story/flipping/DisableStoryFlippingPatch.kt
@@ -0,0 +1,17 @@
+package app.revanced.patches.instagram.story.flipping
+
+import app.revanced.patcher.patch.bytecodePatch
+import app.revanced.util.returnEarly
+
+@Suppress("unused")
+val disableStoryFlippingPatch = bytecodePatch(
+    name = "Disable story flipping",
+    description = "Disable stories automatically flipping/skipping after some seconds.",
+    use = false
+) {
+    compatibleWith("com.instagram.android")
+
+    execute {
+        storyFlippingFingerprint.method.returnEarly()
+    }
+}
---
+++ patches/src/main/kotlin/app/revanced/patches/instagram/story/flipping/Fingerprints.kt
@@ -0,0 +1,12 @@
+package app.revanced.patches.instagram.story.flipping
+
+import app.revanced.patcher.fingerprint
+
+internal val storyFlippingFingerprint = fingerprint {
+    custom { _, classDef ->
+        classDef.type == "Linstagram/features/stories/fragment/ReelViewerFragment;"
+    }
+    strings("userSession")
+    returns("V")
+    parameters("Ljava/lang/Object;")
+}
