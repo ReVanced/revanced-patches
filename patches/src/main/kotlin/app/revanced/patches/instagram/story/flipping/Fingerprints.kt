a55dde1b85f78b8d835cb412e234b072f2ae9da8
brosssh <tiabroch@gmail.com>
2025-11-08 20:45:31 +0100

    order

--- patches/src/main/kotlin/app/revanced/patches/instagram/story/flipping/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/instagram/story/flipping/Fingerprints.kt
@@ -3,8 +3,8 @@ package app.revanced.patches.instagram.story.flipping
 import app.revanced.patcher.fingerprint
 
 internal val onStoryTimeoutActionFingerprint = fingerprint {
-    returns("V")
     parameters("Ljava/lang/Object;")
+    returns("V")
     strings("userSession")
     custom { _, classDef ->
         classDef.type == "Linstagram/features/stories/fragment/ReelViewerFragment;"
