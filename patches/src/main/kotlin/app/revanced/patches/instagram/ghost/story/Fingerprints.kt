d1d914578c1db99a6ee7048a14239855a8f6a24f
brosssh <44944126+brosssh@users.noreply.github.com>
2025-11-08 19:02:33 +0100

    Update patches/src/main/kotlin/app/revanced/patches/instagram/ghost/story/Fingerprints.kt
    Co-authored-by: LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>

--- patches/src/main/kotlin/app/revanced/patches/instagram/ghost/story/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/instagram/ghost/story/Fingerprints.kt
@@ -3,7 +3,7 @@ package app.revanced.patches.instagram.ghost.story
 import app.revanced.patcher.fingerprint
 
 internal val setMediaSeenHashmapFingerprint = fingerprint {
-    strings("media/seen/")
     returns("V")
     parameters()
+    strings("media/seen/")
 }
