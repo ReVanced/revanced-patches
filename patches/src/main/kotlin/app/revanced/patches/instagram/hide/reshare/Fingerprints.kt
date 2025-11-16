3acc8010bedaa6af03ff32dec61a33dcd064d37a
brosssh <tiabroch@gmail.com>
2025-11-16 19:24:12 +0100

    works

--- patches/src/main/kotlin/app/revanced/patches/instagram/hide/reshare/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/instagram/hide/reshare/Fingerprints.kt
@@ -3,9 +3,27 @@ package app.revanced.patches.instagram.hide.reshare
 import app.revanced.patcher.fingerprint
 import app.revanced.util.getReference
 import app.revanced.util.indexOfFirstInstruction
+import app.revanced.util.literal
 import com.android.tools.smali.dexlib2.iface.reference.MethodReference
 
-internal val mediaJsonParserFingerprint = fingerprint {
-    strings("share_count_disabled", "MediaDict")
+
+internal val p = fingerprint {
+    strings("enable_media_notes_production", "MediaDict")
     custom { method, _ -> method.name == "parseFromJson" }
+}
+
+internal val mediaJsonParserFingerprint = fingerprint {
+    custom { method, classDef ->classDef.type == "LX/5rs;" && method.name == "A01"}
+}
+
+internal val mediaJsonParserFingerprint2 = fingerprint {
+    custom { method, classDef ->classDef.type == "LX/6ht;" && method.name == "A2S"}
+}
+
+internal val mediaJsonParserFingerprint3 = fingerprint {
+    custom { method, classDef ->classDef.type == "LX/7Sz;" && method.name == "A03"}
+}
+
+internal val mediaJsonParserFingerprint4 = fingerprint {
+    custom { method, classDef ->classDef.type == "LX/5rs;" && method.name == "A09"}
 }
--- patches/src/main/kotlin/app/revanced/patches/instagram/hide/reshare/HideReshareButtonPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/instagram/hide/reshare/HideReshareButtonPatch.kt
@@ -3,9 +3,12 @@ package app.revanced.patches.instagram.hide.reshare
 import app.revanced.patcher.extensions.InstructionExtensions.addInstruction
 import app.revanced.patcher.extensions.InstructionExtensions.getInstruction
 import app.revanced.patcher.patch.bytecodePatch
+import app.revanced.util.getReference
 import app.revanced.util.indexOfFirstInstruction
+import app.revanced.util.indexOfFirstInstructionOrThrow
 import com.android.tools.smali.dexlib2.Opcode
-import com.android.tools.smali.dexlib2.iface.instruction.OneRegisterInstruction
+import com.android.tools.smali.dexlib2.iface.instruction.*
+import com.android.tools.smali.dexlib2.iface.reference.FieldReference
 
 @Suppress("unused")
 val hideReshareButtonPatch = bytecodePatch(
@@ -16,9 +19,9 @@ val hideReshareButtonPatch = bytecodePatch(
     compatibleWith("com.instagram.android")
 
     execute {
-        mediaJsonParserFingerprint.method.apply {
+        p.method.apply {
             val setCanReshareInstructionIndex = indexOfFirstInstruction(
-                mediaJsonParserFingerprint.stringMatches!!.first().index,
+                p.stringMatches!!.first().index,
                 Opcode.MOVE_RESULT_OBJECT
             )
 
@@ -26,7 +29,39 @@ val hideReshareButtonPatch = bytecodePatch(
 
             addInstruction(
                 setCanReshareInstructionIndex + 1,
-                "sget-object v$canReshareBooleanRegister, Ljava/lang/Boolean;->TRUE:Ljava/lang/Boolean;"
+                "sget-object v$canReshareBooleanRegister, Ljava/lang/Boolean;->FALSE:Ljava/lang/Boolean;"
+            )
+        }
+
+        mediaJsonParserFingerprint.method.apply {
+            addInstruction(
+                0,
+                """
+                    move-object/from16 v2, p3
+                    const/16 v0, 0x94
+                    sget-object v1, Ljava/lang/Boolean;->FALSE:Ljava/lang/Boolean;
+                    invoke-virtual {v2, v0, v1}, Ljava/util/AbstractMap;->put(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;
+                """
+            )
+        }
+
+        mediaJsonParserFingerprint3.method.apply {
+            val i = indexOfFirstInstructionOrThrow {
+                getReference<FieldReference>()?.name=="A3E"
+            }
+
+            addInstruction(i,
+                "sget-object v0, Ljava/lang/Boolean;->FALSE:Ljava/lang/Boolean;"
+            )
+        }
+
+        mediaJsonParserFingerprint4.method.apply {
+            val i = indexOfFirstInstructionOrThrow {
+                opcode == Opcode.CONST_16 && (this as NarrowLiteralInstruction).narrowLiteral == 0x94
+            }
+
+            addInstruction(i,
+                "sget-object v1, Ljava/lang/Boolean;->FALSE:Ljava/lang/Boolean;"
             )
         }
     }
