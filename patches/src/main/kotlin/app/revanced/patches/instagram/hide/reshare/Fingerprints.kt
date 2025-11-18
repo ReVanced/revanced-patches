c79e279371df04427e32f9494386dcd21b8d553a
brosssh <tiabroch@gmail.com>
2025-11-18 10:04:36 +0100

    adjusted

--- patches/src/main/kotlin/app/revanced/patches/instagram/hide/reshare/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/instagram/hide/reshare/Fingerprints.kt
@@ -1,11 +1,32 @@
 package app.revanced.patches.instagram.hide.reshare
 
 import app.revanced.patcher.fingerprint
+import app.revanced.util.indexOfFirstInstruction
+import com.android.tools.smali.dexlib2.AccessFlags
+import com.android.tools.smali.dexlib2.Opcode
+import com.android.tools.smali.dexlib2.iface.instruction.formats.Instruction31i
+import com.android.tools.smali.dexlib2.iface.instruction.formats.SparseSwitchPayload
 
-internal val mediaJsonParserFingerprint = fingerprint {
-    custom { method, classDef ->classDef.type == "LX/5rs;" && method.name == "A01"}
+// The hash code of the field of interest. It is used as the key of a hashmap
+internal val hashedFieldInteger = "enable_media_notes_production".hashCode()
+
+internal val feedResponseMediaParserFingerprint = fingerprint {
+    strings("array_out_of_bounds_exception", "null_pointer_exception", "MediaDict")
+    custom { method, _ ->
+        method.indexOfFirstInstruction {
+            opcode == Opcode.SPARSE_SWITCH_PAYLOAD &&
+                    (this as SparseSwitchPayload).switchElements.any { it.key == hashedFieldInteger }
+        } >= 0
+    }
 }
 
-internal val mediaJsonParserFingerprint3 = fingerprint {
-    custom { method, classDef ->classDef.type == "LX/7Sz;" && method.name == "A04"}
+internal val reelPostsResponseMediaParserFingerprint = fingerprint {
+    accessFlags(AccessFlags.PUBLIC, AccessFlags.STATIC)
+    parameters("L", "L", "L", "[I")
+    returns("L")
+    custom { method, _ ->
+        method.indexOfFirstInstruction {
+            opcode == Opcode.CONST && (this as Instruction31i).narrowLiteral == hashedFieldInteger
+        } >= 0
+    }
 }
--- patches/src/main/kotlin/app/revanced/patches/instagram/hide/reshare/HideReshareButtonPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/instagram/hide/reshare/HideReshareButtonPatch.kt
@@ -1,11 +1,15 @@
 package app.revanced.patches.instagram.hide.reshare
 
 import app.revanced.patcher.extensions.InstructionExtensions.addInstruction
-import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
+import app.revanced.patcher.extensions.InstructionExtensions.getInstruction
 import app.revanced.patcher.patch.bytecodePatch
 import app.revanced.util.getReference
 import app.revanced.util.indexOfFirstInstructionOrThrow
-import com.android.tools.smali.dexlib2.iface.reference.FieldReference
+import com.android.tools.smali.dexlib2.Opcode
+import com.android.tools.smali.dexlib2.builder.instruction.BuilderSparseSwitchPayload
+import com.android.tools.smali.dexlib2.iface.instruction.OneRegisterInstruction
+import com.android.tools.smali.dexlib2.iface.instruction.formats.Instruction31i
+import com.android.tools.smali.dexlib2.iface.reference.MethodReference
 
 @Suppress("unused")
 val hideReshareButtonPatch = bytecodePatch(
@@ -16,29 +20,61 @@ val hideReshareButtonPatch = bytecodePatch(
     compatibleWith("com.instagram.android")
 
     execute {
-        mediaJsonParserFingerprint.method.apply {
-            addInstructions(
-                0,
-                """
-                    move-object/from16 v2, p3
-                    const/16 v0, 0x94
-                    invoke-static {v0}, Ljava/lang/Integer;->valueOf(I)Ljava/lang/Integer;
-                    move-result-object v0
-                    sget-object v1, Ljava/lang/Boolean;->FALSE:Ljava/lang/Boolean;
-                    invoke-virtual {v2, v0, v1}, Ljava/util/AbstractMap;->put(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;
-                """
+        feedResponseMediaParserFingerprint.method.apply {
+            // Each json field is parsed in a switch statement, where the case of the switch is the hashed field name.
+
+            /**
+             * First, find the switch payload where our field of interest is being processed. So find the payload that
+             * has a key == to our field of interest.
+             */
+            val switchPayload = implementation!!.instructions.first { ins ->
+                ins.opcode == Opcode.SPARSE_SWITCH_PAYLOAD &&
+                        (ins as BuilderSparseSwitchPayload).switchElements.any { it.key == hashedFieldInteger }
+            } as BuilderSparseSwitchPayload
+
+            // Get the target label, so find the instruction offset where the switch case is pointing to
+            val switchTargetLabel = switchPayload.switchElements
+                .first { it.key == hashedFieldInteger }
+                .target
+
+            // From that label, navigate forward until our field os interest is being instantiated
+            val moveResultIndex = indexOfFirstInstructionOrThrow(
+                switchTargetLabel.location.index,
+                Opcode.MOVE_RESULT_OBJECT
+            )
+
+            // Get its register
+            val moveResultRegister = getInstruction<OneRegisterInstruction>(moveResultIndex).registerA
+
+            // Override it
+            addInstruction(
+                moveResultIndex + 1,
+                "sget-object v$moveResultRegister, Ljava/lang/Boolean;->FALSE:Ljava/lang/Boolean;"
             )
         }
 
-        mediaJsonParserFingerprint3.method.apply {
-            val i =  indexOfFirstInstructionOrThrow {
-                getReference<FieldReference>()?.name=="A3J"
+        reelPostsResponseMediaParserFingerprint.method.apply {
+            // Each json field is parsed in a series of if statements, where the if comparison is done comparing the hashed field name.
+
+            // Get index of "const v*, -0x207dadd2"
+            val switchIndex = indexOfFirstInstructionOrThrow {
+                opcode == Opcode.CONST && (this as Instruction31i).narrowLiteral == hashedFieldInteger
             }
 
-            addInstruction(i,
-                "sget-object v0, Ljava/lang/Boolean;->FALSE:Ljava/lang/Boolean;"
+            // Here an internal Instagram native library is used to handle settings of experiments, using hash codes of the fields
+            // Find where our field of interest is being instantiated
+            val moveBooleanResultIndex = indexOfFirstInstructionOrThrow(switchIndex) {
+                getReference<MethodReference>()?.name == "getOptionalBooleanValueByHashCode"
+            } + 1
+
+            // Its register
+            val moveBooleanResultRegister = getInstruction<OneRegisterInstruction>(moveBooleanResultIndex).registerA
+
+            // Override
+            addInstruction(
+                moveBooleanResultIndex,
+                "sget-object v$moveBooleanResultRegister, Ljava/lang/Boolean;->FALSE:Ljava/lang/Boolean;"
             )
         }
-
     }
 }
