a81e8212e8e3168a88106f834e0753fbfa3c115f
rospino74 <34315725+rospino74@users.noreply.github.com>
2025-11-20 20:52:13 +0001

    Bump shared minSdk to 24
    Api 23 was used only here, other patches have a higher level.

--- extensions/shared/build.gradle.kts
+++ extensions/shared/build.gradle.kts
@@ -2,3 +2,10 @@ dependencies {
     implementation(project(":extensions:shared:library"))
     compileOnly(libs.okhttp)
 }
+
+android {
+    defaultConfig {
+        minSdk = 24
+    }
+}
+
--- extensions/shared/library/build.gradle.kts
+++ extensions/shared/library/build.gradle.kts
@@ -7,7 +7,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        minSdk = 23
+        minSdk = 24
     }
 
     compileOptions {
