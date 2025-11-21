76bd0a04437f8bef1c23519a9442f0e13fcf7f80
Swakshan <tsswakshan@gmail.com>
2025-11-21 16:17:22 +0530

    feat(Letterboxd): `Hide ads` - set fingerprints as internal

--- patches/src/main/kotlin/app/revanced/patches/letterboxd/ads/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/letterboxd/ads/Fingerprints.kt
@@ -4,25 +4,25 @@ import app.revanced.patcher.fingerprint
 
 internal const val admobHelperClassName = "Lcom/letterboxd/letterboxd/helpers/AdmobHelper;"
 
-val admobHelperSetShowAdsFingerprint = fingerprint {
+internal val admobHelperSetShowAdsFingerprint = fingerprint {
     custom { method, classDef ->
         method.name == "setShowAds" && classDef.type == admobHelperClassName
     }
 }
 
-val admobHelperShouldShowAdsFingerprint = fingerprint {
+internal val admobHelperShouldShowAdsFingerprint = fingerprint {
     custom { method, classDef ->
         method.name == "shouldShowAds" && classDef.type == admobHelperClassName
     }
 }
 
-val filmFragmentShowAdsFingerprint = fingerprint {
+internal val filmFragmentShowAdsFingerprint = fingerprint {
     custom { method, classDef ->
         method.name == "showAds" && classDef.type.endsWith("/FilmFragment;")
     }
 }
 
-val memberExtensionShowAdsFingerprint = fingerprint {
+internal val memberExtensionShowAdsFingerprint = fingerprint {
     custom { method, classDef ->
         method.name == "showAds" && classDef.type.endsWith("/AMemberExtensionKt;")
     }
