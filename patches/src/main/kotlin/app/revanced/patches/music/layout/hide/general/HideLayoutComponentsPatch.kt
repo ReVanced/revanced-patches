b9234e9dfa2c8b69cbb198160d5e8e379de9517b
rospino74 <34315725+rospino74@users.noreply.github.com>
2025-11-22 15:03:15 +0100

    Add HideLayoutComponents patch

---
+++ patches/src/main/kotlin/app/revanced/patches/music/layout/hide/general/HideLayoutComponentsPatch.kt
@@ -0,0 +1,49 @@
+package app.revanced.patches.music.layout.hide.general
+
+import app.revanced.patcher.patch.bytecodePatch
+import app.revanced.patches.all.misc.resources.addResources
+import app.revanced.patches.all.misc.resources.addResourcesPatch
+import app.revanced.patches.music.misc.litho.filter.addLithoFilter
+import app.revanced.patches.music.misc.litho.filter.lithoFilterPatch
+import app.revanced.patches.music.misc.settings.PreferenceScreen
+import app.revanced.patches.music.misc.settings.settingsPatch
+import app.revanced.patches.shared.misc.settings.preference.*
+
+private const val CUSTOM_FILTER_CLASS_NAME = "Lapp/revanced/extension/shared/patches/components/CustomFilter;"
+
+val hideLayoutComponentsPatch = bytecodePatch(
+    name = "Hide layout components",
+    description = "Adds options to hide general layout components.",
+
+    ) {
+    dependsOn(
+        lithoFilterPatch,
+        settingsPatch,
+        addResourcesPatch,
+    )
+
+    compatibleWith(
+        "com.google.android.apps.youtube.music"(
+            "7.29.52", "8.10.52"
+        )
+    )
+
+    execute {
+        // TODO: Move Resources to music/shared specific
+        addResources("youtube", "layout.hide.general.hideLayoutComponentsPatch")
+
+        // TODO: Move to a layout specific settings screen
+        PreferenceScreen.MISC.addPreferences(
+            PreferenceScreenPreference(
+                key = "revanced_custom_filter_screen",
+                sorting = PreferenceScreenPreference.Sorting.UNSORTED,
+                preferences = setOf(
+                    SwitchPreference("revanced_custom_filter"),
+                    TextPreference("revanced_custom_filter_strings", inputType = InputType.TEXT_MULTI_LINE),
+                ),
+            ),
+        )
+
+        addLithoFilter(CUSTOM_FILTER_CLASS_NAME)
+    }
+}
