d0ac08d906a90642fa065cd7ac49237cf04f723a
xeydev <suhotrub1@gmail.com>
2025-11-26 12:33:35 +0000

    Update description, obtain reference from field, change filename to *Patch

--- patches/src/main/kotlin/app/revanced/patches/instagram/reels/DisableReelsScrollingPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/instagram/reels/DisableReelsScrollingPatch.kt
@@ -2,24 +2,32 @@ import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
 import app.revanced.patcher.patch.bytecodePatch
 import app.revanced.patches.instagram.reels.clipsSwipeRefreshLayoutOnInterceptTouchEventFingerprint
 import app.revanced.patches.instagram.reels.clipsViewPagerImplGetViewAtIndexFingerprint
+import app.revanced.util.Utils.trimIndentMultiline
 import app.revanced.util.returnEarly
 
 @Suppress("unused")
 val disableReelsScrollingPatch = bytecodePatch(
     name = "Disable Reels scrolling",
-    description = "Disables the endless scrolling behavior in Instagram Reels, preventing swiping to the next Reel.",
-    use = false
+    description = """
+        Disables the endless scrolling behavior in Instagram Reels, preventing swiping to the next Reel.
+        Note: On a clean install, the 'tip' animation may appear but will stop on its own after a few seconds.
+    """.trimIndentMultiline(),
+    use = true
 ) {
     compatibleWith("com.instagram.android")
 
     execute {
+        val viewPagerField = clipsViewPagerImplGetViewAtIndexFingerprint.classDef.fields.first {
+            it.type == "Landroidx/viewpager2/widget/ViewPager2;"
+        }
+        val viewPagerFieldReference = "${viewPagerField.definingClass}->${viewPagerField.name}"
         // Disable user input on the ViewPager2 to prevent scrolling.
         clipsViewPagerImplGetViewAtIndexFingerprint.method.addInstructions(
             0,
             """
-            iget-object v0, p0, LX/1U3;->A00:Landroidx/viewpager2/widget/ViewPager2;
-            const/4 v1, 0x0
-            invoke-virtual {v0, v1}, Landroidx/viewpager2/widget/ViewPager2;->setUserInputEnabled(Z)V
+                iget-object v0, p0, $viewPagerFieldReference:Landroidx/viewpager2/widget/ViewPager2;
+                const/4 v1, 0x0
+                invoke-virtual { v0, v1 }, Landroidx/viewpager2/widget/ViewPager2;->setUserInputEnabled(Z)V
             """
         )
 
