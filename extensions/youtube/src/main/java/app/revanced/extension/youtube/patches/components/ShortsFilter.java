b323365a1857bb9ae7b6492e5438ddb1eacdb10e
ILoveOpenSouceApplications <em29072chinmayjagalchandrababu@gmail.com>
2025-12-03 02:47:06 +0530

    feat(YouTube - Hide Shorts components): Add "Hide live preview"

--- extensions/youtube/src/main/java/app/revanced/extension/youtube/patches/components/ShortsFilter.java
+++ extensions/youtube/src/main/java/app/revanced/extension/youtube/patches/components/ShortsFilter.java
@@ -161,6 +161,13 @@ public final class ShortsFilter extends Filter {
                 "participation_bar.e"
         );
 
+        StringFilterGroup livePreview = new StringFilterGroup(
+                Settings.HIDE_SHORTS_LIVE_PREVIEW,
+                // Live Shorts preview that can popup while scrolling through Shorts player.
+                // Can be removed if a way to disable live Shorts is found.
+                "live_preview_page_vm.e"
+        );
+
         joinButton = new StringFilterGroup(
                 Settings.HIDE_SHORTS_JOIN_BUTTON,
                 "sponsor_button"
@@ -223,7 +230,7 @@ public final class ShortsFilter extends Filter {
                 shortsCompactFeedVideo, joinButton, subscribeButton, paidPromotionButton,
                 shortsActionBar, suggestedAction, pausedOverlayButtons, channelBar, previewComment,
                 fullVideoLinkLabel, videoTitle, useSoundButton, reelSoundMetadata, soundButton, infoPanel,
-                stickers, likeFountain, likeButton, dislikeButton
+                stickers, likeFountain, likeButton, dislikeButton, livePreview
         );
 
         //
