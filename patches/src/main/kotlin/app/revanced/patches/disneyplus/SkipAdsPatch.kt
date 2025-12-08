afd48cf55cd638a58e626ac557d911b2c0a98841
oSumAtrIX <johan.melkonyan1@web.de>
2025-12-08 12:35:40 +0100

    Apply suggestion from @oSumAtrIX

--- patches/src/main/kotlin/app/revanced/patches/disneyplus/SkipAdsPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/disneyplus/SkipAdsPatch.kt
@@ -8,7 +8,7 @@ val skipAdsPatch = bytecodePatch(
     name = "Skip ads",
     description = "Automatically skips ads.",
 ) {
-    compatibleWith("com.disney.disneyplus"("4.19.3+rc1-2025.11.21"))
+    compatibleWith("com.disney.disneyplus")
 
     execute {
         insertionGetPointsFingerprint.method.addInstructions(
