0652949783dec365569726672e01e53d02839fb8
g9q <83601416+g9q@users.noreply.github.com>
2025-12-09 17:31:33 +0000

    Revert "Patch the API response for comment ads instead of patching the request"
    This reverts commit 41f76417c0c9b6d54379a75eb9c831c12d644e9a.

--- patches/src/main/kotlin/app/revanced/patches/reddit/ad/comments/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/reddit/ad/comments/Fingerprints.kt
@@ -6,5 +6,12 @@ import com.android.tools.smali.dexlib2.iface.instruction.ReferenceInstruction
 import com.android.tools.smali.dexlib2.iface.reference.TypeReference
 
 internal val hideCommentAdsFingerprint = fingerprint {
-    strings("PdpCommentsAds(adPosts=")
+    returns("V")
+    custom { method, _ ->
+        method.implementation?.instructions?.any { instruction ->
+            if (instruction.opcode != Opcode.NEW_INSTANCE) return@any false
+            val reference = (instruction as ReferenceInstruction).reference as TypeReference
+            reference.type.contains("LoadAdsCombinedCall")
+        } == true
+    }
 }
--- patches/src/main/kotlin/app/revanced/patches/reddit/ad/comments/HideCommentAdsPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/reddit/ad/comments/HideCommentAdsPatch.kt
@@ -1,6 +1,6 @@
 package app.revanced.patches.reddit.ad.comments
 
-import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
+import app.revanced.patcher.extensions.InstructionExtensions.replaceInstructions
 import app.revanced.patcher.patch.bytecodePatch
 
 val hideCommentAdsPatch = bytecodePatch(
@@ -8,14 +8,10 @@ val hideCommentAdsPatch = bytecodePatch(
 ) {
 
     execute {
-        hideCommentAdsFingerprint.classDef.methods.find { method ->
-            method.name == "<init>" &&
-                method.parameterTypes.firstOrNull() == "Ljava/util/List;"
-        }!!.addInstructions(
+        hideCommentAdsFingerprint.method.replaceInstructions(
             0,
             """
-                new-instance p1, Ljava/util/ArrayList;
-                invoke-direct {p1}, Ljava/util/ArrayList;-><init>()V
+                return-void
             """,
         )
     }
