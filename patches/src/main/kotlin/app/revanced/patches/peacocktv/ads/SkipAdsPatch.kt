73cd76c4cc583d0398ae5c498e782775fe6c398e
g9q <83601416+g9q@users.noreply.github.com>
2025-12-09 00:02:00 +0000

    Change 'replaceInstructions' to 'returnEarly'

--- patches/src/main/kotlin/app/revanced/patches/peacocktv/ads/SkipAdsPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/peacocktv/ads/SkipAdsPatch.kt
@@ -1,22 +1,16 @@
 package app.revanced.patches.peacocktv.ads
 
-import app.revanced.patcher.extensions.InstructionExtensions.replaceInstructions
 import app.revanced.patcher.patch.bytecodePatch
+import app.revanced.util.returnEarly
 
 @Suppress("unused")
 val skipAdsPatch = bytecodePatch(
-    name = "Skip ads",
-    description = "Automatically skips ads.",
+    name = "Hide ads",
+    description = "Skips ads for movies and TV shows. Not tested for live TV.",
 ) {
     compatibleWith("com.peacocktv.peacockandroid")
 
     execute {
-        mediaTailerAdServiceFingerprint.method.replaceInstructions(
-            0,
-            """
-                const/4 v0, 0
-                return-object v0
-            """,
-        )
+        mediaTailerAdServiceFingerprint.method.returnEarly(false)
     }
 }
