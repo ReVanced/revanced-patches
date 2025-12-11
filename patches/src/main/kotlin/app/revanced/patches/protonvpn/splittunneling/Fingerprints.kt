6bb0e800103a1c9cd396fb13dc4b39cfbb0d76f7
Sylvain Finot <25269226+SF73@users.noreply.github.com>
2025-12-11 20:10:23 +0100

    apply suggestions

--- patches/src/main/kotlin/app/revanced/patches/protonvpn/splittunneling/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/protonvpn/splittunneling/Fingerprints.kt
@@ -4,21 +4,17 @@ import app.revanced.patcher.fingerprint
 import com.android.tools.smali.dexlib2.AccessFlags
 import com.android.tools.smali.dexlib2.Opcode
 
-internal val uiUnlock = fingerprint {
+internal val enableSplitTunnelingUiFingerprint = fingerprint {
     strings("currentModeAppNames")
     opcodes(
         Opcode.MOVE_OBJECT,
         Opcode.MOVE_FROM16,
         Opcode.INVOKE_DIRECT_RANGE
     )
-    custom { method, classdef ->
-        method.name == "<init>" && classdef.type == "Lcom/protonvpn/android/redesign/settings/ui/SettingsViewModel\$SettingViewState\$SplitTunneling;"
-    }
 }
 
-internal val settingInit = fingerprint {
-    custom { method, classdef ->
-        method.name == "applyRestrictions" &&
-        classdef.type == "Lcom/protonvpn/android/settings/data/BaseApplyEffectiveUserSettings;"
+internal val removeSplitTunnelingRestrictionFingerprint = fingerprint {
+    custom { method, _ ->
+        method.name == "applyRestrictions"
     }
 }
