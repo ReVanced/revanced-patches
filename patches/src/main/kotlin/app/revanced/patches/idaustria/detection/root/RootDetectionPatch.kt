0ea3491227fc50c03555d43d3fec78eb82906b26
Alex Katlein <dev@vemilyus.com>
2025-12-15 12:26:59 +0100

    feat(IdAustria - Remove device integrity check): Update patch to work with latest version (#6360)
    Co-authored-by: oSumAtrIX <johan.melkonyan1@web.de>

--- patches/src/main/kotlin/app/revanced/patches/idaustria/detection/root/RootDetectionPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/idaustria/detection/root/RootDetectionPatch.kt
@@ -1,22 +1,10 @@
 package app.revanced.patches.idaustria.detection.root
 
 import app.revanced.patcher.patch.bytecodePatch
-import app.revanced.patches.shared.PATCH_DESCRIPTION_REMOVE_ROOT_DETECTION
-import app.revanced.patches.shared.PATCH_NAME_REMOVE_ROOT_DETECTION
-import app.revanced.util.returnEarly
+import app.revanced.patches.idaustria.detection.deviceintegrity.removeDeviceIntegrityChecksPatch
 
+@Deprecated("Patch was superseded", ReplaceWith("removeDeviceIntegrityChecksPatch"))
 @Suppress("unused")
-val rootDetectionPatch = bytecodePatch(
-    name = PATCH_NAME_REMOVE_ROOT_DETECTION,
-    description = PATCH_DESCRIPTION_REMOVE_ROOT_DETECTION
-) {
-    compatibleWith("at.gv.oe.app")
-
-    execute {
-        setOf(
-            attestationSupportedCheckFingerprint,
-            bootloaderCheckFingerprint,
-            rootCheckFingerprint,
-        ).forEach { it.method.returnEarly(true) }
-    }
+val rootDetectionPatch = bytecodePatch {
+    dependsOn(removeDeviceIntegrityChecksPatch)
 }
