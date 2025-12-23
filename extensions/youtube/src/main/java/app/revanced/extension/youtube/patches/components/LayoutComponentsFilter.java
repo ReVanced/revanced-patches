ReVanced/revanced-patches

commit dc69f2433e2650654e2dffdd76b0b0c8a52bf515

fix(YouTube - Hide layout components): Hide new type of crowdfunding box (#6380)

extensions/youtube/src/main/java/app/revanced/extension/youtube/patches/components/LayoutComponentsFilter.java
@@ -247,8 +247,13 @@ public final class LayoutComponentsFilter extends Filter {
                 "sponsorships"
         );
 
+        final var crowdfundingBox = new StringFilterGroup(
+                Settings.HIDE_CROWDFUNDING_BOX,
+                "donation_shelf"
+        );
+
         final var channelWatermark = new StringFilterGroup(
-                Settings.HIDE_VIDEO_CHANNEL_WATERMARK,
+                Settings.HIDE_CHANNEL_WATERMARK,
                 "featured_channel_watermark_overlay"
         );
 
@@ -312,6 +317,7 @@ public final class LayoutComponentsFilter extends Filter {
                 compactChannelBar,
                 compactChannelBarInner,
                 communityPosts,
+                crowdfundingBox,
                 emergencyBox,
                 expandableMetadata,
                 forYouShelf,

extensions/youtube/src/main/java/app/revanced/extension/youtube/settings/Settings.java
@@ -96,7 +96,6 @@ public class Settings extends BaseSettings {
     public static final BooleanSetting HIDE_CHIPS_SHELF = new BooleanSetting("revanced_hide_chips_shelf", TRUE);
     public static final BooleanSetting HIDE_COMMUNITY_POSTS = new BooleanSetting("revanced_hide_community_posts", FALSE);
     public static final BooleanSetting HIDE_COMPACT_BANNER = new BooleanSetting("revanced_hide_compact_banner", TRUE);
-    public static final BooleanSetting HIDE_CROWDFUNDING_BOX = new BooleanSetting("revanced_hide_crowdfunding_box", FALSE, true);
     public static final BooleanSetting HIDE_DOODLES = new BooleanSetting("revanced_hide_doodles", FALSE, true, "revanced_hide_doodles_user_dialog_message");
     public static final BooleanSetting HIDE_EXPANDABLE_CARD = new BooleanSetting("revanced_hide_expandable_card", TRUE);
     public static final BooleanSetting HIDE_FILTER_BAR_FEED_IN_FEED = new BooleanSetting("revanced_hide_filter_bar_feed_in_feed", FALSE, true);
@@ -158,6 +157,8 @@ public class Settings extends BaseSettings {
     public static final BooleanSetting HIDE_CAPTIONS_BUTTON = new BooleanSetting("revanced_hide_captions_button", FALSE);
     public static final BooleanSetting HIDE_CAST_BUTTON = new BooleanSetting("revanced_hide_cast_button", TRUE, true);
     public static final BooleanSetting HIDE_CHANNEL_BAR = new BooleanSetting("revanced_hide_channel_bar", FALSE);
+    public static final BooleanSetting HIDE_CHANNEL_WATERMARK = new BooleanSetting("revanced_hide_channel_watermark", TRUE);
+    public static final BooleanSetting HIDE_CROWDFUNDING_BOX = new BooleanSetting("revanced_hide_crowdfunding_box", FALSE, true);
     public static final BooleanSetting HIDE_EMERGENCY_BOX = new BooleanSetting("revanced_hide_emergency_box", TRUE);
     public static final BooleanSetting HIDE_ENDSCREEN_CARDS = new BooleanSetting("revanced_hide_endscreen_cards", FALSE);
     public static final BooleanSetting HIDE_END_SCREEN_SUGGESTED_VIDEO = new BooleanSetting("revanced_end_screen_suggested_video", FALSE, true);
@@ -172,7 +173,6 @@ public class Settings extends BaseSettings {
     public static final BooleanSetting HIDE_RELATED_VIDEOS = new BooleanSetting("revanced_hide_related_videos", FALSE);
     public static final BooleanSetting HIDE_SUBSCRIBERS_COMMUNITY_GUIDELINES = new BooleanSetting("revanced_hide_subscribers_community_guidelines", TRUE);
     public static final BooleanSetting HIDE_TIMED_REACTIONS = new BooleanSetting("revanced_hide_timed_reactions", TRUE);
-    public static final BooleanSetting HIDE_VIDEO_CHANNEL_WATERMARK = new BooleanSetting("revanced_hide_channel_watermark", TRUE);
     public static final BooleanSetting OPEN_VIDEOS_FULLSCREEN_PORTRAIT = new BooleanSetting("revanced_open_videos_fullscreen_portrait", FALSE);
     public static final BooleanSetting PLAYBACK_SPEED_DIALOG_BUTTON = new BooleanSetting("revanced_playback_speed_dialog_button", FALSE);
     public static final BooleanSetting VIDEO_QUALITY_DIALOG_BUTTON = new BooleanSetting("revanced_video_quality_dialog_button", FALSE);

patches/src/main/kotlin/app/revanced/patches/youtube/layout/hide/general/HideLayoutComponentsPatch.kt
@@ -174,6 +174,7 @@ val hideLayoutComponentsPatch = bytecodePatch(
             ),
             SwitchPreference("revanced_hide_channel_bar"),
             SwitchPreference("revanced_hide_channel_watermark"),
+            SwitchPreference("revanced_hide_crowdfunding_box"),
             SwitchPreference("revanced_hide_emergency_box"),
             SwitchPreference("revanced_hide_info_panels"),
             SwitchPreference("revanced_hide_join_membership_button"),
@@ -229,7 +230,6 @@ val hideLayoutComponentsPatch = bytecodePatch(
             SwitchPreference("revanced_hide_chips_shelf"),
             SwitchPreference("revanced_hide_community_posts"),
             SwitchPreference("revanced_hide_compact_banner"),
-            SwitchPreference("revanced_hide_crowdfunding_box"),
             SwitchPreference("revanced_hide_expandable_card"),
             SwitchPreference("revanced_hide_floating_microphone_button"),
             SwitchPreference(

patches/src/main/resources/addresources/values/strings.xml
@@ -232,9 +232,6 @@ However, enabling this will also log some user data such as your IP address."</s
             <string name="revanced_hide_compact_banner_title">Hide compact banners</string>
             <string name="revanced_hide_compact_banner_summary_on">Compact banners are hidden</string>
             <string name="revanced_hide_compact_banner_summary_off">Compact banners are shown</string>
-            <string name="revanced_hide_crowdfunding_box_title">Hide crowdfunding box</string>
-            <string name="revanced_hide_crowdfunding_box_summary_on">Crowdfunding box is hidden</string>
-            <string name="revanced_hide_crowdfunding_box_summary_off">Crowdfunding box is shown</string>
             <string name="revanced_hide_expandable_card_title">Hide expandable card</string>
             <string name="revanced_hide_expandable_card_summary_on">Expandable card under videos is hidden</string>
             <string name="revanced_hide_expandable_card_summary_off">Expandable card under videos is shown</string>
@@ -300,8 +297,11 @@ If a Doodle is currently showing in your region and this hide setting is on, the
             <string name="revanced_hide_channel_bar_summary_on">Channel bar is hidden</string>
             <string name="revanced_hide_channel_bar_summary_off">Channel bar is shown</string>
             <string name="revanced_hide_channel_watermark_title">Hide channel watermark</string>
-            <string name="revanced_hide_channel_watermark_summary_on">Watermark is hidden</string>
-            <string name="revanced_hide_channel_watermark_summary_off">Watermark is shown</string>
+            <string name="revanced_hide_channel_watermark_summary_on">Channel watermark is hidden</string>
+            <string name="revanced_hide_channel_watermark_summary_off">Channel watermark is shown</string>
+            <string name="revanced_hide_crowdfunding_box_title">Hide crowdfunding box</string>
+            <string name="revanced_hide_crowdfunding_box_summary_on">Crowdfunding box is hidden</string>
+            <string name="revanced_hide_crowdfunding_box_summary_off">Crowdfunding box is shown</string>
             <string name="revanced_hide_emergency_box_title">Hide emergency boxes</string>
             <string name="revanced_hide_emergency_box_summary_on">Emergency boxes are hidden</string>
             <string name="revanced_hide_emergency_box_summary_off">Emergency boxes are shown</string>

commit 3c885c829fb6ba45feaac7d6d4a27b1ab10b7b94

fix(YouTube): Fix compilation error introduced in dc69f243

extensions/youtube/src/main/java/app/revanced/extension/youtube/patches/components/LayoutComponentsFilter.java
@@ -433,7 +433,7 @@ public final class LayoutComponentsFilter extends Filter {
      * Injection point.
      */
     public static boolean showWatermark() {
-        return !Settings.HIDE_VIDEO_CHANNEL_WATERMARK.get();
+        return !Settings.HIDE_CHANNEL_WATERMARK.get();
     }
 
     /**