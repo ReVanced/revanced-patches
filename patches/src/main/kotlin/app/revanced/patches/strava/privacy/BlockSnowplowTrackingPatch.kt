819aa1a946b7e74df6baf7f9a7669709e731ba2c
oSumAtrIX <johan.melkonyan1@web.de>
2025-12-29 22:20:00 +0100

    Apply suggestions from code review

--- patches/src/main/kotlin/app/revanced/patches/strava/privacy/BlockSnowplowTrackingPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/strava/privacy/BlockSnowplowTrackingPatch.kt
@@ -3,11 +3,6 @@ package app.revanced.patches.strava.privacy
 import app.revanced.patcher.patch.bytecodePatch
 import app.revanced.util.returnEarly
 
-/**
- * The app collects user interaction events and sends them to
- * https://c.strava.com/com.snowplowanalytics.snowplow/tp2.
- * This patch prevents the insertion of events, keeping the list of emittable events empty.
- */
 @Suppress("unused")
 val blockSnowplowTrackingPatch = bytecodePatch(
     name = "Block Snowplow tracking",
@@ -16,6 +11,7 @@ val blockSnowplowTrackingPatch = bytecodePatch(
     compatibleWith("com.strava")
 
     execute {
+    	// Keep events list empty, otherwise sent to https://c.strava.com/com.snowplowanalytics.snowplow/tp2.
         insertEventFingerprint.method.returnEarly()
     }
 }
