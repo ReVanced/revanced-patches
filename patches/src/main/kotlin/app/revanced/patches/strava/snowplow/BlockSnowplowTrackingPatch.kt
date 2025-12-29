7528e12d0fb46f14ba18472c135fdfe40eaeac18
xehpuk <xehpuk.dev@gmail.com>
2025-12-29 11:27:38 +0100

    feat(Strava): Add `Block Snowplow tracking` patch

---
+++ patches/src/main/kotlin/app/revanced/patches/strava/snowplow/BlockSnowplowTrackingPatch.kt
@@ -0,0 +1,21 @@
+package app.revanced.patches.strava.snowplow
+
+import app.revanced.patcher.patch.bytecodePatch
+import app.revanced.util.returnEarly
+
+/**
+ * The app collects user interaction events and sends them to
+ * https://c.strava.com/com.snowplowanalytics.snowplow/tp2.
+ * This patch prevents the insertion of events, keeping the list of emittable events empty.
+ */
+@Suppress("unused")
+val blockSnowplowTrackingPatch = bytecodePatch(
+    name = "Block Snowplow tracking",
+    description = "See https://snowplow.io.",
+) {
+    compatibleWith("com.strava")
+
+    execute {
+        insertEventFingerprint.method.returnEarly()
+    }
+}
