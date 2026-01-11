b6d1f1c62a11ff7b99daa0b4b917e5fa1055dfa5
Pun Butrach <pun.butrach@gmail.com>
2026-01-11 23:29:48 +0700

    refactor(orfon): RemoveRootDetectionPatch

--- patches/src/main/kotlin/app/revanced/patches/orfon/detection/root/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/orfon/detection/root/Fingerprints.kt
@@ -1,13 +1,16 @@
 package app.revanced.patches.orfon.detection.root
 
-import app.revanced.patcher.fingerprint
+import app.revanced.patcher.BytecodePatchContextMethodMatching.gettingFirstMutableMethodDeclaratively
+import app.revanced.patcher.accessFlags
+import app.revanced.patcher.definingClass
+import app.revanced.patcher.name
+import app.revanced.patcher.patch.BytecodePatchContext
+import app.revanced.patcher.returnType
 import com.android.tools.smali.dexlib2.AccessFlags
 
-internal val isDeviceRootedFingeprint = fingerprint {
+internal val BytecodePatchContext.isDeviceRootedMethod by gettingFirstMutableMethodDeclaratively {
     accessFlags(AccessFlags.PUBLIC)
-    returns("Z")
-    custom { method, classDef ->
-        method.name == "isDeviceRooted" &&
-            classDef.endsWith("/RootChecker;")
-    }
+    name("isDeviceRooted")
+    definingClass("/RootChecker;"::endsWith)
+    returnType("Z")
 }
