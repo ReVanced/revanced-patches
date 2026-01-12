198cb93ef275a09891ac59364225198b83524ab3
Pun Butrach <pun.butrach@gmail.com>
2026-01-12 17:10:00 +0700

    refactor(primevideo): SkipAdsPatch

--- patches/src/main/kotlin/app/revanced/patches/primevideo/ads/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/primevideo/ads/Fingerprints.kt
@@ -1,33 +1,39 @@
 package app.revanced.patches.primevideo.ads
 
-import app.revanced.patcher.fingerprint
+import app.revanced.patcher.BytecodePatchContextMethodMatching.gettingFirstMutableMethodDeclaratively
+import app.revanced.patcher.accessFlags
+import app.revanced.patcher.definingClass
+import app.revanced.patcher.instructions
+import app.revanced.patcher.invoke
+import app.revanced.patcher.name
+import app.revanced.patcher.parameterTypes
+import app.revanced.patcher.patch.BytecodePatchContext
+import app.revanced.patcher.returnType
 import com.android.tools.smali.dexlib2.AccessFlags
 import com.android.tools.smali.dexlib2.Opcode
 
-internal val enterServerInsertedAdBreakStateFingerprint = fingerprint {
+internal val BytecodePatchContext.enterServerInsertedAdBreakStateMethod by gettingFirstMutableMethodDeclaratively {
     accessFlags(AccessFlags.PUBLIC)
-    parameters("Lcom/amazon/avod/fsm/Trigger;")
-    returns("V")
-    opcodes(
-        Opcode.INVOKE_VIRTUAL,
-        Opcode.MOVE_RESULT_OBJECT,
-        Opcode.CONST_4,
-        Opcode.CONST_4
+    parameterTypes("Lcom/amazon/avod/fsm/Trigger;")
+    returnType("V")
+    instructions(
+        Opcode.INVOKE_VIRTUAL(),
+        Opcode.MOVE_RESULT_OBJECT(),
+        Opcode.CONST_4(),
+        Opcode.CONST_4()
     )
-    custom { method, classDef ->
-        method.name == "enter" && classDef.type == "Lcom/amazon/avod/media/ads/internal/state/ServerInsertedAdBreakState;"
-    }
+    name("enter")
+    definingClass("Lcom/amazon/avod/media/ads/internal/state/ServerInsertedAdBreakState;")
 }
 
-internal val doTriggerFingerprint = fingerprint {
+internal val BytecodePatchContext.doTriggerMethod by gettingFirstMutableMethodDeclaratively {
     accessFlags(AccessFlags.PROTECTED)
-    returns("V")
-    opcodes(
-        Opcode.IGET_OBJECT,
-        Opcode.INVOKE_INTERFACE,
-        Opcode.RETURN_VOID
+    returnType("V")
+    instructions(
+        Opcode.IGET_OBJECT(),
+        Opcode.INVOKE_INTERFACE(),
+        Opcode.RETURN_VOID()
     )
-    custom { method, classDef ->
-        method.name == "doTrigger" && classDef.type == "Lcom/amazon/avod/fsm/StateBase;"
-    }
+    name("doTrigger")
+    definingClass("Lcom/amazon/avod/fsm/StateBase;")
 }
