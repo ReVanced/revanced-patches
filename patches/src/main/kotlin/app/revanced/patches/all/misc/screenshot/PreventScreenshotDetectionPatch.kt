236174b25a8df7bd196cfc86ca3aea68894b3c60
xehpuk <xehpuk.dev@gmail.com>
2026-01-15 16:13:51 +0100

    feat: Add `Prevent screenshot detection` patch Prevent screen capture callback registration

--- patches/src/main/kotlin/app/revanced/patches/all/misc/screenshot/PreventScreenshotDetectionPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/all/misc/screenshot/PreventScreenshotDetectionPatch.kt
@@ -1,21 +1,47 @@
 package app.revanced.patches.all.misc.screenshot
 
+import app.revanced.patcher.extensions.InstructionExtensions.removeInstruction
 import app.revanced.patcher.patch.bytecodePatch
-import app.revanced.util.filterImplementationsOf
-import app.revanced.util.returnEarly
-import com.android.tools.smali.dexlib2.iface.Method
+import app.revanced.patches.all.misc.transformation.transformInstructionsPatch
+import com.android.tools.smali.dexlib2.Opcode
+import com.android.tools.smali.dexlib2.iface.instruction.ReferenceInstruction
+import com.android.tools.smali.dexlib2.iface.reference.MethodReference
 
 @Suppress("unused")
 val preventScreenshotDetectionPatch = bytecodePatch(
     name = "Prevent screenshot detection",
     description = "Prevents the app from detecting screenshots.",
 ) {
-    execute {
-        fun Method.isOnScreenCaptured() =
-            returnType == "V" && name == "onScreenCaptured" && parameterTypes.isEmpty() && implementation != null
-
-        classes.filterImplementationsOf("Landroid/app/Activity\$ScreenCaptureCallback;")
-            .filter { it.virtualMethods.any { method -> method.isOnScreenCaptured() } }.map { proxy(it).mutableClass }
-            .forEach { it.virtualMethods.first { method -> method.isOnScreenCaptured() }.returnEarly() }
-    }
+    dependsOn(transformInstructionsPatch(
+        filterMap = { _, _, instruction, instructionIndex ->
+            // invoke-virtual Landroid/app/Activity;->registerScreenCaptureCallback(Ljava/util/concurrent/Executor;Landroid/app/Activity$ScreenCaptureCallback;)V
+            // invoke-virtual Landroid/app/Activity;->unregisterScreenCaptureCallback(Landroid/app/Activity$ScreenCaptureCallback;)V
+            if (instruction.opcode != Opcode.INVOKE_VIRTUAL) {
+                null
+            } else {
+                ((instruction as? ReferenceInstruction)?.reference as? MethodReference)?.run {
+                    if (returnType == "V" &&
+                        definingClass == "Landroid/app/Activity;" && (
+                            name == "registerScreenCaptureCallback" &&
+                            parameterTypes == listOf(
+                                "Ljava/util/concurrent/Executor;",
+                                "Landroid/app/Activity\$ScreenCaptureCallback;",
+                            ) || (
+                            name == "unregisterScreenCaptureCallback" &&
+                            parameterTypes == listOf(
+                                "Landroid/app/Activity\$ScreenCaptureCallback;",
+                            )
+                        ))
+                    ) {
+                        instructionIndex
+                    } else {
+                        null
+                    }
+                }
+            }
+        },
+        transform = { mutableMethod, instructionIndex ->
+            mutableMethod.removeInstruction(instructionIndex)
+        }
+    ))
 }
