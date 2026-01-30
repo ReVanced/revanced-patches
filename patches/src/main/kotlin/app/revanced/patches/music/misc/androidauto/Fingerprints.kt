d39c53f6775b6dd7d03259310d2be9633b4fa05f
oSumAtrIX <johan.melkonyan1@web.de>
2026-01-30 16:28:16 +0100

    youtube music patches work now

--- patches/src/main/kotlin/app/revanced/patches/music/misc/androidauto/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/music/misc/androidauto/Fingerprints.kt
@@ -1,14 +1,16 @@
 package app.revanced.patches.music.misc.androidauto
 
 import app.revanced.patcher.gettingFirstMutableMethodDeclaratively
+import app.revanced.patcher.instructions
+import app.revanced.patcher.invoke
 import app.revanced.patcher.parameterTypes
 import app.revanced.patcher.patch.BytecodePatchContext
 import app.revanced.patcher.returnType
 
 internal val BytecodePatchContext.checkCertificateMethod by gettingFirstMutableMethodDeclaratively(
     "X509",
-    "Failed to get certificate" // Partial String match.
 ) {
     returnType("Z")
     parameterTypes("Ljava/lang/String;")
+    instructions("Failed to get certificate"(String::contains))
 }
