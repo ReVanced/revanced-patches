d640bbac2744e6e1e736cd87d939a5325d1ea85c
oSumAtrIX <johan.melkonyan1@web.de>
2026-02-08 22:44:47 +0100

    fix(YouTube Music - Navigation bar): Hide library tab with 8.24+
    Co-authored-by: LisoUseInAIKyrios <lisouseinaikyrios@users.noreply.github.com>

--- extensions/music/src/main/java/app/revanced/extension/music/patches/NavigationBarPatch.java
+++ extensions/music/src/main/java/app/revanced/extension/music/patches/NavigationBarPatch.java
@@ -5,14 +5,15 @@ import static app.revanced.extension.shared.Utils.hideViewUnderCondition;
 import android.view.View;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+
+import java.util.Arrays;
+import java.util.List;
 
 import app.revanced.extension.music.settings.Settings;
 
 @SuppressWarnings("unused")
 public class NavigationBarPatch {
-    @NonNull
     private static String lastYTNavigationEnumName = "";
 
     public static void setLastAppNavigationEnum(@Nullable Enum<?> ytNavigationEnumName) {
@@ -25,7 +26,7 @@ public class NavigationBarPatch {
         hideViewUnderCondition(Settings.HIDE_NAVIGATION_BAR_LABEL.get(), textview);
     }
 
-    public static void hideNavigationButton(@NonNull View view) {
+    public static void hideNavigationButton(View view) {
         // Hide entire navigation bar.
         if (Settings.HIDE_NAVIGATION_BAR.get() && view.getParent() != null) {
             hideViewUnderCondition(true, (View) view.getParent());
@@ -34,7 +35,7 @@ public class NavigationBarPatch {
 
         // Hide navigation buttons based on their type.
         for (NavigationButton button : NavigationButton.values()) {
-            if (button.ytEnumNames.equals(lastYTNavigationEnumName)) {
+            if (button.ytEnumNames.contains(lastYTNavigationEnumName)) {
                 hideViewUnderCondition(button.hidden, view);
                 break;
             }
@@ -43,30 +44,41 @@ public class NavigationBarPatch {
 
     private enum NavigationButton {
         HOME(
-                "TAB_HOME",
+                Arrays.asList(
+                        "TAB_HOME"
+                ),
                 Settings.HIDE_NAVIGATION_BAR_HOME_BUTTON.get()
         ),
         SAMPLES(
-                "TAB_SAMPLES",
+                Arrays.asList(
+                        "TAB_SAMPLES"
+                ),
                 Settings.HIDE_NAVIGATION_BAR_SAMPLES_BUTTON.get()
         ),
         EXPLORE(
-                "TAB_EXPLORE",
+                Arrays.asList(
+                        "TAB_EXPLORE"
+                ),
                 Settings.HIDE_NAVIGATION_BAR_EXPLORE_BUTTON.get()
         ),
         LIBRARY(
-                "LIBRARY_MUSIC",
+                Arrays.asList(
+                        "LIBRARY_MUSIC",
+                        "TAB_BOOKMARK" // YouTube Music 8.24+
+                ),
                 Settings.HIDE_NAVIGATION_BAR_LIBRARY_BUTTON.get()
         ),
         UPGRADE(
-                "TAB_MUSIC_PREMIUM",
+                Arrays.asList(
+                        "TAB_MUSIC_PREMIUM"
+                ),
                 Settings.HIDE_NAVIGATION_BAR_UPGRADE_BUTTON.get()
         );
 
-        private final String ytEnumNames;
+        private final List<String> ytEnumNames;
         private final boolean hidden;
 
-        NavigationButton(@NonNull String ytEnumNames, boolean hidden) {
+        NavigationButton(List<String> ytEnumNames, boolean hidden) {
             this.ytEnumNames = ytEnumNames;
             this.hidden = hidden;
         }
