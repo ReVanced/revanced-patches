e7e3208f17e57aa0cfced20c111cb48d9031e2d4
wowitsjack <no@email>
2026-03-06 12:48:27 +1000

    Apply fingerprint comment style suggestion from code review

--- patches/src/main/kotlin/app/revanced/patches/youtube/layout/buttons/overlay/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/youtube/layout/buttons/overlay/Fingerprints.kt
@@ -38,8 +38,8 @@ internal val BytecodePatchContext.fullscreenButtonMethodMatch by composingFirstM
     returnType("V")
     instructions(
         ResourceType.ID("fullscreen_button"),
-        Opcode.INVOKE_VIRTUAL(),   // findViewById
-        Opcode.MOVE_RESULT_OBJECT() // the actual view, not cast in 21.x
+        Opcode.INVOKE_VIRTUAL(),   // findViewById call.
+        Opcode.MOVE_RESULT_OBJECT() // The actual view, not a check-cast in 21.x.
     )
 }
 
