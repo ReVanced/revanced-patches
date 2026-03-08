9dd341e46173647db123cbffdd4ab6a4d691dfa4
Dylan <dylanpdx@gmail.com>
2026-03-08 23:49:29 +0000

    feat(photoshopmix): Fix style and make patch default enabled

--- patches/src/main/kotlin/app/revanced/patches/photoshopmix/BypassLoginPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/photoshopmix/BypassLoginPatch.kt
@@ -1,14 +1,12 @@
 package app.revanced.patches.photoshopmix
 
-import app.revanced.patcher.firstMethod
 import app.revanced.patcher.patch.bytecodePatch
 import app.revanced.util.returnEarly
 
 @Suppress("unused")
 val bypassLoginPatch = bytecodePatch(
     name = "Bypass login",
-    description = "Allows you to use the app after its discontinuation",
-    use = false,
+    description = "Allows you to use the app after its discontinuation.",
 ) {
     compatibleWith("com.adobe.photoshopmix")
 
