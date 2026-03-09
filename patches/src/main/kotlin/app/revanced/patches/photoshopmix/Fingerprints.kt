5ac0bb6e47053a46cf285fa2629634a3c11a92a9
Dylan <dylanpdx@gmail.com>
2026-03-09 00:05:26 +0000

    feat(photoshopmix): Fix improper fingerprints

--- patches/src/main/kotlin/app/revanced/patches/photoshopmix/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/photoshopmix/Fingerprints.kt
@@ -2,22 +2,27 @@ package app.revanced.patches.photoshopmix
 
 import app.revanced.patcher.definingClass
 import app.revanced.patcher.gettingFirstMethodDeclaratively
+import app.revanced.patcher.name
 import app.revanced.patcher.patch.BytecodePatchContext
 import app.revanced.patcher.returnType
 
-internal val BytecodePatchContext.disableLoginMethod by gettingFirstMethodDeclaratively("isLoggedIn") {
+internal val BytecodePatchContext.disableLoginMethod by gettingFirstMethodDeclaratively{
+    name("isLoggedIn")
+    definingClass("CreativeCloudSource;")
     returnType("Z")
-    definingClass("/CreativeCloudSource;")
 }
 
-internal val BytecodePatchContext.libButtonClickedMethod by gettingFirstMethodDeclaratively("ccLibButtonClickHandler") {
-    definingClass("/PSMixFragment;")
+internal val BytecodePatchContext.libButtonClickedMethod by gettingFirstMethodDeclaratively {
+    name("ccLibButtonClickHandler")
+    definingClass("PSMixFragment;")
 }
 
-internal val BytecodePatchContext.lightroomButtonClickedMethod by gettingFirstMethodDeclaratively("lightroomButtonClickHandler") {
-    definingClass("/PSMixFragment;")
+internal val BytecodePatchContext.lightroomButtonClickedMethod by gettingFirstMethodDeclaratively {
+    name("lightroomButtonClickHandler")
+    definingClass("PSMixFragment;")
 }
 
-internal val BytecodePatchContext.ccButtonClickedMethod by gettingFirstMethodDeclaratively("ccButtonClickHandler") {
-    definingClass("/PSMixFragment;")
+internal val BytecodePatchContext.ccButtonClickedMethod by gettingFirstMethodDeclaratively {
+    name("ccButtonClickHandler")
+    definingClass("PSMixFragment;")
 }
