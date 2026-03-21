37ac390a2e168f82fd7419eb1b7c5d0a94fab518
obXfuse <obXfuse>
2026-03-21 03:39:04 -0400

    refactor(amazonmusic): use Fingerprints.kt file for matching

--- patches/src/main/kotlin/app/revanced/patches/amazonmusic/misc/UnlimitedTrackSkippingPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/amazonmusic/misc/UnlimitedTrackSkippingPatch.kt
@@ -1,19 +1,17 @@
-package app.revanced.patches.amznmusic.misc.skips
+package app.revanced.patches.amazonmusic.misc
 
 import app.revanced.patcher.extensions.addInstructions
-import app.revanced.patcher.firstMethod
 import app.revanced.patcher.patch.bytecodePatch
 
 @Suppress("unused")
-val unlimitedSkipsPatch = bytecodePatch(
+val unlimitedTrackSkippingPatch = bytecodePatch(
     name = "Unlimited track skipping",
     description = "Unlocks the ability to skip tracks without restriction.",
 ) {
     compatibleWith("com.amazon.mp3")
 
     apply {
-        firstMethod { name == "getRule" && definingClass == $$"Lcom/amazon/music/freetier/featuregating/FMPMFeatureGating$STATION_UNLIMITED_SKIPS;" }
-        .addInstructions(0,
+        getRuleMethod.addInstructions(0,
         """
             new-instance p0, Lcom/amazon/music/platform/featuregate/rules/TrueRule;
             invoke-direct {p0}, Lcom/amazon/music/platform/featuregate/rules/TrueRule;-><init>()V
