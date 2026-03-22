3f0f75406b9cb0391d476e939e047706a70bd828
stopper2408 <219104212+stopper2408@users.noreply.github.com>
2026-03-22 22:36:32 +0000

    ﻿fix(instagram/signature): restore signature verifier helper

--- patches/src/main/kotlin/app/revanced/patches/instagram/misc/signature/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/instagram/misc/signature/Fingerprints.kt
@@ -2,6 +2,13 @@ package app.revanced.patches.instagram.misc.signature
 
 import app.revanced.patcher.*
 import app.revanced.patcher.patch.BytecodePatchContext
+import com.android.tools.smali.dexlib2.iface.ClassDef
+
+context(_: BytecodePatchContext)
+internal fun ClassDef.getIsValidSignatureClassMethod() = firstMethodDeclaratively(
+    "The provider for uri '",
+    "' is not trusted: ",
+)
 
 internal val BytecodePatchContext.isValidSignatureMethodMethod by gettingFirstMethodDeclaratively {
     parameterTypes("L", "Z")
